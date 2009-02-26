1ff8c5ec98b6a9a64457a89e51ba56564e996054
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-02-26 03:11:05 +0000

    Machine Man
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@2 40ef8932-b262-0410-9730-8c1c2ddff0ae

---
+++ page0001.mm
@@ -0,0 +1,20 @@
+<p>Page 1.</p>
+<h5>CHAPTER ONE</h5>
+<p>One Tuesday afternoon my left leg was
+severed. It wasn&rsquo;t as bad as it sounds. Well, it was. It was
+agonizing. There was a lot of screaming and flopping around and
+trying to tear my shirt into pieces to stem the bleeding. While I was
+busy with this, my co-workers stared through two-inch polycarbonate
+security glass and beat on the door. They couldn&rsquo;t get in. It
+was sealed for their safety. I had to apply my own tourniquet and try
+not to pass out for eight minutes. While I lay there, waiting for the
+time-release, I could see the top of what used to be my leg poking
+out from between two thick slabs of steel, gently dripping blood to
+the floor. I felt sorry for it. My leg hadn&rsquo;t asked for this.
+It had been a good leg. A faithful leg. And now look at it.</p>
+<p>But in the weeks afterward, as I lay in
+my hospital bed, I came to see the bright side. I remembered that
+expression: <em>A setback is just an opportunity in disguise</em>.
+I decided that was true. Because while I was sad to lose my leg, now
+I could build a better one.</p>
+
---
+++ page0005.mm
@@ -0,0 +1,17 @@
+<p>Page 5.</p>
+<p>I am not a people
+person. I don&rsquo;t know why. Maybe you can figure it out and let
+me know. All I know is that any time I am evaluated, I score very low
+on social metrics. Also, if there is a party, I am not invited. Or if
+I am, and I&rsquo;m seated between people, they will both talk to the
+person on the other side. There is something about me that repels
+people. Well, maybe not <em>repels</em>: that bores them. People look
+at me and know I am a waste of their time.</p>
+<p>I am a smart guy. I
+am good and conscientious and sometimes funny. These are qualities I
+keep hearing are desireable. I can only think there must be some
+other vital ingrediant that no-one is letting on, because I have no
+friends, don&rsquo;t speak to my family, and my last date was in high
+school. There is a guy in Media Relations who once killed a woman
+with his car, and he gets invited to parties. I don&rsquo;t
+understand that.</p>
