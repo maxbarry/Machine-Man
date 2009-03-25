c844fabcbb3c0e0c008127bfe4644743ce121adf
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-03-25 10:04:41 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@155 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0006.mm
+++ page0006.mm
@@ -11,9 +11,9 @@ got a transfemoral. May I?&rdquo; Before I could respond, she
 lifted up my sheet. &ldquo;Oh, yeah. That&rsquo;s a nice stump. Can I
 touch?&rdquo;</p>
 <p>&ldquo;Uh...&rdquo;</p>
-<p>Her fingers probed my bandages. &ldquo;Oh.
-Yes. Yes.&rdquo; She dropped the sheet. &ldquo;I have to confess,
-Charlie, I do prefer a transfemoral. We get a lot of
+<p>Her fingers probed my blighted flesh.
+&ldquo;Oh. Yes. Yes.&rdquo; She dropped the sheet. &ldquo;I have to
+confess, Charlie, I do prefer a transfemoral. We get a lot of
 transtibials&mdash;that&rsquo;s below the knee&mdash;and, no offense
 to those people, but it&rsquo;s like fitting shoes. There&rsquo;s no
 art to it. This...&rdquo; She patted my leg, the part I had left.
