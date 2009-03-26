c65dffb3c09a6713bf7a4b2fe4dbc84292ae571a
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-03-26 23:48:32 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@166 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0008.mm
+++ page0008.mm
@@ -5,8 +5,8 @@ as Lola Banks might have said. I thought about what I really wanted
 from a leg. It seemed to me, as I lay in my hospital bed, that
 the designer of biological legs had not been particularly ambitious.
 I mean, I don&rsquo;t mean to criticize. Legs are perfectly adequate
-at conveying a small biological mass from A to B at low speed, so
-long as A and B are not particularly far apart. That&rsquo;s pretty
-good, when you&rsquo;re limited to raw materials you can grow
-yourself. But I had to think that if anyone was designing a leg
-today, they would build in a lot more features.</p>
+at conveying a small biological mass from A to B, so long as A and B
+are not particularly far apart, and you don&rsquo;t mind if it takes
+a while. That&rsquo;s not bad, when you&rsquo;re limited to raw
+materials you can grow yourself. But I had to think that if anyone
+was designing a leg today, they would build in a lot more features.</p>
