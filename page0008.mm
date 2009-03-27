99c8ae29d50be4963a4eda01a2ab18bd2ef92c91
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-03-27 05:25:33 +0000

    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@167 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0008.mm
+++ page0008.mm
@@ -1,12 +1,12 @@
 <p>Page 8.</p>
 <p>I asked a nurse for a sketch book and
-began drawing up specs. <em>Fleshing out the concept</em>,
-as Lola Banks might have said. I thought about what I really wanted
-from a leg. It seemed to me, as I lay in my hospital bed, that
-the designer of biological legs had not been particularly ambitious.
-I mean, I don&rsquo;t mean to criticize. Legs are perfectly adequate
-at conveying a small biological mass from A to B, so long as A and B
-are not particularly far apart, and you don&rsquo;t mind if it takes
-a while. That&rsquo;s not bad, when you&rsquo;re limited to raw
-materials you can grow yourself. But I had to think that if anyone
-was designing a leg today, they would build in a lot more features.</p>
+began drawing up specs. Fleshing out
+the concept, you might say. I thought about what I really wanted from
+a leg. It seemed to me, as I lay in my hospital bed, that the
+designer of biological legs had not been particularly ambitious. I
+mean, I don&rsquo;t mean to criticize. Legs are perfectly adequate at
+conveying a small biological mass from A to B, so long as A and B are
+not particularly far apart, and you&rsquo;re in no hurry. That&rsquo;s
+not bad, when you&rsquo;re limited to raw materials you can grow
+yourself. But I had to think that if anyone was designing a leg
+today, they would build in a lot more features.</p>
