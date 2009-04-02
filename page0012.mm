017900df8b0b13645c310ac6912034b709459f55
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-04-02 02:54:36 +0000

    Page update: 0 new 3 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@180 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0012.mm
+++ page0012.mm
@@ -1,15 +1,16 @@
 <p>Page 12.</p>
 <p>Carl the security guard deposited me in
 my chair in the Glass Room. We called it the Glass Room because it
-looked over the adjoining labs, even though the walls were a
-transparent plastic polymer. They used to be plain glass, apparently,
-until an unfortunate incident involving a spilled beaker, an
+looked over the adjoining labs, but actually the walls were a
+green-tinged polycarbonate plastic. They used to be glass,
+apparently, until an incident involving a spilled beaker, an
 experimental strain of weapons-grade pathogen, and some panicked
-researchers with chairs. When I heard this story, the pathogen was
-harmless, and served only as a wake-up call to management. But later
-I overheard a version in which six people died before the complex
-could be locked down, and thirty-one afterward, when they flooded the
-labs with gas. This was according to Jason, one of my graduate lab
-assistants. Since he was telling Kathline, also a lab assistant, I
-guessed he was embellishing. But I&rsquo;ve never been able to tell
-when people are lying. All I know is the walls are plastic now.</p>
+researchers with office chairs. When I heard this story, the pathogen
+was harmless, and served as a wake-up call to management. But later I
+overheard a version in which six people died before the complex could
+be locked down, and thirty-one afterward, when they flooded the labs
+with gas. This was according to Jason, one of my graduate lab
+assistants. Since he was telling it to Elaine, the best-looking
+graduate lab assistant, I figured he was embellishing. But I&rsquo;ve
+never been able to tell when people are lying. All I know is the
+walls are plastic now.</p>
--- page0013.mm
+++ page0013.mm
@@ -1,11 +1,10 @@
 <p>Page 13.</p>
-<p>My three graduate assistants were
-already in the Glass Room: Jason, Kathline, and Elaine. Kathline was
-the one who wasn&rsquo;t Chinese. I was supposed to be teaching these
-kids something while they worked for me, but I had never been sure
-what. They in turn had figured out that I was unlikely to help their
-careers, and spent their time making the robot arms perform obscene
-gestures and shooting each other with cytoplasms. The only time I
-ever saw them look like they were at all interested in what I was
-doing, I was lying in Lab 4, tying my shirt around my stump, trying
-not to die.</p>
+<p>My three graduate lab assistants were
+already in the Glass Room: Jason, Elaine, and Kathline. Kathline was
+the one who wasn&rsquo;t Chinese. I was supposed to be teaching them
+something while they worked for me, but I had never been sure what.
+They in turn had decided I was unlikely to help their careers, and
+spent their time making the robot arms perform obscene gestures and
+shooting each other with cytoplasms. I only once saw them look like
+they were at all interested in what I was doing, and that was when I
+was in Lab 4, tying my shirt around my stump, trying not to die.</p>
