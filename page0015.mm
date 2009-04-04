25764c9dcc4c2c9745650e5504b07c5a39294b25
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-04-04 01:20:34 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@186 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0015.mm
+++ page0015.mm
@@ -1,16 +1,17 @@
 <p>Page 15.</p>
 <p>I waited in the
-lobby while Lola Banks had the inside of her mouth swabbed and
-answered questions in a harshly lit room about everyone she had ever
-met, everywhere she had ever been, and her Facebook profile. This
-took about forty minutes. Then she emerged, waved, and was escorted
-into the Dispassionate Waveform Sifter, which was like a metal
-detector for a very advanced definition of metal. As a side-benefit,
-it could also tell you if you had cancer. Usually this was a
-two-minute procedure, but when Lola stepped out she was re-buttoning
-her blouse and two hours had elapsed.</p>
+Better Future lobby while Lola Banks had the inside of her mouth
+swabbed and answered questions in a harshly lit room about everyone
+she had ever met, everywhere she had ever been, and her Facebook
+profile. This took about forty minutes. Then she emerged, waved, and
+was escorted into the Dispassionate Waveform Sifter, which was like a
+metal detector for a very advanced definition of metal. As a
+side-benefit, it could also tell you if you had cancer. Usually this
+was a two-minute procedure, but when Lola stepped out she was
+re-buttoning her blouse and two hours had elapsed.</p>
 <p>I clanked over
 to her. &ldquo;What was that about?&rdquo;</p>
 <p>&ldquo;Oh.
 Nothing. I always have trouble with metal detectors.&rdquo; She
-smiled. &ldquo;Tell me about your leg.&rdquo;</p>
+pushed her glasses up her nose and smiled. &ldquo;Tell me about your
+leg.&rdquo;</p>
