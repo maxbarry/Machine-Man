f9e81c5bd5b4b315613f5b9156f8ec137f7892c0
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-04-14 06:52:45 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@207 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0022.mm
+++ page0022.mm
@@ -1,12 +1,12 @@
 <p>Page 22.</p>
 <p>The nurses were not pleased to see me
 back at the hospital. I detected resentment in the clatter of my meal
-tray onto my bedside table, and the new roughness of the sponge.
-Sometimes, when the painkillers were wearing off, I pushed the call
-button and no-one came. At first this was a mystery to me. Then one
-night, sweating and unable to stand the terrible crawling sensation
-in my leg that was no longer there, I held down the call button for
-as long as it took. Nineteen minutes later, a nurse appeared. She
-stared down at me like I was a stain. &ldquo;Oh, I&rsquo;m sorry
+tray onto my bedside table, a new roughness in the sponge. Sometimes,
+when the painkillers were wearing off, I pushed the call button and
+no-one came. At first it was a mystery. Then one night, sweating and
+unable to stand the terrible crawling sensation in my leg that was no
+longer there, I held down the call button for as long as it took.
+Nineteen minutes later, a nurse appeared. She stared down at me like
+I was a stain. &ldquo;Oh, I&rsquo;m sorry
 for the delay,&rdquo; she said. &ldquo;I was busy with the
 patients who want to get better.&rdquo;</p>
