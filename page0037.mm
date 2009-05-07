11fb48e4d15a4b2ae0c6dbbc36427d1ec603c24b
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-07 07:37:34 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@247 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0037.mm
+++ page0037.mm
@@ -1,19 +1,20 @@
 <p>Page 37.</p>
-<p>The mother yanked her son&rsquo;s arm. This kid was no toddler: he
-was seven or eight. I have seen women wrestling with children of this
-age before, in supermarket aisles and parking lots, and usually the
-kids don&rsquo;t budge. But apparently a man plummeting out of the
-sky like a missile changed everything, because this kid flew through
-the air like he was hollow. It really was a convincing example of the
-strength-boosting properties of adrenaline.</p>
+<p>On the sidewalk below me, the mother yanked her son&rsquo;s arm. This
+kid was no toddler: he was seven or eight. I have seen women
+wrestling with children of this age before, in supermarket aisles and
+parking lots, and usually the kids don&rsquo;t budge. But apparently
+a man plummeting out of the sky changed everything, because this kid
+flew through the air like he was hollow. It was a convincing example
+of the strength-boosting properties of adrenaline.</p>
 <p>My legs could not correct my flight path, and thus were forced to
 abandon their original plan to use the boy and his mother as shock
-absorbers. I impacted the sidewalk about ten inches from them. The
-concrete split beneath my feet. Dust burst into the air. My spine
-bent in a way that felt very, very wrong. I lost my breath, then
-sucked in a lungful of dust. I felt the legs moving beneath me, and
-tried to tell them to wait up a second, because I had to apologize to
-the mother, make sure she and her son were okay, and so was I. But
-the legs didn&rsquo;t care. Their world was defined by a location and
-a destination and the optimum path between the two. Everything else
-was a waste of time.</p>
+absorbers. I impacted the sidewalk ten inches from them. The concrete
+split beneath my feet. Dust burst into the air. My spine bent in a
+way that felt very, very wrong. I lost my breath and sucked in a
+lungful of dust. I felt the Contours moving beneath me, and tried to
+tell them to wait a second, because I had to apologize to the mother,
+and check she and her son were okay, and so was I. But the legs
+didn&rsquo;t care. Their world was defined by a location, a
+destination, and the optimum path between the two; everything else
+was a waste of time. I was pretty sure now they were going to kill
+me.</p>
