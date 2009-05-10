22bf82806355c9ec13a5d18a4c13e5e3207d77a3
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-10 05:54:13 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@250 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0039.mm
+++ page0039.mm
@@ -6,22 +6,21 @@ take me where I wanted to go, i.e. to meet Lola Banks. Because I had
 serious doubts about my ability to control these legs. They had run
 me through traffic and over a truck and now deposited me at some
 random place I&rsquo;d never been before. Then I realized I was being
-very stupid, because I was outside a cafe, and inside it, sipping
-coffee, was Lola.</p>
+stupid, because I was outside a cafe, and inside it, sipping coffee,
+was Lola.</p>
 <p>My heart jumped, and so did my legs. &ldquo;No!&rdquo; I said. This
 was a nice cafe, with wrought iron furniture and a deep green awning
 and people in suits munching foccachias; I didn&rsquo;t want to crash
 in there and embarrass myself. But the legs were unstoppable. It was
-all I could do to duck beneath the doorway. Then I was inside. Heads
-turned toward me. Eyes widened. Pasta hung suspended from forks. I
-looked like a vagrant, you may recall. Plus I was six-foot-six, and
-had titanium hooves. I cringed. This was exactly what I didn&rsquo;t
+all I could do to duck beneath the doorway, and then I was inside.
+Heads turned toward me. Eyes widened. Pasta hung suspended from
+forks. I looked like a vagrant, you may recall. Plus I was
+six-foot-six, and had titanium hooves. This was exactly what I didn&rsquo;t
 want. Lola turned toward me.</p>
-<p>Her hair was in a pony-tail. It seemed resentful about that. She wore
-a long dress, a yellow one, and I am no fashion expert, but there was
-something fundamentally wrong with it: it billowed unexpectedly in
-the chest and gripped her like death under the arms, as if designed
-for a person with very different specs. Like maybe a chimpanzee. But
-I didn&rsquo;t care, because Lola&rsquo;s face was radiant with joy,
-as if nothing mattered except that I was here, and I grinned, because
-I felt the exact same way.</p>
+<p>Her hair was in a pony-tail, and seemed resentful about that. She
+wore a long dress, a yellow one. It billowed unexpectedly in the
+chest and shoulders, but gripped her like death under the arms, as if
+designed for a person with very different specs. Like maybe a
+chimpanzee. But Lola&rsquo;s face was radiant with joy, as if nothing
+mattered except that I was here, and I grinned, because I felt the
+same way.</p>
