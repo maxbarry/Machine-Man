d63ddd25ab641bfea44fbfde53a8c0694b9ceb70
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-12 05:23:56 +0000

    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@258 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- disabled.mm
+++ disabled.mm
@@ -2,5 +2,5 @@ Feed Not Active
 <p>This feed hasn't been turned on yet. 
 It is disabled by default for security reasons.
 <p>You can activate the feed in your 
-<a href="http://www.maxbarry.com/machineman/entercp.html">Control Panel</a>.
+<a href="http://www.maxbarry.com/cgi-bin/enter.cgi?mk=---MK---">Control Panel</a>.
 <p>Max.
