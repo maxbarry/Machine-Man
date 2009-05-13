3b697f0ca560bc727aa936021fe7f149b572f77c
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-13 06:34:00 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@282 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0041.mm
+++ page0041.mm
@@ -1,19 +1,19 @@
 <p>Page 41.</p>
 <p>Lola rose from the table. She offered me her hand, and I took it. As
 the Contours extended to full height, her eyes held mine. Sunlight
-bouncing off a building across the street played across her cheeks.
-Stray hairs that had escaped her pony-tail glowed orange. I felt
-myself lifting out of my body, because nobody had looked at me like
-this in my entire life. That&rsquo;s how it felt: like I was leaving
-physicality behind, becoming something weightless and untouchable.</p>
-<p>A hard shard of light skipped across Lola&rsquo;s face. It was a
-reflection. I turned, because by now I&rsquo;d heard the motor, and
-recognized it. Outside the cafe, a large white van jumped the curb
-and began to disgorge security guards.</p>
+bouncing off a building across the street played across her cheeks,
+and a few stray hairs that had escaped her pony-tail glowed orange. I
+felt myself lifting out of my body. That&rsquo;s how it felt: like I
+was leaving physical form behind, becoming something weightless and
+untouchable.</p>
+<p>A hard shard of light skipped across Lola&rsquo;s face. I turned,
+because by now I&rsquo;d heard the motor. Outside the cafe window, a
+large white van jumped the curb and disgorged Better Future security
+guards.</p>
 <p>&ldquo;I think&mdash;&rdquo; I said, but that was all I managed
-before they burst into the cafe, and people began to scream and
-overturn tables. Carl was there, and when he saw me he stopped dead
-and pointed his gun at me and yelled for me to <em>let go of that
+before they burst into the cafe and began to knock over tables.
+People screamed. Plates hit the floor. In the midst of all this, Carl
+spotted me. He raised his gun and yelled for me to <em>let go of that
 woman right now</em>. I felt exasperated, because this was really
-terrible timing for me. That was my mistake: thinking he wouldn&rsquo;t
-really shoot.</p>
+terrible timing for me. That was my mistake: thinking he would never
+actually shoot.</p>
