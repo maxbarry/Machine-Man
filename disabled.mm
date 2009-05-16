496083d2c0b363245d156a69481f15a89b6ad890
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-16 05:13:05 +0000

    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@290 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- disabled.mm
+++ disabled.mm
@@ -1,6 +1,5 @@
 Feed Not Active
 <p>This feed hasn't been turned on yet. 
-It is disabled by default for security reasons.
+It is disabled by default for security reasons.</p>
 <p>You can activate the feed in your 
-<a href="http://www.maxbarry.com/cgi-bin/enter.cgi?mk=---MK---">Control Panel</a>.
-<p>Max.
+<a href="http://www.maxbarry.com/cgi-bin/enter.cgi?mk=---MK---">Control Panel</a>.</p>
---
+++ endoffreefeed.mm
@@ -0,0 +1,13 @@
+End of the Gravy Train
+<p>That was the final page in the free feed of <em>Machine Man</em>.
+If you'd like to continue the story, please
+<a href="http://www.maxbarry.com/machineman/buy.html">buy your copy</a>!
+If not, thanks for reading this far. I hope you enjoyed it.</p>
+<p>(If you have already purchased your own feed, activate RSS
+delivery in your Control Panel and subscribe 
+to the address it gives you. Login details for your
+Control Panel are contained in the email sent to you when you purchased
+your feed.)</p>
+<p>Thanks again!</p>
+<p>&mdash;Max</p>
+<p><a href="http://www.maxbarry.com">www.maxbarry.com</a></p>
