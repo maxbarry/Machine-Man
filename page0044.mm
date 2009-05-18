6b5008603a021a7a23a7278dcf2ee96f5a015036
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-18 09:25:00 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@293 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0044.mm
+++ page0044.mm
@@ -6,10 +6,10 @@ feel terrible about this.&rdquo;</p>
 <p>I couldn&rsquo;t see her. I was having trouble opening my eyes. They
 had sedated me, back at the cafe, while I was watching Lola Banks.
 While I was transfixed by the opening and closing of her mouth, the
-slowly spreading shock and fear on her face, someone jabbed a needle
-in my arm. My veins filled with chemicals as Lola&rsquo;s fingers
-described a slow arc that terminated at the deep red flower
-blossoming beneath her yellow dress.</p>
+slowly spreading shock and fear on her face, someone had jabbed a
+needle in my arm. My veins filled with chemicals as Lola&rsquo;s
+fingers described a slow arc through the air, terminating at the deep
+red flower that blossomed beneath her yellow dress.</p>
 <p>&ldquo;What we need now,&rdquo; said Cassandra Cautery, &ldquo;is
 just to take a few deep breaths.&rdquo;</p>
 <p>My right eyelid peeled open. My left was still gummed up, but I could
@@ -19,27 +19,27 @@ But I wasn&rsquo;t. I was at work.</p>
 <p>&ldquo;Would you like some water?&rdquo; she said. &ldquo;I bet
 you&rsquo;re thirsty.&rdquo;</p>
 <p>Lola had reached for me. I had tried to catch her, but my legs
-wouldn&rsquo;t move. They were inert. They were anchors. I strained
-uselessly and Lola fell to the floor in front of me.</p>
+wouldn&rsquo;t move. They were inert. They were anchors. I strained,
+and Lola fell to the floor in front of me.</p>
 <p>&ldquo;I think there&rsquo;s been some miscommunication on both
 sides,&rdquo; said Cassandra Cautery. &ldquo;I think there are some
 real lessons to be learned.&rdquo;</p>
 <p><em>Lola</em>, I croaked. Cassandra Cautery leaned closer. I don&rsquo;t
 think she heard me. But she knew what I meant.</p>
 <p>&ldquo;It&rsquo;s understandable you&rsquo;re upset, Charlie. I&rsquo;d
-be upset, in your place. But please bear in mind, it was a
-high-pressure situation. Our guys were forced into split-second
+be upset, in your place. But please bear in mind: this was a
+high-pressure situation. Our people were forced into split-second
 decisions.&rdquo;</p>
 <p>&ldquo;Lola.&rdquo; This time it was a real word. My left eye
 fluttered open. In a minute, I would be able to sit up. Shortly after
 that, I would be able to get my hand around Cassandra Cautery&rsquo;s
 tiny neck and squeeze some answers out of her.</p>
 <p>&ldquo;It&rsquo;s the unknown,&rdquo; she said. &ldquo;It scares
-people. It makes everyone think worst-case. All this could have been
+people. Makes everyone think worst-case. All this could have been
 avoided, if only there had been full disclosure.&rdquo;</p>
 <p>I managed to co-ordinate an arm and levered myself up. I was in a
 medical room. It was small and windowless. The walls were a pale,
 nostalgic blue. I could smell something acrid and unforgiving, like
-detergent. Or maybe that was her.</p>
-<p>&ldquo;With that in mind,&rdquo; Cassandra Cautery said, &ldquo;I&rsquo;d
-like to figure out why your girlfriend has a metal heart.&rdquo;</p>
+detergent. Or maybe that was Cautery.</p>
+<p>&ldquo;With that in mind,&rdquo; she said, &ldquo;I&rsquo;d like to
+figure out why your girlfriend has a metal heart.&rdquo;</p>
