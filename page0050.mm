c9a4456dcf9442b027c51359906407b8e2a7ad6a
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-26 07:35:55 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@316 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0050.mm
+++ page0050.mm
@@ -1,46 +1,43 @@
 <p>Page 50.</p>
 <p>Then Lola stopped doing anything. She lay listless. She didn&rsquo;t
 respond to my questions. She stared at nothing. And I started
-thinking, well, I don&rsquo;t really know her that well. Sure, we had
-shared a couple of very intense experiences, but you add up the time
-we&rsquo;d spent in each other&rsquo;s company and it was like four
-hours. It was an amazing four hours. I&rsquo;m not saying it wasn&rsquo;t.
-I&rsquo;m saying that when a person you&rsquo;ve just met completely
-changes personality, you start to wonder whether you&rsquo;ve really
-met them at all.</p>
+thinking, well, I don&rsquo;t really know her that well. We had
+shared a few intense experiences, but you add up the time we&rsquo;d
+spent in each other&rsquo;s company, and it was like four hours. It
+was an amazing four hours. I&rsquo;m not saying otherwise. I&rsquo;m
+saying that when a person you&rsquo;ve just met completely changes
+personality, you wonder which one they really are.</p>
 <p>&ldquo;Get her to talk about the heart,&rdquo; said Cassandra
 Cautery. This was later, in the hallway outside Lola Banks&rsquo;s
 room. I had given up trying to get a reaction out of Lola and wheeled
 myself away for some thinking time.</p>
-<p>&ldquo;I can&rsquo;t get her to talk about anything,&rdquo; I said.
-&ldquo;She&rsquo;s practically comatose.&rdquo;</p>
+<p>&ldquo;I can&rsquo;t get her to talk about anything. She&rsquo;s
+practically comatose.&rdquo;</p>
 <p>&ldquo;You&rsquo;re not trying. You&rsquo;re just saying her name
 over and over.&rdquo;</p>
-<p>I blinked. The odd wall in Lola&rsquo;s recovery room, the one with
-the big mirror, that started to make sense. &ldquo;You&rsquo;re
-spying on us?&rdquo;</p>
+<p>I blinked. Now I understood why one wall in Lola&rsquo;s recovery
+room was a big mirror. &ldquo;You&rsquo;re spying on us?&rdquo;</p>
 <p>&ldquo;Charlie,&rdquo; said Cassandra Cautery. &ldquo;I do not want
 to put any pressure on you, but what we&rsquo;ve done here, this
 in-house surgery, that&rsquo;s not exactly legal. Do you know how
 that feels, to a lawyer?&rdquo; She put one hand on her breast. I
 mean her chest.</p>
 <p>&ldquo;No.&rdquo;</p>
-<p>&ldquo;It&rsquo;s horrible.&rdquo; Her voice wavered. &ldquo;It&rsquo;s
-like spending your whole life training to swim the hundred meters,
-then putting on a pair of flippers and hoping nobody notices.&rdquo;</p>
+<p>&ldquo;It&rsquo;s horrible. It&rsquo;s like spending your whole life
+training to swim the hundred meters, then putting on a pair of
+flippers and hoping nobody notices.&rdquo;</p>
 <p>&ldquo;What?&rdquo; I said.</p>
-<p>&ldquo;We don&rsquo;t break the law, Charlie. The entire point of
-being a lawyer is to get what you want without breaking the law. Now
-this... this...&rdquo; She gestured at Lola&rsquo;s door. &ldquo;This
-could ruin everything.&rdquo;</p>
-<p>She snuffled. It was a noise I had heard before. I had been lying on
-the floor of my lab, after crushing my right leg. Cassandra Cautery
-and Dick Peters had been standing over me. Dick Peters had said:
-<em>Don&rsquo;t do that thing</em>.</p>
-<p>&ldquo;I&rsquo;m sorry,&rdquo; she said. Her eyes were turning
-bloodshot. Her cheeks flushed, becoming puffy. Tears spilled from her
-eyes. &ldquo;When I get emotional... it&rsquo;s a chemical
-imbalance.&rdquo; Her chest hitched unstoppably, like a car someone
+<p>&ldquo;We don&rsquo;t break the law. The entire point of being a
+lawyer is to get what you want <em>without breaking the law</em>. Now
+this...&rdquo; She gestured at Lola&rsquo;s door. &ldquo;It
+jeopardizes everything.&rdquo;</p>
+<p>Her lips pressed together. Then she snuffled. It was a noise I had
+heard before. I had been lying on the floor of my lab, after crushing
+my right leg. Cassandra Cautery and D. Peters had been standing over
+me. D. Peters had said: <em>Don&rsquo;t do that thing</em>.</p>
+<p>&ldquo;Just... one moment.&rdquo; Tears began to spill from her eyes.
+Her cheeks grew flushed and puffy. &ldquo;When I get angry... it&rsquo;s
+a chemical imbalance.&rdquo; Her chest hitched, like a car someone
 was trying to jump-start. I was transfixed with horror. &ldquo;It...
-passes... in a... minute.&rdquo;</p>
-<p>&ldquo;I&rsquo;ll talk to Lola,&rdquo; I said.</p>
+passes... in a... minute.&rdquo; She began to hiccup.</p>
+<p>&ldquo;I&rsquo;ll ask about the heart,&rdquo; I said.</p>
