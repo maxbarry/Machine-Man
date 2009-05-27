38b57594c9d0e68c24d20eaca0209d6fe317b869
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-05-27 07:20:53 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@317 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0051.mm
+++ page0051.mm
@@ -1,26 +1,17 @@
 <p>Page 51.</p>
 <p>I wheeled back into Lola&rsquo;s room. No change: she was still lying
-on her side, staring blankly at the window. No, not the window: the
+on her side, staring at the window. Actually, not the window: the
 wall near the window. That was even worse. I wheeled myself around in
-front of her empty stare. &ldquo;Lola?&rdquo; Nothing. I moved
-closer. I felt a little tense. Like she might leap up and bite me. I
-think I was still shaky from the encounter with Cassandra Cautery,
-where she had cried in the hallway. That had been a horror movie.</p>
+front of her. &ldquo;Lola?&rdquo; Nothing. I moved closer. I felt
+tense, like she might leap up and bite me. I was still shaky from
+watching Cassandra Cautery cry in the hallway. That had been a horror
+movie.</p>
 <p>I
-reached out and stroked Lola&rsquo;s hair. Still no reaction. &ldquo;Lola,
-it&rsquo;s okay,&rdquo; I said. &ldquo;Everything&rsquo;s okay.&rdquo;
-I began to relax. I stroked her hair for a while. Sometimes I said,
-<em>Everything&rsquo;s
-okay</em>.
-I was soothing myself.</p>
-<p>Lola&rsquo;s hand came up and closed over mine. And I looked down and
-it was her again. Of course I knew her. What had I been thinking? I
-felt closer to Lola than anyone I knew.</p>
-<p>She started crying. It had been a hell of a day for tears. I felt
-like joining in. But I held firm.</p>
-<p>
-</p>
-<p>&lt;&lt; I think it&rsquo;d be cool to end this page with her ready
-to tell him, but not talking yet. &gt;&gt;</p>
-<p>
-</p>
+reached out and touched Lola&rsquo;s shoulder. Still no reaction.
+&ldquo;Lola, it&rsquo;s okay,&rdquo; I said. &ldquo;Everything&rsquo;s
+okay.&rdquo; I began to relax. I stroked her hair for a while.
+Sometimes I repeated: &ldquo;Everything&rsquo;s okay.&rdquo; I was
+soothing myself.</p>
+<p>Just as I was drifting off, Lola&rsquo;s hand closed over mine. Our
+eyes met. She was the person I knew again. Before I could even think
+to bring it up, she said, &ldquo;I need to tell you about my heart.&rdquo;</p>
