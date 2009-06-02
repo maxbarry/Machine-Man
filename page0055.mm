453e26e91630097a66268aa4509ca2183d3c88c7
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-02 09:01:20 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@325 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0055.mm
+++ page0055.mm
@@ -5,13 +5,13 @@ ten feet away. &ldquo;Well,&rdquo; she said. &ldquo;That was some
 story.&rdquo;</p>
 <p>&ldquo;Yeah,&rdquo; I said.</p>
 <p>&ldquo;I&rsquo;m always curious about
-what motivates people to choose their careers,&rdquo; Cassandra
-Cautery said. &ldquo;It&rsquo;s never what you think.&rdquo;</p>
+what motivates people to choose their careers. It&rsquo;s never what
+you think.&rdquo;</p>
 <p>I said nothing.</p>
 <p>&ldquo;She can have the heart. We
 haven&rsquo;t disposed of it. So if it has sentimental value... well,
 she can have it.&rdquo;</p>
-<p>&ldquo;I&rsquo;ll tell her that.&rdquo;</p>
+<p>&ldquo;I&rsquo;ll tell her.&rdquo;</p>
 <p>Silence fell.</p>
 <p>I said, &ldquo;I want to continue the
 project.&rdquo;</p>
