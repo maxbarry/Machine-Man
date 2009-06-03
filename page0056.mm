01d7461218e34f419119fcf50158b436e8bbe9d8
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-03 07:06:15 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@326 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0056.mm
+++ page0056.mm
@@ -3,31 +3,31 @@
 the corridors of Research Division to the Repository. There was not
 much talking. While waiting for an elevator, he said, &ldquo;I was
 really relieved to hear Ms. Banks is recovering,&rdquo; and I didn&rsquo;t
-say anything, because I hated him. He seemed to get the message.</p>
-<p>The Repository door was protected by an
-electronic lock, like most doors in Better Future. You couldn&rsquo;t
-go to the bathroom without swiping in first. I think it had less to
-do with keeping unauthorized people out than monitoring where
+say anything, because I hated him.</p>
+<p>Like most doors in Better Future, the
+Repository was protected by an electronic lock. You couldn&rsquo;t go
+to the bathroom in this place without swiping first. I think it had
+less to do with keeping unauthorized people out than monitoring where
 authorized people went. &ldquo;I don&rsquo;t have my pass,&rdquo; I
-said.</p>
-<p>Carl swiped his own card, and the
-lock&rsquo;s display blinked a friendly green. I was surprised,
-because as far as I knew, we did not grant lab access to random
-security guards. Carl had been given special access.</p>
-<p>He turned the handle and the door swung
-open. Until this moment, I had not really believed they would let me
-have my legs back. I had been bracing myself for Cassandra Cautery to
-slide out from a doorway, look sympathetic, and say there was a
-last-minute problem. But no. Carl wheeled me inside.</p>
+said. This was kind of an accusation, because I&rsquo;d had it before
+Carl shot Lola and made them sedate me.</p>
+<p>&ldquo;That&rsquo;s okay.&rdquo; Carl
+swiped his own card. The lock&rsquo;s display blinked a friendly
+green. I was surprised, because as far as I knew, we did not grant
+lab access to random security guards. Carl had special access.</p>
+<p>He turned the handle and the door
+opened. Until this moment, I had not really believed they would let
+me have my legs back. I had been bracing myself for Cassandra Cautery
+to slide out from a doorway, look sympathetic, and say there was a
+last-minute problem. But Carl just wheeled me inside.</p>
 <p>It was a large room, with many shelves.
 On slabs of gleaming steel lay half a dozen sets of legs in various
 stages of completion. My Contours, the set I&rsquo;d been forcibly
-removed from in the cafe, were amongst them. To my right was a
-smorgasbord of fingers. Next to them, the hands. I was not really
-happy with the hands. So far I&rsquo;d been unable to come up with
-anything I could honestly say was a clear improvement on Nature. It
-was frustrating, because I had some great arms in here, ready to go.
-Some really great arms.</p>
+removed from, were amongst them. To my right was a smorgasbord of
+fingers. Next to them, the hands. I was not really happy with the
+hands. So far I&rsquo;d been unable to come up with anything I could
+honestly say was a clear improvement on Nature. It was frustrating,
+because I had some great arms, ready to go. Some really great arms.</p>
 <p>Carl said, &ldquo;Should I fetch you a
 sweater?&rdquo;</p>
 <p>I jumped a little. I&rsquo;d forgotten
