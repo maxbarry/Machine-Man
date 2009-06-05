d6b97339ee5ced43532225f867b0bcd2df2ac87a
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-05 04:49:04 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@332 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0058.mm
+++ page0058.mm
@@ -1,30 +1,21 @@
 <p>Page 58.</p>
-<p></p>
-<p>&lt;&lt; He maybe goes back to the lab.
-Eager to see if his equipment is there. But it would be good to set
-up a dynamic with Lola, too. Maybe leave the lab for next week. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Charlie back in the lab. CC. 
-She&rsquo;s not willing to risk Charlie sever more parts of himself.
-He says, what&rsquo;s the point of making artificial limbs if I can&rsquo;t
-attach them anywhere? &gt;&gt;</p>
-<p></p>
-<p>And then I realized I hadn&rsquo;t seen
-Carl in a while.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+<p>Once Carl had left, I strapped on my
+fingers and prepared to head to the lab. I took two steps, then
+stopped. I had been quite excited about the prospect of returning to
+the lab, but now, all of a sudden, I wasn&rsquo;t. I felt like
+walking around. Maybe doing some standing. I wanted to rediscover the
+sensation of moving without someone pushing me. So I did. And before
+I left the Repository, I tipped the wheelchair on its side and
+stepped on it.</p>
+<p>I walked to the elevators and pushed
+the button myself. While I waited, my hands behind my back, I hummed
+a little. The elevator doors opened on two white-coated technicians,
+who said, &ldquo;Good morning, Dr. Neumann,&rdquo; and moved to make
+room for me. &ldquo;Hello,&rdquo; I said. I had no idea who they
+were.</p>
+<p>When, inevitably, I found my way to
+Lola&rsquo;s medical room, she was asleep. But the Contours were not
+especially quiet, and a few steps into the room, her eyelids
+fluttered open. She saw me and smiled. It was a small smile, but very
+beautiful, and I thought about this for a long time afterward: this
+perfect moment, which could not be improved.</p>
---
+++ page0059.mm
@@ -0,0 +1,64 @@
+<p>Page 59.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>All of a sudden I had an urge to do
+something different. 
+</p>
+<p>I had been quite excited about the idea
+of hitting the lab again, but 
+</p>
+<p>Then a funny feeling overcame me. I had
+to stop and think about it, because I wasn&rsquo;t sure where this
+fee</p>
+<p></p>
+<p></p>
+<p>I had meant to head straight to the
+lab, but once I got my legs and fingers on, a different urge overcame
+me. I headed back the way I had come, only this time under my own
+steam. At the elevators, I pushed the button myself and waited with
+my hands folded behind my back.</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; crush the wheelchair &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; He maybe goes back to the lab.
+Eager to see if his equipment is there. But it would be good to set
+up a dynamic with Lola, too. Maybe leave the lab for next week. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Charlie back in the lab. CC. 
+She&rsquo;s not willing to risk Charlie sever more parts of himself.
+He says, what&rsquo;s the point of making artificial limbs if I can&rsquo;t
+attach them anywhere? &gt;&gt;</p>
+<p></p>
+<p>And then I realized I hadn&rsquo;t seen
+Carl in a while.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+</BODY>
+</HTML>
