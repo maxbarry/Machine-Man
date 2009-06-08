5e94786f9d58b12eab0db6cbad107980a88e15de
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-08 05:49:36 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@333 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0059.mm
+++ page0059.mm
@@ -1,64 +1,20 @@
 <p>Page 59.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>All of a sudden I had an urge to do
-something different. 
-</p>
-<p>I had been quite excited about the idea
-of hitting the lab again, but 
-</p>
-<p>Then a funny feeling overcame me. I had
-to stop and think about it, because I wasn&rsquo;t sure where this
-fee</p>
-<p></p>
-<p></p>
-<p>I had meant to head straight to the
-lab, but once I got my legs and fingers on, a different urge overcame
-me. I headed back the way I had come, only this time under my own
-steam. At the elevators, I pushed the button myself and waited with
-my hands folded behind my back.</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; crush the wheelchair &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; He maybe goes back to the lab.
-Eager to see if his equipment is there. But it would be good to set
-up a dynamic with Lola, too. Maybe leave the lab for next week. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Charlie back in the lab. CC. 
-She&rsquo;s not willing to risk Charlie sever more parts of himself.
-He says, what&rsquo;s the point of making artificial limbs if I can&rsquo;t
-attach them anywhere? &gt;&gt;</p>
-<p></p>
-<p>And then I realized I hadn&rsquo;t seen
-Carl in a while.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+<h5>CHAPTER 
+THIRTEEN</h5>
+<p>I went to work with a spring in my
+step. Not literally. If you want to be literal, I went to work with
+servo-hydraulics in my step. I mean I was happy. I was optimistic. I
+was glad to be alive. I read somewhere that for a person to be
+content with life, they need two out of three things: health, work,
+and love. Right now, I was all over that. I was a triple threat.</p>
+<p>It was early days with Lola. I knew
+that. But this thing we had, it was something. It was something.</p>
+<p>As I strolled toward the Glass Room, I
+tried to think what I should do next, on the project. Maybe I had
+gotten too caught up with the hand problem. Did I have to let the
+fact that I hadn&rsquo;t come up with a perfect hand design block the
+whole project? No. No, I did not. Screw the hand. I would use
+something stop-gap. Then I could attach an arm.</p>
+<p>I smiled. I liked the idea of going
+back to Lola with a new arm. She had been pleased enough just to see
+me in the legs.</p>
