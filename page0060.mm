a024db16e2cc5c043d1414a62a9396cddfa9d327
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-09 06:39:42 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@337 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0060.mm
+++ page0060.mm
@@ -5,24 +5,26 @@ Sometimes it was the other way around. I lost track of daylight for a
 while there. There wasn&rsquo;t much of it, in the labs or bunk
 rooms. I learned to tell the time by the ebb and flow of lab
 assistants, the way they were quieter and more focused in the
-mornings, scattier and prone to risk-taking at nights. I had twelve
-of them now, too many to remember. They were very deferential. That
-was nice.</p>
+mornings, scattier and prone to risk-taking at nights. I had too many
+of them to remember, now. Occasionally their faces blended together,
+and I would refer to them by their function, like: &ldquo;Waveform
+Damper, show that to Nerve Interfacer, please.&rdquo; But they didn&rsquo;t
+seem to mind. They were very deferential.</p>
 <p>In between, I visited Lola. They had
 moved her to a live-in suite in an upper section of the building. It
 was a managerial level, I think, although I never saw any managers. I
 did see lots of potted plants. Those cheerful splashes of green made
-me a little wistful, and I felt like installing some plants in the
-lab, to brighten it up, although of course I couldn&rsquo;t, because
-of contamination. But I decided to take one from beside the elevator
-and put it in the corner of my bunk room. I could do things like
-that, now I was important. The plant sat there for a while and I kind
-of forgot about it until one day, or maybe night, I noticed it
-slumped over the side of its pot, brown and limp. I hadn&rsquo;t
-watered it. Also, the lack of natural light may have been a problem.</p>
+me wistful, and I felt like installing some plants in the lab, to
+brighten it up, although of course I couldn&rsquo;t, because of
+contamination. But I did take one from beside the elevator and put it
+in the corner of my bunk room. I could do things like that, now I was
+important. The plant sat there a while and I kind of forgot about it
+until one day, or maybe night, I saw it slumped over the side of its
+pot, brown and limp. I hadn&rsquo;t watered it. Also, the lack of
+natural light may have been a problem.</p>
 <p>As I sat on my bunk and looked at that
 plant, I felt annoyed. There was something pathetic about an organism
 that couldn&rsquo;t even live if you left it alone a while. This was
-maybe a little hard on the plant, which after all had been removed to
+maybe a little hard on the plant, which had after all been removed to
 a hostile environment, but still, it reminded me why I was doing
 this.</p>
