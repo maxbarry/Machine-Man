a55b54a22910a0f689464abff6cfe4f023fa655b
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-09 06:31:04 +0000

    Page update: 3 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@336 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0060.mm
+++ page0060.mm
@@ -1,93 +1,28 @@
 <p>Page 60.</p>
-<p>I swiped for access to the Glass Room.
-Already I could see my assistants inside, tapping at terminals and
-peering at screens. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I didn&rsquo;t want to get carried
-away, but in the back of my mind, a lot of ideas were percolating
-around Lola. Like how she had lost the heart her father gave her, and
-maybe, one day, if things went right, I could give her one of mine.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>for a guy whose last serious
-relationship was in high school, what we had going was something. It
-was really something.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I wasn&rsquo;t getting carried away. It
-was early days. But this thing with Lola, it was something. It was
-something.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>My work was fulfilling, my employer
-supported me, and, for the first time ever, I had something
-resembling the beginnings of a love life. I mean, 
-</p>
-<p></p>
-<p></p>
-<p>already the most significant romantic
-liaison 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Give her a heart. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>But Lola had opened up to me; she had .
-I had seen her heart. That was pretty good.</p>
-<p></p>
-<p></p>
-<p>I had an engineering project that
-interested me, a supportive work environment in which to conduct it,</p>
-<p>For the first time in my life, I had
-not only a promising 
-</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; He maybe goes back to the lab.
-Eager to see if his equipment is there. But it would be good to set
-up a dynamic with Lola, too. Maybe leave the lab for next week. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Charlie back in the lab. CC. 
-She&rsquo;s not willing to risk Charlie sever more parts of himself.
-He says, what&rsquo;s the point of making artificial limbs if I can&rsquo;t
-attach them anywhere? &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>And then I realized I hadn&rsquo;t seen
-Carl in a while.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+<p>The next weeks flew by. I spent my days
+in the lab and my nights sleeping in one of the private bunk rooms.
+Sometimes it was the other way around. I lost track of daylight for a
+while there. There wasn&rsquo;t much of it, in the labs or bunk
+rooms. I learned to tell the time by the ebb and flow of lab
+assistants, the way they were quieter and more focused in the
+mornings, scattier and prone to risk-taking at nights. I had twelve
+of them now, too many to remember. They were very deferential. That
+was nice.</p>
+<p>In between, I visited Lola. They had
+moved her to a live-in suite in an upper section of the building. It
+was a managerial level, I think, although I never saw any managers. I
+did see lots of potted plants. Those cheerful splashes of green made
+me a little wistful, and I felt like installing some plants in the
+lab, to brighten it up, although of course I couldn&rsquo;t, because
+of contamination. But I decided to take one from beside the elevator
+and put it in the corner of my bunk room. I could do things like
+that, now I was important. The plant sat there for a while and I kind
+of forgot about it until one day, or maybe night, I noticed it
+slumped over the side of its pot, brown and limp. I hadn&rsquo;t
+watered it. Also, the lack of natural light may have been a problem.</p>
+<p>As I sat on my bunk and looked at that
+plant, I felt annoyed. There was something pathetic about an organism
+that couldn&rsquo;t even live if you left it alone a while. This was
+maybe a little hard on the plant, which after all had been removed to
+a hostile environment, but still, it reminded me why I was doing
+this.</p>
---
+++ page0063.mm
@@ -0,0 +1,98 @@
+<p>Page 63.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I swiped for access to the Glass Room.
+Already I could see my assistants inside, tapping at terminals and
+peering at screens. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I didn&rsquo;t want to get carried
+away, but in the back of my mind, a lot of ideas were percolating
+around Lola. Like how she had lost the heart her father gave her, and
+maybe, one day, if things went right, I could give her one of mine.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>for a guy whose last serious
+relationship was in high school, what we had going was something. It
+was really something.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I wasn&rsquo;t getting carried away. It
+was early days. But this thing with Lola, it was something. It was
+something.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>My work was fulfilling, my employer
+supported me, and, for the first time ever, I had something
+resembling the beginnings of a love life. I mean, 
+</p>
+<p></p>
+<p></p>
+<p>already the most significant romantic
+liaison 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Give her a heart. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>But Lola had opened up to me; she had .
+I had seen her heart. That was pretty good.</p>
+<p></p>
+<p></p>
+<p>I had an engineering project that
+interested me, a supportive work environment in which to conduct it,</p>
+<p>For the first time in my life, I had
+not only a promising 
+</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; He maybe goes back to the lab.
+Eager to see if his equipment is there. But it would be good to set
+up a dynamic with Lola, too. Maybe leave the lab for next week. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; Charlie back in the lab. CC. 
+She&rsquo;s not willing to risk Charlie sever more parts of himself.
+He says, what&rsquo;s the point of making artificial limbs if I can&rsquo;t
+attach them anywhere? &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>And then I realized I hadn&rsquo;t seen
+Carl in a while.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+</BODY>
+</HTML>
