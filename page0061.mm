f694f3b1bb5b1e44322e05cb6ddd3bba518f0a83
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-10 06:58:26 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@339 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0061.mm
+++ page0061.mm
@@ -15,14 +15,14 @@ microbes, in my opinion. I felt someone from one of the military
 departments should have come up with a safe receptacle. But anyway.
 There would be no such problem with the arm, because there was only
 one customer: me. And I loved it.</p>
-<p>I was excited about being able to do
-this amputation properly: in a sterile environment, while
+<p>I was also excited about being able to
+do this amputation properly: in a sterile environment, while
 unconscious. I imagined myself slipping gently off to sleep in Better
 Future&rsquo;s brand new operating theater under the gaze of a
 green-masked anesthetist, waking to the new arm already attached. I
 could see myself swinging my legs off the table, saying, &ldquo;I&rsquo;ll
 get that,&rdquo; when the nurse went to fix my gown, and my new arm
 swiveling behind me, knotting the cord with machine precision. I
-imagined opening the door to Lola&rsquo;s suite with metal fingers,
-the desire on her face when she saw what I had become now. But none
-of that worked out.</p>
+imagined opening the door to Lola&rsquo;s suite with articulated
+fingers, the desire on her face when she saw what I had become now.
+But none of that worked out.</p>
