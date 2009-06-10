6a15ce8010fce4214cc0c1e2789476d4dcb0d5ed
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-10 06:55:46 +0000

    Page update: 0 new 3 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@338 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0061.mm
+++ page0061.mm
@@ -1,8 +1,28 @@
 <p>Page 61.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+<p>I planned to lose my left arm on
+Friday. I didn&rsquo;t tell Lola this. It would be a surprise. I had
+a robotic arm ready to snap in, plus two backups in case anything
+went wrong with the primary, which it wouldn&rsquo;t, because the
+primary was a piece of art. It was by far the smartest thing I&rsquo;d
+ever built, and, not wanting to boast, but I have built some pretty
+smart things. Once I created a microbe that ate garbage. The idea was
+you open your trash can, throw in your table scraps, and they&rsquo;re
+gone within the hour. They were really hungry. But that project never
+got out of QA, because if the microbes ever got out of the trash can,
+well, they would eat everything. There were some PR concerns about a
+trash-can-eats-man scenario. Which was not really the fault of the
+microbes, in my opinion. I felt someone from one of the military
+departments should have come up with a safe receptacle. But anyway.
+There would be no such problem with the arm, because there was only
+one customer: me. And I loved it.</p>
+<p>I was excited about being able to do
+this amputation properly: in a sterile environment, while
+unconscious. I imagined myself slipping gently off to sleep in Better
+Future&rsquo;s brand new operating theater under the gaze of a
+green-masked anesthetist, waking to the new arm already attached. I
+could see myself swinging my legs off the table, saying, &ldquo;I&rsquo;ll
+get that,&rdquo; when the nurse went to fix my gown, and my new arm
+swiveling behind me, knotting the cord with machine precision. I
+imagined opening the door to Lola&rsquo;s suite with metal fingers,
+the desire on her face when she saw what I had become now. But none
+of that worked out.</p>
--- page0063.mm
+++ page0063.mm
@@ -4,73 +4,22 @@
 <p></p>
 <p></p>
 <p></p>
-<p>I swiped for access to the Glass Room.
-Already I could see my assistants inside, tapping at terminals and
-peering at screens. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
 <p>I didn&rsquo;t want to get carried
 away, but in the back of my mind, a lot of ideas were percolating
 around Lola. Like how she had lost the heart her father gave her, and
 maybe, one day, if things went right, I could give her one of mine.</p>
 <p></p>
 <p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>for a guy whose last serious
-relationship was in high school, what we had going was something. It
-was really something.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I wasn&rsquo;t getting carried away. It
-was early days. But this thing with Lola, it was something. It was
-something.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>My work was fulfilling, my employer
-supported me, and, for the first time ever, I had something
-resembling the beginnings of a love life. I mean, 
-</p>
-<p></p>
-<p></p>
-<p>already the most significant romantic
-liaison 
-</p>
-<p></p>
-<p></p>
-<p></p>
+<p>Cassandra Cautery stopped me in the
+corridor. &ldquo;Did you crush a wheelchair?&rdquo;</p>
 <p></p>
 <p></p>
 <p></p>
 <p>&lt;&lt; Give her a heart. &gt;&gt;</p>
 <p></p>
 <p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
 <p>But Lola had opened up to me; she had .
 I had seen her heart. That was pretty good.</p>
-<p></p>
-<p></p>
-<p>I had an engineering project that
-interested me, a supportive work environment in which to conduct it,</p>
-<p>For the first time in my life, I had
-not only a promising 
-</p>
-<p></p>
 <p></p>
 <p>&lt;&lt; He maybe goes back to the lab.
 Eager to see if his equipment is there. But it would be good to set
