7d04e441b67b04626d76da28f0193be5985728eb
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-11 06:15:32 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@341 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0062.mm
+++ page0062.mm
@@ -15,41 +15,42 @@ this. But it seemed to help. After a while, I was able to lie back.</p>
 pain. I&rsquo;d heard of it, of course. But it had always struck me
 as the product of irrational minds. I suspected that if you were to
 plot the data sets of people who experienced phantom pain following
-the loss of a limb with belief sets involving spiritual energy, you
-would see a fairly serious correlation. It seemed to be what you got
-for allowing your brain to operate without discipline.</p>
+the loss of a limb with people who believed in higher beings,
+reincarnation, and spiritual energy, you would see a fairly serious
+correlation. It seemed like the logical consequences of allowing your
+brain to operate without discipline.</p>
 <p>I&rsquo;d thought this because in the
 first weeks following my own amputations, I&rsquo;d felt nothing
 worse than itching or crawling sensations. Well, that&rsquo;s not
-quite true. There were twinges. Some minor cramps. The occasional
+quite true. There had been twinges. Some minor cramps. The occasional
 shooting, blinding pain. But that had been easily attributable to
 nerve issues. I was, after all, doing some fairly serious messing
 around with the nerves, to interface them to my mechanical parts. It
-was perfectly reasonable to believe the occasional cross-wire would
-produce pain. Not phantom pain. Real pain.</p>
+was reasonable to believe an occasional cross-wire would produce
+pain. Not phantom pain. Real pain.</p>
 <p>But four of the last five nights, I&rsquo;d
-woken to some kind of pain in a body part I didn&rsquo;t have. And
-that other night, I had somehow turned myself completely around in my
-sleep, I think trying to find my legs. It was hard to believe this
-was damaged nerves. It was my brain, punishing me.</p>
+woken to discomfort in a body part I didn&rsquo;t have. And that
+other night, I had somehow turned myself completely around in my
+sleep, I think trying to find my legs. I no longer believed this was
+damaged nerves. It was my brain, punishing me.</p>
+<p>I had to give it to my brain. It had
+taken a while, but it had figured out how to fight back. It had
+absorbed an impressive arsenal of pain-dampening drugs and come out
+swinging. 
+</p>
 <p>I sat up again. The cramp was coming
-back. It was preparing to clench tight my phantom toes. I had to give
-it to my brain: it had taken a while, but it had figured out how to
-fight back. It had absorbed an impressive arsenal of pain-dampening
-drugs and come back swinging.</p>
-<p>But I had one trick left. Phantom pain,
-a theory went, was caused by the brain thinking the limb was still
-there, needing to be cared for: I had read about successful
-treatments with mirrors, patients staring at optical illusions of
-their whole, healthy bodies. Apparently that was enough for the
-brain.</p>
-<p>I didn&rsquo;t have a mirror. Or a
-spare foot, for that matter, to mirror. But I swung my thighs off the
-edge of the bunk and reached down for the Contours, which lay on the
-floor. The cramp hit when I was halfway through fitting. I had to
-alternate between slotting needles and grabbing at the air to massage
-ghost toes, whimpering. My fingers shook. By the end, I was jabbing
-those needles in there, hoping they hit something, gnashing my teeth
-against the pain. But finally I finished. The Contour extended. The
-hoof flexed, up and down, up and down. &ldquo;Ahhh,&rdquo; I said.
-That <em>was</em> better.</p>
+back. It was preparing to clench tight my phantom toes. I had to
+think fast. Phantom pain, one theory went, was caused by the brain
+thinking the limb was still there, needing to be cared for. I had
+read about treatments with mirrors, patients staring at optical
+illusions of their whole, healthy bodies. That was enough,
+apparently. I didn&rsquo;t have a mirror. Or a spare foot to mirror,
+for that matter. But I had something. I swung my thighs off the edge
+of my bunk and reached down for the Contours.</p>
+<p>The cramp hit when I was halfway
+through fitting. I had to alternate between slotting needles and
+grabbing at the air to massage ghost toes, whimpering. My fingers
+shook. By the end, I was jabbing those needles in there, hoping they
+hit something, gnashing my teeth against the pain. Finally, I
+finished. I extended the Contour. My hoof flexed, up and down, up and
+down. &ldquo;Ahhh,&rdquo; I said. That <em>was</em> better.</p>
