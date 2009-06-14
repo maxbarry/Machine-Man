848ae76703ce622ab1d910e7138b1741f4105f40
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-14 11:25:04 +0000

    Page update: 2 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@350 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0064.mm
+++ page0064.mm
@@ -1,233 +1,32 @@
 <p>Page 64.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Because the body,
-it&rsquo;s a dangerous place right now. There are parts being chopped
-off, experimental parts being added... who knows what might go
-wrong?&rdquo; She spread her palms. &ldquo;I mean, you said yourself,
-you&rsquo;re experiencing an unexpected technical problem right now.&rdquo;</p>
-<p>&ldquo;Glitch.
-Temporary glitch.&rdquo;</p>
-<p>&ldquo;Mmm,&rdquo;
-said Cassandra Cautery. &ldquo;What I think we can both agree on,
-Charlie, is that the ideal place for this project&rsquo;s brain is
-not inside the body.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>project. What we&rsquo;re
-building, from my point of view, is the ability for Better Future to
-design and fabricate artificial bodies.</p>
-<p>It&rsquo;s the
-project. It&rsquo;s the ability for Better Future to design and
-fabricate 
-</p>
-<p>the project. It&rsquo;s
-not those pieces you&rsquo;re wearing. It&rsquo;s the ability 
-</p>
-<p>nd you are the
-brain.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>That&rsquo;s a fair
-assessment of our situation, wouldn&rsquo;t you say?&rdquo;</p>
-<p>And we have a
-brain, from which comes all the wonderful ideas on how to build the
-body. 
-</p>
-<p>We have a brain,
-which produces all the wonderful ideas on how to build the body. 
-</p>
-<p>&ldquo;But we can&rsquo;t
-separate them,&rdquo; I said. &ldquo;They&rsquo;re both here.&rdquo;</p>
-<p></p>
-<p>We have a brain,
-which comes up with wonderful ideas on how to build the body, 
-</p>
-<p></p>
-<p></p>
-<p>&ldquo;Now, for me,
-this project isthe body 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>without which the
-body cannot survive, and a body, 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Then I realized I
-hadn&rsquo;t seen Carl for a while.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Because I was in the Contours, we were
-about the same height. I did not like 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I didn&rsquo;t want to get carried
-away, but in the back of my mind, a lot of ideas were percolating
-around Lola. Like how she had lost the heart her father gave her, and
-maybe, one day, if things went right, I could give her one of mine.</p>
-<p></p>
-<p></p>
-<p>Cassandra Cautery stopped me in the
-corridor. &ldquo;Did you crush a wheelchair?&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Give her a heart. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>But Lola had opened up to me; she had .
-I had seen her heart. That was pretty good.</p>
-<p></p>
-<p>&lt;&lt; He maybe goes back to the lab.
-Eager to see if his equipment is there. But it would be good to set
-up a dynamic with Lola, too. Maybe leave the lab for next week. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Charlie back in the lab. CC. 
-She&rsquo;s not willing to risk Charlie sever more parts of himself.
-He says, what&rsquo;s the point of making artificial limbs if I can&rsquo;t
-attach them anywhere? &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>And then I realized I hadn&rsquo;t seen
-Carl in a while.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>But I finally finished and sent a
-mental command to extend the leg. It did. 
-</p>
-<p>It was not a smart move, rushing a
-fitting like this. One bad connection and the Contours might launch
-me into the ceiling. But 
-</p>
-<p>ead to me trying to flex an ankle and
-launching myself into the ceiling. 
-</p>
-<p>inally I got the last essential
-connection hooked up. 
-</p>
-<p>There was one thing I could do.</p>
-<p>One theory with phantom pain was that
-the brain still thinks</p>
-<p>arises because the brain has lost its
-ability 
-</p>
-<p></p>
-<p></p>
-<p>You had to expect that would have
-side-effects. 
-</p>
-<p>had only limited phantom sensations</p>
-<p>This feeling strengthened when,
-following my own limb 
-</p>
-<p>was what you got for allowing your
-brain to 
-</p>
-<p>I didn&rsquo;t think that people made
-it up, exactly</p>
-<p></p>
-<p>This was not the first time this had
-happened. I had felt odd sensations&mdash;itching and crawling, the
-odd twinge&mdash;in body parts I no longer owned since the beginning.
-But I had 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>The Contours lay on the floor; my
-finger-glove 
-</p>
-<p>Then enough of my brain came online to
-allow me to realize I didn&rsquo;t have any toes.</p>
-<p>I found the lamp switch and pulled
-myself into a sitting position. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>spent a few foggy moments groping
-around in the sheets, trying to find my toes, before enough of my
-brain came up to speed to realize why I couldn&rsquo;t find them. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+<h5>CHAPTER 
+FOURTEEN</h5>
+<p>I sat silently in
+Cassandra Cautery&rsquo;s office while she talked about processes and
+due diligence and ticking boxes. She was telling me why she did not
+want me to have surgery on Friday. There had maybe been an alarming
+expression on my face right after she said, <em>Imagine they could</em>,
+because since then she had been speaking very quickly, and of all the
+reasons she offered for keeping me out of the operating theater, none
+of them was: <em>Because we&rsquo;re doing Carl.</em></p>
+<p>Maybe they had
+already done Carl. Maybe he was in that medical room they had used
+for Lola, before sending her upstairs to her suite. Maybe some of my
+legs were missing.</p>
+<p>There was silence.
+Cassandra Cautery had paused. I mentally backtracked: her last word
+had been something like <em>eventually</em>.</p>
+<p>&ldquo;Okay,&rdquo;
+I said. &ldquo;I understand.&rdquo;</p>
+<p>She blinked. Her
+forehead didn&rsquo;t move, though. Botox? &ldquo;That&rsquo;s...&rdquo;
+she said. &ldquo;That&rsquo;s good to hear, Charlie. That&rsquo;s
+really good to hear.&rdquo;</p>
+<p>I nodded. I wasn&rsquo;t
+an idiot. Letting Cassandra Cautery know how much I cared about
+swapping out my arm would make things difficult. They had locked me
+in a room, once. And while I&rsquo;d been in there, I vowed never to
+let myself be put in that position again. It was much better if
+everyone believed I was with the program. Planning no more
+amputations. It meant I wouldn&rsquo;t get to use that operating
+theater. But that hadn&rsquo;t stopped me before.</p>
---
+++ page0066.mm
@@ -0,0 +1,50 @@
+<p>Page 65.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I didn&rsquo;t want to get carried
+away, but in the back of my mind, a lot of ideas were percolating
+around Lola. Like how she had lost the heart her father gave her, and
+maybe, one day, if things went right, I could give her one of mine.</p>
+<p></p>
+<p></p>
+<p>Cassandra Cautery stopped me in the
+corridor. &ldquo;Did you crush a wheelchair?&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Give her a heart. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>But Lola had opened up to me; she had .
+I had seen her heart. That was pretty good.</p>
+<p></p>
+<p>&lt;&lt; He maybe goes back to the lab.
+Eager to see if his equipment is there. But it would be good to set
+up a dynamic with Lola, too. Maybe leave the lab for next week. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; Charlie back in the lab. CC. 
+She&rsquo;s not willing to risk Charlie sever more parts of himself.
+He says, what&rsquo;s the point of making artificial limbs if I can&rsquo;t
+attach them anywhere? &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+</BODY>
+</HTML>
