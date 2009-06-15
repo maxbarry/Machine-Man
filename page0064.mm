1bbf8146839aace24682c9056ecba4b030577b39
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-15 07:13:37 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@352 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0064.mm
+++ page0064.mm
@@ -19,14 +19,13 @@ something like <em>eventually</em>.</p>
 <p>&ldquo;Okay,&rdquo;
 I said. &ldquo;I understand.&rdquo;</p>
 <p>She blinked. Her
-forehead didn&rsquo;t move, though. &ldquo;That&rsquo;s...&rdquo; she
-said. &ldquo;That&rsquo;s good to hear, Charlie. I really appreciate
-your attitude.&rdquo;</p>
+forehead didn&rsquo;t move, though. &ldquo;That&rsquo;s... that&rsquo;s
+good to hear, Charlie. I appreciate your attitude.&rdquo;</p>
 <p>I nodded. I wasn&rsquo;t
 an idiot. Letting Cassandra Cautery know how much I cared about
-swapping out my arm would make things difficult. I had been locked in
-a room, not long ago. And while I&rsquo;d been in there, I vowed
-never to let myself be put in that position again. It was better if
-everyone believed I was with the program. Planning no more
-amputations. I wouldn&rsquo;t get to use that operating theater. But
-that hadn&rsquo;t stopped me before.</p>
+swapping out my arm would make things difficult for me. They had
+locked me in a room, not long ago. And while I&rsquo;d been in there,
+I&rsquo;d vowed never to let myself be put in that position again. It
+was better if everyone believed I was with the program. Planning no
+more amputations. It meant I wouldn&rsquo;t get to use that operating
+theater. But that hadn&rsquo;t stopped me before.</p>
--- page0066.mm
+++ page0066.mm
@@ -6,16 +6,16 @@
 couldn&rsquo;t replace my arm on my own. The irony was that if I
 already had a mechanical arm, it would have been easy. I could have
 programmed it, sedated myself, and woken to a finished job. Well,
-that&rsquo;s exagerating a little. I wouldn&rsquo;t be comfortable
+that&rsquo;s exaggerating a little. I wouldn&rsquo;t be comfortable
 trusting it quite that far. But even conscious, it would have been
 handy. The attachments you could snap onto that arm, you wouldn&rsquo;t
 believe.</p>
 <p>Stuck with two meat
 arms, though, there was no way I could sever one of them, prepare the
 stump, and fit the machine. The difficulty wasn&rsquo;t the
-ampututation. The amputation I could handle, with sufficient
-quantities of analgesia. The hard part was what came next. All those
-nerves in the shoulder had to be 
+amputation. The amputation I could handle, with sufficient quantities
+of analgesia. The hard part was what came next. All those nerves in
+the shoulder had to be 
 </p>
 <p>There were a lot of
 nerves in the shoulder. There were a lot of connectors in the arm. 
