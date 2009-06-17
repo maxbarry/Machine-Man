caaa822356ef5131b2d019019af812f0cc4d1f5b
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-17 06:08:50 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@354 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0066.mm
+++ page0066.mm
@@ -1,47 +1,28 @@
 <p>Page 66.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>I had a problem: I
-couldn&rsquo;t replace my arm on my own. The irony was that if I
-already had a mechanical arm, it would have been easy. I could have
-programmed it, sedated myself, and woken to a finished job. Well,
-that&rsquo;s exaggerating a little. I wouldn&rsquo;t be comfortable
-trusting it quite that far. But even conscious, it would have been
-handy. The attachments you could snap onto that arm, you wouldn&rsquo;t
-believe.</p>
-<p>Stuck with two meat
-arms, though, there was no way I could sever one of them, prepare the
-stump, and fit the machine. The difficulty wasn&rsquo;t the
-amputation. The amputation I could handle, with sufficient quantities
-of analgesia. The hard part was what came next. All those nerves in
-the shoulder had to be 
-</p>
-<p>There were a lot of
-nerves in the shoulder. There were a lot of connectors in the arm. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>That would require
-a level of pain-numbing drugs.</p>
-<p>For one thing, I
-didn&rsquo;t especially want to be conscious when it came off. For
-another, 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+<p>As the Contours
+took me through the corridors, I ate my muffin and tried to figure
+out how to replace my own arm. There was an obvious logistical
+problem: once I removed my meat arm, that left me with just one, and
+I wasn&rsquo;t sure that was enough to attach the metal one. Which
+made it a frustrating little logic puzzle, like having to transport a
+fox, chicken, and bag of seed on a boat that could only bear two of
+them. If I <em>had</em> a mechanical arm, there would be no problem: I
+could program it for a slice and dice, sedate myself, and wake to a
+finished job. Well, that&rsquo;s an exaggeration. I wouldn&rsquo;t be
+comfortable trusting it that far. Not to the point of doing surgery
+on me while I slept. But still. It was like needing a ladder that&rsquo;s
+on the roof.</p>
+<p>I reached the
+elevator, swiped my pass, and pressed the button for sub-floor level
+three. The panel emitted a regretful tone. The screen read: CONTACT
+MANAGEMENT.</p>
+<p>&ldquo;Hmm,&rdquo;
+I said. I pressed the button again, just in case. But no. I did not
+have access.</p>
+<p>Stymied. It wasn&rsquo;t
+a total surprise. But it made certain things clear.</p>
+<p>I
+stepped out of the elevator and chewed my lip. After a minute, I
+decided two things. First, I should get Lola to help me with this
+arm. Second, it would be smart to go to the Repository, collect all
+my parts, and never let them out of my sight again.</p>
--- page0067.mm
+++ page0067.mm
@@ -1,10 +1,32 @@
-<p>Page 65.</p>
+<p>Page 99.</p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
+<p>even conscious, it
+would have been handy. The attachments you could snap onto that arm,
+you wouldn&rsquo;t believe.</p>
+<p>Stuck with two meat
+arms, though, there was no way I could sever one of them, prepare the
+stump, and fit the machine. The difficulty wasn&rsquo;t the
+amputation. The amputation I could handle, with sufficient quantities
+of analgesia. The hard part was what came next. All those nerves in
+the shoulder had to be 
+</p>
+<p>There were a lot of
+nerves in the shoulder. There were a lot of connectors in the arm. 
+</p>
 <p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>In the elevator, I
+tried some lateral thinking, and briefly entertained the idea of
+attaching the metal arm <em>first.</em> Potentially, I could splice
+some nerves, install the thing just above my shoulder. 
+</p>
 <p></p>
 <p></p>
 </BODY>
