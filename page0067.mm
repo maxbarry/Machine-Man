a92d4d0d1dc96b0e6a7506bd63e9f4f48f095ca0
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-18 06:09:25 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@356 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0067.mm
+++ page0067.mm
@@ -1,33 +1,38 @@
-<p>Page 99.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>even conscious, it
-would have been handy. The attachments you could snap onto that arm,
-you wouldn&rsquo;t believe.</p>
-<p>Stuck with two meat
-arms, though, there was no way I could sever one of them, prepare the
-stump, and fit the machine. The difficulty wasn&rsquo;t the
-amputation. The amputation I could handle, with sufficient quantities
-of analgesia. The hard part was what came next. All those nerves in
-the shoulder had to be 
-</p>
-<p>There were a lot of
-nerves in the shoulder. There were a lot of connectors in the arm. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>In the elevator, I
-tried some lateral thinking, and briefly entertained the idea of
-attaching the metal arm <em>first.</em> Potentially, I could splice
-some nerves, install the thing just above my shoulder. 
-</p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+<p>Page 67.</p>
+<p>My heart thumped as
+I approached the Repository. I was being silly: my parts would still
+be there, of course they would. The alternative was, what, that
+Cassandra Cautery had stolen them away to trial in other people? That
+was ridiculous. Even if that fitted into her grand corporate plan, I
+refused to believe she would do it. Because they were mine. They were
+my parts.</p>
+<p>I reached the door
+and fumbled for my card. My biological fingers shook. Finally I got
+the card into the slot and swiped.</p>
+<p>The lock blinked
+red. This was such a shock that I stood there for a moment, staring
+at it. This lock had never shown me red, ever. There was no reason
+for it. It was my room.</p>
+<p>&ldquo;No.&rdquo;
+The slipped out of my mouth in a groan, sounding like it belonged to
+someone else. I tried the card again. Red. Red. Red.</p>
+<p>I couldn&rsquo;t
+breathe. A roaring grew in my ears, like a waterfall. My vision
+flared white. I grabbed at the wall for balance. Then I laughed
+giddily, because of course I was in the Contours, which would keep me
+upright whether I was conscious or not, not like treacherous meat
+legs, and then I did start to faint, and hit my skull on the wall.
+&ldquo;Ow,&rdquo; I said, and realized I was crying; or, at least,
+that there was salty water on my cheeks, and the bottom-line was that
+I needed to get into the Repository right away to see if my parts
+were okay.</p>
+<p>I kicked the door.
+It flew off its hinges and broke against the far wall. I had kicked
+it pretty hard. I was not thinking clearly. I walked into the
+Repository and everything was all right, because surrounding me were
+the gleaming steel shelves, lined with prosthetics. I started to
+laugh. I <em>had</em> been silly. It was going to be hard to explain
+this door.</p>
+<p>Then my stomach
+clenched. There was a gap. A space, where none should exist. My arms
+were missing. And not in the good way.</p>
