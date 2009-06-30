bb548954f66c086bd8d2d1d583c4801eb5211622
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-06-30 07:26:19 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@374 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0075.mm
+++ page0075.mm
@@ -3,19 +3,19 @@
 night I couldn&rsquo;t sleep. It wasn&rsquo;t the phantom pain. It
 was Carl. He crawled into my mind and I couldn&rsquo;t get him out. I
 woke every half-hour, sticky with sweat, trying to escape Carl
-as he chased me armlessly through the corridors. One time, he stood
-in the Clamp, looking at me as the plates closed in. His eyes said:
-<em>How could you do this, Charlie? You know I need some parts</em>.</p>
+as he chased me armlessly through the corridors. Once he stood in the
+Clamp, looking at me as the plates closed in. His eyes said: <em>How
+could you do this, Charlie? You know I need some parts</em>.</p>
 <p>I sat up, shivering. Carl was a bad
-guy, I told myself: he had tried to kill Lola and steal my arms. And
-even if that wasn&rsquo;t quite true, he was a destroyer of
-relationships. I had seen the look in Lola&rsquo;s eyes, when she&rsquo;d
-said: <em>He doesn&rsquo;t have any arms</em>.
+guy, wasn&rsquo;t he? He had tried to kill Lola and steal my arms. Or
+if that wasn&rsquo;t quite true, he was a destroyer of relationships.
+I had seen the look in Lola&rsquo;s eyes, when she&rsquo;d said: <em>He
+doesn&rsquo;t have any arms</em>.
 Carl was poison.</p>
 <p>I had asked Cassandra Cautery to fire
-him. That was what I&rsquo;d asked, in her office yesterday. She
-nodded once. &ldquo;I&rsquo;ll take care of it,&rdquo; she said. I
-walked out of that room happy, thinking my problems were solved.</p>
+him. That was what I&rsquo;d said, in her office yesterday. She
+nodded once. I walked out of that room happy, thinking my problems
+were solved.</p>
 <p>But here I was wracked with guilt. The
 guy had no arms. Without my help, he would get, what, hospital
 prosthetics? Those were awful. You might as well strap on flippers.</p>
