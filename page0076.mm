2aef67294cd23a010ece0d8758f319c7a1545781
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-01 09:22:18 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@377 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0076.mm
+++ page0076.mm
@@ -1,14 +1,14 @@
 <p>Page 76.</p>
 <p>I lay still while the fMRI machine
-thrummed around me. At first this was unsettling, because you have to
-lie back and poke your head into a small hole in a large machine. The
+thrummed around me. At first this was unsettling, because I had to
+lie back and poke my head into a small hole in a large machine. The
 issue is the hole looks like a mouth. Then there&rsquo;s the noise,
 and it&rsquo;s hard to forget the unit is generating enough magnetic
 force to pull a pin clean through your body. But after a while, the
-<em>whoomp-whoomp-whoomp</em> becomes comfortingly rhythmic. I felt
-like I was in a womb.</p>
+<em>whoomp-whoomp-whoomp</em> became comfortingly rhythmic. I felt I
+was in a womb.</p>
 <p>&ldquo;That&rsquo;s good,&rdquo; said a
-disembodied voice. It was Jason, my lab assistant. &ldquo;Now regret.
+disembodied voice. Jason, my lab assistant. &ldquo;Now regret.
 Something you wish you could change.&rdquo;</p>
 <p>I thought a moment. &ldquo;An uncle of
 mine died from colon cancer. I was about twelve. I remember thinking
@@ -17,35 +17,35 @@ whole thing.&rdquo;</p>
 <p>I had figured that as long as I was
 here, I might as well map the whole brain. That would come in handy
 later. Also, if things went like I planned, it would soon be
-difficult to take all the metal out of my body.</p>
+difficult to MRI myself non-fatally.</p>
 <p>&ldquo;Sorry, Dr. Neumann, I&rsquo;m
 not seeing it. Can you try again?&rdquo;</p>
 <p>&ldquo;Uh, okay. One time in junior
 high, I didn&rsquo;t go to a school dance because I thought nobody
 would want to go with me, then afterward, I heard this girl I liked
 really wished that I&rsquo;d asked her.&rdquo; Isabelle. She had been
-good at chess. Always under-developed her rooks, though. &ldquo;How
-was that?&rdquo;</p>
-<p>&ldquo;Not definitive.&rdquo;</p>
+good at chess. Always under-developed her rooks, though.</p>
+<p>&ldquo;Still not definitive.&rdquo;</p>
 <p>I almost said: <em>Let&rsquo;s skip
-regret</em>. Because, really, how
-useful was it? It was like guilt; good for promoting social behavior
-amongst tribe members, but not great for the individual. I mean,
-ideally, everyone would feel guilty about treating you unfairly, but
-you&rsquo;d be able to screw over whomever you liked and have a solid
-night&rsquo;s sleep. I&rsquo;m not saying that&rsquo;s a great set of
-values. I&rsquo;m just saying, logically, that&rsquo;s what would
-work best for you. And, incidentally, you can&rsquo;t tell me there
-aren&rsquo;t already a lot of people out there with abnormally low
-guilt-regret indexes doing pretty well for themselves. I&rsquo;m no
-expert on social behavior, but I have spent some time with Better
-Future management, and I would like to plot that data set.</p>
-<p>&ldquo;There
-was a kid in the cafeteria the other day,&rdquo; I said. &ldquo;He
-told me he admired my work. And I didn&rsquo;t know what to say. He
-was just excited, but I kind of brushed him off. I wanted to say
-something, like, you know, thanks. But I couldn&rsquo;t think. I wish
-I&rsquo;d been able to say something to that kid.&rdquo;</p>
+regret</em>. Because, really, what
+good was it? It was like guilt; effective at promoting social
+behavior amongst tribe members, but not great for the individual. I
+mean, in an ideal world, everyone would feel guilty about treating
+<em>you</em> unfairly, but
+you&rsquo;d be able to cheat your grandmother and get a solid night&rsquo;s
+sleep. I&rsquo;m not saying that&rsquo;s a great set of values. I&rsquo;m
+just saying, logically, that&rsquo;s what would work best for you.
+It&rsquo;s not coincidence that every well-paid person I&rsquo;ve
+ever met seems to possess an abnormally low guilt-regret index, is my
+point. Not that I&rsquo;m an expert on social behavior. But I have
+spent some time around Better Future managers, and I would like to
+plot that data set.</p>
+<p>&ldquo;There was a
+kid in the cafeteria the other day,&rdquo; I said. &ldquo;He told me
+he admired my work. And I didn&rsquo;t know what to say. He was just
+excited, but I kind of brushed him off. I wanted to say something.
+Like, you know, thanks. I wish I&rsquo;d been able to say something
+to that kid. Because he&rsquo;s probably a lot like me.&rdquo;</p>
 <p>There was silence.
 &ldquo;I think that&rsquo;s as close as we&rsquo;re going to get,&rdquo;
-Jason said. &ldquo;Now do happy.&rdquo;</p>
+Jason said. &ldquo;Let&rsquo;s try happy.&rdquo;</p>
