00e0067335cd857abf746bebac3765e0b9f1697a
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-09 01:49:26 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@389 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0082.mm
+++ page0082.mm
@@ -2,11 +2,11 @@
 <p>I hit the button for the elevator and
 bounced up and down in the Contours while I waited. I was tempted to
 take an express route, like leaping out a window and walking back in
-through the lobby. Because I could do that. But that would be a bad
-idea for the same reason I wasn&rsquo;t walking Lola Banks to the
-labs: it would let everyone know that something was up.</p>
+through the lobby. Because I could probably do that. But it would be
+a bad move for the same reason I wasn&rsquo;t walking Lola Banks to
+the labs: it would let people know that something was up.</p>
 <p>Of course, I was being stupid. I was
 assuming that Lola&rsquo;s suite was private. And this was clearly
-not the case, because when the elevator finally arrived, it had
-Cassandra Cautery in it. &ldquo;Charlie.&rdquo; She did not look
-happy. &ldquo;Come here.&rdquo;</p>
+not the case, because when the elevator arrived, it had Cassandra
+Cautery in it. &ldquo;Charlie,&rdquo; she said. &ldquo;We need to
+talk.&rdquo;</p>
--- page0083.mm
+++ page0083.mm
@@ -1,7 +1,7 @@
 <p>Page 83.</p>
 <p>&ldquo;The first thing I&rsquo;d like
 to lay on the table,&rdquo; said Cassandra Cautery when the elevator
-doors had closed, &ldquo;just so it&rsquo;s out there, is that we&rsquo;ve
+doors closed, &ldquo;just so it&rsquo;s out there, is that we&rsquo;ve
 made a fairly significant investment in Lola Banks. That life-saving
 operation did not come cheap.&rdquo;</p>
 <p>&ldquo;The life-saving operation she
@@ -19,28 +19,29 @@ the right thing by you. I really have. When you asked me to dispose
 of Carl, did I quibble? Did I say, <em>Gee, that&rsquo;s a little
 heartless, he&rsquo;s a ten-year employee with no arms?</em>
 No. I did not.&rdquo;</p>
-<p>&ldquo;Fire.&rdquo;</p>
+<p>&ldquo;Fire,&rdquo;
+I said, to clarify.</p>
 <p>She glanced at me.
 &ldquo;What?&rdquo;</p>
 <p>&ldquo;I
 asked you to <em>fire</em>
 Carl.&rdquo;</p>
 <p>&ldquo;Actually,
-Charlie, your precise words were <em>get rid of</em>.&rdquo;</p>
+your precise words were <em>get rid of</em>.&rdquo;</p>
 <p>&ldquo;That&rsquo;s
 the same thing.&rdquo;</p>
 <p>She hesitated. &ldquo;Of
-course it is. But the point is, I&rsquo;ve done my best to make this
-a supportive environment for you. I&rsquo;ve sheltered you from the
+course it is. But the point is, I&rsquo;ve done my best to provide
+you with a supportive environment. I&rsquo;ve sheltered you from
 painful decisions that had to be made. And there have been painful
 decisions, Charlie. There have been many.&rdquo;</p>
 <p>The elevator doors
 opened. We were not on the labs sublevel. We were somewhere else.</p>
 <p>&ldquo;But you had
 to go and excite Miss Banks. Well, now I can&rsquo;t shelter you any
-more.&rdquo; I realized what that tightness was in Cassandra
-Cautery&rsquo;s voice. It was nerves. &ldquo;Now we&rsquo;re all
-going to get a little reality check.&rdquo;</p>
+more.&rdquo; I realized what the tightness was in Cassandra Cautery&rsquo;s
+voice. It was nerves. &ldquo;Now we&rsquo;re all going to get a
+little reality check.&rdquo;</p>
 <p>I could see myself:
 on the wall outside the elevator hung a huge, ornate silver mirror.
 To one side sat a little table bearing a lamp and a vase of white
