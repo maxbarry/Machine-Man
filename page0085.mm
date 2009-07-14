76cb51756dc7e0af2604c3cf1c7bed3bb6262e9e
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-14 09:34:18 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@394 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0085.mm
+++ page0085.mm
@@ -4,8 +4,8 @@ led Cassandra Cautery and I to what I can only call a sitting room. I
 say this as someone who is not totally sure what a sitting room is. I
 mean something that looked stolen from an 18th Century Victorian
 mansion: heavy drapes, busy wallpaper, and chairs with curving,
-detailed legs. Turned, I think is the word. That was the room:
-turned. I felt elegant just being here.</p>
+detailed legs. Turned, I think is the word. I felt elegant just being
+here.</p>
 <p>&ldquo;You know who
 we&rsquo;re meeting,&rdquo; said Cassandra Cautery, once the girl had
 gently closed the door on us. This was not posed as a question,
