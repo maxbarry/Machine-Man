bd4cc92fcad01ec3f0de69c7b428b12e896ce4bc
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-18 01:41:52 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@402 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0089.mm
+++ page0089.mm
@@ -3,7 +3,7 @@
 NINETEEN</h5>
 <p>Earlier, I hadn&rsquo;t
 paid much attention to which floor we were on. It was somewhere I
-hadn&rsquo;t been before, was all that really registered. But as I
+hadn&rsquo;t been before; that&rsquo;s all that registered. But as I
 moved to the shattered glass window, and pushed aside the drapes
 flicking and twisting in the sudden wind, I realized: we were really,
 really high up.</p>
--- page0090.mm
+++ page0090.mm
@@ -1,188 +1,28 @@
 <p>Page 90.</p>
 <p>&ldquo;You&rsquo;ve
-killed him.&rdquo; I hadn&rsquo;t realized Cassandra Cautery had come
-over to stand next to me. But there she was. Below us</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I wanted to say:
-<em>What area?</em> But my mouth wouldn&rsquo;t move. My brain was
-still processing.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>the raw,
-instinctive jolt of hostility</p>
-<p></p>
-<p></p>
-<p>He stepped toward
-me again, and I guess I was in a heightened state of anxiety, because
-my brain barked THREAT, and my Contours twitched, and I kicked him
-through the window.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>I did next was
-determined by the raw, instinctive jolt of hostility</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I made a decision
-with the 
-</p>
-<p></p>
-<p>I made a decision
-with 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>We came to believe
-that it was better to install 
-</p>
-<p></p>
-<p></p>
-<p>Even in the glare,
-I could see strain on her face. &ldquo;Nobody knows what an
-Augmentation is like better than you, Charlie.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>your primary
-usefulness is no longer in design. Thanks to your tutelage, we have a
-half-dozen people who can do design. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>that there are now
-a half-dozen people in this organization who, under your tutelage,
-have gained the necessary expertise to design Better Future Combat
-Augmentations. Again, that&rsquo;s not to underplay your
-significance. It&rsquo;s my fervent hope that you will tell me right
-now that you want to continue to lead the team down this exciting
-road. But if you don&rsquo;t...&rdquo; He shrugged. &ldquo;Well, we
-can do this without you.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>You no longer have
-to be the only guy. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>, we see that as
-having passed. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>confined to
-open-ended, experimental research, we see that as having passed.
-Because 
-</p>
-<p>Your usefulness
-today, Dr. Neumann, is in passing on that knowledge to your peers</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>we see the time
-when your sole role was experimenting 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>you have useful
-skills</p>
-<p></p>
-<p></p>
-<p></p>
-<p>There&rsquo;s a
-range of useful duties you can perform for us,&rdquo; said the
-Manager. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>He paused, in case
-I wanted to say something. But I was still busy.</p>
-<p></p>
-<p>This project exists
-because of you. I&rsquo;m not forgetting your contribution. But 
-</p>
-<p>there comes a time,
-Dr. Neumann, when 
-</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Describing
-weapons.  &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; &ldquo;Like
-Lola Banks.&rdquo; &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p>I once did one of
-those personality tests.</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; OK: The
-Manager says they&rsquo;re interested in making super soldiers. He
-says Lola&rsquo;s got an EMP in her chest. Explains how it works. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt;
-I want to end here with Charlie busting the fuck out, don&rsquo;t I?
-I think so. I think Charlie says he&rsquo;s got to get back to Lola,
-and the Manager says Lola is taken care of. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt;
-I think the Manager should make it very clear to Charlie that there&rsquo;s
-only one way out here. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt;
-Charlie leaps out through the drapes. They protect him from shattered
-glass. Then he grabs Lola and they run off. I think he cleans out the
-Repo of parts, too. Maybe some guards shoot at him. It&rsquo;s a cool
-action scene. Come up with some unusual things. And remember Charlie
-is not tough. He&rsquo;s a smart guy afraid for his life with some
-kick ass legs. &gt;&gt;</p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+killed him.&rdquo; I didn&rsquo;t realized Cassandra Cautery was
+beside me until she spoke. She braced herself carefully against what
+was left of the window frame and peered out. The drapes flapped
+around her. &ldquo;Look at him. He&rsquo;s just lying there.&rdquo;</p>
+<p>&ldquo;I didn&rsquo;t...&rdquo;
+The rest of this sentence was: <em>mean to</em>. But I couldn&rsquo;t
+get it out. My chest was compressing my lungs.</p>
+<p>&ldquo;The Manager
+is dead.&rdquo; There was a touch of awe in her voice. &ldquo;He is
+definitely dead.&rdquo;</p>
+<p>I looked again,
+then wished I hadn&rsquo;t. Occupying most of the space between the
+Better Future building and the road was a wide, healthy lawn.
+Bisecting this was a narrow concrete path, and on this the Manager
+was sprawled. I am tempted to say this was extraordinary bad luck,
+but given his position, I can&rsquo;t honestly believe he would have
+survived if he&rsquo;d landed on the grass, either. I couldn&rsquo;t
+see his head.</p>
+<p>My legs took a step
+backward. Whatever part of me was in charge at this time, I will give
+it this: it analyzed the situation faster than I did.</p>
+<p>Cassandra Cautery
+looked at me. &ldquo;Oh, Charlie. You are in so very much trouble.&rdquo;</p>
+<p>My legs were
+backing me out of the room. I finally reached the conclusion they
+had, and turned and ran.</p>
