a2587ef667a982ec7500701620235d949826b501
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-23 07:02:10 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@407 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0092.mm
+++ page0092.mm
@@ -4,9 +4,9 @@ thumped into my left bicep. I felt it not so much as a physical pain
 as an insult. I hadn&rsquo;t realized how deeply offensive it was to
 have someone deliberately injure you. I shouted, &ldquo;Hey!&rdquo;
 and my voice was thick with outrage. I was going to march up to this
-guy and explain I was a human being, dammit, with hopes and dreams
-and an ID card, and you can&rsquo;t just <em>shoot people</em>. You
-can&rsquo;t just <em>kill them</em>. Which may have been a little rich,
+guard and explain I was a human being, dammit, with hopes and dreams
+and an ID card, and you can&rsquo;t just <em>shoot</em> people. You
+can&rsquo;t just <em>kill</em> them. Which may have been a little rich,
 given I was standing next to the mangled body of the Manager, but I
 didn&rsquo;t think of that at the time. I was aflame with indignation
 about my violated bicep. The only thing that drove this plan from my
@@ -23,4 +23,5 @@ out before me and my hooves drove into the lawn. They almost slipped
 once, and then we reached the sidewalk and I felt them settle. They
 liked concrete. I think we both did. I clung on, cars and trees
 blurring past me, until the security guards were far, far behind, and
-I was safe, and I realized I had forgotten something very important.</p>
+I was safe, and I realized I had left behind something very
+important.</p>
