67ca43bee0252bc0423ae148cac065cf40879760
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-29 02:08:06 +0000

    Page update: 1 new 3 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@413 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0096.mm
+++ page0096.mm
@@ -1,62 +1,28 @@
 <p>Page 96.</p>
-<p>(WEDNESDAY)</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>What was I doing? I
-was a thinker, not a soldier. I wasn&rsquo;t maximizing my strengths.
-</p>
-<p>This assault
-business, this was a really stupid idea. 
-</p>
-<p>I should get out of
-here and make some phone calls.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>This was easier
-said than done. Well, not <em>easier</em>; all I had to do was tell the
-Contours to jump up there. Then I&rsquo;d discover if they could. But
-that was some leap of faith. It was one thing to know my legs were
-programmed to never exceed my physical tolerances; it was another to
-believe it.</p>
-<p>What if Lola wasn&rsquo;t
-there? What if Cassandra Cautery had already moved her? That would
-screw me. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>The issue with this
-is I had to exit the same way. 
-</p>
-<p>Squeezing through
-the gash was a reminder that my bottom half was titanium 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I recalled that
-expression: <em>When all you have is a hammer, everything looks like a
-nail</em>. I had two hammers. And I was seeing a lot of nails.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+<p>The Hummer came at
+me and I kicked it. You know that expression, <em>When all you have is
+a hammer, everything looks like a nail?</em> Well, I had a hammer. I
+had a double-action titanium hammer, and I was starting to see a lot
+of nails.</p>
+<p>The Hummer went up
+on two wheels. The other two passed over my head. The car went a good
+ten yards, teetering on the brink of tipping over, as if it were in
+the circus. It sailed toward the building and I realized that the
+driver faced two mutually exclusive objectives: to bring the car down
+onto four wheels and to not ram a ground-floor meeting room. This was
+really an either-or decision, but the driver tried valiantly to
+accomplish both. The Hummer hit the building at a thirty degree angle
+and disappeared halfway inside.</p>
+<p>From one
+perspective, I can&rsquo;t take much credit for this. It wasn&rsquo;t
+like I had anything much to do with that kick: my input was pretty
+much limited to cowering in the bucket seat and being very clear in
+my mind that I did not want to be run over. The Contours took care of
+the rest: the way one leg braced against the ground, the timing of
+the swing to catch the Hummer at the right point, the delivery of the
+perfect amount of force. But then, that was my code. I had written it
+weeks earlier, but the fact remained I had told them what to do in
+situations like this. So from this perspective, I was just as
+responsible for my legs as anyone else&mdash;more so&mdash;and I had
+just kicked over a car. I felt a tidal wave of pride, and confidence,
+and I looked up, chose a balcony, and jumped.</p>
--- page0097.mm
+++ page0097.mm
@@ -3,3 +3,94 @@
 <p></p>
 <p></p>
 <p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>At that point I
+suspect the driver had to make a call between flipping the vehicle
+and coming down on four wheels only in time to ram the building, and
+he chose to flip. 
+</p>
+<p></p>
+<p></p>
+<p>and then it went. 
+</p>
+<p></p>
+<p>A good driver could
+probably have avoided this, I think, but 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>What was I doing? I
+was a thinker, not a soldier. I wasn&rsquo;t maximizing my strengths.
+</p>
+<p>This assault
+business, this was a really stupid idea. 
+</p>
+<p>I should get out of
+here and make some phone calls.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>This was easier
+said than done. Well, not <em>easier</em>; all I had to do was tell the
+Contours to jump up there. Then I&rsquo;d discover if they could. But
+that was some leap of faith. It was one thing to know my legs were
+programmed to never exceed my physical tolerances; it was another to
+believe it.</p>
+<p>What if Lola wasn&rsquo;t
+there? What if Cassandra Cautery had already moved her? That would
+screw me. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>The issue with this
+is I had to exit the same way. 
+</p>
+<p>Squeezing through
+the gash was a reminder that my bottom half was titanium 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I recalled that
+expression: <em>When all you have is a hammer, everything looks like a
+nail</em>. I had two hammers. And I was seeing a lot of nails.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
