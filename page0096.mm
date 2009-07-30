1dc6b38dd4427e6c84b500cbb8ffb93d0c4c5e04
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-30 05:56:28 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@416 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0096.mm
+++ page0096.mm
@@ -7,12 +7,12 @@ of nails.</p>
 <p>The Hummer went up
 on two wheels. The other two passed over my head. The car went a good
 ten yards, teetering on the brink of tipping over, as if it were in
-the circus. It sailed toward the building and I realized that the
-driver faced two mutually exclusive objectives: to bring the car down
-onto four wheels and to not ram a ground-floor meeting room. This was
-really an either-or decision, but the driver tried valiantly to
-accomplish both. The Hummer hit the building at a thirty degree angle
-and disappeared halfway inside.</p>
+the circus. It sailed toward the Better Future building and I
+realized that the driver faced two mutually exclusive objectives: to
+bring the car down onto four wheels and to not ram a ground-floor
+meeting room. This was really an either-or decision, but the driver
+tried valiantly to accomplish both. The Hummer hit the building at a
+thirty degree angle and disappeared halfway inside.</p>
 <p>From one
 perspective, I can&rsquo;t take much credit for this. It wasn&rsquo;t
 like I had anything much to do with that kick: my input was limited
@@ -20,8 +20,8 @@ to cowering and being very sure that I did not want to be run over.
 The Contours took care of the rest: the bracing of one leg against
 the ground, the timing of the swing of the other, the delivery of the
 correct amount of force. But then, that was my code. I had written it
-weeks earlier, without this particular situation in mind, but the
-fact remained they were acting on my instructions. So from this
+weeks earlier, sure, without this particular situation in mind, but
+the fact remained they were acting on my instructions. So from this
 perspective, I was as responsible for my legs as anyone. And I had
 just kicked a goddamn car into a building. I felt a tidal wave of
 pride, and new confidence. I looked up, located Lola&rsquo;s balcony,
