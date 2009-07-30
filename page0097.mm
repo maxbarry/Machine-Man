d78690da38e525a974edf362cfa8034ff7d0aefb
max <max@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-30 06:04:54 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@417 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0097.mm
+++ page0097.mm
@@ -3,7 +3,7 @@
 side of the Better Future building. Smoked glass offices flashed past
 my face. At the top of my arc through the air, I cleared a railing
 with about two inches to spare, and landed on the balcony as gently
-as if I was stepping out of my house. This would be by far the
+as if I was stepping out of my house. This would have been by far the
 coolest thing I&rsquo;d ever done, if I hadn&rsquo;t just kicked a
 Hummer into a building. But I barely even felt surprise. I had
 realized something, down there on the lawn: I could trust my legs.</p>
@@ -11,13 +11,13 @@ realized something, down there on the lawn: I could trust my legs.</p>
 It was Lola, coming out of the suite. I had picked the right balcony;
 of course I had. She threw herself into my arms. &ldquo;Did you feel
 that, a second ago? Was that an earthquake?&rdquo;</p>
-<p>&ldquo;That was
+<p>&ldquo;No. That was
 me.&rdquo;</p>
 <p>She looked at me,
 then leaned over the balcony. &ldquo;Did you jump up here? Did you
 <em>jump</em>?&rdquo;</p>
-<p>&ldquo;Some stuff
-has happened. We need to get out of here.&rdquo;</p>
+<p>&ldquo;There have
+been some developments. We need to get out of here.&rdquo;</p>
 <p>&ldquo;What&rsquo;s
 that smoke? Is that a car?&rdquo;</p>
 <p>&ldquo;Lola.&rdquo;
@@ -26,15 +26,15 @@ the world. &ldquo;It&rsquo;s important we get out of here as fast as
 possible.&rdquo;</p>
 <p>She sucked in
 breath. &ldquo;Okay.&rdquo;</p>
-<p>This was the point
-I realized I couldn&rsquo;t jump out of here. Or rather, I couldn&rsquo;t
-do it with Lola. The moment my legs touched the ground and began to
+<p>Then I realized I
+couldn&rsquo;t jump out of here. Or rather, I couldn&rsquo;t do it
+with Lola. The moment my legs touched the ground and began to
 decelerate the rest of me, Lola would weigh the equivalent of two
-thousand pounds. There was no way I could hold her.</p>
+thousand pounds. She would be torn out of my arms.</p>
 <p>Lola took my hand,
 the biological one. &ldquo;I&rsquo;m ready.&rdquo;</p>
 <p>Outside, I heard
 the ding of the elevator. That would be security.</p>
 <p>&ldquo;I knew you&rsquo;d
-come back,&rdquo; she said. She was shining again: looking at me like
-I was the greatest thing in the world. &ldquo;I knew it.&rdquo;</p>
+come back.&rdquo; She was shining again: looking at me like I was the
+greatest thing in the world. &ldquo;I knew it.&rdquo;</p>
