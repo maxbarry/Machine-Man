c844f0842afe0784c1b78e8995bff41f19ca513d
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-07-31 05:49:45 +0000

    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@419 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0098.mm
+++ page0098.mm
@@ -1,33 +1,89 @@
 <p>Page 98.</p>
-<p></p>
-<p>(FRIDAY)</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>(THURSDAY)</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Maybe he
-and Lola cower somewhere, and then they talk while Carl hunts them
-down... they never actually leave the premesis? &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
+<p>&ldquo;We have a
+problem,&rdquo; I said.</p>
+<p>&ldquo;What?&rdquo;
+said Lola. &ldquo;Let&rsquo;s go. Pick me up, so you can run real
+fast.&rdquo; She held out her arms.</p>
+<p>&ldquo;I&rsquo;m
+not good on stairs.&rdquo;</p>
+<p>&ldquo;What?&rdquo;</p>
+<p>&ldquo;The
+Contours. They don&rsquo;t handle stairs well.&rdquo;</p>
+<p>&ldquo;What does
+that mean?&rdquo;</p>
+<p>&ldquo;A bug, I
+guess. I haven&rsquo;t had a chance to go through the software&mdash;&rdquo;</p>
+<p>&ldquo;What does
+that mean for <em>us</em>?&rdquo;</p>
+<p>&ldquo;Oh. It means
+we can&rsquo;t take the stairs.&rdquo;</p>
+<p>&ldquo;Who&rsquo;s
+talking about stairs?&rdquo; Lola said. &ldquo;Pick me up and let&rsquo;s
+jump out of here.&rdquo;</p>
+<p>&ldquo;I won&rsquo;t
+be able to hold you.&rdquo;</p>
+<p>&ldquo;Sure you
+can.&rdquo; She spread her hands. &ldquo;I&rsquo;m little.&rdquo;</p>
+<p>&ldquo;Lola, at
+12Gs, you&rsquo;ll weigh as much as a small car.&rdquo;</p>
+<p>&ldquo;Hey.&rdquo;</p>
+<p>&ldquo;That&rsquo;s
+not me. That&rsquo;s physics.&rdquo;</p>
+<p>&ldquo;Maybe we can
+sneak down an elevator.&rdquo;</p>
+<p>&ldquo;I just heard
+the elevator. Between us and it are people who want to kill us.&rdquo;</p>
+<p>&ldquo;Did you say
+kill?&rdquo;</p>
+<p>&ldquo;Yes! Look at
+this.&rdquo; I showed her my bicep.</p>
+<p>&ldquo;Oh, no!&rdquo;</p>
+<p>&ldquo;Exactly. I&rsquo;m
+damaged.&rdquo;</p>
+<p>&ldquo;Injured.&rdquo;</p>
+<p>&ldquo;What?&rdquo;</p>
+<p>&ldquo;Injured,&rdquo;
+Lola said. &ldquo;You&rsquo;re injured.&rdquo;</p>
+<p>&ldquo;That&rsquo;s
+what I said.&rdquo;</p>
+<p>&ldquo;You said...&rdquo;
+She shook her head. &ldquo;They shot you?&rdquo;</p>
+<p>&ldquo;Yes.&rdquo;</p>
+<p>&ldquo;Why would
+they do that?&rdquo;</p>
+<p>I hesitated. &ldquo;I
+kind of killed the CEO. By accident,&rdquo; I added, because Lola&rsquo;s
+eyebrows were doing alarming things.</p>
+<p>&ldquo;Oh, no.&rdquo;</p>
+<p>&ldquo;There are
+extenuating circumstances. Trust me.&rdquo;</p>
+<p>&ldquo;I do,&rdquo;
+she said. Outside, I heard approaching footsteps.</p>
+<p>&ldquo;The thing
+that really irritates me is ten floors down, I have a set of drill
+legs just <em>sitting</em> there.&rdquo;</p>
+<p>&ldquo;Can you
+stomp through the floor?&rdquo;</p>
+<p>&ldquo;It&rsquo;s
+reinforced concrete.&rdquo;</p>
+<p>&ldquo;That&rsquo;s
+a no?&rdquo;</p>
+<p>&ldquo;Obviously
+it&rsquo;s a no! It&rsquo;s not going to collapse if it&rsquo;s&mdash;&rdquo;</p>
+<p>&ldquo;Don&rsquo;t
+get snippy.&rdquo;</p>
+<p>I breathed. &ldquo;I&rsquo;m
+sorry. It&rsquo;s just that I strongly believe our lives are in
+danger unless we figure a way out of this room in the next ten
+seconds.&rdquo;</p>
+<p>Someone thumped on
+the door. Lola and I fell silent. She snaked one hand into mine. I
+felt that strange attractive force stir once more, trying to pull my
+fingers into her chest.</p>
+<p>&ldquo;Get us out
+of here, Charlie,&rdquo; Lola whispered.</p>
+<p>I decided: I would
+kick anyone who came through that door. I took a step forward, moving
+Lola behind me. The door banged open, and I tensed. But then I just
+stood there, my plan already forgotten, because the person in the
+doorway was Carl.</p>
--- page0099.mm
+++ page0099.mm
@@ -1,5 +1,111 @@
-<p>Page 97.</p>
+<p>Page 99.</p>
+<h5>CHAPTER 
+TWENTY</h5>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;If they come
+in,&rdquo; Lola whispered. &ldquo;Don&rsquo;t kill them.&rdquo;</p>
+<p>&ldquo;I wasn&rsquo;t
+going to kill them,&rdquo; I hissed. &ldquo;The CEO was a one-time
+thing.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I breathed.</p>
+<p>&ldquo;Can you kick
+all the guards between here and the elevator?&rdquo;</p>
+<p>&ldquo;What? Kill
+them?&rdquo;</p>
+<p>&ldquo;Not
+necessarily kill them. Just&mdash;&rdquo;</p>
+<p>&ldquo;You see
+these? They kick someone, that person is dead.&rdquo;</p>
+<p>&ldquo;You&rsquo;re
+being snippy again.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;I kicked him
+out a window.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>Lola stared at my
+bicep. &ldquo;Your arm is bleeding.&rdquo;</p>
+<p>&ldquo;They shot
+me.&rdquo;</p>
+<p>&ldquo;Who?&rdquo;</p>
+<p>&ldquo;<em>Them</em>.
+The security guys who are coming right now to probably kill us both. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;I got
+damaged. They shot me.&rdquo;</p>
+<p>&ldquo;Injured,&rdquo;
+Lola said.</p>
+<p>&ldquo;Right.
+Injured.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>(FRIDAY)</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
 <p>(THURSDAY)</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Maybe he
+and Lola cower somewhere, and then they talk while Carl hunts them
+down... they never actually leave the premesis? &gt;&gt;</p>
 <p></p>
 <p></p>
 <p></p>
