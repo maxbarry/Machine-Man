3b42a799a22b81a11a118ff449a7be87ad79bbf2
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-02 01:29:38 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@424 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0099.mm
+++ page0099.mm
@@ -1,6 +1,39 @@
 <p>Page 99.</p>
 <h5>CHAPTER 
-TWENTY</h5>
+TWENTY-ONE</h5>
+<p>Carl looked
+different. At first I couldn&rsquo;t put my finger on it. I was
+distracted by competing thoughts, like why was he even here. I was
+sure Cassandra Cautery had said she&rsquo;d fired him. In fact, I&rsquo;d
+got the impression she&rsquo;d maybe had him disposed of permanently.
+But Carl was not disposed of. He was right here, filling the doorway.
+Blocking the only exit to Lola&rsquo;s suite.</p>
+<p>&ldquo;Oh,&rdquo; I
+said. &ldquo;Hi, Carl.&rdquo; The light from the hallway behind him
+made it hard to see his face. &ldquo;How are you?&rdquo;</p>
+<p>He didn&rsquo;t
+move. Lola peeked around me. &ldquo;Carl?&rdquo;</p>
+<p>Still nothing. He
+was wearing his security uniform, although something about that
+looked different, too. &ldquo;Kind of a crazy day, huh?&rdquo; I
+said. &ldquo;I mean... I don&rsquo;t know if you heard, but...&rdquo;</p>
+<p>Carl stepped into
+the room. And I suddenly realized what was different. It was obvious.
+I just hadn&rsquo;t clicked because Carl had always been a big guy.
+But not this big. Not so big he had to turn sideways to fit through a
+door.</p>
+<p>&ldquo;Miss Banks,&rdquo;
+Carl said. &ldquo;You were always very kind to me.&rdquo;</p>
+<p>I felt a rush of
+relief, and opened my mouth to express this. But Carl&rsquo;s eyes
+stabbed at me, and I shut it again. I think Carl knew I had tried to
+get him fired.</p>
+<p>&ldquo;So, in
+appreciation of that,&rdquo; Carl said, &ldquo;I&rsquo;ll give you a
+head start.&rdquo;</p>
+<p>Page
+100.</p>
+<p>(TUES)</p>
 <p></p>
 <p></p>
 <p></p>
@@ -11,7 +44,12 @@ TWENTY</h5>
 <p></p>
 <p></p>
 <p></p>
-<p>(FRIDAY)</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;&rdquo;</p>
 <p></p>
 <p></p>
 <p></p>
@@ -27,18 +65,47 @@ TWENTY</h5>
 <p></p>
 <p></p>
 <p></p>
-<p></p>
-<p></p>
-<p>(THURSDAY)</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Maybe he
-and Lola cower somewhere, and then they talk while Carl hunts them
-down... they never actually leave the premesis? &gt;&gt;</p>
+<p>Things about Carl:
+he&rsquo;s got artificial arms. He likes Lola. He kinda likes
+Charlie. He&rsquo;s not dead, or fired. He doesn&rsquo;t know Charlie
+wanted him fired. He&rsquo;s been told to get them. But he gives them
+a head start.</p>
 <p></p>
 <p></p>
 <p></p>
+<p></p>
+<p>Page
+101.</p>
+<p>(WED)</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>Page
+NOTES.</p>
+<p></p>
+<p></p>
+<p>I once did one of
+those personality tests.</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; OK: The
+Manager says they&rsquo;re interested in making super soldiers. He
+says Lola&rsquo;s got an EMP in her chest. Explains how it works. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt;
+Charlie leaps out through the drapes. They protect him from shattered
+glass. Then he grabs Lola and they run off. I think he cleans out the
+Repo of parts, too. Maybe some guards shoot at him. It&rsquo;s a cool
+action scene. Come up with some unusual things. And remember Charlie
+is not tough. He&rsquo;s a smart guy afraid for his life with some
+kick ass legs. &gt;&gt;</p>
+<p></p>
+<p></p>
+</BODY>
+</HTML>
