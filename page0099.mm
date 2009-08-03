bdde957cae11c37f7ce00042f3d0a254a3e37291
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-03 02:26:48 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@425 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0099.mm
+++ page0099.mm
@@ -3,80 +3,120 @@
 TWENTY-ONE</h5>
 <p>Carl looked
 different. At first I couldn&rsquo;t put my finger on it. I was
-distracted by competing thoughts, like why was he even here. I was
-sure Cassandra Cautery had said she&rsquo;d fired him. In fact, I&rsquo;d
-got the impression she&rsquo;d maybe had him disposed of permanently.
-But Carl was not disposed of. He was right here, filling the doorway.
-Blocking the only exit to Lola&rsquo;s suite.</p>
-<p>&ldquo;Oh,&rdquo; I
-said. &ldquo;Hi, Carl.&rdquo; The light from the hallway behind him
-made it hard to see his face. &ldquo;How are you?&rdquo;</p>
-<p>He didn&rsquo;t
+distracted by other thoughts, like why was he even here. Cassandra
+Cautery had said she&rsquo;d fired him. In fact, I&rsquo;d got the
+impression she&rsquo;d maybe disposed of him permanently. But Carl
+was not disposed of. He was right here, filling the doorway, blocking
+the only exit to Lola&rsquo;s suite that didn&rsquo;t require falling
+eighty feet.</p>
+<p>&ldquo;Hi, Carl.&rdquo;
+The light from the hallway behind him made it hard to see his face.
+&ldquo;How are you?&rdquo;</p>
+<p>Carl didn&rsquo;t
 move. Lola peeked around me. &ldquo;Carl?&rdquo;</p>
 <p>Still nothing. He
 was wearing his security uniform, although something about that
 looked different, too. &ldquo;Kind of a crazy day, huh?&rdquo; I
-said. &ldquo;I mean... I don&rsquo;t know if you heard, but...&rdquo;</p>
+said. &ldquo;I mean... I don&rsquo;t know how up you are on current
+events, but...&rdquo;</p>
 <p>Carl stepped into
-the room. And I suddenly realized what was different. It was obvious.
-I just hadn&rsquo;t clicked because Carl had always been a big guy.
+the room. Suddenly what was different about him became obvious. I
+hadn&rsquo;t clicked earlier because Carl had always been a big guy.
 But not this big. Not so big he had to turn sideways to fit through a
 door.</p>
+<p>His arms were
+concealed by what must have been a custom-tailored uniform, but his
+hands protruded from his sleeves: thick blocks of gray metal. They
+looked like sledgehammers. I had not designed these.</p>
 <p>&ldquo;Miss Banks,&rdquo;
 Carl said. &ldquo;You were always very kind to me.&rdquo;</p>
 <p>I felt a rush of
-relief, and opened my mouth to express this. But Carl&rsquo;s eyes
-stabbed at me, and I shut it again. I think Carl knew I had tried to
-get him fired.</p>
-<p>&ldquo;So, in
-appreciation of that,&rdquo; Carl said, &ldquo;I&rsquo;ll give you a
+relief. I opened my mouth to express this, but Carl&rsquo;s eyes
+flicked at me, and I shut up again. It was clear to me in that moment
+that Carl knew I had tried to get him fired.</p>
+<p>&ldquo;In
+appreciation of that,&rdquo; he said, &ldquo;I&rsquo;ll give you a
 head start.&rdquo;</p>
 <p>Page
 100.</p>
-<p>(TUES)</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Things about Carl:
-he&rsquo;s got artificial arms. He likes Lola. He kinda likes
-Charlie. He&rsquo;s not dead, or fired. He doesn&rsquo;t know Charlie
-wanted him fired. He&rsquo;s been told to get them. But he gives them
-a head start.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+<p>Carl walked us to a
+supply elevator I&rsquo;d never used before and swiped his ID card. I
+found this interesting, because a moment ago, Carl&rsquo;s hands had
+looked like blocks of stone. Now they&rsquo;d split into two thick
+fingers, supple enough to grasp the tag around his neck. It seemed
+they could split a few different ways, then come together to deliver
+a punching force. That wasn&rsquo;t a bad idea, for security guard
+hands. It&rsquo;s what I would have done, if I&rsquo;d designed them.
+Which I hadn&rsquo;t.</p>
+<p>&ldquo;Take it down
+all the way,&rdquo; Carl said. &ldquo;It opens in the parking lot.
+Then you&rsquo;re on your own.&rdquo;</p>
+<p>Lola and I shuffled
+inside. &ldquo;Thank you, Carl.&rdquo;</p>
+<p>&ldquo;I&rsquo;m
+not doing you a favor. I&rsquo;m just paying my debts.&rdquo;</p>
+<p>&ldquo;Well...
+thanks anyway. And I&rsquo;m sorry.&rdquo;</p>
+<p>The elevators doors
+began to slide closed, but Carl raised a hand to send them back. His
+fist was a block of solid metal again. &ldquo;Sorry, Dr. Neumann?&rdquo;</p>
+<p>&ldquo;Yes.&rdquo;</p>
+<p>&ldquo;What are you
+sorry for?&rdquo;</p>
+<p>&ldquo;Well... you
+know.&rdquo;</p>
+<p>&ldquo;For having
+them take away my prosthetics? That&rsquo;s what you mean?&rdquo;</p>
+<p>&ldquo;Ah...&rdquo;</p>
+<p>&ldquo;When I was
+left unable to unzip my own fly, because you ordered that nobody
+could use your arms but you? That&rsquo;s what you&rsquo;re talking
+about?&rdquo;</p>
+<p>&ldquo;Uh...&rdquo;</p>
+<p>&ldquo;When I had
+to be hidden out of your sight, because you wouldn&rsquo;t tolerate
+me in the same building?&rdquo;</p>
+<p>&ldquo;Carl&mdash;&rdquo;</p>
+<p>&ldquo;That&rsquo;s
+not a problem, Dr. Neumann.&rdquo; He lowered his arm, releasing the
+elevator door. I heard the thin whine of servo-magnetics. &ldquo;I
+have my own parts now.&rdquo;</p>
 <p>Page
 101.</p>
-<p>(WED)</p>
+<p>Lola was silent as
+the elevator descended. I risked a glance at her: she was staring
+straight ahead, her arms stiff. &ldquo;All right,&rdquo; I said.
+&ldquo;Before we reach the parking lot, you should&mdash;&rdquo;</p>
+<p>&ldquo;Did you make
+them take away Carl&rsquo;s arms?&rdquo;</p>
+<p>&ldquo;I&rsquo;m
+not sure now is the best time to discuss this.&rdquo;</p>
+<p>&ldquo;<em>Did you
+make them take his arms?</em>&rdquo;</p>
+<p>&ldquo;They weren&rsquo;t
+his arms!&rdquo; I said. There was more to this argument, but Lola&rsquo;s
+lips thinned to a dramatic white slash, and I decided it would be
+counter-productive to advance it. &ldquo;Let&rsquo;s address this
+later.&rdquo;</p>
+<p>The elevator
+thumped to a stop. The doors seemed to take a long time to open. &ldquo;I
+want you to know, I am working extremely hard right now to not yell
+at you.&rdquo;</p>
+<p>&ldquo;Okay.
+Noted.&rdquo; I held out my arms. &ldquo;Come here.&rdquo;</p>
+<p>She glared at me.</p>
+<p>&ldquo;There&rsquo;s
+probably about to be some running,&rdquo; I said. &ldquo;And you need
+to be on board for that.&rdquo;</p>
+<p>The doors opened.
+It took my eyes a moment to adjust to the contrast: the darkness of
+the parking lot, and the sunlight pouring in the entry ramp. So I
+first became aware of security by the shouting.</p>
+<p>Lola jumped into my
+arms. I wrapped her up tight. Then there was some running.</p>
+<p>Page
+102.</p>
+<p>(THU)</p>
 <p></p>
 <p></p>
 <p></p>
