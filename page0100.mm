ae3b33fd94bfc1ce60618d2758833a5d687cff74
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-03 04:54:10 +0000

    Page update: 1 new 3 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@426 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0100.mm
+++ page0100.mm
@@ -1,5 +1,44 @@
-<p>Page 97.</p>
-<p>(THURSDAY)</p>
-<p></p>
-<p></p>
-<p></p>
+<p>Page
+100.</p>
+<p>Carl walked us to a
+supply elevator I&rsquo;d never used before and swiped his ID card. I
+found this interesting, because a moment ago, Carl&rsquo;s hands had
+looked like blocks of stone. Now they&rsquo;d split into two thick
+fingers, supple enough to grasp the tag around his neck. It seemed
+they could split a few different ways, then come together to deliver
+a punching force. That wasn&rsquo;t a bad idea, for security guard
+hands. It&rsquo;s what I would have done, if I&rsquo;d designed them.
+Which I hadn&rsquo;t.</p>
+<p>&ldquo;Take it down
+all the way,&rdquo; Carl said. &ldquo;It opens in the parking lot.
+Then you&rsquo;re on your own.&rdquo;</p>
+<p>Lola and I shuffled
+inside. &ldquo;Thank you, Carl.&rdquo;</p>
+<p>&ldquo;I&rsquo;m
+not doing you a favor. I&rsquo;m just paying my debts.&rdquo;</p>
+<p>&ldquo;Well...
+thanks anyway. And I&rsquo;m sorry.&rdquo;</p>
+<p>The elevators doors
+began to slide closed, but Carl raised a hand to send them back. His
+fist was a block of solid metal again. &ldquo;Sorry, Dr. Neumann?&rdquo;</p>
+<p>&ldquo;Yes.&rdquo;</p>
+<p>&ldquo;What are you
+sorry for?&rdquo;</p>
+<p>&ldquo;Well... you
+know.&rdquo;</p>
+<p>&ldquo;For having
+them take away my prosthetics? That&rsquo;s what you mean?&rdquo;</p>
+<p>&ldquo;Ah...&rdquo;</p>
+<p>&ldquo;When I was
+left unable to unzip my own fly, because you ordered that nobody
+could use your arms but you? That&rsquo;s what you&rsquo;re talking
+about?&rdquo;</p>
+<p>&ldquo;Uh...&rdquo;</p>
+<p>&ldquo;When I had
+to be hidden out of your sight, because you wouldn&rsquo;t tolerate
+me in the same building?&rdquo;</p>
+<p>&ldquo;Carl&mdash;&rdquo;</p>
+<p>&ldquo;That&rsquo;s
+not a problem, Dr. Neumann.&rdquo; He lowered his arm, releasing the
+elevator door. I heard the thin whine of servo-magnetics. &ldquo;I
+have my own parts now.&rdquo;</p>
--- page0101.mm
+++ page0101.mm
@@ -1,34 +1,33 @@
-<p>Page 98.</p>
-<p>(FRIDAY)</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
 <p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p>I once did one of
-those personality tests.</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; OK: The
-Manager says they&rsquo;re interested in making super soldiers. He
-says Lola&rsquo;s got an EMP in her chest. Explains how it works. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt;
-Charlie leaps out through the drapes. They protect him from shattered
-glass. Then he grabs Lola and they run off. I think he cleans out the
-Repo of parts, too. Maybe some guards shoot at him. It&rsquo;s a cool
-action scene. Come up with some unusual things. And remember Charlie
-is not tough. He&rsquo;s a smart guy afraid for his life with some
-kick ass legs. &gt;&gt;</p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+101.</p>
+<p>Lola was silent as
+the elevator descended. I risked a glance at her: she was staring
+straight ahead, her arms stiff. &ldquo;All right,&rdquo; I said.
+&ldquo;Before we reach the parking lot, you should&mdash;&rdquo;</p>
+<p>&ldquo;Did you make
+them take away Carl&rsquo;s arms?&rdquo;</p>
+<p>&ldquo;I&rsquo;m
+not sure now is the best time to discuss this.&rdquo;</p>
+<p>&ldquo;<em>Did you
+make them take his arms?</em>&rdquo;</p>
+<p>&ldquo;They weren&rsquo;t
+his arms!&rdquo; I said. There was more to this argument, but Lola&rsquo;s
+lips thinned to a dramatic white slash, and I decided it would be
+counter-productive to advance it. &ldquo;Let&rsquo;s address this
+later.&rdquo;</p>
+<p>The elevator
+thumped to a stop. The doors seemed to take a long time to open. &ldquo;I
+want you to know, I am working extremely hard right now to not yell
+at you.&rdquo;</p>
+<p>&ldquo;Okay.
+Noted.&rdquo; I held out my arms. &ldquo;Come here.&rdquo;</p>
+<p>She glared at me.</p>
+<p>&ldquo;There&rsquo;s
+probably about to be some running,&rdquo; I said. &ldquo;And you need
+to be on board for that.&rdquo;</p>
+<p>The doors opened.
+It took my eyes a moment to adjust to the contrast: the darkness of
+the parking lot, and the sunlight pouring in the entry ramp. So I
+first became aware of security by the shouting.</p>
+<p>Lola jumped into my
+arms. I wrapped her up tight. Then there was some running.</p>
