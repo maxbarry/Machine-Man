6533059db30c018f9574020b04074368a32df164
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-04 05:46:50 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@429 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0100.mm
+++ page0100.mm
@@ -8,7 +8,7 @@ four digits, then come together to deliver a punching force. That
 wasn&rsquo;t a bad idea, for security guard hands. It&rsquo;s what I
 would have done, if I&rsquo;d designed them. Which I hadn&rsquo;t.</p>
 <p>&ldquo;This opens
-in the parkling lot,&rdquo; Carl said. &ldquo;Then you&rsquo;re on
+in the parking lot,&rdquo; Carl said. &ldquo;Then you&rsquo;re on
 your own.&rdquo;</p>
 <p>Lola and I shuffled
 inside. Lola said, &ldquo;Thank you, Carl.&rdquo;</p>
@@ -24,17 +24,16 @@ sorry.&rdquo;</p>
 <p>&ldquo;What are you
 sorry for?&rdquo;</p>
 <p>&ldquo;Well...&rdquo;</p>
-<p>&ldquo;For telling
-them you wanted me thrown out? Is that what you mean?&rdquo;</p>
+<p>&ldquo;For asking
+them to get rid of me? Is that what you mean?&rdquo;</p>
 <p>&ldquo;Ah...&rdquo;</p>
-<p>&ldquo;Or do you
-mean you&rsquo;re sorry for asking them to take away my prosthetic
-arms? Is that what you mean? For ordering that nobody be allowed to
-use your parts but you? That&rsquo;s what you&rsquo;re talking about?
-Making them leave me in a bed with a button to push with my foot when
-I needed someone to help me go to the bathroom?&rdquo;</p>
+<p>&ldquo;For making
+them take away my prosthetic arms, because you didn&rsquo;t want
+nobody to use your parts but you? That&rsquo;s what you&rsquo;re
+talking about? That time I spent in a bed with a button to push with
+my foot when I needed someone to help me go to the bathroom?&rdquo;</p>
 <p>&ldquo;Carl&mdash;&rdquo;</p>
-<p>&ldquo;No apology
-is necessary, Dr. Neumann.&rdquo; Carl lowered his arm, releasing the
-elevator door. I heard the thin whine of servo-magnetics. &ldquo;It&rsquo;s
-in the past. I have my own parts now.&rdquo;</p>
+<p>&ldquo;It&rsquo;s
+in the past, Dr. Neumann.&rdquo; Carl lowered his arm, releasing the
+elevator door. I heard the thin whine of servo-magnetics. &ldquo;I
+have my own parts now.&rdquo;</p>
