86068363fb04fff514d7806bdbcb5ea9ca9a5630
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-05 07:04:51 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@432 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0101.mm
+++ page0101.mm
@@ -2,32 +2,33 @@
 <p>Lola was silent as
 the elevator descended. I risked a glance at her: she was staring
 straight ahead, her arms stiff. &ldquo;Well,&rdquo; I said. &ldquo;Do
-you think this elevator really takes us to the parking lot, or Carl
-is setting us up?&rdquo;</p>
-<p>&ldquo;Did you make
-them take away Carl&rsquo;s arms?&rdquo;</p>
+you think this elevator really takes us to the parking lot, or is it
+a trap?&rdquo;</p>
+<p>Lola said nothing.</p>
+<p>I cleared my
+throat. &ldquo;When the doors open&mdash;&rdquo;</p>
+<p>&ldquo;Did you take
+away Carl&rsquo;s arms?&rdquo;</p>
 <p>&ldquo;I don&rsquo;t
 think now is the best time to discuss this.&rdquo;</p>
 <p>&ldquo;<em>Did you
 make them take his arms?</em>&rdquo;</p>
 <p>&ldquo;They weren&rsquo;t
-his arms!&rdquo; There was more to this argument, but Lola&rsquo;s
-lips thinned to a dramatic white slash, and I decided it would be
-counter-productive to advance it. &ldquo;Let&rsquo;s address this
-later.&rdquo;</p>
+his arms!&rdquo; I had more to this argument, but Lola&rsquo;s lips
+thinned to a dramatic slash, and I decided it would be
+counter-productive to advance any further. &ldquo;Let&rsquo;s talk
+about this later.&rdquo;</p>
 <p>The elevator
 thumped to a stop. The doors seemed to take a long time to open. Lola
-said, &ldquo;I want you to know, I&rsquo;m working hard to not yell
-at you.&rdquo;</p>
-<p>&ldquo;Noted.&rdquo;
-I held out my arms. &ldquo;Come here.&rdquo;</p>
-<p>She glared at me.</p>
-<p>&ldquo;I think
-there&rsquo;s about to be some running,&rdquo; I said. &ldquo;And you
-need to be on board for that.&rdquo;</p>
-<p>The doors opened.
-It took my eyes a moment to adjust to the contrast: the darkness of
-the parking lot, and the sunlight pouring in the entry ramp. But I
-could hear the shouting.</p>
+said, &ldquo;I&rsquo;m very disappointed, Charlie.&rdquo;</p>
+<p>&ldquo;Okay.&rdquo;
+I held out my arms. &ldquo;Come here.&rdquo; Her expression became
+alarming, so I added, &ldquo;There&rsquo;s probably going to be some
+running. I need to carry you.&rdquo;</p>
+<p>The doors slid
+apart. It took my eyes a moment to adjust to the contrast: the
+darkness of the parking lot against the rectangle of briliant
+sunlight pouring down the ramp. The shouting was clear enough,
+though: phrases like <em>there he is</em> and <em>take him</em>.</p>
 <p>Lola jumped into my
-arms. I wrapped her up tight. Then there was some running.</p>
+arms. Then there was some running.</p>
--- page0102.mm
+++ page0102.mm
@@ -1,30 +1,23 @@
 <p>Page 102.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>(THU)</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p>I once did one of
-those personality tests.</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; OK: The
-Manager says they&rsquo;re interested in making super soldiers. He
-says Lola&rsquo;s got an EMP in her chest. Explains how it works. &gt;&gt;</p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+<p>The funny thing
+about running with Lola was I nearly dropped her. Well, not funny.
+Dropping Lola while trying to escape a dimly-lit subterranean parking
+from Better Future security personnel, that would not have been
+funny. After all the effort to get this far, that would have been
+incredibly frustrating. Also possibly fatal. But what I mean is I
+didn&rsquo;t realize how I&rsquo;d already gotten used to the
+Contours&mdash;how to lean with them as they moved, how a tiny click
+near the hip meant they were about to jag&mdash;until I tried to do
+it with a hundred ten-pound woman in my arms.</p>
+<p>The Contours bolted
+out of the elevator and Lola slipped around my side. She yelped; her
+fingers clawed at my back. I managed to pull her back when the
+Contours sidestepped a guard I hadn&rsquo;t even seen and Lola
+slipped right out of my grasp. She kept hold of me: she locked her
+legs around my waist, and, as she slid around behind me, wrapped her
+arms around my neck. I choked. My eyes watered. I had no idea what
+the hell was going on. Then we burst into sunlight and Lola bounced
+around to my front again and the top of her skull popped me right in
+the nose. As we hit the road, we clung to each other like lovers,
+lovers who were very angry with each other. I closed my eyes and
+willed the Contours to take us somewhere safe.</p>
