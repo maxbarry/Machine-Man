6be07f72e567e1514514aed0a3cc77dc5a9235e6
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-25 06:57:55 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@469 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0115.mm
+++ page0115.mm
@@ -19,33 +19,34 @@ to you.&rdquo;</p>
 <p>Harrison was
 Angelica&rsquo;s son. He visited two weekends per month. Lola had
 told me this earlier, her eyes shimmering with tragedy and outrage,
-before launching into a long, painful story full of people I didn&rsquo;t
+before launching into a long, painful story of people I didn&rsquo;t
 know doing things I was supposed to care about. I have trouble with
 these kinds of stories because they contain no useful information. I
-mean, I&rsquo;m sorry to hear that Rod was a cold, uncaring bastard
-who prioritized his own career over Angelica&rsquo;s, but I don&rsquo;t
+mean, I&rsquo;m sorry to hear that Rod was an uncaring bastard who
+prioritized his own career over Angelica&rsquo;s, but I don&rsquo;t
 know Rod. I&rsquo;m never going to meet him. I don&rsquo;t understand
 what I&rsquo;m expected to do with this information.</p>
 <p>I smiled through
 toothpaste. &ldquo;Mmm.&rdquo; 
 </p>
 <p>&ldquo;It&rsquo;s
-been so crazy.&rdquo; Her voice dropped. &ldquo;We haven&rsquo;t had
-a chance to get to know each other.&rdquo;</p>
+been so crazy.&rdquo; Lola&rsquo;s voice dropped. &ldquo;We haven&rsquo;t
+had a chance to... get to know each other.&rdquo;</p>
 <p>I leaned forward to
 spit into the sink. Then I frowned, because the Contours weren&rsquo;t
 helping. They were rigid, when I wanted them to tilt. I tried again:
 nothing.</p>
 <p>Lola&rsquo;s
 fingers addressed a wrinkle on my shirt. &ldquo;I think we&rsquo;ve
-earned a chance to relax, don&rsquo;t you?&rdquo;</p>
+earned a chance to relax.&rdquo;</p>
 <p>A thought whispered
 inside my brain: the Contours did not like Lola. They did not want
-her touching my chest. 
+her touching me. 
 </p>
 <p>&ldquo;And you,
 Charlie... you <em>really</em> need to relax.&rdquo;</p>
 <p>It was silly. It
-couldn&rsquo;t be true. But my thought was: Lola was making them mad.</p>
+couldn&rsquo;t be true. But my thought was: Lola was making them
+jealous.</p>
 <p>&ldquo;Come on.&rdquo;
 Lola&rsquo;s fingers entwined mine. &ldquo;Let&rsquo;s go to bed.&rdquo;</p>
