fedc30ad10e58402792195d6043eb102c7b31d3a
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-27 02:03:48 +0000

    Page update: 0 new 5 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@474 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0117.mm
+++ page0117.mm
@@ -1,114 +1,64 @@
 <p>Page 117.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Lola&rsquo;s eyes
-grew huge. She reached like</p>
-<p></p>
-<p>Her chest rose and
-fell. Her hands trembled and clutched at the curtains. 
-</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Shall we
-have Lola go ballistic? Knock out the legs? I kinda think yes.
-Charlie can restore them over a few weeks. Send Lola out for parts.
-Think about going back to BF. &gt;&gt;</p>
-<p></p>
-<p></p>
+<p>Lola&rsquo;s chest
+rose and fell like a heaving sea. This would have been quite erotic
+had there not been a Better Future Combat Augmentation powering up
+inside her chest. Since there was, not so much. &ldquo;Try to lower
+your heart rate,&rdquo; I said. &ldquo;Think about something else.&rdquo;</p>
+<p>&ldquo;What did
+they put in there? <em>What is it doing?</em>&rdquo;</p>
+<p>&ldquo;How about
+those dogs of Angelica&rsquo;s? They&rsquo;re so cute and soft.&rdquo;
+At this moment, they began howling. Paws came scampering up the
+hallway, thumped into the door, then fled. Those useless fur sacks.
+They were not helping.</p>
+<p>&ldquo;<em>Help me!</em>&rdquo;</p>
+<p>&ldquo;Okay. Okay.
+Let&rsquo;s think about this. It&rsquo;s probably some kind of
+weapon.&rdquo;</p>
 <p>&ldquo;<em>What?</em>&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Lola wants
-to get jiggy, Charlie making excuses. There&rsquo;s that magnetic
-force again, which is a good excuse. So Charlie maybe leaps on that,
-but Lola sees that he&rsquo;s happy about it. Then Charlie says, &ldquo;I&rsquo;m
-having an issue with the legs.&rdquo; &gt;&gt;</p>
-<p>Then Lola&rsquo;s
-solution is to take them off. Charlie can&rsquo;t.</p>
-<p></p>
-<p>&ldquo;So Take them
-off,&rdquo; says Lola.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;Right.&rdquo;
-A crazy panic blossomed inside my brain: that Lola was making the
-Contours jealous. I had no grounds for this. I wasn&rsquo;t even
-prepared to grant that the Contours possessed any kind of
-consciousness, despite recent evidence. But I felt suddenly, insanely
-sure that they were getting mad.</p>
-<p>&ldquo;I figure the
-kid&rsquo;s mattress is perfect for you,&rdquo; said Lola. &ldquo;Your
-legs can just lie on the floor.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I smiled. Lola
-smiled. Then an odd thought popped into my head. The thought was that
-Lola and I should not be having this conversation in front of the
-Contours.</p>
-<p></p>
-<p>I said, which
-That...&rdquo; And then I stopped, because a cold thought had stolen
-into my brain.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>We pulled the
-mattress off the kid&rsquo;s bed and lay it beside an unrolled
-camping mat. The mat was for Lola, because there was more of her that
-required cushioning. I could happily sleep standing up, given a wall
-to lean against...</p>
-<p></p>
-<p></p>
-<p>Lola snuggled
-against me. 
-</p>
-<p></p>
-<p>&ldquo;We could
-pull the mattress off that bed,&rdquo; said Lola. &ldquo;And put it
-next to my mat.&rdquo;</p>
-<p>&ldquo;You have a
-mat?&rdquo;</p>
+<p>&ldquo;That&rsquo;s
+what they&rsquo;re doing. Better Future. They&rsquo;re trialing parts
+for super-soldiers.&rdquo;</p>
+<p>Her eyes bulged.
+&ldquo;In <em>me</em>? <em>In</em> me?&rdquo;</p>
+<p>&ldquo;I was just
+as pissed, when I found out,&rdquo; I said. &ldquo;That&rsquo;s why I
+kicked the Manager through that window. But the point is, it probably
+won&rsquo;t hurt you. It will hurt other people.&rdquo;</p>
+<p>&ldquo;I&rsquo;m
+not a super-soldier, Charlie!&rdquo;</p>
+<p>&ldquo;I know
+you&rsquo;re not.&rdquo; I became aware of a tone: a whine so
+high-pitched it was only just entering detectable range. That
+explained the dogs.</p>
 <p>&ldquo;It&rsquo;s a
-camping mat. I slept on it last night. But I can bring it in here so
-we can sleep next to each other.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Maybe Lola
-starts playing with him, he feels uncomfortable bc of the Contours
-(watching him). &gt;&gt;</p>
-<p>&lt;&lt; Maybe Lola
-</p>
-<p></p>
-<p>&ldquo;It&rsquo;s
-actually quite difficult for me to lie down,&rdquo; I said.</p>
-<p>&ldquo;Because of
-the legs.&rdquo;</p>
-<p>&ldquo;Yes.&rdquo;</p>
-<p>Lola went silent.
-She was not saying: <em>You should take them off, then.</em></p>
+bomb.&rdquo; Lola&rsquo;s lips were turning blue. &ldquo;Oh God. It&rsquo;s
+a bomb, I can feel it.&rdquo;</p>
+<p>&ldquo;Mmm, I don&rsquo;t
+think so. Not cost-effective.&rdquo;</p>
+<p>&ldquo;<em>What?</em>&rdquo;</p>
+<p>&ldquo;Well, you
+want to blow something up, is the best way to do it really to go to
+all the trouble of surgically installing&mdash;&rdquo;</p>
+<p>&ldquo;<em>It&rsquo;s
+shaking!</em>&rdquo; Lola shrieked. Her teeth chattered. &ldquo;Run,
+Charlie!&rdquo;</p>
+<p>&ldquo;We just need
+to lower your heart rate. You can do it, Lola.&rdquo;</p>
+<p>&ldquo;I can&rsquo;t!&rdquo;</p>
+<p>&ldquo;I believe in
+you. You can&mdash;&rdquo;</p>
+<p>&ldquo;Please!
+Run!&rdquo;</p>
+<p>Angelica&rsquo;s
+voice came down the hall, over the howling of the dogs. &ldquo;What
+the hell is happening?&rdquo;</p>
+<p>&ldquo;No,&rdquo; I
+said. &ldquo;I&rsquo;m not running, Lola.&rdquo;</p>
+<p>&ldquo;But
+Charlie&mdash;&rdquo;</p>
+<p>&ldquo;What we have
+here is a technical problem. And&mdash;&rdquo; There was more to this
+sentence. I had a little speech forming about how we were two
+rational people and there was nothing we couldn&rsquo;t solve. But
+before I could get it out, Lola exploded.</p>
--- page0121.mm
+++ page0121.mm
@@ -1,7 +1,38 @@
-<p>Page 119.</p>
-<h5>CHAPTER 
-TWENTY-FIVE</h5>
+<p>Page 117.</p>
+<p>(THUR)</p>
 <p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; soft
+sciences: Charlie asks Lola about id. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>It seemed unlikely
+that the Contours were conscious. I knew every part of them. I had
+programmed a good chunk of the core software. They possessed no
+capacity for consciousness. They were mindless metal machines. They
+just weren&rsquo;t acting like it.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p><em>The Id comprises the unorganised
+part of the personality structure that contains the basic drives. It
+acts according to the pleasure principle, seeking to avoid pain or
+unpleasure. </em>
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>had anything like a
+consciousness. I 
+</p>
+<p></p>
+<p>mean, I was a good
+designer, but not that good. 
+</p>
 <p></p>
 <p></p>
 <p></p>
