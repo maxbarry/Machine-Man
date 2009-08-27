0b05c22536cfb40f7c965913271cb7bbe0808e93
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-27 05:43:07 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@475 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0117.mm
+++ page0117.mm
@@ -8,9 +8,9 @@ your heart rate,&rdquo; I said. &ldquo;Think about something else.&rdquo;</p>
 they put in there? <em>What is it doing?</em>&rdquo;</p>
 <p>&ldquo;How about
 those dogs of Angelica&rsquo;s? They&rsquo;re so cute and soft.&rdquo;
-At this moment, they began howling. Paws came scampering up the
-hallway, thumped into the door, then fled. Those useless fur sacks.
-They were not helping.</p>
+At this moment, they all began howling. A set of paws came scampering
+up the hallway, bayed, then fled. Those useless fur sacks. They were
+not helping.</p>
 <p>&ldquo;<em>Help me!</em>&rdquo;</p>
 <p>&ldquo;Okay. Okay.
 Let&rsquo;s think about this. It&rsquo;s probably some kind of
@@ -19,23 +19,22 @@ weapon.&rdquo;</p>
 <p>&ldquo;That&rsquo;s
 what they&rsquo;re doing. Better Future. They&rsquo;re trialing parts
 for super-soldiers.&rdquo;</p>
-<p>Her eyes bulged.
-&ldquo;In <em>me</em>? <em>In</em> me?&rdquo;</p>
+<p>Lola&rsquo;s eyes
+bulged. &ldquo;In me? <em>In</em> me?&rdquo;</p>
 <p>&ldquo;I was just
 as pissed, when I found out,&rdquo; I said. &ldquo;That&rsquo;s why I
 kicked the Manager through that window. But the point is, it probably
-won&rsquo;t hurt you. It will hurt other people.&rdquo;</p>
+won&rsquo;t hurt you. It will be designed to hurt other people.&rdquo;</p>
 <p>&ldquo;I&rsquo;m
 not a super-soldier, Charlie!&rdquo;</p>
-<p>&ldquo;I know
-you&rsquo;re not.&rdquo; I became aware of a tone: a whine so
-high-pitched it was only just entering detectable range. That
-explained the dogs.</p>
+<p>I became aware of a
+tone: a whine so high-pitched it was only just entering my detectable
+range. So that explained the dogs.</p>
 <p>&ldquo;It&rsquo;s a
 bomb.&rdquo; Lola&rsquo;s lips were turning blue. &ldquo;Oh God. It&rsquo;s
-a bomb, I can feel it.&rdquo;</p>
-<p>&ldquo;Mmm, I don&rsquo;t
-think so. Not cost-effective.&rdquo;</p>
+a bomb.&rdquo;</p>
+<p>&ldquo;I don&rsquo;t
+know. I don&rsquo;t think that would be cost-effective.&rdquo;</p>
 <p>&ldquo;<em>What?</em>&rdquo;</p>
 <p>&ldquo;Well, you
 want to blow something up, is the best way to do it really to go to
@@ -43,22 +42,22 @@ all the trouble of surgically installing&mdash;&rdquo;</p>
 <p>&ldquo;<em>It&rsquo;s
 shaking!</em>&rdquo; Lola shrieked. Her teeth chattered. &ldquo;Run,
 Charlie!&rdquo;</p>
-<p>&ldquo;We just need
-to lower your heart rate. You can do it, Lola.&rdquo;</p>
-<p>&ldquo;I can&rsquo;t!&rdquo;</p>
+<p>&ldquo;We need to
+lower your heart rate. You can do it, Lola.&rdquo;</p>
+<p>&ldquo;I can&rsquo;t!
+Run!&rdquo;</p>
 <p>&ldquo;I believe in
 you. You can&mdash;&rdquo;</p>
-<p>&ldquo;Please!
-Run!&rdquo;</p>
-<p>Angelica&rsquo;s
-voice came down the hall, over the howling of the dogs. &ldquo;What
-the hell is happening?&rdquo;</p>
-<p>&ldquo;No,&rdquo; I
-said. &ldquo;I&rsquo;m not running, Lola.&rdquo;</p>
+<p>&ldquo;Please!&rdquo;</p>
+<p>The whine had
+become so loud it was difficult to hear anything else. &ldquo;No,&rdquo;
+I said. &ldquo;I&rsquo;m not running, Lola.&rdquo;</p>
 <p>&ldquo;But
 Charlie&mdash;&rdquo;</p>
 <p>&ldquo;What we have
-here is a technical problem. And&mdash;&rdquo; There was more to this
-sentence. I had a little speech forming about how we were two
-rational people and there was nothing we couldn&rsquo;t solve. But
-before I could get it out, Lola exploded.</p>
+here is a technical problem. And I know that if we&mdash;&rdquo;
+There was more to this sentence. I was forming a little speech about
+how we were two rational people, and logic could move mountains. The
+idea was that this would either reassure Lola or bore her, and either
+way we would get that heart rate down. I still think this was a
+pretty good idea. But before I could get it out, Lola exploded.</p>
