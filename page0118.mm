a9c23ed6f3c3f4369c58348338e0a9465a05c218
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-28 07:28:27 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@478 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0118.mm
+++ page0118.mm
@@ -1,10 +1,11 @@
 <p>Page 118.</p>
 <p>Something blew
-through me like a gust of wind. My legs twitched. And that was it.
-The house was silent. I looked at Lola and she looked at me and both
-seemed okay. Lola said, &ldquo;Are you...&rdquo; and so did I at the
-same time. She took a step toward me, and, when nothing bad happened,
-smiled, and took another. I held out my arms and she ran into them.</p>
+through me in a gust, like wind made of needles. My legs twitched. My
+ears rang. And that was it. The house was silent. I looked at Lola
+and she looked at me and we both seemed to be okay. Lola said, &ldquo;Are
+you...&rdquo; and so did I. She took a step toward me, and, when
+nothing bad happened, took another. We smiled. I held out my arms and
+she ran into them.</p>
 <p>&ldquo;Oh, Charlie.
 I was scared.&rdquo;</p>
 <p>&ldquo;It&rsquo;s
@@ -15,8 +16,9 @@ that?&rdquo;</p>
 that didn&rsquo;t work.&rdquo; I kissed the top of her head. &ldquo;I
 guess.&rdquo;</p>
 <p>&ldquo;I thought
-you were going to die. I thought I was going to kill you.&rdquo;</p>
-<p>Moments ticked by.
+you were going to die.&rdquo; She shuddered. &ldquo;I thought I was
+going to kill you.&rdquo;</p>
+<p>A moment passed.
 There was an odd scent in the air: something acrid.</p>
 <p>Lola looked up.
 &ldquo;The dogs are quiet.&rdquo;</p>
@@ -26,20 +28,20 @@ check on Angelica.&rdquo; Lola reached for the door handle. I went to
 step out of the way, but didn&rsquo;t. I tried that again.</p>
 <p>&ldquo;Charlie?&rdquo;</p>
 <p>That scent was
-familiar. It was the smell you got when you plugged a circuit into
-the wrong power supply. The smell of fused transistors.</p>
+familiar. It was the smell you got when you plugged a circuit into a
+power supply it wasn&rsquo;t designed for. The smell of fused
+transistors.</p>
 <p>&ldquo;Charlie?&rdquo;</p>
-<p>The digital clock
-radio on the bedside table: its face was dark. On a shelf, a CD
-player that normally glowed red in standby mode: nothing.</p>
-<p>&ldquo;You guys
+<p>A digital clock
+radio sat on the bedside table: its face had gone dark. On a shelf, a
+small stereo that normally glowed red from its power button: nothing.</p>
+<p>&ldquo;Are you two
 okay?&rdquo; Angelica&rsquo;s voice floated toward us. &ldquo;The
-power&rsquo;s out!&rdquo;</p>
-<p>I began to shake.</p>
-<p> Lola touched my
+power&rsquo;s gone out!&rdquo;</p>
+<p>Lola touched my
 arm. &ldquo;Charlie?&rdquo;</p>
-<p>&ldquo;No.&rdquo; I
-clutched at my thighs. &ldquo;No, no, no.&rdquo;</p>
+<p>&ldquo;No,&rdquo; I
+said. &ldquo;No, no, no.&rdquo;</p>
 <p>&ldquo;What is it?&rdquo;</p>
 <p>I opened my mouth
 and closed it again. I couldn&rsquo;t bring myself to say it: <em>You
