652d74707e5c4b5f3140381945687c09d3fa4e17
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-31 07:09:37 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@481 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0119.mm
+++ page0119.mm
@@ -6,9 +6,10 @@ stages of grief. The first is Denial. For example: <em>My legs can&rsquo;t
 be dead. They can&rsquo;t be</em>. Next is Anger: <em>You killed my
 legs, get out, get your hands off me,</em> etc. There is some
 screaming and raging in this stage. Some unfair accusations. Tears
-and bruising of feelings. Then comes Bargaining. Much quieter. <em>Just
-let the reactor be all right. Please let the reactor be functional.</em>
-Fourth is Depression. <em>They&rsquo;re dead. I&rsquo;m dead.</em> And
-many not quite precise thoughts. This is a kind of wallowing. A
-voluntary shutdown. The final stage is Acceptance. I include no
-example here because I was a long, long way from Acceptance.</p>
+and bruising of feelings.</p>
+<p>Then comes
+Bargaining. Much quieter. <em>Just let the reactor be all right.
+Please let the reactor be functional.</em> Fourth, Depression. <em>They&rsquo;re
+dead. I&rsquo;m dead.</em> This is a kind of wallowing. A shutdown.
+The final stage is Acceptance. I include no example here because I
+was a long, long way from Acceptance.</p>
