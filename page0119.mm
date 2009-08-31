0912ce8fac721af69b36d3305b236f73d45a8d3d
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-08-31 07:08:21 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@480 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0119.mm
+++ page0119.mm
@@ -8,8 +8,7 @@ legs, get out, get your hands off me,</em> etc. There is some
 screaming and raging in this stage. Some unfair accusations. Tears
 and bruising of feelings. Then comes Bargaining. Much quieter. <em>Just
 let the reactor be all right. Please let the reactor be functional.</em>
-Fourth is Depression. <em>I&rsquo;m dead. They&rsquo;re dead.</em>
-Although these are not exactly precise thoughts. This stage is more a
-kind of wallowing. A voluntary shutdown. The final stage is
-Acceptance. I include no example here because I was a long, long way
-from Acceptance.</p>
+Fourth is Depression. <em>They&rsquo;re dead. I&rsquo;m dead.</em> And
+many not quite precise thoughts. This is a kind of wallowing. A
+voluntary shutdown. The final stage is Acceptance. I include no
+example here because I was a long, long way from Acceptance.</p>
