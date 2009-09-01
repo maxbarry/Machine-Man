e9a07b665ea74b68418d6942edf4e7215e8b7517
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-01 05:50:39 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@482 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0120.mm
+++ page0120.mm
@@ -1,192 +1,65 @@
 <p>Page 120.</p>
 <p>On the third day,
-Lola came into my room. Until this, she had been leaving trays of
-food outside. I had learned to wait until her footsteps receded, then
-pull myself to the door and drag in the food before the dogs
-descended. I would shut the door in their little faces and chew,
-alone.</p>
+Lola entered my room. Until this, she had been leaving trays of food
+outside. I had learned to wait until her footsteps receded, then pull
+myself to the door and drag in the food before the dogs descended. I
+would shut the door in their little faces and chew, alone.</p>
 <p>But this day she
-opened the door. It was a small shock, seeing another human. She was
-wearing a green blouse and an air of hopelessness. As she stared at
-me, her eyes glimmered.</p>
-<p>I was sitting in
-the middle of the carpet. Arranged around me in concentric circles
-were pieces of my legs and left hand. It was as if I had suffered the
-world&rsquo;s neatest explosion. Which I had. The electromagnetic
-pulse that had come out of Lola had hurt no-one, disturbed nothing
-that wasn&rsquo;t electronic. It had only fried half the circuits in
-a half-block radius. More specifically, it had fried all the delicate
-ones.</p>
+opened the door. It was a small shock, seeing another human. She wore
+a green blouse and an air of hopelessness. She looked at me and her
+eyes glimmered. I was sitting on the carpet, surrounded by my parts.
+Well, parts of my parts. I had disassembled my legs and hand and
+arranged them in concentric circles, radiating out. It looked as if I
+had suffered the world&rsquo;s neatest explosion. Which I had. The
+electromagnetic pulse that had come out of Lola had hurt no-one but
+me. It had done nothing but fry half the circuits in a hundred-foot
+radius.</p>
 <p>&ldquo;I want...&rdquo;
-said Lola. &ldquo;I think...&rdquo;</p>
-<p>This arrangement,
-it didn&rsquo;t mean I was fixing the Contours. It might have looked
-that way. In truth, I had disassembled them because I couldn&rsquo;t
-do anything else. I was trying to break my problem into pieces until
-it was small enough to handle. I had no real faith that I could reach
-that point. But that was how you solved anything: you divided it. 
-</p>
-<p>&ldquo;I think it
-was really unfair of you to say this is my fault.&rdquo;</p>
+she said. &ldquo;I think...&rdquo;</p>
+<p>These disassembled
+components, that didn&rsquo;t mean I was fixing them. I had taken the
+Contours apart because I couldn&rsquo;t think of anything else. I was
+trying to break my problem into pieces until it was small enough to
+handle. That was how you solved anything: you divided it. I was
+clinging to the hope of eventually reaching something fixable.</p>
+<p>Lola said, &ldquo;I
+think it&rsquo;s really unfair of you to say this is my fault.&rdquo;</p>
 <p>&ldquo;It&rsquo;s
-not your fault.&rdquo; I did not look at Lola as I said this, because
+not your fault.&rdquo; I did not look at her as I said this, because
 I did not really believe it.</p>
 <p>&ldquo;They put it
-in me,&rdquo; she said. &ldquo;Without even <em>telling</em> me.&rdquo;</p>
-<p>&ldquo;You could
-have calmed down.&rdquo;</p>
-<p>Lola inhaled in a
-hiss. 
-</p>
-<p></p>
-<p></p>
-<p><em>You could have calmed down</em>,
-said a part of my brain. This was unfair, of course, which is why I
-didn&rsquo;t say it. But it was a fair point. 
-</p>
-<p></p>
-<p>&lt;&lt; Charlie is
-irritated or something about the body divide. Take some
-responsibility for your biology. Either own it or don&rsquo;t. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+in me.&rdquo; She took one step closer, on the narrow, component-free
+path between the door and me. Her foot came down beside a three-foot
+long section of titanium that had once controlled my plane
+stabilization. This was part of my problem: there were big sections
+that were machine-welded. I couldn&rsquo;t open them up. &ldquo;They
+put that thing in my chest without even <em>telling</em> me.&rdquo;</p>
+<p>I almost didn&rsquo;t
+say it. Then I did. &ldquo;You could have calmed down.&rdquo;</p>
+<p>&ldquo;I could have
+calmed down.&rdquo;</p>
+<p>&ldquo;Yes.&rdquo;</p>
+<p>&ldquo;I tried,
+Charlie.&rdquo;</p>
+<p>I shrugged.</p>
+<p>&ldquo;Charlie. I
+<em>tried</em>.&rdquo;</p>
 <p>I picked up a
-curved piece of titanium. The problem with this, like most of the
-pieces around me, was it was machine-welded. I couldn&rsquo;t get
-inside. There were real technical limits here.</p>
-<p>&ldquo;I just want
-to know what I should do.&rdquo;</p>
-<p>&ldquo;Get me an
-arc welder,&rdquo; I said. &ldquo;A big one.&rdquo;</p>
-<p>Lola was silent. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>She just stood.</p>
-<p></p>
-<p>She looked as if
-she wanted to come in, maybe sit on the bed, but there wasn&rsquo;t
-much carpet that didn&rsquo;t have a component of my legs on it. I
-sat in the middle of the floor, and there was a path to the door, but
-apart from that the pieces were arranged in concentric circles,
-radiating out from me, as if experiencing the world&rsquo;s neatest
-explosion.</p>
-<p>Lola wriggled the
-door handle, back and forth. &ldquo;I just want to know how I can
-help.&rdquo;</p>
-<p>I looked at my
-parts.</p>
-<p>&ldquo;Have a
-bath,&rdquo; Lola said. &ldquo;Please.&rdquo;</p>
-<p>There were big
-sections I couldn&rsquo;t dissassemble. That was the real problem.
-They were machine-welded. But still. Breaking the problem into
-smaller parts made it feel more manageable. 
-</p>
-<p>&ldquo;Please,
-Charlie,&rdquo; said Lola. &ldquo;Tell me what to do.&rdquo;</p>
-<p>&ldquo;Ack.&rdquo;
-I swallowed and tried again. It had been a few days since I&rsquo;d
-used my voice. &ldquo;Bring me a torque wrench. And a soldering iron.
-And transistors. Lots of transistors.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;I need some
-equipment.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; I think
-Charlie doesn&rsquo;t really believe he can fix this. He&rsquo;s
-despondent. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;I just want
-to know how I can help you.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Some time
-passes here, I think. A few weeks, as Charlie rebuilds himself. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Rebuilding
-brings Charlie closer to his legs. He&rsquo;s hands-on. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Lola comes
-in and finds him sitting there with all his parts scattered around.
-Charlie sends her out for stuff. She&rsquo;s worried about him.
-Charlie versus the dogs. Hive mind. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; This is
-nice. Another step in the descent into madness. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; I think as
-he goes crazy, he&rsquo;s aware that he&rsquo;s doing it. He&rsquo;s
-not unaware of his own insanity. He just feels like he has to get
-this done and then he&rsquo;ll fix all the social stuff. In the
-meantime he&rsquo;s really determined. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Hard for
-him to work with only one hand. He should probably fix the hand
-first. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Charlie
-should start running conversations in his head with his hand. He
-knows it&rsquo;s not real, but he finds it helpful. Like that
-cardboard cutout dog. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Charlie
-would want to sedate himself into unconsciousness. &gt;&gt;</p>
-<p>&lt;&lt; Maybe
-Charlie wants to be alone. Maybe he doesn&rsquo;t want them to take
-him out of his legs. Maybe he won&rsquo;t let them come into his
-room. He needs to grieve. What are the stages of grief? 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+radial bolt. I wasn&rsquo;t even sure where this had come from. I had
+made notes, in the beginning. I should have kept that up.</p>
+<p>&ldquo;Charlie&mdash;&rdquo;</p>
+<p>&ldquo;I notice
+people are very selective about their bodies,&rdquo; I told the bolt.
+&ldquo;Any time their body does something good, they claim it. They
+look good, they play football, they run a marathon, it&rsquo;s <em>I</em>
+did this. But something goes wrong, it&rsquo;s not <em>I</em> any more.
+It&rsquo;s a problem with their foot. Their brain needs more sleep.
+Suddenly it&rsquo;s not <em>them</em>. It&rsquo;s just the body they&rsquo;re
+stuck in.&rdquo;</p>
+<p>&ldquo;What are you
+saying?&rdquo;</p>
+<p>&ldquo;Nothing.&rdquo;
+I turned the bolt over in my hands. &ldquo;I&rsquo;m just making an
+observation.&rdquo;</p>
+<p>Silence. The door
+closed with a <em>click</em>.</p>
--- page0121.mm
+++ page0121.mm
@@ -1,4 +1,154 @@
 <p>Page 121.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; depression
+brings benefits: analytical thinking:
+<A CLASS="western" HREF="http://www.scientificamerican.com/article.cfm?id=depressions-evolutionary&amp;page=2">http://www.scientificamerican.com/article.cfm?id=depressions-evolutionary&amp;page=2</A>
+&gt;&gt;</p>
+<p></p>
+<p><em>You could have calmed down</em>,
+said a part of my brain. This was unfair, of course, which is why I
+didn&rsquo;t say it. But it was a fair point. 
+</p>
+<p></p>
+<p>I picked up a
+curved piece of titanium. The problem with this, like most of the
+pieces around me, was it was machine-welded. I couldn&rsquo;t get
+inside. There were real technical limits here.</p>
+<p>&ldquo;I just want
+to know what I should do.&rdquo;</p>
+<p>&ldquo;Get me an
+arc welder,&rdquo; I said. &ldquo;A big one.&rdquo;</p>
+<p>Lola was silent. 
+</p>
+<p></p>
+<p>She just stood.</p>
+<p></p>
+<p>She looked as if
+she wanted to come in, maybe sit on the bed, but there wasn&rsquo;t
+much carpet that didn&rsquo;t have a component of my legs on it. I
+sat in the middle of the floor, and there was a path to the door, but
+apart from that the pieces were arranged in concentric circles,
+radiating out from me, as if experiencing the world&rsquo;s neatest
+explosion.</p>
+<p>Lola wriggled the
+door handle, back and forth. &ldquo;I just want to know how I can
+help.&rdquo;</p>
+<p>I looked at my
+parts.</p>
+<p>&ldquo;Have a
+bath,&rdquo; Lola said. &ldquo;Please.&rdquo;</p>
+<p>There were big
+sections I couldn&rsquo;t dissassemble. That was the real problem.
+They were machine-welded. But still. Breaking the problem into
+smaller parts made it feel more manageable. 
+</p>
+<p>&ldquo;Please,
+Charlie,&rdquo; said Lola. &ldquo;Tell me what to do.&rdquo;</p>
+<p>&ldquo;Ack.&rdquo;
+I swallowed and tried again. It had been a few days since I&rsquo;d
+used my voice. &ldquo;Bring me a torque wrench. And a soldering iron.
+And transistors. Lots of transistors.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;I need some
+equipment.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; I think
+Charlie doesn&rsquo;t really believe he can fix this. He&rsquo;s
+despondent. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;I just want
+to know how I can help you.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Some time
+passes here, I think. A few weeks, as Charlie rebuilds himself. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; Rebuilding
+brings Charlie closer to his legs. He&rsquo;s hands-on. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; Lola comes
+in and finds him sitting there with all his parts scattered around.
+Charlie sends her out for stuff. She&rsquo;s worried about him.
+Charlie versus the dogs. Hive mind. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; This is
+nice. Another step in the descent into madness. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; I think as
+he goes crazy, he&rsquo;s aware that he&rsquo;s doing it. He&rsquo;s
+not unaware of his own insanity. He just feels like he has to get
+this done and then he&rsquo;ll fix all the social stuff. In the
+meantime he&rsquo;s really determined. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; Hard for
+him to work with only one hand. He should probably fix the hand
+first. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; Charlie
+should start running conversations in his head with his hand. He
+knows it&rsquo;s not real, but he finds it helpful. Like that
+cardboard cutout dog. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Charlie
+would want to sedate himself into unconsciousness. &gt;&gt;</p>
+<p>&lt;&lt; Maybe
+Charlie wants to be alone. Maybe he doesn&rsquo;t want them to take
+him out of his legs. Maybe he won&rsquo;t let them come into his
+room. He needs to grieve. What are the stages of grief? 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
 <p>&lt;&lt;RW this for
 Charlie fighting dogs to rebuild legs.&gt;&gt;</p>
 <p></p>
