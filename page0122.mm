b9f90bac25dc7227cf2f2b2c2d3fffd29ecf612a
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-02 07:10:14 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@487 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0122.mm
+++ page0122.mm
@@ -1,21 +1,32 @@
 <p>Page 122.</p>
+<p>(THUR)</p>
 <p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-</BODY>
-</HTML>
+<p>It was useful to
+talk to my parts as I worked on them. For example, I would pick up a
+mirror plate and say, &ldquo;And what&rsquo;s your problem?&rdquo; Or
+when contemplating the reactor sheild: &ldquo;You need an arc welder.
+That&rsquo;s what you need.&rdquo; They didn&rsquo;t answer back. I
+wasn&rsquo;t crazy. It was just a way to focus. But sometimes I heard
+footsteps outside the door, and this probably wasn&rsquo;t obvious to
+anyone else.</p>
+<p>I regretted what I
+had said to Lola. How I was shutting myself away. But I couldn&rsquo;t
+bring myself to confront her. Surrounded by the Contours&rsquo;
+dismembered corpse, I couldn&rsquo;t bear to look at the person who
+had killed them. It wasn&rsquo;t Lola&rsquo;s fault. I told the
+Contours that. &ldquo;She tried to calm down,&rdquo; I said. This was
+late one night, after a particularly frustrating day prying apart
+transistors. &ldquo;She did everything she could. She didn&rsquo;t
+want you to die.&rdquo; Then I wrapped my arms around myself and
+cried a little, because I was really tired.</p>
+<p>In the morning, I
+decided I would say sorry. I would stop wallowing. I would just fix
+one thing first. Because if I could do that, take one step toward
+restoring the Contours to function, that would mean they weren&rsquo;t
+dead. They were just temporarily knocked out. This idea filled me
+with joy. I didn&rsquo;t want to be here, filthy, stinking, grieving,
+dragging myself around like some deformed animal. I didn&rsquo;t want
+to be alone. I wanted to get past this. So I just had to find one
+thing that was fixable.</p>
+<p>But that day never
+came.</p>
