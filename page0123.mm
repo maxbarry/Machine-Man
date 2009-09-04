aedcaa40630c2384a9a3b0079225b484f70a2a2d
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-04 06:40:52 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@491 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0123.mm
+++ page0123.mm
@@ -1,33 +1,35 @@
 <p>Page 123.</p>
-<p>Finally I had an
+<p>Then I had an
 epiphany. I was on my stomach, straining to reach some titanium
 pieces that had somehow wound up under the bed, and I thought: <em>These
 are just metal</em>. I guess that doesn&rsquo;t sound very revelatory.
-But it was. I stared at these pieces, which had once comprised the
-core of my fingers, and they did not look like part of me.</p>
+But it was. I stared at these pieces, which had once been the core of
+my fingers, and they did not look like part of me.</p>
 <p>I sat up. I would
 never put the Contours back together: I knew that now. Previously,
-this idea had paralyzed me with fear, but now I just felt relieved.
-Not completely. A part of me wailed, wanted me to try to fix them
-just one more time, but it was a small part, and I ignored it. I
-looked down and thought: <em>Man, I&rsquo;m a mess</em>.</p>
-<p>I pushed my parts
-into a heap and thought about Lola. I had done some damage there. I
-wasn&rsquo;t sure it was repairable. In fact, I wasn&rsquo;t even
-sure Lola was still around. Someone was bringing me food, and
-sometimes listening outside the door as I talked to my parts, but
-that could have been Angelica. I wouldn&rsquo;t blame Lola if she had
-left. It was the rational thing to do.</p>
+this fear had paralyzed me, but now I mostly just felt relief. A part
+of me still wanted to fix them, to try just one more time, but it was
+a small part, receding, and I could ignore it. I looked down and
+thought: <em>Man, I&rsquo;m a mess</em>.</p>
+<p>I thought about
+Lola. I had done so much damage there. I wasn&rsquo;t sure it was
+repairable. A sickening thought struck me: maybe Lola was not even
+around any more. Someone had been bringing me food, and listening
+outside the door as I talked to my parts, but that could have been
+Angelica. I wouldn&rsquo;t blame Lola for leaving me. It was the
+rational thing to do.</p>
 <p>Eventually, I
-opened the door. Fresh air swept into the room. I saw a piece of
+opened the door. Fresh air swept hit me like a slap. I saw a piece of
 paper on the corridor floor: no, two pieces. One was a clipping from
-a trade supplies catalogue. It featured an arc welder. The other page
-was a note, in Lola&rsquo;s handwriting. It said: <em>Garage</em>.</p>
-<p>I didn&rsquo;t hear
-Lola&rsquo;s footsteps, because I was crying. She said: &ldquo;I
-heard you say you needed one.&rdquo;</p>
+a trade supplies catalogue. It featured an arc welder. The other was
+a note, in Lola&rsquo;s handwriting. It said: <em>It&rsquo;s in the
+garage.</em></p>
+<p>I didn&rsquo;t
+realize she was there until she spoke. &ldquo;You said...&rdquo; She
+shifted from one foot to the other. &ldquo;I heard you needed one.&rdquo;</p>
 <p>I opened my mouth.
-My jaw worked. But the things I wanted to say couldn&rsquo;t fit in
-my brain. All I could do was shake.</p>
+My jaw worked. The things I wanted to say couldn&rsquo;t fit through
+my throat. I tried to say, <em>I&rsquo;m sorry</em>, but all I did was
+shake.</p>
 <p>&ldquo;It&rsquo;s
 all right,&rdquo; Lola said. &ldquo;It&rsquo;s okay, Charlie.&rdquo;</p>
--- page0124.mm
+++ page0124.mm
@@ -1,118 +1,55 @@
 <p>Page 124.</p>
 <h5>CHAPTER 
 TWENTY-SIX</h5>
-<p>Dude.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; OK! What
-next? He&rsquo;s clearly not going to fix his legs. He&rsquo;s going
-to start thinking about Better Future. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Phantom
-pain is bad. Really bad. Angelica maybe drugs him up. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; So I think
-we hit an upswing here. The only problem is Dog. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Aw, the
-thing is, Charlie can&rsquo;t step on the dogs any more. I guess he
-can squash one with his skateboard though. Yeah, that&rsquo;ll do. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Maybe Lola
-wants Charlie to rebuild himself. I think she does. She doesn&rsquo;t
-realize that it&rsquo;s kind of her vs the legs. She wants Charlie to
-be the best guy he can be. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Actually
-lots of potential for plots here. Lola gets in touch with Carl or BF,
-doesn&rsquo;t tell Charlie. Maybe she has been talking with them
-while Charlie&rsquo;s been insane. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Lola has
-some prosthetics for Charlie&mdash;some really basic ones. So he can
-at least walk around. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Major
-drivers: Angry, betrayed Carl. Sentient legs. Charlie&rsquo;s drive
-to improve himself. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p>Lola came toward
-me, bent down, hugged me cautiously. I must have smelt abominable.
-&ldquo;It&rsquo;s all right,&rdquo; she said. &ldquo;It&rsquo;s okay,
-Charlie.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p> 
+<p>I had to bathe
+three times, one after the other. At first, the water turned black as
+soon I entered it. &ldquo;Sorry,&rdquo; I told Lola. &ldquo;I guess I
+really stink.&rdquo;</p>
+<p>&ldquo;I&rsquo;m
+more impressed by the layer of grease.&rdquo; She pointed at the
+water. &ldquo;Look at that. It&rsquo;s like an oil spill.&rdquo;</p>
+<p>She held my hand to
+help me out of the bath. She refilled the tub and scrubbed my back
+with a cloth. When I was finally clean, she dried me with a fluffy
+towel. &ldquo;Wait here,&rdquo; she said. When she came back, she was
+carrying two prosthetic legs. &ldquo;These are nothing special...&rdquo;
+She set them down, watching my face.</p>
+<p>She was right. They
+were the entry-level models she had shown me the first day we met:
+two thin buckets on crutch-like poles. They were the kind of
+prosthetics you saw on Vietnam veterans who had been abandoned by
+their government.</p>
+<p>&ldquo;They&rsquo;re
+just temporary,&rdquo; said Lola. &ldquo;We can&mdash;&rdquo;</p>
+<p>&ldquo;They&rsquo;re
+fine,&rdquo; I said. &ldquo;They&rsquo;re perfect.&rdquo;</p>
+<p>She smiled. 
 </p>
 <p></p>
+<p>She held my hand
+and helped me strap on the legs. &lt;&lt; Worn around the buckle.
+Chafing. I wondered how many people had tried these on before me.</p>
+<p></p>
+<p>I couldn&rsquo;t
+walk in them, had no balance, but Lola assured me I would learn, and
+I believed her.</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Finish on
+an upswing. Something happy/funny. &gt;&gt;</p>
 <p></p>
 <p></p>
 <p></p>
+<p>&lt;&lt; Would be
+better as the next page. &gt;&gt;</p>
+<p>Looking back, I
+wonder if this could have gone in another direction. It seems
+possible. At this point, Lola and I could have run off somewhere, to
+some part of the world Better Future didn&rsquo;t care enough to
+track. We could have lived out our lives in a tiny Canadian snow
+town, this quirky couple, the woman and the man with a prosthetic
+hand and prosthetic legs, and he used to be a scientist once, did you
+know? Lola could have baked pies. I could have repaired things. The
+townspeople could have called me <em>Doc</em>.</p>
+<p>Yeah. Or maybe not.</p>
 <p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt;RW this for
-Charlie fighting dogs to rebuild legs.&gt;&gt;</p>
-<p></p>
-<p>I came to see
-Angelica&rsquo;s dogs as a hive mind. They were many, but one. They
-communicated via telepathy. If I yelled at one in the bathroom, I
-would emerge to find three more, trembling on the carpet with shared
-outrage. Taken as individuals, these were some of the stupidest
-mammals I had ever encountered. When I walked toward one, more often
-than not it would freeze, wanting to flee but unable to figure out if
-I would maintain my current trajectory or maybe start teleporting
-around the room. As a collective, though, the dogs were plotting
-something. I was sure of it. They wanted me out of the house and were
-beginning to figure out methods. Any time I entered a room to
-multiple sets of glittering eyes, I got nervous.</p>
-<p>&ldquo;You&rsquo;re
-so silly,&rdquo; said Lola, scratching a tiny brown head. &ldquo;They&rsquo;re
-gorgeous. Aren&rsquo;t you, baby? Yes, you are.&rdquo;</p>
-<p>&ldquo;They hate
-me.&rdquo;</p>
-<p>&ldquo;Try being
-nicer to them.&rdquo; The dog began licking Lola&rsquo;s neck. She
-shrieked and tried to push it away, but not very effectively. &ldquo;Stop
-that! Bad puppy!&rdquo;</p>
-<p>I noticed another
-dog sitting on the arm of the sofa. It was staring at me, despite the
-play-fight going on right in front of it. I got the feeling this dog
-was sending me a message. Something like: <em>We have your girlfriend</em>.</p>
-<p>My legs stepped
-forward. The dog leaped off the sofa and scampered away. I smiled
-despite myself. <em>Stop that</em>, I said. <em>Bad legs</em>.</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
