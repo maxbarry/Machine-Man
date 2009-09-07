9bbdc24d963922f963ff7aa497e2df8376cac47f
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-07 04:48:09 +0000

    Page update: 2 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@493 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0124.mm
+++ page0124.mm
@@ -2,63 +2,30 @@
 <h5>CHAPTER 
 TWENTY-SIX</h5>
 <p>I had to bathe
-three times, one after the other. At first, the water turned black as
-soon I entered it. &ldquo;Sorry,&rdquo; I told Lola. &ldquo;I guess I
-really stink.&rdquo;</p>
+three times, one after the other. The first time, the water turned
+black as soon I entered it. &ldquo;Sorry,&rdquo; I told Lola. &ldquo;I
+guess I stink.&rdquo;</p>
 <p>&ldquo;I&rsquo;m
 more impressed by the layer of grease.&rdquo; She pointed at the
 water. &ldquo;Look at that. It&rsquo;s like an oil spill.&rdquo;</p>
-<p>She held my hand to
-help me out of the bath. She refilled the tub and scrubbed my back
-with a cloth. When I was finally clean, she dried me with a fluffy
-towel. &ldquo;Wait here,&rdquo; she said. When she came back, she was
-carrying two prosthetic legs. &ldquo;These are nothing special...&rdquo;
-She set them down, watching my face.</p>
-<p>She was right. They
-were the entry-level models she had shown me the first day we met:
-two thin buckets on crutch-like poles. They were the kind of
-prosthetics you saw on Vietnam veterans who had been abandoned by
-their government.</p>
-<p>&ldquo;They&rsquo;re
-just temporary,&rdquo; said Lola. &ldquo;We can&mdash;&rdquo;</p>
-<p>&ldquo;They&rsquo;re
-fine,&rdquo; I said. &ldquo;They&rsquo;re perfect.&rdquo;</p>
-<p>She smiled. 
-</p>
-<p></p>
-<p>She held my hand
-and helped me strap on the legs. &lt;&lt; Worn around the buckle.
-Chafing. I wondered how many people had tried these on before me.</p>
-<p></p>
-<p>I couldn&rsquo;t
-walk in them, had no balance, but Lola assured me I would learn, and
-I believed her.</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Finish on
-an upswing. Something happy/funny. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Would be
-better as the next page. &gt;&gt;</p>
+<p>I laughed, and we
+looked at each other, and what I still could not really believe was
+that her eyes held no vindictiveness. I had not known this about
+love: that you did not have to deserve it. I&rsquo;d always thought I
+was missing something, something essential, that left me unqualified
+for love. Confidence, maybe, or a tan. And I was okay with that, if
+not quite happy, because at least I was being me. I wasn&rsquo;t
+pretending to be someone else just to be loved. I was choosing
+integrity. But here I was, the same pasty mess of social incompetence
+as always, and here was Lola.</p>
 <p>Looking back, I
-wonder if this could have gone in another direction. It seems
-possible. At this point, Lola and I could have run off somewhere, to
-some part of the world Better Future didn&rsquo;t care enough to
-track. We could have lived out our lives in a tiny Canadian snow
-town, this quirky couple, the woman and the man with a prosthetic
-hand and prosthetic legs, and he used to be a scientist once, did you
-know? Lola could have baked pies. I could have repaired things. The
-townspeople could have called me <em>Doc</em>.</p>
-<p>Yeah. Or maybe not.</p>
-<p></p>
-<p>Mon: This but no
-prosthetics.</p>
-<p>Tue: Prosthetics.
-Charlie grinning and bearing it. Kinda ominous because you know it
-can&rsquo;t well, even though Charlie&rsquo;s trying.</p>
-<p>Wed: Sleeping. But
-I had forgotten Dog.</p>
-<p>Thu: Steps on Dog</p>
-<p>Fri: Ang calls CC.</p>
+wonder if this could have been a turning point. It seems possible. I
+could have emerged from that bath, scrubbed clean to my core, and
+everything might have turned out differently. Lola and I could have
+run away, left behind Better Future and artificial augmentation and
+built new lives in a tiny Canadian snow town, become a quirky couple,
+the woman and the man with a prosthetic hand and prosthetic legs, and
+he used to be a scientist once, did you know? Lola could have baked
+pies. I could have repaired things. The townspeople could have called
+me <em>Doc</em>.</p>
+<p>Or maybe not.</p>
--- page0125.mm
+++ page0125.mm
@@ -1,139 +1,42 @@
 <p>Page 125.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>and Lola was
-looking at me. &ldquo;Do you miss your legs?&rdquo;</p>
-<p>I wasn&rsquo;t sure
-how to answer this.</p>
-<p>&ldquo;Do you feel
-like you want them back?&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; So I think
-we hit an upswing here. The only problem is Dog. But maybe Charlie&rsquo;s
-even optimistic about Dog. He tackles the crap prosthetics with good
-humor. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; OK! What
-next? He&rsquo;s clearly not going to fix his legs. He&rsquo;s going
-to start thinking about Better Future. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Phantom
-pain is bad. Really bad. Angelica maybe drugs him up. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Aw, the
-thing is, Charlie can&rsquo;t step on the dogs any more. I guess he
-can squash one with his skateboard though. Yeah, that&rsquo;ll do. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Maybe Lola
-wants Charlie to rebuild himself. I think she does. She doesn&rsquo;t
-realize that it&rsquo;s kind of her vs the legs. She wants Charlie to
-be the best guy he can be. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Actually
-lots of potential for plots here. Lola gets in touch with Carl or BF,
-doesn&rsquo;t tell Charlie. Maybe she has been talking with them
-while Charlie&rsquo;s been insane. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Lola has
-some prosthetics for Charlie&mdash;some really basic ones. So he can
-at least walk around. &gt;&gt;</p>
-<p></p>
-<p>&lt;&lt; Major
-drivers: Angry, betrayed Carl. Sentient legs. Charlie&rsquo;s drive
-to improve himself. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p>Lola came toward
-me, bent down, hugged me cautiously. I must have smelt abominable.
-&ldquo;It&rsquo;s all right,&rdquo; she said. &ldquo;It&rsquo;s okay,
-Charlie.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p> 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt;RW this for
-Charlie fighting dogs to rebuild legs.&gt;&gt;</p>
-<p></p>
-<p>I came to see
-Angelica&rsquo;s dogs as a hive mind. They were many, but one. They
-communicated via telepathy. If I yelled at one in the bathroom, I
-would emerge to find three more, trembling on the carpet with shared
-outrage. Taken as individuals, these were some of the stupidest
-mammals I had ever encountered. When I walked toward one, more often
-than not it would freeze, wanting to flee but unable to figure out if
-I would maintain my current trajectory or maybe start teleporting
-around the room. As a collective, though, the dogs were plotting
-something. I was sure of it. They wanted me out of the house and were
-beginning to figure out methods. Any time I entered a room to
-multiple sets of glittering eyes, I got nervous.</p>
-<p>&ldquo;You&rsquo;re
-so silly,&rdquo; said Lola, scratching a tiny brown head. &ldquo;They&rsquo;re
-gorgeous. Aren&rsquo;t you, baby? Yes, you are.&rdquo;</p>
-<p>&ldquo;They hate
-me.&rdquo;</p>
-<p>&ldquo;Try being
-nicer to them.&rdquo; The dog began licking Lola&rsquo;s neck. She
-shrieked and tried to push it away, but not very effectively. &ldquo;Stop
-that! Bad puppy!&rdquo;</p>
-<p>I noticed another
-dog sitting on the arm of the sofa. It was staring at me, despite the
-play-fight going on right in front of it. I got the feeling this dog
-was sending me a message. Something like: <em>We have your girlfriend</em>.</p>
-<p>My legs stepped
-forward. The dog leaped off the sofa and scampered away. I smiled
-despite myself. <em>Stop that</em>, I said. <em>Bad legs</em>.</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+<p>I sat on the edge
+of the bath, wrapped in a towel. When Lola came back, it was like the
+day we&rsquo;d met, because she was carrying prosthetic legs. She set
+them down, watching my face. &ldquo;Now, these are nothing
+special...&rdquo;</p>
+<p>They were
+entry-level models: crutch-like poles, with plastic buckets for my
+thighs. They were the kind of prosthetics you saw on Vietnam veterans
+who had been abandoned by their government.</p>
+<p>&ldquo;They were
+all Angelica could swipe,&rdquo; said Lola. &ldquo;I know they&rsquo;re
+basic, but&mdash;&rdquo;</p>
+<p>&ldquo;They&rsquo;re
+perfect,&rdquo; I said. &ldquo;Thanks.&rdquo;</p>
+<p>Lola wrapped her
+arms around me and squeezed. &ldquo;I&rsquo;m proud of you, Charlie.&rdquo;
+She straightened, and looked very happy. &ldquo;Want to try them on?&rdquo;</p>
+<p>The straps were
+ragged, dark in patches. A lot of amputated thighs must have sweated
+into these prosthetics before mine. The sockets were loose in some
+places and pinched tight in others. I couldn&rsquo;t just sit in
+them, like the Contours: I had to walk, as if they were stilts. They
+<em>were</em> stilts. My first step, with one arm slung over Lola&rsquo;s
+shoulder, I accidentally kicked the wall and left a mark. &ldquo;That&rsquo;s
+okay,&rdquo; said Lola. &ldquo;Keep trying.&rdquo; The scar tissue of
+my thighs screamed in outrage: it had never had to bear pressure. It
+was tender from all those jabs from the Contours&rsquo; nerve
+interface needles. Within four steps, I felt like they it was
+splitting open. I looked down, to check whether the sockets were
+filling with blood. Sweat popped out around my hairline and ran down
+my nose.</p>
+<p>Four steps later,
+Lola helped lower me to the toilet seat. &ldquo;That was great,
+Charlie. That was a really good first effort.&rdquo; She reached for
+a buckle. &ldquo;I&rsquo;ll take them off. Early days, you take it
+slow.&rdquo;</p>
+<p>&ldquo;No.&rdquo; I
+panted, trying to catch my breath. &ldquo;Let&rsquo;s go again.&rdquo;</p>
+<p>Her face lit up.
+&ldquo;That&rsquo;s the spirit, Charlie!&rdquo; She grinned, kissed
+my cheek. &ldquo;That&rsquo;s the spirit!&rdquo;</p>
