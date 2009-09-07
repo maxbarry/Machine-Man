d45ed39da385573be29070392890dc2142a277c3
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-07 05:00:34 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@494 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0124.mm
+++ page0124.mm
@@ -5,27 +5,26 @@ TWENTY-SIX</h5>
 three times, one after the other. The first time, the water turned
 black as soon I entered it. &ldquo;Sorry,&rdquo; I told Lola. &ldquo;I
 guess I stink.&rdquo;</p>
-<p>&ldquo;I&rsquo;m
-more impressed by the layer of grease.&rdquo; She pointed at the
-water. &ldquo;Look at that. It&rsquo;s like an oil spill.&rdquo;</p>
+<p>&ldquo;Actually,
+I&rsquo;m more impressed by the layer of grease.&rdquo; She pointed
+at the water. &ldquo;Look at that. It&rsquo;s like an oil spill.&rdquo;</p>
 <p>I laughed, and we
 looked at each other, and what I still could not really believe was
 that her eyes held no vindictiveness. I had not known this about
 love: that you did not have to deserve it. I&rsquo;d always thought I
-was missing something, something essential, that left me unqualified
-for love. Confidence, maybe, or a tan. And I was okay with that, if
-not quite happy, because at least I was being me. I wasn&rsquo;t
-pretending to be someone else just to be loved. I was choosing
-integrity. But here I was, the same pasty mess of social incompetence
-as always, and here was Lola.</p>
-<p>Looking back, I
+was missing something, something essential, that left me unqualified.
+Confidence, or a tan; something like that. And I was okay with that,
+because at least I was being me. I wasn&rsquo;t pretending to be
+someone else just to be loved. But here I was, the same pasty mess of
+social incompetence as always, and here was Lola.</p>
+<p>In retrospect, I
 wonder if this could have been a turning point. It seems possible. I
 could have emerged from that bath, scrubbed clean to my core, and
 everything might have turned out differently. Lola and I could have
 run away, left behind Better Future and artificial augmentation and
-built new lives in a tiny Canadian snow town, become a quirky couple,
-the woman and the man with a prosthetic hand and prosthetic legs, and
-he used to be a scientist once, did you know? Lola could have baked
-pies. I could have repaired things. The townspeople could have called
-me <em>Doc</em>.</p>
-<p>Or maybe not.</p>
+the arc welder in the garage, built new lives in a tiny Canadian snow
+town, become a quirky couple, the woman and the man with a prosthetic
+hand and prosthetic legs and he used to be a scientist once, did you
+know? Lola could have baked pies. I could have repaired things. The
+townspeople could have called me <em>Doc</em>.</p>
+<p>No. I guess not.</p>
