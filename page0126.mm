8f7d1a1bae9f8d9dc6977c29ae9ddf275ccaae66
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-09 05:51:09 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@498 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0126.mm
+++ page0126.mm
@@ -3,7 +3,7 @@
 tough. I won&rsquo;t recount the whole sweating, groaning ordeal.
 I&rsquo;ll just say it was one of the most horrible experiences of my
 life. I speak as someone who has had both legs crushed in an
-industrial clamp. These new prosthetics, the war veteran legs,
+industrial die stamp. These new prosthetics, the war veteran legs,
 weren&rsquo;t realistic enough to fool my brain. They were poles.
 When I waved them around, when I massaged them with shaking fingers,
 my brain did not buy it. It had bought the Contours. It had bought
@@ -12,8 +12,8 @@ the Contours even when they were in pieces.</p>
 Lola was rubbing my back and telling me I could get through this, I
 realized the pain was different. At Better Future, before I&rsquo;d
 figured out to wear the Contours at night, the phantom pain had felt
-distinctly like muscle cramp. Like toes I didn&rsquo;t have were
-curling into shrieking clumps. This was a kind of pressure ache&mdash;a
-lack of pressure ache, as if they were stretching out, inflating like
-balloons. The problem here wasn&rsquo;t that I was missing my
-biological legs. I had phantom Contours.</p>
+distinctly like muscle cramp. Like toes I didn&rsquo;t have, curling
+until they screamed. This was a kind of pressure ache&mdash;a <em>lack</em>
+of pressure ache, as if my legs were stretching, inflating like
+balloons. I didn&rsquo;t think I was missing my biological legs any
+more. I had phantom Contours.</p>
