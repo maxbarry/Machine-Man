ac83d8e1f3f96e70b27cd0c432694a551a659d94
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-17 07:25:18 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@507 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0132.mm
+++ page0132.mm
@@ -16,20 +16,20 @@ sell that to Lola. All she knew was there were gunshots. But I was
 not really thinking about Lola: I was concentrating on crushing rose
 bushes while not getting killed.</p>
 <p>We bumped onto the
-road, and I hauled on the wheel, which was not easy to do with one
-hand from the passenger seat. &ldquo;Less acceleration!&rdquo; I
-said, but not quickly enough, and we thumped into a parked wagon. I
-was thrown against Lola. Her head rebounded from the side window. She
-said something that sounded like <em>Guk</em> but probably wasn&rsquo;t.
-I got my hand back on the wheel. &ldquo;More acceleration!&rdquo;</p>
+road, and I hauled on the wheel: not easy to do with one hand from
+the passenger seat. &ldquo;Less acceleration!&rdquo; I said, but not
+quickly enough, and we thumped into a parked wagon. I was thrown
+against Lola. Her head rebounded from the side window. She said
+something that sounded like <em>Guk</em> but probably wasn&rsquo;t. I
+got my hand back on the wheel. &ldquo;More acceleration!&rdquo;</p>
 <p>Lola&rsquo;s eyes
 fluttered open. She looked short of breath for some reason, her skin
 waxy. &ldquo;Oh. We hit... we hit a car.&rdquo;</p>
 <p>&ldquo;My fault.&rdquo;
 I threw a glance out the rear window. Better Future guards were
 jogging after us. Still no Carl! I couldn&rsquo;t think where he
-could be. But we weren&rsquo;t moving: that was more urgent. &ldquo;Let&rsquo;s
-go!&rdquo;</p>
+could be. But we weren&rsquo;t moving, and that was more urgent.
+&ldquo;Let&rsquo;s go!&rdquo;</p>
 <p>&ldquo;Maybe...
 that was a mistake. Closing my eyes.&rdquo;</p>
 <p>The car dash was
@@ -43,7 +43,7 @@ I couldn&rsquo;t think of a word for it. &ldquo;Discharge?&rdquo;</p>
 thought...&rdquo;</p>
 <p>&ldquo;<em>Put your
 hands on the dash! Do it now!</em>&rdquo;</p>
-<p>&ldquo;... we
+<p>&ldquo;I thought we
 hit...&rdquo; Then she exhaled, like: <em>Cah</em>. At least, that&rsquo;s
-what I thought. It wasn&rsquo;t until much later that I realized she
-had said: <em>Carl</em>.</p>
+what I thought. It wasn&rsquo;t until much later I realized she&rsquo;d
+said: <em>Carl</em>.</p>
--- page0133.mm
+++ page0133.mm
@@ -1,264 +1,64 @@
 <p>Page 133.</p>
-<p>(FRI)</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;<em>Let me
-see your hands!</em>&rdquo;</p>
-<p></p>
-<p></p>
-<p>Except I don&rsquo;t
-think that was it. I think it was Carl.</p>
-<p>one yelled, and
-another said, &ldquo;<em>Now! </em>
-</p>
-<p></p>
-<p></p>
-<p>I hadn&rsquo;t even
-felt it. It occurred to me that last time, maybe what I had felt was
-not the electromagnetic pulse. Maybe it was my body&rsquo;s reaction
-to it. Maybe I had felt the Contours dying.</p>
-<p>&ldquo;Oh,&rdquo; I
-said.</p>
-<p></p>
 <p>Better Future
-guards encircled the car, pointing weapons at us. &ldquo;<em>Put your
-hands on the dash!</em>&rdquo; one yelled, and another said, &ldquo;<em>Now!
+guards encircled the car, pointing weapons at us. &ldquo;<em>Let me
+see your hands!</em>&rdquo; one yelled, and another said, &ldquo;<em>Now!
 Do it!</em>&rdquo; They seemed more nervous now than when Lola and I
-had been trying to drive a car through them.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I... ow...
-think...&rdquo;</p>
-<p>Her foot was
-pressed on the accelerator. But we weren&rsquo;t going anywhere. The
-dash was dark. The car coasted another twenty feet down the road,
-then ran out of momentum. 
-</p>
-<p>&ldquo;Sorry.&rdquo;</p>
-<p></p>
-<p></p>
-<p>&ldquo;<em>Put your
-hands on the dash!</em>&rdquo;</p>
-<p>I glanced out the
-window to see the mustache guard. 
-</p>
-<p></p>
-<p></p>
-<p> 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Car sails
-to a halt. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p> and we sideswiped
-a parked sedan. &ldquo;More!&rdquo;</p>
-<p>&ldquo;Eeee,&rdquo;
-said Lola. Her face was pale and sweaty. She had looked a lot like
-this right before murdering my legs.</p>
-<p>&ldquo;Open your
-eyes. We&rsquo;re okay.&rdquo;</p>
-<p>Her eyes popped
-open. &ldquo;Oh,&rdquo; she said. &ldquo;Oh, thank God.&rdquo; Her
-eyes flicked to the rear view mirror. 
-</p>
-<p></p>
-<p></p>
-<p>I was busy crushing
-a series of rose bushes while not getting killed.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>The guard shot at
-us. Multiple guards shot at us, actually. I believe they were aiming
-for the tires, not our bodies, but this wasn&rsquo;t clear at the
-time and it was absolutely terrifying. It sounded like we were being
-attacked by a gang wielding baseball bats. Which would have been an
-improvement on reality, now I think about it, and maybe I should have
-tried to pretend that was happening to calm Lola down. But I was busy
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Now we&rsquo;ve
-got Lola fearing for <em>Carl</em> when she goes off. That&rsquo;s
-good. Keep it romantic. Later, we can make the observation that she&rsquo;s
-a healer/lover. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>A low, keening
-noise came out of Lola. 
-</p>
-<p></p>
-<p>A four-way
-intersection loomed ahead. &ldquo;I think you should open your eyes
-now.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Trees flashed by.
-Ahead was an intersection, and I wasn&rsquo;t sure 
-</p>
-<p></p>
-<p> four-way
-intersection, followed by another, and realized that job-sharing was
-about to become difficult.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I threw a glance
-out of the rear window in time to see the Better Future van peel onto
-the road behind us. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Guards
-haul him out of the car. Laughable, humiliating, nervous. We realize
-they&rsquo;re nervous about Carl. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; I&rsquo;d
-like a conversation between Charlie and CC. She&rsquo;s saying it&rsquo;s
-okay, Charlie, we just want what&rsquo;s best for you. &gt;&gt;</p>
-<p>&lt;&lt; Maybe CC
-says the problem is Carl, Carl&rsquo;s gone nuts. You don&rsquo;t
-want to run into Carl. It&rsquo;s really frustrating, she says,
-because you people keep going off the deep end. &gt;&gt;</p>
-<p>&lt;&lt; Actually,
-that&rsquo;s quite an interesting idea for an entire plot. CC says
-Carl is the bad guy, she wants to arm up Charlie to fight him.
-&ldquo;You&rsquo;re our only hope.&rdquo; &gt;&gt;</p>
-<p>&lt;&lt; The appeal
-for Charlie is to get new parts. &gt;&gt;</p>
-<p>&lt;&lt; Carl is
-out for revenge. It&rsquo;s those psychological problems the Manager
-referred to. He thinks Lola loves him. &gt;&gt;</p>
-<p>&lt;&lt; &ldquo;Oh,
-shit,&rdquo; she said. &ldquo;He&rsquo;s found us.&rdquo; &gt;&gt;</p>
-<p>&lt;&lt; I think
-that&rsquo;s really good: having a terminator-like guy after you
-who&rsquo;s genuinely intending you harm. Because I like Carl but I
-also like Carl hard. &gt;&gt;</p>
-<p>&lt;&lt; So maybe
-the BF guards cut off their escape, and they&rsquo;re all, &ldquo;Watch
-out! Don&rsquo;t let him kick over the car! He can jump!&rdquo; And
-it&rsquo;s kind of laughable because they don&rsquo;t realize Charlie
-is impaired. Actually, could have a nice moment where they realize
-he&rsquo;s crippled and they relax, laugh; it&rsquo;s humiliating for
-Charlie. &gt;&gt;</p>
-<p>&lt;&lt; Moment:
-everyone seems nervous, even though they&rsquo;ve got Charlie; he
-realizes they&rsquo;re not nervous <em>about</em> him. &gt;&gt;</p>
-<p>&lt;&lt; Car chase?
-I think Lola busts out of the garage, Charlie in the passenger seat,
-cuts off the van, away they go. Trying to keep heart rate under
-control because that will kill the car&rsquo;s electronics. (Charlie
-should explain this: at first Lola thinks why not let &lsquo;er rip.)
-&gt;&gt;</p>
-<p>&lt;&lt; The other
-possibility with Lola&rsquo;s heart is that it will run out of
-charge. I.e. If she goes off one more time, it will stop working. An
-alternative to a bad guy shutting her off. Lola could get really sick
-at the end, weak, Charlie has to save her in a race against time.
-That&rsquo;s pretty neat. &gt;&gt;</p>
-<p>&lt;&lt; OK, to set
-that off, she should explode at some point. Maybe when the van pulls
-alongside. Lola blacks out for a minute, airbags. &gt;&gt;</p>
-<p>&lt;&lt; So: (1)
-They go to the garage. (2) They get in the car and zoom off. (3) They
-get chased. (4) Lola goes off. (5) BF catches them. (6) Cassandra
-Cautery speaks to Charlie. (7) Tells him about Carl. (8) Carl finds
-them. (9) Carl overturns a van or two as Charlie and Lola are whisked
-away. That&rsquo;s all pretty cool. Good action, just make sure I
-interlace it with humor and human moments, don&rsquo;t make it too
-grim. &gt;&gt;</p>
-<p>&lt;&lt; And maybe
-Carl isn&rsquo;t the bad guy so much as Carl&rsquo;s <em>parts</em> are
-the bad guy. That&rsquo;s a nice way to go from Carl vs Charlie to
-Charlie vs Charlie. &gt;&gt;</p>
-<p>&lt;&lt; I think
-it&rsquo;s definitely more mindblowing if Charlie gets into a fight
-with his parts. A battle with his id. I&rsquo;m not sure what that
-actually means. But I want to think about it. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>Lola was looking at
-me. &ldquo;Do you miss your legs?&rdquo;</p>
-<p>I wasn&rsquo;t sure
-how to answer this.</p>
-<p>&ldquo;Do you feel
-like you want them back?&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-</BODY>
-</HTML>
+had been trying to drive a car through them. A young guard pulled
+open my door and jumped back, as if I might bite. When I turned my
+head to follow him he screamed: &ldquo;<em>He&rsquo;s moving!</em>&rdquo;</p>
+<p>&ldquo;Hold it!&rdquo;
+This was the older guy with the mustache. He edged closer. &ldquo;He&rsquo;s
+not wearing the legs!&rdquo;</p>
+<p>&ldquo;Confirm
+that!&rdquo;</p>
+<p>&ldquo;Roger!
+Subject has no legs!&rdquo;</p>
+<p>Guns disappeared
+into holsters. I felt humiliated.</p>
+<p>&ldquo;Get them
+into the van,&rdquo; said the Mustache. &ldquo;Double time.&rdquo;</p>
+<p>Hands reached for
+me. &ldquo;Go to hell,&rdquo; I said, and was ignored. Two guys got
+me under the armpits and pulled me out of the car. &ldquo;At least
+bring my legs, you pricks!&rdquo; I tried to twist around to see if I
+was being obeyed. I caught a glimpse of guards dragging Lola out of
+the driver&rsquo;s seat, and another guy peering through the side
+window.</p>
+<p>&ldquo;Here... no,
+wait, they&rsquo;re not the legs! They&rsquo;re just crutches!&rdquo;</p>
+<p>&ldquo;Where are
+your legs?&rdquo; said one of the guys carrying me. He spoke with no
+strain: I was that light.</p>
+<p>&ldquo;I broke
+them.&rdquo;</p>
+<p>A black town car
+slid to a halt in front of us. All its doors popped open at the same
+time. From the rear emerged someone I hadn&rsquo;t seen for a while:
+Cassandra Cautery. Her hair was different. I&rsquo;m not sure how. It
+had changed from one style I don&rsquo;t know the name of to another
+style I don&rsquo;t know the name of. But she looked different in a
+way that made clear the unidirectional nature of time. She looked
+like progress.</p>
+<p>Her glance flicked
+over me and settled on the Mustache. &ldquo;Where are the legs?&rdquo;</p>
+<p>&ldquo;We haven&rsquo;t&mdash;&rdquo;</p>
+<p>&ldquo;Find them.&rdquo;</p>
+<p>&ldquo;Yes, ma&rsquo;am.&rdquo;</p>
+<p>&ldquo;But get him
+out of here first.&rdquo; She glanced over her shoulder, at nothing
+but an empty road, as far as I could tell. &ldquo;Actually, put him
+in the car. You stay and recover the legs.&rdquo;</p>
+<p>&ldquo;Yes, ma&rsquo;am.&rdquo;</p>
+<p>I realized that the
+Mustache was nervous. Which would have made sense, if he had
+initially been nervous because he thought I was an artificially
+augmented fighting machine, and now because his boss had showed up.
+Except he had not relaxed in between. And, additionally, Cassandra
+Cautery seemed nervous. What all this implied was that the
+nervousness was due to some external factor I had not yet identified.</p>
+<p>The guards carried
+me toward the town car. Then there was a far-off noise, a vaguely
+metallic crunch, and everybody stopped. A siren began to wail; either
+a car or a house alarm, I couldn&rsquo;t tell which.</p>
+<p>&ldquo;Shit,&rdquo;
+said Cassandra Cautery. &ldquo;He&rsquo;s coming.&rdquo;</p>
