aee420198df77219ce844f385d4270f1c9693f9f
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-23 07:27:19 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@515 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0136.mm
+++ page0136.mm
@@ -3,25 +3,26 @@
 your parts,&rdquo; said Cassandra Cautery. Behind her, the
 streetscape turned urban: we were entering the industrial district,
 getting closer to Better Future. &ldquo;He&rsquo;s set on the idea of
-taking away your Bioreplacements, like you had us take away his
-arms.&rdquo;</p>
+taking away your Bioreplacements, the same way you had us take away
+his arms.&rdquo;</p>
 <p>&ldquo;But I don&rsquo;t
 have any parts.&rdquo;</p>
 <p>&ldquo;None of
-ours, no...&rdquo; She tapped her lips. &ldquo;I&rsquo;m just
-wondering if that will matter.&rdquo;</p>
+ours, no...&rdquo; She tapped her lips.</p>
 <p>&ldquo;What?&rdquo;</p>
-<p>&ldquo;Look, all I
-know is that Carl wants your parts, and his psychological state is
-not very stable.&rdquo;</p>
+<p> &ldquo;I&rsquo;m
+just wondering if that will matter.&rdquo;</p>
+<p>&ldquo;<em>What?</em>&rdquo;</p>
+<p>&ldquo;All I know
+is Carl wants your parts, and his mental state is not very stable.&rdquo;</p>
 <p>&ldquo;You think
-he&rsquo;ll, what, pull my arms off? My actual arms? You think he
-wants to <em>kill</em> me?&rdquo;</p>
+he&rsquo;ll, what, pull my arms off? You think he wants to <em>kill</em>
+me?&rdquo;</p>
 <p>&ldquo;I don&rsquo;t
 know what&rsquo;s running through his head. Last time I saw him, he
-was throwing our people around the lobby.&rdquo;</p>
-<p>I squirmed in my
-seat, checking the windows.</p>
+was throwing people around the lobby.&rdquo;</p>
+<p>I threw a glance
+out the rear window.</p>
 <p>&ldquo;But don&rsquo;t
-worry.&rdquo; Cassandra Cautery patted my thigh. &ldquo;I have a
-plan. Everything&rsquo;s going to be just fine.&rdquo;</p>
+worry.&rdquo; Cassandra Cautery patted my thigh. &ldquo;Everything&rsquo;s
+going to be fine. I have a plan.&rdquo;</p>
