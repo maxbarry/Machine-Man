79a254c0f46dfef34edbef97c55bd1692556e908
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-23 04:51:54 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@514 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0137.mm
+++ page0137.mm
@@ -1,18 +1,215 @@
-<p>Page 136.</p>
-<p>&ldquo;I feel like
-we&rsquo;re making some really positive progress here,&rdquo; said
-Cassandra Cautery. &ldquo;It&rsquo;s back to you and me. That&rsquo;s
-pretty much how this got started, right? It was only when other
-people got involved that problems appeared.&rdquo;</p>
+<p>Page 137.</p>
+<p>&ldquo;I&rsquo;ll
+be honest with you, Charlie,&rdquo; said Cassandra Cautery. &ldquo;After
+you killed the Manager, a lot of people were upset.&rdquo;</p>
+<p>&ldquo;About that&mdash;&rdquo;</p>
+<p>&ldquo;Not to
+mention the car you put into the building. That whole wing had to be
+checked for structural damage. For two weeks, Accounts Receivable was
+working out of the cafeteria. Two weeks! It was a nightmare.&rdquo;</p>
+<p>&ldquo;I didn&rsquo;t&mdash;&rdquo;</p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
-<p>&ldquo;The trouble
-with Carl began when he let you and Lola Banks get away,&rdquo; said
-CC. &ldquo;Well, that&rsquo;s not quite true. If I&rsquo;m objective,
-there were worrying signs from the very beginning. He came to me
-about replacing his arms, you know. It wasn&rsquo;t my idea. 
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;We were
+wrong,&rdquo; said Cassandra Cautery. &ldquo;We thought what worked
+for you would work for everybody, and it didn&rsquo;t. I don&rsquo;t
+know why, but I&rsquo;m sitting here and telling you: you were right.
+We should never have expanded this project beyond you.&rdquo;</p>
+<p>&ldquo;Okay,&rdquo;
+I said.</p>
+<p>&ldquo;Having said
+that, you did kill our CEO. So there&rsquo;s fault on both sides.&rdquo;</p>
+<p>&ldquo;About that&mdash;&rdquo;</p>
+<p>She raised a hand.
+&ldquo;Let me explain something about our company to you, Charlie. It
+has no emotion. It can seem like it does, because it acts through its
+employees, and those of course are people. But behind them is a legal
+entity that has no regard for feelings. You can insult our company.
+You can poke it in the eye. And it won&rsquo;t poke you back. Not
+unless it&rsquo;s beneficial to do so.&rdquo;  She eyed me. &ldquo;Like
+a machine, you know? Think of Better Future as a machine with eight
+thousand employees. An artificial intelligence programmed to shift
+resources around to generate wealth and power.&rdquo;</p>
+<p>&ldquo;So... you&rsquo;re
+not mad?&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;You mean...
+nobody&rsquo;s mad at me?&rdquo;</p>
+<p>&ldquo;No, that&rsquo;s
+not what I mean. People are pissed as hell. There was a strong
+argument, advanced by several key people, that we should throw you to
+the wolves. My point is that the company itself doesn&rsquo;t share
+those emotions. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>And and it has a
+way of reallocating its resources accordingly.&rdquo;</p>
+<p>I stared at her
+dumbly.</p>
+<p>&ldquo;Oh, people
+are pissed at you. Some wanted to throw you to the wolves. But I
+lobbied for you. What, I argued, do we gain from sending one of our
+finest minds to prison? What do we stand to lose, if this becomes an
+investigation? It was a good meeting. I don&rsquo;t mind saying,
+Charlie, it was some of my best work. Did you know that the British
+specifically appointed a wartime Prime Minister during the Second
+World War? They did. Winston Churchill. He was one bloodthirsty
+bastard. Firebombed Dresden. You wouldn&rsquo;t want him running your
+country in peacetime. But a time of crisis demands a particular
+skillset. It requires a leader prepared to achieve victory at any
+cost. That&rsquo;s what I told the board.&rdquo;</p>
+<p>&ldquo;You mean...&rdquo;</p>
+<p>&ldquo;Better
+Future&rsquo;s Interim CEO. Right here.&rdquo;</p>
+<p>Now I understood
+why she wasn&rsquo;t mad at me. &ldquo;Congratulations.&rdquo;</p>
+<p>&ldquo;To be
+honest, Charlie, I&rsquo;m not entirely sure they didn&rsquo;t simply
+want someone to pin this on.&rdquo; She smiled. &ldquo;But thanks.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>amasses wealth and
+power by always actings in its logical best interest, regardless of
+emotion.</p>
+<p>But the
+corporation, the legal entity that outlives any single CEOs 
+</p>
+<p>itself will always
+act in its own self-interest, with no regard for anyone&rsquo;s
+feelings. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>They are not
+emotional creatures. They 
+</p>
+<p>&ldquo;We presented
+that to the police as an accident, by the way. Many wanted to throw
+you to the wolves, 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Original.
+&gt;&gt;</p>
+<p>&ldquo;You might
+recall the Manager mentioned we were experiencing a few teething
+problems with Bioreplacements,&rdquo; said Cassandra Cautery. &ldquo;Well.
+More specifically, our test subjects were. Do you recall the nature
+of the problem?&rdquo;</p>
+<p>I remembered the
+Manager telling me they had implanted a device in Lola without her
+knowledge. I remembered kicking him out a window. Everything else was
+fuzzy.</p>
+<p>&ldquo;Depression.
+Stress.&rdquo;</p>
+<p>&ldquo;Oh. Right.&rdquo;</p>
+<p>&ldquo;Psychotic
+breaks.&rdquo; She adjusted her skirt. &ldquo;For reasons we haven&rsquo;t
+adequately explored, not everyone adjusted to their Bioreplacement as
+effortlessly as you did.&rdquo;</p>
+<p>&ldquo;I thought
+you were calling them Combat Augmentations.&rdquo;</p>
+<p>&ldquo;I changed
+it. I never liked <em>Augmentation</em>. It sounded like something you
+choose to match your purse.&rdquo; She frowned at her nails. &ldquo;But
+these psychological issues, they&rsquo;ve been very troublesome. You
+must understand how frustrating it is, having a million-odd dollars
+worth of experimental hardware locked up inside someone who sits in
+their room all day crying. Who won&rsquo;t do any of the prescribed
+tests. Although at least that&rsquo;s better than...&rdquo; She shook
+her head. &ldquo;The point is: we were wrong, Charlie. We were wrong
+and you were right. We got ahead of ourselves, tried to do too much
+too quickly. We thought what worked for you would work for everybody.
+And it didn&rsquo;t. It doesn&rsquo;t. We don&rsquo;t know why. But
+we&rsquo;ve learned our lesson. I&rsquo;m sitting here and telling
+you: we should never have tried to expand this project to other
+people.&rdquo;</p>
+<p>&ldquo;Okay,&rdquo;
+I said.</p>
+<p>&ldquo;Having said
+that, you did kill our CEO. So there&rsquo;s fault on both sides.&rdquo;
+She glanced again out the rear window, and seemed satisifed with what
+she saw. &ldquo;We presented that to the police as an accident, by
+the way. There were those who wanted you drawn and quartered. But I
+lobbied for you. What, I argued, do we gain from sending one of our
+finest minds to prison? What do we stand to lose, if this becomes an
+investigation? It was a good meeting. I don&rsquo;t mind saying,
+Charlie, it was some of my best work. Did you know that the British
+specifically appointed a wartime Prime Minister during the Second
+World War? They did. Winston Churchill. He was one bloodthirsty
+bastard. Firebombed Dresden. You wouldn&rsquo;t want him running your
+country in peacetime. But a time of crisis demands a particular
+skillset. It requires a leader prepared to achieve victory at any
+cost. That&rsquo;s what I told the board.&rdquo;</p>
+<p>&ldquo;You mean...&rdquo;</p>
+<p>&ldquo;Better
+Future&rsquo;s Interim CEO. Right here.&rdquo;</p>
+<p>&ldquo;Oh,&rdquo; I
+said. &ldquo;Well, congratulations.&rdquo;</p>
+<p>&ldquo;To be
+honest, Charlie, I&rsquo;m not entirely sure they didn&rsquo;t simply
+want someone to pin this on.&rdquo; She smiled. &ldquo;But thanks.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Company
+always has to make the most of opportunity. It&rsquo;s emotionless.
+Purely logical. Like a machine. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;This whole
+operation now is about clean-up,&rdquo; said Cassandra Cautery. &ldquo;The
+</p>
+<p></p>
+<p></p>
+<p> Bioreplacement
+product line, that&rsquo;s finished. 
 </p>
 <p></p>
 <p></p>
@@ -20,34 +217,44 @@ about replacing his arms, you know. It wasn&rsquo;t my idea.
 <p></p>
 <p></p>
 <p></p>
-<p>&lt;&lt; CC: No
-more Bioreplacements. I don&rsquo;t care about that any more. I just
-want to clean up. &gt;&gt;</p>
+<p>&lt;&lt; This page:
+interim ceo, carl. Next page: &lsquo;that&rsquo;s where you come in&rsquo;
+stop him, cleanup. &gt;&gt;</p>
 <p></p>
-<p>&lt;&lt; CC: Carl
-came to BF. He said, I want metal arms. We didn&rsquo;t ask why. In
-retrospect, probably should have dug around there. Problem with no
-expert in a particular field, everyone underestimates it. We have a
-million engineers, you talk about skipping a prototype phase and
-everyone starts jumping up and down. But nobody rated the mind.
-Anyway, we put Carl under the knife. He was to be the body to your
-mind, you may remember. We thought: we&rsquo;ll give Carl the parts,
-and if something goes terribly wrong, well, he signed the release.
-And he seemed to recover well, was very chirpy, couldn&rsquo;t wait
-for the new arms. Then you made me take them off him. He got really
-angry. So we gave him more parts. Reinforced spine. &gt;&gt;</p>
+<p>&ldquo;For reasons
+we haven&rsquo;t adequately explored, not everyone adjusted to their
+Bioreplacement as effortlessly as you did.&rdquo;</p>
 <p></p>
 <p></p>
-<p></p>
-<p>fell into a funk.
-We left him alone. 
+<p>Do you recall the
+Manager describing the teething problems 
 </p>
 <p></p>
 <p></p>
-<p>, and when
-management heard about that, they said well hey, we&rsquo;ve got this
-repository full of parts, let&rsquo;s start rounding up a few more
-volunteers. &gt;&gt;</p>
+<p>with
+Bioreplacements,&rdquo; said Cassandra Cautery. &ldquo;Well. More
+specifically, our test subjects were. Do you recall the nature of the
+problem?&rdquo;</p>
+<p></p>
+<p>&lt;&lt; The whole
+time, ever since I heard Angelica on the phone, louder since the
+white Better Future and a drumbeat since Cautery: parts. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>P135: &lt;&lt;
+&ldquo;Carl came to us.&rdquo; No shrinks. Experts. Didn&rsquo;t
+inquire further. He got angry. You might recall our problems.
+Psychotic breaks. Carl&rsquo;s nuts, and we can&rsquo;t contain
+him.&gt;&gt;</p>
+<p></p>
+<p></p>
+<p>P136+: Interim CEO.
+Fresh starts. Shut down the program. No parts for soliders. But you,
+Charlie, we need you to help stop him. Charlie shaking, his legs
+crawling, thinking: Mmm, lab.</p>
 <p></p>
 <p></p>
 <p></p>
@@ -55,19 +262,6 @@ volunteers. &gt;&gt;</p>
 <p></p>
 <p></p>
 <p></p>
-<p>, you don&rsquo;t
-know enough to ask the right questions, 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;Now, to
-Carl,&rdquo; said Cassandra Cautery. &ldquo;He&rsquo;s gone nuts. And
-we haven&rsquo;t been able to contain him.&rdquo;</p>
 <p></p>
 <p></p>
 <p></p>
@@ -77,92 +271,16 @@ we haven&rsquo;t been able to contain him.&rdquo;</p>
 <p></p>
 <p></p>
 <p></p>
-<p>&ldquo;So now we
-have this enormous cleanup,&rdquo; she said. 
-</p>
-<p>&ldquo;We need to
-take this back to square one. That&rsquo;s you and a lab. We give you
-all the resources you need. 
-</p>
-<p></p>
-<p></p>
-<p>Cassandra Cautery&rsquo;s
-hand went <em>bloop bloop</em>.</p>
-<p></p>
-<p></p>
-<p>...In which we hear
-The Story of Carl!</p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
-<p>&gt;&gt;&gt; Save
-this for a finish: &gt;&gt;&gt; I hadn&rsquo;t even felt it. It
-occurred to me that last time, maybe what I had felt was not the
-electromagnetic pulse. Maybe it was my body&rsquo;s reaction to it.
-Maybe I had felt the Contours dying.</p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
-<p>&lt;&lt; I&rsquo;d
-like a conversation between Charlie and CC. She&rsquo;s saying it&rsquo;s
-okay, Charlie, we just want what&rsquo;s best for you. &gt;&gt;</p>
-<p>&lt;&lt; CC says
-the problem is Carl, Carl&rsquo;s gone nuts. You don&rsquo;t want to
-run into Carl. It&rsquo;s really frustrating, she says, because you
-people keep going off the deep end. &gt;&gt;</p>
-<p>&lt;&lt; Actually,
-that&rsquo;s quite an interesting idea for an entire plot. CC says
-Carl is the bad guy, she wants to arm up Charlie to fight him.
-&ldquo;You&rsquo;re our only hope.&rdquo; &gt;&gt;</p>
-<p>&lt;&lt; The appeal
-for Charlie is to get new parts. &gt;&gt;</p>
-<p>&lt;&lt; Carl is
-out for revenge. It&rsquo;s those psychological problems the Manager
-referred to. He thinks Lola loves him. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
 <p></p>
 <p></p>
 <p></p>
-<p>Lola was looking at
-me. &ldquo;Do you miss your legs?&rdquo;</p>
-<p>I wasn&rsquo;t sure
-how to answer this.</p>
-<p>&ldquo;Do you feel
-like you want them back?&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>A low, keening
-noise came out of Lola. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I had never seen
-him before in my life, but this condescending look coming from a guy
-resting one foot on the accelerator of a 240 horsepower vehicle made
-me want to hurt him.</p>
-</BODY>
-</HTML>
---
+++ page0138.mm
@@ -0,0 +1,168 @@
+<p>Page 136.</p>
+<p>&ldquo;I feel like
+we&rsquo;re making some really positive progress here,&rdquo; said
+Cassandra Cautery. &ldquo;It&rsquo;s back to you and me. That&rsquo;s
+pretty much how this got started, right? It was only when other
+people got involved that problems appeared.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;The trouble
+with Carl began when he let you and Lola Banks get away,&rdquo; said
+CC. &ldquo;Well, that&rsquo;s not quite true. If I&rsquo;m objective,
+there were worrying signs from the very beginning. He came to me
+about replacing his arms, you know. It wasn&rsquo;t my idea. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; CC: No
+more Bioreplacements. I don&rsquo;t care about that any more. I just
+want to clean up. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; CC: Carl
+came to BF. He said, I want metal arms. We didn&rsquo;t ask why. In
+retrospect, probably should have dug around there. Problem with no
+expert in a particular field, everyone underestimates it. We have a
+million engineers, you talk about skipping a prototype phase and
+everyone starts jumping up and down. But nobody rated the mind.
+Anyway, we put Carl under the knife. He was to be the body to your
+mind, you may remember. We thought: we&rsquo;ll give Carl the parts,
+and if something goes terribly wrong, well, he signed the release.
+And he seemed to recover well, was very chirpy, couldn&rsquo;t wait
+for the new arms. Then you made me take them off him. He got really
+angry. So we gave him more parts. Reinforced spine. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>fell into a funk.
+We left him alone. 
+</p>
+<p></p>
+<p></p>
+<p>, and when
+management heard about that, they said well hey, we&rsquo;ve got this
+repository full of parts, let&rsquo;s start rounding up a few more
+volunteers. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>, you don&rsquo;t
+know enough to ask the right questions, 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;Now, to
+Carl,&rdquo; said Cassandra Cautery. &ldquo;He&rsquo;s gone nuts. And
+we haven&rsquo;t been able to contain him.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;So now we
+have this enormous cleanup,&rdquo; she said. 
+</p>
+<p>&ldquo;We need to
+take this back to square one. That&rsquo;s you and a lab. We give you
+all the resources you need. 
+</p>
+<p></p>
+<p></p>
+<p>Cassandra Cautery&rsquo;s
+hand went <em>bloop bloop</em>.</p>
+<p></p>
+<p></p>
+<p>...In which we hear
+The Story of Carl!</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&gt;&gt;&gt; Save
+this for a finish: &gt;&gt;&gt; I hadn&rsquo;t even felt it. It
+occurred to me that last time, maybe what I had felt was not the
+electromagnetic pulse. Maybe it was my body&rsquo;s reaction to it.
+Maybe I had felt the Contours dying.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; I&rsquo;d
+like a conversation between Charlie and CC. She&rsquo;s saying it&rsquo;s
+okay, Charlie, we just want what&rsquo;s best for you. &gt;&gt;</p>
+<p>&lt;&lt; CC says
+the problem is Carl, Carl&rsquo;s gone nuts. You don&rsquo;t want to
+run into Carl. It&rsquo;s really frustrating, she says, because you
+people keep going off the deep end. &gt;&gt;</p>
+<p>&lt;&lt; Actually,
+that&rsquo;s quite an interesting idea for an entire plot. CC says
+Carl is the bad guy, she wants to arm up Charlie to fight him.
+&ldquo;You&rsquo;re our only hope.&rdquo; &gt;&gt;</p>
+<p>&lt;&lt; The appeal
+for Charlie is to get new parts. &gt;&gt;</p>
+<p>&lt;&lt; Carl is
+out for revenge. It&rsquo;s those psychological problems the Manager
+referred to. He thinks Lola loves him. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>Page
+NOTES.</p>
+<p></p>
+<p></p>
+<p></p>
+<p>Lola was looking at
+me. &ldquo;Do you miss your legs?&rdquo;</p>
+<p>I wasn&rsquo;t sure
+how to answer this.</p>
+<p>&ldquo;Do you feel
+like you want them back?&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; soft
+sciences: Charlie asks Lola about id. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p><em>The Id comprises the unorganised
+part of the personality structure that contains the basic drives. It
+acts according to the pleasure principle, seeking to avoid pain or
+unpleasure. </em>
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>A low, keening
+noise came out of Lola. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I had never seen
+him before in my life, but this condescending look coming from a guy
+resting one foot on the accelerator of a 240 horsepower vehicle made
+me want to hurt him.</p>
+</BODY>
+</HTML>
