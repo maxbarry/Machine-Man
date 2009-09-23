94627291d4afa758c5ae6600435206980601dccd
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-23 07:40:41 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@517 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0136.mm
+++ page0136.mm
@@ -1,12 +1,11 @@
 <p>Page 136.</p>
 <p>&ldquo;Carl wants
 your parts,&rdquo; said Cassandra Cautery. Behind her, the
-streetscape turned urban: we were entering the industrial district,
-getting closer to Better Future. &ldquo;He&rsquo;s set on the idea of
-taking away your Bioreplacements, the same way you had us take away
-his arms.&rdquo;</p>
+streetscape grew industrial: we were getting closer to Better Future.
+&ldquo;He&rsquo;s set on the idea of taking away your
+Bioreplacements, the same way you had us take away his arms.&rdquo;</p>
 <p>&ldquo;But I don&rsquo;t
-have any parts left.&rdquo;</p>
+have any parts.&rdquo;</p>
 <p>&ldquo;None of
 ours, no...&rdquo; She tapped her lips.</p>
 <p>&ldquo;What?&rdquo;</p>
@@ -16,7 +15,7 @@ just wondering if that will matter.&rdquo;</p>
 <p>&ldquo;All I know
 is Carl wants your parts, and his mental state is not very stable.&rdquo;</p>
 <p>&ldquo;You think
-he&rsquo;ll, what, pull my arms off? You think he wants to <em>kill</em>
+he&rsquo;ll... what, pull my arms off? You think he wants to <em>kill</em>
 me?&rdquo;</p>
 <p>&ldquo;I don&rsquo;t
 know what&rsquo;s running through his head. Last time I saw him, he
