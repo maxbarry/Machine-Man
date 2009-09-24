0090e9edd0933abe5309dd379f208d7202c1ff50
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-24 07:07:04 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@518 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0137.mm
+++ page0137.mm
@@ -1,286 +1,61 @@
 <p>Page 137.</p>
 <p>&ldquo;I&rsquo;ll
 be honest with you, Charlie,&rdquo; said Cassandra Cautery. &ldquo;After
-you killed the Manager, a lot of people were upset.&rdquo;</p>
+you killed the Manager, some people were very upset.&rdquo;</p>
 <p>&ldquo;About that&mdash;&rdquo;</p>
 <p>&ldquo;Not to
-mention the car you put into the building. That whole wing had to be
-checked for structural damage. For two weeks, Accounts Receivable was
-working out of the cafeteria. Two weeks! It was a nightmare.&rdquo;</p>
+mention that car you put into the building. That whole area had to be
+checked out for structural damage. Human Resources was working out of
+the cafeteria for two weeks. It was a nightmare.&rdquo;</p>
 <p>&ldquo;I didn&rsquo;t&mdash;&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;We were
-wrong,&rdquo; said Cassandra Cautery. &ldquo;We thought what worked
-for you would work for everybody, and it didn&rsquo;t. I don&rsquo;t
-know why, but I&rsquo;m sitting here and telling you: you were right.
-We should never have expanded this project beyond you.&rdquo;</p>
-<p>&ldquo;Okay,&rdquo;
-I said.</p>
-<p>&ldquo;Having said
-that, you did kill our CEO. So there&rsquo;s fault on both sides.&rdquo;</p>
-<p>&ldquo;About that&mdash;&rdquo;</p>
-<p>She raised a hand.
-&ldquo;Let me explain something about our company to you, Charlie. It
-has no emotion. It can seem like it does, because it acts through its
-employees, and those of course are people. But behind them is a legal
-entity that has no regard for feelings. You can insult our company.
-You can poke it in the eye. And it won&rsquo;t poke you back. Not
-unless it&rsquo;s beneficial to do so.&rdquo;  She eyed me. &ldquo;Like
-a machine, you know? Think of Better Future as a machine with eight
-thousand employees. An artificial intelligence programmed to shift
-resources around to generate wealth and power.&rdquo;</p>
-<p>&ldquo;So... you&rsquo;re
-not mad?&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;You mean...
-nobody&rsquo;s mad at me?&rdquo;</p>
-<p>&ldquo;No, that&rsquo;s
-not what I mean. People are pissed as hell. There was a strong
-argument, advanced by several key people, that we should throw you to
-the wolves. My point is that the company itself doesn&rsquo;t share
-those emotions. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>And and it has a
-way of reallocating its resources accordingly.&rdquo;</p>
-<p>I stared at her
-dumbly.</p>
-<p>&ldquo;Oh, people
-are pissed at you. Some wanted to throw you to the wolves. But I
-lobbied for you. What, I argued, do we gain from sending one of our
-finest minds to prison? What do we stand to lose, if this becomes an
-investigation? It was a good meeting. I don&rsquo;t mind saying,
-Charlie, it was some of my best work. Did you know that the British
-specifically appointed a wartime Prime Minister during the Second
-World War? They did. Winston Churchill. He was one bloodthirsty
-bastard. Firebombed Dresden. You wouldn&rsquo;t want him running your
-country in peacetime. But a time of crisis demands a particular
-skillset. It requires a leader prepared to achieve victory at any
-cost. That&rsquo;s what I told the board.&rdquo;</p>
-<p>&ldquo;You mean...&rdquo;</p>
-<p>&ldquo;Better
-Future&rsquo;s Interim CEO. Right here.&rdquo;</p>
-<p>Now I understood
-why she wasn&rsquo;t mad at me. &ldquo;Congratulations.&rdquo;</p>
+<p>&ldquo;But here&rsquo;s
+the thing about Better Future: we&rsquo;re forward-focused. We have
+an expression in management: history is a sunk cost. Because it&rsquo;s
+very easy, Charlie, to slip into emotive thinking, like, &lsquo;This
+guy, he murdered our chief executive, he has to pay.&rsquo; But why?
+Will that actually help us? We didn&rsquo;t get where we are today by
+worrying about hurt feelings. We got here by applying ruthless,
+dispassionate logic to opportunity maximization.&rdquo;</p>
+<p>&ldquo;Like
+software,&rdquo; I said. &ldquo;Like a machine.&rdquo;</p>
+<p>She looked
+momentarily surprised. &ldquo;Yes. I suppose so. But the point is, we
+understand that accidents happen. That great advances are never made
+without setbacks. We know better than anyone that, despite the best
+intentions, sometimes things get messy.&rdquo; She looked very
+solemn. &ldquo;With the benefit of hindsight, was it a mistake to
+install a Bioreplacement in Lola Banks without her knowledge? Yes.
+Should we have rushed to broaden testing to include other people?
+No.&rdquo; She shrugged.</p>
+<p>&ldquo;Putting that
+<em>thing</em> in Lola is a hell of a mistake. You&mdash;&rdquo;</p>
+<p>&ldquo;So is
+kicking a man out a window to his death. You see? Fault on both
+sides. The important thing, Charlie, is where we go from here.&rdquo;</p>
+<p>I was no longer
+sure that Cassandra Cautery&rsquo;s hair was different. I was
+starting to think it was her manner. She was carrying herself
+differently. &ldquo;Cassie,&rdquo; I said. &ldquo;I&rsquo;m really
+not sure&mdash;&rdquo;</p>
+<p>&ldquo;Cassandra.&rdquo;
+She turned her grimace into a smile. &ldquo;Not Cassie. Cassandra.&rdquo;</p>
+<p>It was definitely
+her manner. &ldquo;Did you say <em>Bioreplacement</em>? What happened
+to <em>Combat Augmentation</em>?&rdquo;</p>
+<p>&ldquo;I never
+liked <em>Augmentation</em>. It sounded like something you choose to
+match your shoes. I changed it. Did you know the British specifically
+appointed a wartime Prime Minister during the Second World War?&rdquo;
+She nodded. &ldquo;Winston Churchill. He was one cold-hearted
+bastard. You wouldn&rsquo;t want him running your country in
+peacetime. But a crisis demands a particular skillset. It calls for
+unflinching leadership. That&rsquo;s what I told the board.&rdquo;</p>
+<p>I said, &ldquo;You&rsquo;re
+in charge.&rdquo;</p>
+<p>&ldquo;Interim CEO.
+It&rsquo;s temporary.&rdquo;</p>
+<p>It took me a while
+to figure out what to say. &ldquo;Congratulations.&rdquo;</p>
 <p>&ldquo;To be
 honest, Charlie, I&rsquo;m not entirely sure they didn&rsquo;t simply
 want someone to pin this on.&rdquo; She smiled. &ldquo;But thanks.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>amasses wealth and
-power by always actings in its logical best interest, regardless of
-emotion.</p>
-<p>But the
-corporation, the legal entity that outlives any single CEOs 
-</p>
-<p>itself will always
-act in its own self-interest, with no regard for anyone&rsquo;s
-feelings. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>They are not
-emotional creatures. They 
-</p>
-<p>&ldquo;We presented
-that to the police as an accident, by the way. Many wanted to throw
-you to the wolves, 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Original.
-&gt;&gt;</p>
-<p>&ldquo;You might
-recall the Manager mentioned we were experiencing a few teething
-problems with Bioreplacements,&rdquo; said Cassandra Cautery. &ldquo;Well.
-More specifically, our test subjects were. Do you recall the nature
-of the problem?&rdquo;</p>
-<p>I remembered the
-Manager telling me they had implanted a device in Lola without her
-knowledge. I remembered kicking him out a window. Everything else was
-fuzzy.</p>
-<p>&ldquo;Depression.
-Stress.&rdquo;</p>
-<p>&ldquo;Oh. Right.&rdquo;</p>
-<p>&ldquo;Psychotic
-breaks.&rdquo; She adjusted her skirt. &ldquo;For reasons we haven&rsquo;t
-adequately explored, not everyone adjusted to their Bioreplacement as
-effortlessly as you did.&rdquo;</p>
-<p>&ldquo;I thought
-you were calling them Combat Augmentations.&rdquo;</p>
-<p>&ldquo;I changed
-it. I never liked <em>Augmentation</em>. It sounded like something you
-choose to match your purse.&rdquo; She frowned at her nails. &ldquo;But
-these psychological issues, they&rsquo;ve been very troublesome. You
-must understand how frustrating it is, having a million-odd dollars
-worth of experimental hardware locked up inside someone who sits in
-their room all day crying. Who won&rsquo;t do any of the prescribed
-tests. Although at least that&rsquo;s better than...&rdquo; She shook
-her head. &ldquo;The point is: we were wrong, Charlie. We were wrong
-and you were right. We got ahead of ourselves, tried to do too much
-too quickly. We thought what worked for you would work for everybody.
-And it didn&rsquo;t. It doesn&rsquo;t. We don&rsquo;t know why. But
-we&rsquo;ve learned our lesson. I&rsquo;m sitting here and telling
-you: we should never have tried to expand this project to other
-people.&rdquo;</p>
-<p>&ldquo;Okay,&rdquo;
-I said.</p>
-<p>&ldquo;Having said
-that, you did kill our CEO. So there&rsquo;s fault on both sides.&rdquo;
-She glanced again out the rear window, and seemed satisifed with what
-she saw. &ldquo;We presented that to the police as an accident, by
-the way. There were those who wanted you drawn and quartered. But I
-lobbied for you. What, I argued, do we gain from sending one of our
-finest minds to prison? What do we stand to lose, if this becomes an
-investigation? It was a good meeting. I don&rsquo;t mind saying,
-Charlie, it was some of my best work. Did you know that the British
-specifically appointed a wartime Prime Minister during the Second
-World War? They did. Winston Churchill. He was one bloodthirsty
-bastard. Firebombed Dresden. You wouldn&rsquo;t want him running your
-country in peacetime. But a time of crisis demands a particular
-skillset. It requires a leader prepared to achieve victory at any
-cost. That&rsquo;s what I told the board.&rdquo;</p>
-<p>&ldquo;You mean...&rdquo;</p>
-<p>&ldquo;Better
-Future&rsquo;s Interim CEO. Right here.&rdquo;</p>
-<p>&ldquo;Oh,&rdquo; I
-said. &ldquo;Well, congratulations.&rdquo;</p>
-<p>&ldquo;To be
-honest, Charlie, I&rsquo;m not entirely sure they didn&rsquo;t simply
-want someone to pin this on.&rdquo; She smiled. &ldquo;But thanks.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Company
-always has to make the most of opportunity. It&rsquo;s emotionless.
-Purely logical. Like a machine. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;This whole
-operation now is about clean-up,&rdquo; said Cassandra Cautery. &ldquo;The
-</p>
-<p></p>
-<p></p>
-<p> Bioreplacement
-product line, that&rsquo;s finished. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; This page:
-interim ceo, carl. Next page: &lsquo;that&rsquo;s where you come in&rsquo;
-stop him, cleanup. &gt;&gt;</p>
-<p></p>
-<p>&ldquo;For reasons
-we haven&rsquo;t adequately explored, not everyone adjusted to their
-Bioreplacement as effortlessly as you did.&rdquo;</p>
-<p></p>
-<p></p>
-<p>Do you recall the
-Manager describing the teething problems 
-</p>
-<p></p>
-<p></p>
-<p>with
-Bioreplacements,&rdquo; said Cassandra Cautery. &ldquo;Well. More
-specifically, our test subjects were. Do you recall the nature of the
-problem?&rdquo;</p>
-<p></p>
-<p>&lt;&lt; The whole
-time, ever since I heard Angelica on the phone, louder since the
-white Better Future and a drumbeat since Cautery: parts. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>P135: &lt;&lt;
-&ldquo;Carl came to us.&rdquo; No shrinks. Experts. Didn&rsquo;t
-inquire further. He got angry. You might recall our problems.
-Psychotic breaks. Carl&rsquo;s nuts, and we can&rsquo;t contain
-him.&gt;&gt;</p>
-<p></p>
-<p></p>
-<p>P136+: Interim CEO.
-Fresh starts. Shut down the program. No parts for soliders. But you,
-Charlie, we need you to help stop him. Charlie shaking, his legs
-crawling, thinking: Mmm, lab.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
--- page0138.mm
+++ page0138.mm
@@ -1,4 +1,287 @@
-<p>Page 136.</p>
+<p>Page 138.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>this company</p>
+<p>accidents happen. 
+</p>
+<p>continually seeking
+the best outcomes.&rdquo;</p>
+<p>continually
+assessing the situation in dispassionate terms and acting 
+</p>
+<p>dispassionately 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>why? 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; CC DOES
+want to build BIOreplacements, just clean up</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;We were
+wrong,&rdquo; said Cassandra Cautery. &ldquo;We thought what worked
+for you would work for everybody, and it didn&rsquo;t. I don&rsquo;t
+know why, but I&rsquo;m sitting here and telling you: you were right.
+We should never have expanded this project beyond you.&rdquo;</p>
+<p>&ldquo;Okay,&rdquo;
+I said.</p>
+<p>&ldquo;Having said
+that, you did kill our CEO. So there&rsquo;s fault on both sides.&rdquo;</p>
+<p>&ldquo;About that&mdash;&rdquo;</p>
+<p>She raised a hand.
+&ldquo;Let me explain something about our company to you, Charlie. It
+has no emotion. It can seem like it does, because it acts through its
+employees, and those of course are people. But behind them is a legal
+entity that has no regard for feelings. You can insult our company.
+You can poke it in the eye. And it won&rsquo;t poke you back. Not
+unless it&rsquo;s beneficial to do so.&rdquo;  She eyed me. &ldquo;Like
+a machine, you know? Think of Better Future as a machine with eight
+thousand employees. An artificial intelligence programmed to shift
+resources around to generate wealth and power.&rdquo;</p>
+<p>&ldquo;So... you&rsquo;re
+not mad?&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;You mean...
+nobody&rsquo;s mad at me?&rdquo;</p>
+<p>&ldquo;No, that&rsquo;s
+not what I mean. People are pissed as hell. There was a strong
+argument, advanced by several key people, that we should throw you to
+the wolves. My point is that the company itself doesn&rsquo;t share
+those emotions. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>And and it has a
+way of reallocating its resources accordingly.&rdquo;</p>
+<p>I stared at her
+dumbly.</p>
+<p>&ldquo;Oh, people
+are pissed at you. Some wanted to throw you to the wolves. But I
+lobbied for you. What, I argued, do we gain from sending one of our
+finest minds to prison? What do we stand to lose, if this becomes an
+investigation? 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>amasses wealth and
+power by always actings in its logical best interest, regardless of
+emotion.</p>
+<p>But the
+corporation, the legal entity that outlives any single CEOs 
+</p>
+<p>itself will always
+act in its own self-interest, with no regard for anyone&rsquo;s
+feelings. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>They are not
+emotional creatures. They 
+</p>
+<p>&ldquo;We presented
+that to the police as an accident, by the way. Many wanted to throw
+you to the wolves, 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Original.
+&gt;&gt;</p>
+<p>&ldquo;You might
+recall the Manager mentioned we were experiencing a few teething
+problems with Bioreplacements,&rdquo; said Cassandra Cautery. &ldquo;Well.
+More specifically, our test subjects were. Do you recall the nature
+of the problem?&rdquo;</p>
+<p>I remembered the
+Manager telling me they had implanted a device in Lola without her
+knowledge. I remembered kicking him out a window. Everything else was
+fuzzy.</p>
+<p>&ldquo;Depression.
+Stress.&rdquo;</p>
+<p>&ldquo;Oh. Right.&rdquo;</p>
+<p>&ldquo;Psychotic
+breaks.&rdquo; She adjusted her skirt. &ldquo;For reasons we haven&rsquo;t
+adequately explored, not everyone adjusted to their Bioreplacement as
+effortlessly as you did.&rdquo;</p>
+<p>&ldquo;I thought
+you were calling them Combat Augmentations.&rdquo;</p>
+<p>&ldquo;I changed
+it. I never liked <em>Augmentation</em>. It sounded like something you
+choose to match your purse.&rdquo; She frowned at her nails. &ldquo;But
+these psychological issues, they&rsquo;ve been very troublesome. You
+must understand how frustrating it is, having a million-odd dollars
+worth of experimental hardware locked up inside someone who sits in
+their room all day crying. Who won&rsquo;t do any of the prescribed
+tests. Although at least that&rsquo;s better than...&rdquo; She shook
+her head. &ldquo;The point is: we were wrong, Charlie. We were wrong
+and you were right. We got ahead of ourselves, tried to do too much
+too quickly. We thought what worked for you would work for everybody.
+And it didn&rsquo;t. It doesn&rsquo;t. We don&rsquo;t know why. But
+we&rsquo;ve learned our lesson. I&rsquo;m sitting here and telling
+you: we should never have tried to expand this project to other
+people.&rdquo;</p>
+<p>&ldquo;Okay,&rdquo;
+I said.</p>
+<p>&ldquo;Having said
+that, you did kill our CEO. So there&rsquo;s fault on both sides.&rdquo;
+She glanced again out the rear window, and seemed satisifed with what
+she saw. &ldquo;We presented that to the police as an accident, by
+the way. There were those who wanted you drawn and quartered. But I
+lobbied for you. What, I argued, do we gain from sending one of our
+finest minds to prison? What do we stand to lose, if this becomes an
+investigation? It was a good meeting. I don&rsquo;t mind saying,
+Charlie, it was some of my best work. Did you know that the British
+specifically appointed a wartime Prime Minister during the Second
+World War? They did. Winston Churchill. He was one bloodthirsty
+bastard. Firebombed Dresden. You wouldn&rsquo;t want him running your
+country in peacetime. But a time of crisis demands a particular
+skillset. It requires a leader prepared to achieve victory at any
+cost. That&rsquo;s what I told the board.&rdquo;</p>
+<p>&ldquo;You mean...&rdquo;</p>
+<p>&ldquo;Better
+Future&rsquo;s Interim CEO. Right here.&rdquo;</p>
+<p>&ldquo;Oh,&rdquo; I
+said. &ldquo;Well, congratulations.&rdquo;</p>
+<p>&ldquo;To be
+honest, Charlie, I&rsquo;m not entirely sure they didn&rsquo;t simply
+want someone to pin this on.&rdquo; She smiled. &ldquo;But thanks.&rdquo;</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Company
+always has to make the most of opportunity. It&rsquo;s emotionless.
+Purely logical. Like a machine. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;This whole
+operation now is about clean-up,&rdquo; said Cassandra Cautery. &ldquo;The
+</p>
+<p></p>
+<p></p>
+<p> Bioreplacement
+product line, that&rsquo;s finished. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; This page:
+interim ceo, carl. Next page: &lsquo;that&rsquo;s where you come in&rsquo;
+stop him, cleanup. &gt;&gt;</p>
+<p></p>
+<p>&ldquo;For reasons
+we haven&rsquo;t adequately explored, not everyone adjusted to their
+Bioreplacement as effortlessly as you did.&rdquo;</p>
+<p></p>
+<p></p>
+<p>Do you recall the
+Manager describing the teething problems 
+</p>
+<p></p>
+<p></p>
+<p>with
+Bioreplacements,&rdquo; said Cassandra Cautery. &ldquo;Well. More
+specifically, our test subjects were. Do you recall the nature of the
+problem?&rdquo;</p>
+<p></p>
+<p>&lt;&lt; The whole
+time, ever since I heard Angelica on the phone, louder since the
+white Better Future and a drumbeat since Cautery: parts. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>P135: &lt;&lt;
+&ldquo;Carl came to us.&rdquo; No shrinks. Experts. Didn&rsquo;t
+inquire further. He got angry. You might recall our problems.
+Psychotic breaks. Carl&rsquo;s nuts, and we can&rsquo;t contain
+him.&gt;&gt;</p>
+<p></p>
+<p></p>
+<p>P136+: Interim CEO.
+Fresh starts. Shut down the program. No parts for soliders. But you,
+Charlie, we need you to help stop him. Charlie shaking, his legs
+crawling, thinking: Mmm, lab.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
 <p>&ldquo;I feel like
 we&rsquo;re making some really positive progress here,&rdquo; said
 Cassandra Cautery. &ldquo;It&rsquo;s back to you and me. That&rsquo;s
