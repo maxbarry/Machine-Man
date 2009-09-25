89215bf4e4c0fc98a52955e0f2cc10cb841e1f91
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-25 07:18:21 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@522 40ef8932-b262-0410-9730-8c1c2ddff0ae

---
+++ page0139.mm
@@ -0,0 +1,67 @@
+<p>Page 139.</p>
+<h5>CHAPTER 
+TWENTY-NINE</h5>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;How about
+that EMP weapon?&rdquo;</p>
+<p>&ldquo;Carl&rsquo;s
+shielded.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt; CC wants to
+build Bioreplacements, just clean up Carl first. &gt;&gt;</p>
+<p></p>
+<p>&ldquo;For reasons
+we haven&rsquo;t adequately explored, not everyone adjusted to their
+Bioreplacement as effortlessly as you did.&rdquo;</p>
+<p></p>
+<p>Page
+NOTES.</p>
+<p></p>
+<p></p>
+<p></p>
+<p>Lola was looking at
+me. &ldquo;Do you miss your legs?&rdquo;</p>
+<p>I wasn&rsquo;t sure
+how to answer this.</p>
+<p>&ldquo;Do you feel
+like you want them back?&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; soft
+sciences: Charlie asks Lola about id. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p><em>The Id comprises the unorganised
+part of the personality structure that contains the basic drives. It
+acts according to the pleasure principle, seeking to avoid pain or
+unpleasure. </em>
+</p>
+<p></p>
+<p></p>
+<p>A low, keening
+noise came out of Lola. 
+</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Company
+always has to make the most of opportunity. It&rsquo;s emotionless.
+Purely logical. Like a machine. &gt;&gt;</p>
+<p>&ldquo;Let me
+explain something about our company to you, Charlie. It has no
+emotion. It can seem like it does, because it acts through its
+employees, and those of course are people. But behind them is a legal
+entity that has no regard for feelings. You can insult our company.
+You can poke it in the eye. And it won&rsquo;t poke you back. Not
+unless it&rsquo;s beneficial to do so.&rdquo;  She eyed me. &ldquo;Like
+a machine, you know? Think of Better Future as a machine with eight
+thousand employees. An artificial intelligence programmed to shift
+resources around to generate wealth and power.&rdquo;</p>
+<p></p>
+</BODY>
+</HTML>
