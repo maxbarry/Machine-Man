9cc81b0d7517179fc4ca53dbd2f7571946ca5f64
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-28 06:37:52 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@524 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0139.mm
+++ page0139.mm
@@ -1,67 +1,28 @@
 <p>Page 139.</p>
 <h5>CHAPTER 
 TWENTY-NINE</h5>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;How about
-that EMP weapon?&rdquo;</p>
-<p>&ldquo;Carl&rsquo;s
-shielded.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt; CC wants to
-build Bioreplacements, just clean up Carl first. &gt;&gt;</p>
-<p></p>
-<p>&ldquo;For reasons
-we haven&rsquo;t adequately explored, not everyone adjusted to their
-Bioreplacement as effortlessly as you did.&rdquo;</p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>Lola was looking at
-me. &ldquo;Do you miss your legs?&rdquo;</p>
-<p>I wasn&rsquo;t sure
-how to answer this.</p>
-<p>&ldquo;Do you feel
-like you want them back?&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p>A low, keening
-noise came out of Lola. 
-</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Company
-always has to make the most of opportunity. It&rsquo;s emotionless.
-Purely logical. Like a machine. &gt;&gt;</p>
-<p>&ldquo;Let me
-explain something about our company to you, Charlie. It has no
-emotion. It can seem like it does, because it acts through its
-employees, and those of course are people. But behind them is a legal
-entity that has no regard for feelings. You can insult our company.
-You can poke it in the eye. And it won&rsquo;t poke you back. Not
-unless it&rsquo;s beneficial to do so.&rdquo;  She eyed me. &ldquo;Like
-a machine, you know? Think of Better Future as a machine with eight
-thousand employees. An artificial intelligence programmed to shift
-resources around to generate wealth and power.&rdquo;</p>
-<p></p>
-</BODY>
-</HTML>
+<p>I have always had a
+strong focus. When something gets my attention, I forget about
+everything else, like who I was just talking to or where I was
+supposed to be soon. When I was six years old, I got distracted at my
+own birthday party by the washing machine, which was new, and sat in
+the laundry watching it tick through cycles until my mother found me
+and asked what was I doing, everybody was going home. In high school
+I was crossing the road when a beautiful girl walked by in the other
+direction, and I didn&rsquo;t realize I was holding up traffic until
+she turned around to see what all the honking was about. The look she
+gave me makes my ears burn even thinking about it today.</p>
+<p>It&rsquo;s useful,
+of course. I&rsquo;m not sure I could have advanced this far in
+engineering without it. But it&rsquo;s not always appropriate, is the
+thing. And sometimes this doesn&rsquo;t bother me, because that
+washing machine really was more interesting than the birthday party,
+but other times I wish in retrospect that the shutters hadn&rsquo;t
+closed. I wish I had retained enough self-awareness to realize I was
+standing in the middle of the road gaping at a girl like an idiot. I
+wish that when we cruised to a halt beside the elevator in the Better
+Future parking lot, my brain left some room for thoughts besides <em>Will
+I get parts now</em> and <em>I wonder if they made more Contours</em>
+and <em>What did that mean, she&rsquo;s hoping to blow me away?</em>
+Because the door opened and I saw a lab assistant with a wheelchair,
+ready to bear me inside, and I totally forgot about Lola.</p>
---
+++ page0140.mm
@@ -0,0 +1,108 @@
+<p>Page 140.</p>
+<p>(TUES)</p>
+<p></p>
+<p>&ldquo;So here&rsquo;s
+the choice, Charlie.&rdquo; Cassandra Cautery was in front of me,
+walking backwards, as I was wheeled along the corridor. For a short
+person, she could really move those legs. &ldquo;We can get you into
+a set of legs, a new hand, right now.&rdquo; She glanced over her
+shoulder, navigated a turn. &ldquo;Or...&rdquo;</p>
+<p>&ldquo;Or?&rdquo;</p>
+<p>She stopped. So,
+dutifully, did the lab assistant pushing my wheelchair. I realized we
+were at a junction. To the left were the labs. Ahead were elevators.
+&ldquo;Or we run you straight into surgery.&rdquo;</p>
+<p>I didn&rsquo;t say
+anything. I felt that if I moved too suddenly, the delicate porcelien
+of the world would shatter and fall to pieces.</p>
+<p>Cassandra Cautery
+crouched in front of me and pressed her hands together, as if she
+were praying. This was pretty unnecessary&mdash;the crouching, I
+mean&mdash;because it reversed the height differential between us
+without reducing it. There was no reason for this unless she wanted
+to be lower than me. &ldquo;We have some drugs.&rdquo; Her eyes were
+very large all of a sudden. &ldquo;Some very good drugs, which we&rsquo;ve
+used successfully on several test subjects, that dramatically reduce
+the recovery time from major surgery. Well.&rdquo; She tilted her
+head. &ldquo;Maybe postpone is a better word. They make you
+functional pretty much immediately.&rdquo;</p>
+<p>I licked my lips.
+The world held together. &ldquo;So... I could have... new parts.&rdquo;</p>
+<p>&ldquo;Yes.&rdquo;</p>
+<p>&ldquo;Such as
+arms.&rdquo;</p>
+<p>&ldquo;Exactly.&rdquo;
+She nodded. &ldquo;Arms.&rdquo; She opened her mouth, as if to say
+something else, then closed it.</p>
+<p>I took a breath.
+&ldquo;It&rsquo;s all so sudden.&rdquo;</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Finish
+with either CC imploring Charlie, you&rsquo;re our only hope, you
+need to be top of your game to compete with Carl, or: Show me. I
+think Show Me, save the former for the pre-req conversation. &gt;&gt;</p>
+<p></p>
+<p>And not just arms.
+We have a pelvis. A lower torso that regulates energy 
+</p>
+<p></p>
+<p>&ldquo;Arms.
+Pelvis. Torso. We have a range.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Actually,
+the pre-reqs should require it. That&rsquo;d be a good page for
+tomorrow. The pre-reqs. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>Page
+NOTES.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; This week
+I think they maybe prepare for Carl, but he doesn&rsquo;t come. So
+then they bulk up a little more. &gt;&gt;</p>
+<p>&lt;&lt; The big
+story here is Charlie getting new parts. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; soft
+sciences: Charlie asks Lola about id. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p><em>The Id comprises the unorganised
+part of the personality structure that contains the basic drives. It
+acts according to the pleasure principle, seeking to avoid pain or
+unpleasure. </em>
+</p>
+<p></p>
+<p></p>
+<p>A low, keening
+noise came out of Lola. 
+</p>
+<p></p>
+<p></p>
+<p>&ldquo;Think of
+Better Future as a machine with eight thousand employees. An
+artificial intelligence programmed to shift resources around to
+generate wealth and power.&rdquo;</p>
+<p></p>
+</BODY>
+</HTML>
