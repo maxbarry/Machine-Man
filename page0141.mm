0d76164b172d306598208d9a093a10c4de7be967
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-09-30 07:25:39 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@527 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0141.mm
+++ page0141.mm
@@ -1,19 +1,26 @@
 <p>Page 141.</p>
 <p>&ldquo;We&rsquo;re
-calling these the Contour Threes.&rdquo; Cassandra Cautery&rsquo;s
-hand rested lightly on ______. &ldquo;Don&rsquo;t ask what happened
-to model Two. It was awful. The Threes are the same as the Contours
-you used, but upgraded for strength and durability, and with an
-improved tactile interface. Oh, and there are some bug fixes. Did you
-know the Contours couldn&rsquo;t handle stairs?&rdquo;</p>
-<p>&ldquo;Yes.&rdquo;</p>
+calling these the Contour Threes.&rdquo; Cassandra Cautery gestured,
+like a model on a game show. Only instead of a car or a set of steak
+knives it was a pair of black titanium legs with cloven hooves. My
+throat constricted, because it was like my old legs had come back to
+life. &ldquo;Don&rsquo;t ask what happened to Mark Two. The Threes
+are basically a revision of the original Contours, upgraded for
+strength and with an improved tactile interface. Oh, and there are
+some bug fixes. Did you know the Contours couldn&rsquo;t handle
+stairs?&rdquo;</p>
+<p>I unclogged my
+throat. &ldquo;Yes.&rdquo;</p>
 <p>&ldquo;Oh. Well,
 that would have been useful to know before our Model Two tester ended
 up at the bottom of a stairwell.&rdquo;</p>
-<p>I wheeled myself
-closer. They were ____.</p>
-<p>&ldquo;I think,&rdquo;
-I said, &ldquo;I just want to put these on.&rdquo;</p>
-<p>&ldquo;Right.&rdquo;
-Cassandra Cautery pursed her lips. &ldquo;Well, there is just one
-tiny issue.&rdquo;</p>
+<p>I tried to wheel
+myself forward, and was reminded that I only had one hand. The lab
+assistant came to my rescue, sliding me within touching range. There
+were a lot of parts around me, parts on shelves and parts in foam
+packing, parts under tantalizingly translucent sheets of plastic, but
+right now I couldn&rsquo;t take my eyes off the Contours.</p>
+<p>I said, &ldquo;I
+think I want to put these on.&rdquo;</p>
+<p>&ldquo;Yes,&rdquo;
+said Cassandra Cautery. &ldquo;There is just one tiny issue.&rdquo;</p>
