6b6c656f087336985f3e8f49782c78375f95e4c6
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-01 07:22:55 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@533 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0143.mm
+++ page0143.mm
@@ -1,26 +1,24 @@
 <p>Page 143.</p>
 <p>The ceiling of this
-section of Better Future was a checkerboard, and every second check
-glowed. I had never really appreciated how pretty this was, until I
-was pushed along beneath it on a steel gurney. &ldquo;These lights
-are really neat,&rdquo; I said.</p>
+section of Better Future was a checkerboard. Every second check
+glowed. I had never appreciated how pretty this was, until I was
+pushed along beneath it on a steel gurney. &ldquo;These lights are
+really neat,&rdquo; I said.</p>
 <p>&ldquo;Can we get
-him on the gas?&rdquo; Cassandra Cautery asked. There were four
-people walking alongside my gurney, none of whom I had seen before.
-&ldquo;We have a time pressure here.&rdquo;</p>
+him on the gas?&rdquo; This was Cassandra Cautery. She was walking
+alongside my gurney, accumulating lab assistants and other people I
+had never seen before. &ldquo;We have a time pressure.&rdquo;</p>
 <p>&ldquo;I&rsquo;ll
 find out.&rdquo;</p>
 <p>&ldquo;Thank you.&rdquo;
-She glanced down at me. &ldquo;Almost there, Charlie. The operating
-theater is standing by.&rdquo;</p>
+She glanced down at me. &ldquo;Almost there, Charlie.&rdquo;</p>
 <p>&ldquo;And Lola is
-meeting us there?&rdquo;</p>
-<p>&ldquo;Yes,&rdquo;
-she said.</p>
+meeting us at the operating theater?&rdquo;</p>
+<p>&ldquo;Yes.&rdquo;</p>
 <p>&ldquo;Are you
-sure?&rdquo; I was feeling light-headed, and I hadn&rsquo;t even had
-any drugs. It was the imminent prospect of parts. &ldquo;You really
-mean that?&rdquo;</p>
+sure?&rdquo; I was feeling light-headed. I hadn&rsquo;t had any
+drugs. It was the imminent prospect of parts. &ldquo;You really mean
+that?&rdquo;</p>
 <p>&ldquo;Charlie,&rdquo;
 she said, &ldquo;in a short while, you are going to be the fastest,
 strongest man on the planet. I wouldn&rsquo;t lie to you.&rdquo;</p>
@@ -30,13 +28,16 @@ A man raised a mask. &ldquo;Head forward, Dr. Neumann.&rdquo;</p>
 The mask was strapped around my face. I heard a hiss. &ldquo;What
 about Lola?&rdquo;</p>
 <p>&ldquo;She&rsquo;ll
-be here,&rdquo; said Cassandra Cautery. &ldquo;I swear, on my
-sister&rsquo;s life.&rdquo;</p>
+be here to hold your hand,&rdquo; said Cassandra Cautery. &ldquo;Uh.
+Bad choice of words. But she&rsquo;ll be here.&rdquo;</p>
+<p>&ldquo;Promise?&rdquo;</p>
+<p>&ldquo;I swear on
+my sister&rsquo;s life.&rdquo;</p>
 <p>We passed through a
-doorway, and the checkerboard glow-squares were replaced with harsh
+doorway. The soft checkerboard glow-squares were replaced with harsh
 surgical lights. I raised my head and saw a lot of people in green
-surgical gowns. They looked very fuzzy, though. Fuzzy and warm. I lay
-back, because my head was getting heavy.</p>
+surgical gowns. They looked very fuzzy. Fuzzy and warm. I lay back,
+because my head was getting heavy.</p>
 <p>We stopped moving.
 I closed my eyes. Someone squeezed my hand. I felt this from a
 distance, as if it wasn&rsquo;t my hand at all. &ldquo;Charlie?&rdquo;
