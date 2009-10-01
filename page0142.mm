b4541b59b307271b5751830d579f7d6b26b3b999
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-01 07:12:15 +0000

    Page update: 2 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@530 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0142.mm
+++ page0142.mm
@@ -1,199 +1,63 @@
 <p>Page 142.</p>
-<p>&ldquo;The new
-functionality required more power,&rdquo; she said. &ldquo;We had to
-move beyond the pocket reactor.&rdquo;</p>
-<p>I could see that
-now. &lt;&lt;Different shape.&gt;&gt; &ldquo;So where&rsquo;s the
-reactor?&rdquo;</p>
-<p>&ldquo;In the
-torso.&rdquo; &lt;&lt; She touched this. &gt;&gt; &ldquo;This made a
-lot of sense to all our people. You have a single supply around the
-abdomen region which can feed power to any and all components.&rdquo;</p>
-<p>&ldquo;That means
-they&rsquo;re not modular. They&rsquo;re dependent on a single
-source.&rdquo;</p>
+<p>&ldquo;The Threes
+draw more power,&rdquo; said Cassandra Cautery. &ldquo;We had to
+replace the pocket reactor. Which, frankly, was not very well
+shielded. We were a little surprised you didn&rsquo;t go Three Mile
+Island, what with all the leaping and kicking of cars.&rdquo;</p>
+<p>I ran my fingers
+down the the Contours. The calves were definitely slimmer. &ldquo;What
+did you use?&rdquo;</p>
+<p>&ldquo;A bigger
+reactor.&rdquo; She wandered away. I exhaled. Having her hovering was
+making me tense.</p>
+<p>&ldquo;It doesn&rsquo;t
+look bigger,&rdquo; I said. &ldquo;It looks&mdash;&rdquo;</p>
+<p>&ldquo;Which we put
+in an abdomen unit.&rdquo; I turned. She was standing beside a low
+shelf, on which sat a steel object about the size of vaccuum cleaner.
+On one side was stamped the international warning symbol for
+radiation. &ldquo;This baby provides juice not just for the legs but
+any other components you care to hook in.&rdquo;</p>
+<p>&ldquo;But... but
+then the legs aren&rsquo;t modular. They&rsquo;re not
+self-sufficient.&rdquo;</p>
+<p>&ldquo;I thought
+you&rsquo;d be pleased,&rdquo; she said. &ldquo;Our people thought
+this was a real step forward.&rdquo;</p>
+<p>&ldquo;No! It&rsquo;s
+not!&rdquo; I wasn&rsquo;t sure why I was so outraged. This wasn&rsquo;t
+a terrible design. It just felt wrong.</p>
 <p>&ldquo;Well, I
 don&rsquo;t know what to tell you, Charlie. This is the direction the
-team took without you.&rdquo;</p>
+team took after you ran off.&rdquo;</p>
 <p>I chewed my lip.
-&ldquo;So... to wear the legs I also need a torso unit?&rdquo;</p>
-<p>&ldquo;Right.&rdquo;
-She hesitated. &ldquo;And actually, the torso unit requires a spine
-upgrade.&rdquo;</p>
-<p>I looked at her.</p>
+&ldquo;So... to wear the legs I need the abdomen unit?&rdquo;</p>
+<p>&ldquo;Right. Also,
+the abdomen requires a spine upgrade.&rdquo;</p>
+<p>&ldquo;What?&rdquo;</p>
 <p>&ldquo;It weighs
-like eight hundred kilograms,&rdquo; she said. &ldquo;Without that,
-it&rsquo;d fall right out of you.&rdquo;</p>
-<p>&ldquo;Hmm,&rdquo;
-I said.</p>
+like eight hundred kilograms. Without some spine work, it&rsquo;d
+fall right out of you.&rdquo;</p>
+<p>I eyed the abdomen.</p>
 <p>&ldquo;And if we&rsquo;re
 doing the spine... well...&rdquo;</p>
-<p>&ldquo;What?&rdquo;</p>
-<p>She held up her
-hands. &ldquo;You can just do the spine. It&rsquo;s totally up to
-you. It&rsquo;ll just push a little on your lungs and stomach, is
-all.&rdquo;</p>
+<p>&ldquo;<em>What?</em>&rdquo;</p>
+<p>&ldquo;You can just
+do the spine. It&rsquo;s just...&rdquo; She shrugged. &ldquo;Well,
+it&rsquo;ll push on your internal organs some.&rdquo;</p>
 <p>&ldquo;That doesn&rsquo;t
 sound good.&rdquo;</p>
-<p>&ldquo;It&rsquo;s
-workable. I&rsquo;m not sure it&rsquo;s comfortable. But you can do
-it.&rdquo; She bit her lip. &ldquo;The alternative, of course, is&mdash;&rdquo;</p>
-<p>&ldquo;We do the
-entire torso.&rdquo;</p>
-<p>Her eyes lit up.
-&ldquo;Exactly. Then you have no interface issues with the arms.
-Otherwise... well, I&rsquo;m telling you things you already know. But
-the more places you try to connect meat to metal, the more chance
-something will go wrong.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>the hard part is
-always matching old and new. 
-</p>
-<p></p>
-<p></p>
-<p>&ldquo;What does
-that mean?&rdquo;</p>
-<p>&ldquo;</p>
-<p></p>
-<p>it doesn&rsquo;t
-leave much room for your stomach.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;It&rsquo;s
-totally up to you.&rdquo;</p>
-<p>I said, &ldquo;So...
-I could... have... new parts.&rdquo;</p>
-<p>&ldquo;Yes.&rdquo;</p>
-<p>&ldquo;Such as...&rdquo;</p>
-<p>Cassandra Cautery
-hesitated. Then she nodded. &ldquo;All right. Let&rsquo;s show you.&rdquo;</p>
-<p></p>
-<p>But we won&rsquo;t
-have any trouble fitting you up.&rdquo;</p>
-<p>So what we&rsquo;ll
-do is stop by and get you into a new set of legs.&rdquo;</p>
-<p></p>
-<p>Suffice to say,
-Charlie, we have plenty of parts. 
-</p>
-<p></p>
-<p></p>
-<p>She was walking
-alongside me as a lab assistant wheeled me down a corridor. For such
-a short person, she could really motor. &ldquo;We ran out of space.
-Not all the new products are completely workable, of course, but...
-well, 
-</p>
-<p></p>
-<p></p>
-<p>&ldquo;We can get
-you into a new set of legs, a hand, right now.&rdquo; She navigated a
-turn without a backward glance. &ldquo;Or...&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;Where&rsquo;s
-Lola?&rdquo; I felt cold. &ldquo;Did I... where&rsquo;s Lola?&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p> arms.&rdquo;</p>
-<p>&ldquo;Exactly.&rdquo;
-She nodded. &ldquo;Arms.&rdquo; She opened her mouth, as if to say
-something else, then closed it.</p>
-<p>I took a breath.
-&ldquo;It&rsquo;s all so sudden.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Finish
-with either CC imploring Charlie, you&rsquo;re our only hope, you
-need to be top of your game to compete with Carl, or: Show me. I
-think Show Me, save the former for the pre-req conversation. And put
-the crouch there too.&gt;&gt;</p>
-<p></p>
-<p>, because I was
-worried that if I did, the delicate porcelien of the world would
-shatter and fall to pieces.</p>
-<p>Cassandra Cautery
-crouched in front of me. This seemed unnecessary to me, because it
-reversed our height differential without significantly reducing it. 
-</p>
-<p></p>
-<p>And not just arms.
-We have a pelvis. A lower torso that regulates energy 
-</p>
-<p></p>
-<p>&ldquo;Arms.
-Pelvis. Torso. We have a range.&rdquo;</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Mention
-sheilding. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Actually,
-the pre-reqs should require it. That&rsquo;d be a good page for
-tomorrow. The pre-reqs. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; This week
-I think they maybe prepare for Carl, but he doesn&rsquo;t come. So
-then they bulk up a little more. &gt;&gt;</p>
-<p>&lt;&lt; The big
-story here is Charlie getting new parts. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p>A low, keening
-noise came out of Lola. 
-</p>
-<p></p>
-<p></p>
-<p>&ldquo;Think of
-Better Future as a machine with eight thousand employees. An
-artificial intelligence programmed to shift resources around to
-generate wealth and power.&rdquo;</p>
-<p></p>
-</BODY>
-</HTML>
+<p>&ldquo;I&rsquo;m
+not sure it&rsquo;s comfortable. But you can probably live with it.
+It&rsquo;s totally your choice. I will say, though, that if you do
+replace the chest, it gives you a much better spread of arm options.&rdquo;</p>
+<p>I was silent. This
+still felt wrong. I abruptly realized why: these were not my parts. I
+had not built them. They were deviations from my designs. It was like
+coming home to the woman I loved, only to find she had changed in&mdash;</p>
+<p>The shutters
+rattled up. &ldquo;<em>Where&rsquo;s Lola?</em>&rdquo;</p>
+<p>&ldquo;She&rsquo;s
+coming.&rdquo; Cassandra Cautery looked at the doorway, as if Lola
+might suddenly walk in. She didn&rsquo;t. &ldquo;Charlie,
+unfortunately, the clock is ticking. Do you want the parts? Or not?&rdquo;</p>
---
+++ page0143.mm
@@ -0,0 +1,48 @@
+<p>Page 143.</p>
+<p>The ceiling of this
+section of Better Future was a checkerboard, and every second check
+glowed. I had never really appreciated how pretty this was, until I
+was pushed along beneath it on a steel guerney. &ldquo;These lights
+are really neat,&rdquo; I said.</p>
+<p>&ldquo;Can we get
+him on the gas?&rdquo; Cassandra Cautery asked. There were four
+people walking alongside my guerney, none of whom I had seen before.
+&ldquo;We have a time pressure here.&rdquo;</p>
+<p>&ldquo;I&rsquo;ll
+find out.&rdquo;</p>
+<p>&ldquo;Thank you.&rdquo;
+She glanced down at me. &ldquo;Almost there, Charlie. The operating
+theater is standing by.&rdquo;</p>
+<p>&ldquo;And Lola is
+meeting us there?&rdquo;</p>
+<p>&ldquo;Yes,&rdquo;
+she said.</p>
+<p>&ldquo;Are you
+sure?&rdquo; I was feeling light-headed, and I hadn&rsquo;t even had
+any drugs. It was the imminent prospect of parts. &ldquo;You really
+mean that?&rdquo;</p>
+<p>&ldquo;Charlie,&rdquo;
+she said, &ldquo;in a short while, you are going to be the fastest,
+strongest man on the planet. I wouldn&rsquo;t lie to you.&rdquo;</p>
+<p>&ldquo;Gas here.&rdquo;
+A man raised a mask. &ldquo;Head forward, Dr. Neumann.&rdquo;</p>
+<p>&ldquo;But Lola.&rdquo;
+The mask was strapped around my face. I heard a hiss. &ldquo;What
+about Lola?&rdquo;</p>
+<p>&ldquo;She&rsquo;ll
+be here,&rdquo; said Cassandra Cautery. &ldquo;I swear, on my
+sister&rsquo;s life.&rdquo;</p>
+<p>We passed through a
+doorway, and the checkerboard glow-squares were replaced with harsh
+surgical lights. I raised my head and saw a lot of people in green
+surgical gowns. They looked very fuzzy, though. Fuzzy and warm. I lay
+back, because my head was getting heavy.</p>
+<p>We stopped moving.
+I closed my eyes. Someone squeezed my hand. I felt this from a
+distance, as if it wasn&rsquo;t my hand at all. &ldquo;Charlie?&rdquo;
+I didn&rsquo;t feel like answering. &ldquo;Okay,&rdquo; said
+Cassandra Cautery. &ldquo;Let&rsquo;s take it all off.&rdquo;</p>
+<p>...&gt;&gt;&ldquo;How
+much are we doing?&rdquo;</p>
+<p>&ldquo;Okay,&rdquo;
+she said. &ldquo;Let&rsquo;s take it all off.&rdquo;</p>
