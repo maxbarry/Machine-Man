f3165fe37d1e4567772bb78b64296e5c6c5d9b1f
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-04 01:16:53 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@535 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0144.mm
+++ page0144.mm
@@ -1,126 +1,32 @@
 <p>Page 144.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>...</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;Of course
-they are, Charlie.&rdquo; Cassandra Cautery was walking alongside. I
-had never been so close to her face.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>you can do it.&rdquo;
-She bit her lip. &ldquo;The alternative, of course, is&mdash;&rdquo;</p>
-<p>&ldquo;We do the
-entire torso.&rdquo;</p>
-<p>Her eyes lit up.
-&ldquo;Exactly. Then you have no interface issues with the arms.
-Otherwise... well, I&rsquo;m telling you things you already know. But
-the more places you try to connect meat to metal, the more chance
-something will go wrong.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>the hard part is
-always matching old and new. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Finish
-with either CC imploring Charlie, you&rsquo;re our only hope, you
-need to be top of your game to compete with Carl, or: Show me. I
-think Show Me, save the former for the Friday after the pre-req
-conversation. And put the crouch there too.&gt;&gt;</p>
-<p></p>
-<p>, because I was
-worried that if I did, the delicate porcelien of the world would
-shatter and fall to pieces.</p>
-<p>Cassandra Cautery
-crouched in front of me. This seemed unnecessary to me, because it
-reversed our height differential without significantly reducing it. 
-</p>
-<p></p>
-<p>And not just arms.
-We have a pelvis. A lower torso that regulates energy 
-</p>
-<p></p>
-<p>&ldquo;Arms.
-Pelvis. Torso. We have a range.&rdquo;</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Mention
-sheilding. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Actually,
-the pre-reqs should require it. That&rsquo;d be a good page for
-tomorrow. The pre-reqs. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; This week
-I think they maybe prepare for Carl, but he doesn&rsquo;t come. So
-then they bulk up a little more. &gt;&gt;</p>
-<p>&lt;&lt; The big
-story here is Charlie getting new parts. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p>A low, keening
-noise came out of Lola. 
-</p>
-<p></p>
-<p></p>
-<p>&ldquo;Think of
-Better Future as a machine with eight thousand employees. An
-artificial intelligence programmed to shift resources around to
-generate wealth and power.&rdquo;</p>
-<p></p>
-</BODY>
-</HTML>
+<h5>CHAPTER 
+THIRTY</h5>
+<p>I became aware of
+smoke. I felt alarmed, in some small, sectioned-off part of my body.
+Smoke is rarely good. In my line of work, it usually signals
+stupidity: failure to adequately anticipate logical consquences, or
+check a tolerance, or convert from imperial. This smoke was curling
+along the ceiling above me. I wasn&rsquo;t sure what it signaled. I
+wasn&rsquo;t sure where I was. I just knew there was smoke.</p>
+<p><em>Get up</em>,
+said the part of me that was worried about the smoke. But some other
+part said: <em>Lie here a little longer</em>,
+and that felt more persuasive. I was doped. I was relaxed. I would
+never feel this peaceful again, not without chemical assistance. I
+had to savor it.</p>
+<p>Something
+was slooshing. It sounded soothing. Something else went: <em>Sssssss</em>,
+like an old man easing into a favorite chair. I felt safe. It was
+like being in the womb. Except for the smoke, which was becoming
+quite thick. But I could ignore that. I closed my eyes.</p>
+<p>Someone
+coughed. I opened my eyes again, because that was a disconcerting
+sound to hear in a womb. Especially this particular cough, which
+sounded wet. I waited, hoping it would go away, but it didn&rsquo;t.
+<em>Cark. Cark</em>. It was a
+perfunctory cough. Like the owner didn&rsquo;t expect it to do much
+good.</p>
+<p>This I
+couldn&rsquo;t ignore. I felt the dream-state dissolving and felt
+sad, but also better, because this was me, coming back. My thoughts
+began to organize themselves. I raised my head.</p>
---
+++ page0145.mm
@@ -0,0 +1,136 @@
+<p>Page 145.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>...</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;Of course
+they are, Charlie.&rdquo; Cassandra Cautery was walking alongside. I
+had never been so close to her face.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>you can do it.&rdquo;
+She bit her lip. &ldquo;The alternative, of course, is&mdash;&rdquo;</p>
+<p>&ldquo;We do the
+entire torso.&rdquo;</p>
+<p>Her eyes lit up.
+&ldquo;Exactly. Then you have no interface issues with the arms.
+Otherwise... well, I&rsquo;m telling you things you already know. But
+the more places you try to connect meat to metal, the more chance
+something will go wrong.&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>the hard part is
+always matching old and new. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Finish
+with either CC imploring Charlie, you&rsquo;re our only hope, you
+need to be top of your game to compete with Carl, or: Show me. I
+think Show Me, save the former for the Friday after the pre-req
+conversation. And put the crouch there too.&gt;&gt;</p>
+<p></p>
+<p>, because I was
+worried that if I did, the delicate porcelien of the world would
+shatter and fall to pieces.</p>
+<p>Cassandra Cautery
+crouched in front of me. This seemed unnecessary to me, because it
+reversed our height differential without significantly reducing it. 
+</p>
+<p></p>
+<p>And not just arms.
+We have a pelvis. A lower torso that regulates energy 
+</p>
+<p></p>
+<p>&ldquo;Arms.
+Pelvis. Torso. We have a range.&rdquo;</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Mention
+sheilding. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Actually,
+the pre-reqs should require it. That&rsquo;d be a good page for
+tomorrow. The pre-reqs. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>Page
+NOTES.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; This week
+I think they maybe prepare for Carl, but he doesn&rsquo;t come. So
+then they bulk up a little more. &gt;&gt;</p>
+<p>&lt;&lt; The big
+story here is Charlie getting new parts. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; soft
+sciences: Charlie asks Lola about id. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p><em>The Id comprises the unorganised
+part of the personality structure that contains the basic drives. It
+acts according to the pleasure principle, seeking to avoid pain or
+unpleasure. </em>
+</p>
+<p></p>
+<p></p>
+<p>A low, keening
+noise came out of Lola. 
+</p>
+<p></p>
+<p></p>
+<p>&ldquo;Think of
+Better Future as a machine with eight thousand employees. An
+artificial intelligence programmed to shift resources around to
+generate wealth and power.&rdquo;</p>
+<p></p>
+</BODY>
+</HTML>
