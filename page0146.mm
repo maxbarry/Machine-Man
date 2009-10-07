8a8fb7bf2225514f48237ba45b8f92f37eab669a
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-07 06:24:03 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@541 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0146.mm
+++ page0146.mm
@@ -1,46 +1,46 @@
 <p>Page 146.</p>
 <p>Panic swept through
-me. It was not a good time to panic. It was really a time for some of
-that clinical detachment. But seeing tides of blood gushing from that
-gaping canyon in my flesh overrode my objectivitiy. My brain
-screamed, <em>That&rsquo;s fatal, you&rsquo;re going to lose
-consciousness</em>, and I couldn&rsquo;t shut it out. I couldn&rsquo;t
-think. I lifted my right arm&mdash;I still had a right arm&mdash;without
-any real plan for what to do with it and saw it was bright red.
-<em>They&rsquo;ve skinned it!</em> shrieked my brain. It looked like
-raw tendon and muscle. But it wasn&rsquo;t. It was just soaked in
-blood. I got my head up and confirmed that I was lying in a red bath.
-Blood was running off the sides of the table and onto the floor. I
-was a water feature. And possibly this looked worse than it was
-because of the added volume from the water falling from the ceiling,
-but still: there was more blood than should have been possible. I
-should be dead.</p>
-<p>&lt;&lt; Little
-more panic here; let&rsquo;s not get under control immediately. &gt;&gt;</p>
-<p>Looking down, I
-discovered some other pertinent facts: my legs weren&rsquo;t right.
-By which I mean, I had some. I was lying beneath layers of green
-surgical cloth, saturated from water or blood or both, but they
-couldn&rsquo;t disguise the fact that I was some kind of shape. I was
-Contoured. The other important discovery was that I was connected to
-things. Half a dozen plastic tubes ran from a gap in the cloth I
-couldn&rsquo;t quite see through to bags of fluid on stands and other
-equipment behind my head. Fluid was moving through these tubes: dark
-and red.</p>
-<p>I began to reach
-conclusions. The first was that these tubes were keeping me alive.
-They were replenishing the blood I was losing out my shoulder. The
-second conclusion was that this was temporary. Even as I watched, a
-tube ran dry. Almost immediately, I began to feel woozy. I didn&rsquo;t
-want to leap ahead to conclusions of cause and effect, because I had
-plenty of reasons to feel woozy right now, but staunching this wound
-instantly became my top priority. I grabbed my left bicep with my
-right hand. It felt like a piece of meat: I had no feeling in it at
-all. It was not a nice sensation. I tried to mash the wound closed.
-Blood squished around the wound. It was an improvement. But it was
-not a solution.</p>
-<p>&ldquo;Help,&rdquo;
-I said, just in case. That person who had been coughing: I could
-really use them about now. But apparently this was one of those
-situations where you have to help yourself. I looked around,
-desperate, and my eye fell on the electric saw.</p>
+me. It was not a good time to panic. It was time for some of that
+clinical detachment. But tides of blood were gushing from a gaping
+canyon in my flesh and my brain screamed, <em>That&rsquo;s fatal,
+you&rsquo;re going to lose consciousness any second now</em>. I lifted
+my right arm&mdash;I still had a right arm&mdash;without any real
+plan for what to do with it and it was bright red. <em>They&rsquo;ve
+skinned it!</em> my brain shrieked. And it did look like raw tendon
+and muscle, but it wasn&rsquo;t, it was only soaked in blood. I was
+lying in a shallow red bath. Blood ran over the sides to spatter on
+the tiles below, like a water feature. And possibly this looked worse
+because of the added volume of water falling from the ceiling, but
+still: there was more blood than should have been possible, a lot
+more. I should already be dead.</p>
+<p>I looked down. My
+legs weren&rsquo;t right. By which I mean, I had some. I was lying
+beneath saturated green surgical cloth, which rose and fell in a
+definite legs-and-torso shape. Contoured. Also, tubes ran from
+between layers of the cloth to devices near my head: a large black
+box, four different drips. The box was making the slooshing sound I&rsquo;d
+heard earlier. With every sloosh, two of the tubes bucked a little,
+as dark fluid moved through them.</p>
+<p>The second I
+decided this setup was keeping me alive, the box stopped slooshing
+and started sounding like an enthusiastic six-year-old chasing down
+the last of his milkshake. As I watched in horror, one of the tubes
+began to lighten, starting at the end that disappeared into the box
+and running quickly toward my body.</p>
+<p>I grabbed at my
+poor severed arm and began to mash it back onto my shoulder. Now, I&rsquo;m
+not particularly squeamish, but seriously, this was disgusting. I had
+no feeling there; it was like handling a steak. And the sounds&mdash;it
+wasn&rsquo;t the squishing that got me, the sucking or squelching; it
+was the rasping. I almost couldn&rsquo;t do it. But I didn&rsquo;t
+want to die. So I did.</p>
+<p>Blood continued to
+squirt from the wound. I couldn&rsquo;t seal it properly; of course I
+couldn&rsquo;t. &ldquo;Help,&rdquo; I said, just in case whoever I&rsquo;d
+heard coughing wanted to step in. But no-one came. I realized this
+was it: I was actually, genuinely going to expire unless I staunched
+this wound in the next thirty seconds, and felt terror, and
+dizziness, and regret. I felt cheated and angry at someone or
+something. Biology, perhaps. I looked around, hoping there was
+something useful nearby I hadn&rsquo;t noticed earlier, like maybe a
+surgeon, and my eyes fell again on the saw.</p>
---
+++ page0148.mm
@@ -0,0 +1,209 @@
+<p>Page 148.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>One of the tubes
+lightened, then ran dry. 
+</p>
+<p></p>
+<p>Immediately, I
+began to feel woozy. I didn&rsquo;t want to leap ahead to conclusions
+of cause and effect, because I had plenty of reasons to feel woozy
+right now, but staunching this wound instantly became my top
+priority. 
+</p>
+<p></p>
+<p>I began to reach
+conclusions. The first was that these tubes were keeping me alive.
+They were replenishing the blood I was losing out my shoulder. The
+second conclusion was that this was temporary. Even as I watched, a
+tube ran dry. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>Looking down, I
+discovered some other pertinent facts: my legs weren&rsquo;t right.
+By which I mean, I had some. I was lying beneath layers of green
+surgical cloth, saturated from water or blood or both, but they
+couldn&rsquo;t disguise the fact that I was some kind of shape. I was
+Contoured. The other important discovery was that I was connected to
+things. Half a dozen plastic tubes ran from a gap in the cloth I
+couldn&rsquo;t quite see through to bags of fluid on stands and other
+equipment behind my head. Fluid was moving through these tubes: dark
+and red.</p>
+<p></p>
+<p></p>
+<p>Two of these tubes
+seemed 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>not flat. They were
+Contoured. 
+</p>
+<p></p>
+<p>covering a definite
+legs-and-torso 
+</p>
+<p></p>
+<p>was pushing them
+into a definite legs-and-torso outline. Contoured. 
+</p>
+<p></p>
+<p>they were making
+some kind of shape</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Little
+more panic here; let&rsquo;s not get under control immediately. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I was lying in my
+own blood: I could smell it. I raised I had been bleeding for a
+while. I should</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I really needed
+some clinical detachment. 
+</p>
+<p></p>
+<p>The amount of blood
+coming out of my shoulder suggested I would be dead very soon. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>It had not
+completed, obviously: I still had fingers. But it had started.
+Someone had begun to sever my left arm from my shoulder. They had
+almost made it all the way through. From the gash they had left,
+blood pumped enthusiastically. I was lying in a pool of it.</p>
+<p></p>
+<p></p>
+<p></p>
+<p>Panic jolted
+through me. This was the kind of wound that killed you, very quickly.
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p> It was a method
+that had worked well enough in the past. This time, though, I
+collapsed halfway through. I looked at my left arm, to see what the
+problem was, and saw: 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>An electric saw,
+its blade dark with what I could only assume was blood. 
+</p>
+<p></p>
+<p>...</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Finish
+with either CC imploring Charlie, you&rsquo;re our only hope, you
+need to be top of your game to compete with Carl, or: Show me. I
+think Show Me, save the former for the Friday after the pre-req
+conversation. And put the crouch there too.&gt;&gt;</p>
+<p></p>
+<p>, because I was
+worried that if I did, the delicate porcelien of the world would
+shatter and fall to pieces.</p>
+<p>Cassandra Cautery
+crouched in front of me. This seemed unnecessary to me, because it
+reversed our height differential without significantly reducing it. 
+</p>
+<p></p>
+<p>And not just arms.
+We have a pelvis. A lower torso that regulates energy 
+</p>
+<p></p>
+<p>&ldquo;Arms.
+Pelvis. Torso. We have a range.&rdquo;</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Mention
+sheilding. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Actually,
+the pre-reqs should require it. That&rsquo;d be a good page for
+tomorrow. The pre-reqs. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>Page
+NOTES.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; This week
+I think they maybe prepare for Carl, but he doesn&rsquo;t come. So
+then they bulk up a little more. &gt;&gt;</p>
+<p>&lt;&lt; The big
+story here is Charlie getting new parts. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; soft
+sciences: Charlie asks Lola about id. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p><em>The Id comprises the unorganised
+part of the personality structure that contains the basic drives. It
+acts according to the pleasure principle, seeking to avoid pain or
+unpleasure. </em>
+</p>
+<p></p>
+<p></p>
+<p>A low, keening
+noise came out of Lola. 
+</p>
+<p></p>
+<p></p>
+<p>&ldquo;Think of
+Better Future as a machine with eight thousand employees. An
+artificial intelligence programmed to shift resources around to
+generate wealth and power.&rdquo;</p>
+<p></p>
+</BODY>
+</HTML>
