3e0ff4073e38ade1d3fd84d75d6cb95dc7cb1db4
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-07 06:36:49 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@542 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0146.mm
+++ page0146.mm
@@ -11,36 +11,34 @@ and muscle, but it wasn&rsquo;t, it was only soaked in blood. I was
 lying in a shallow red bath. Blood ran over the sides to spatter on
 the tiles below, like a water feature. And possibly this looked worse
 because of the added volume of water falling from the ceiling, but
-still: there was more blood than should have been possible, a lot
-more. I should already be dead.</p>
+still: there was way too much blood here. I should already be dead.</p>
 <p>I looked down. My
 legs weren&rsquo;t right. By which I mean, I had some. I was lying
-beneath saturated green surgical cloth, which rose and fell in a
-definite legs-and-torso shape. Contoured. Also, tubes ran from
-between layers of the cloth to devices near my head: a large black
-box, four different drips. The box was making the slooshing sound I&rsquo;d
-heard earlier. With every sloosh, two of the tubes bucked a little,
-as dark fluid moved through them.</p>
+beneath saturated green surgical cloth, and this was, well,
+contoured, in a definite legs-and-torso shape. Tubes ran from between
+layers in the cloth to devices nearby: a large black box on a
+trolley, two different drips. The box was making the slooshing sound
+I&rsquo;d heard earlier. With every sloosh, the tubes connected to it
+bucked a little, dark fluid moving through them.</p>
 <p>The second I
-decided this setup was keeping me alive, the box stopped slooshing
-and started sounding like an enthusiastic six-year-old chasing down
-the last of his milkshake. As I watched in horror, one of the tubes
-began to lighten, starting at the end that disappeared into the box
-and running quickly toward my body.</p>
+decided this was keeping me alive, the box stopped slooshing and
+started sounding like an enthusiastic six-year-old chasing the last
+of his milkshake. As I watched, beige spots appeared around where the
+tube was connected to the box, and rapidly elsewhere along it, racing
+toward my body.</p>
 <p>I grabbed at my
-poor severed arm and began to mash it back onto my shoulder. Now, I&rsquo;m
-not particularly squeamish, but seriously, this was disgusting. I had
-no feeling there; it was like handling a steak. And the sounds&mdash;it
-wasn&rsquo;t the squishing that got me, the sucking or squelching; it
+mostly-severed arm and tried to mash it back on. Now, I&rsquo;m not
+particularly squeamish, but seriously, this was disgusting. I had no
+feeling there; it was like handling a steak. And the sounds&mdash;it
+wasn&rsquo;t the squishing that got me, the sucking or squelching. It
 was the rasping. I almost couldn&rsquo;t do it. But I didn&rsquo;t
 want to die. So I did.</p>
-<p>Blood continued to
-squirt from the wound. I couldn&rsquo;t seal it properly; of course I
-couldn&rsquo;t. &ldquo;Help,&rdquo; I said, just in case whoever I&rsquo;d
-heard coughing wanted to step in. But no-one came. I realized this
-was it: I was actually, genuinely going to expire unless I staunched
-this wound in the next thirty seconds, and felt terror, and
-dizziness, and regret. I felt cheated and angry at someone or
-something. Biology, perhaps. I looked around, hoping there was
-something useful nearby I hadn&rsquo;t noticed earlier, like maybe a
-surgeon, and my eyes fell again on the saw.</p>
+<p>Blood squirted from
+the wound. I couldn&rsquo;t seal it properly; of course I couldn&rsquo;t.
+&ldquo;Help,&rdquo; I said, just in case whoever I&rsquo;d heard
+coughing before wanted to step in. But no-one came. This was it, I
+realized: I was actually about to cease living. I felt terror and
+dizziness and regret. I felt cheated and angry at someone or
+something. I looked around one last time, in case there was something
+useful nearby I hadn&rsquo;t noticed earlier, like maybe a surgeon,
+and my eyes fell again on the electric saw.</p>
