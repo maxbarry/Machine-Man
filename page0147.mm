5cf7d421b8678213a5eb162256e64c69673f3598
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-08 06:26:14 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@545 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0147.mm
+++ page0147.mm
@@ -7,13 +7,11 @@ grip, I had to hold the cord in my teeth. Pull, bite, move hand,
 pull. It was a real trial. Then I got the thing up and thumbed on the
 power and wished I hadn&rsquo;t been so enterprising. If the cord had
 been a little further away, beyond my reach, would that have been
-such a bad way to die? No-one would have blamed me. I would have
-tried.</p>
+such a bad way to die? I would have been blameless. Even noble.</p>
 <p>One time at a mall,
-I saw a guy demonstrating an electric carving knife. It was white
-with steel blades, and he had a steaming roast chicken on a plate. As
-I walked by, he was demonstrating to a middle-aged couple how the
-knife whirred easily through the meat, slicing off delicious strips
-with a noise like: <em>vrrrreeeee</em>.</p>
+I saw a guy demonstrating an electric carving knife. He had a
+steaming roast chicken on a plate, and as I walked by, he showed a
+middle-aged couple how easily the knife whirred through the meat,
+slicing off delicious strips with a noise like: <em>vrrrreeeee</em>.</p>
 <p>For some reason I
 expected this to sound different.</p>
