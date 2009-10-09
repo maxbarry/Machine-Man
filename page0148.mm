c9a12eccb620e304ced74afd544220610a85ab77
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-09 06:02:02 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@551 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0148.mm
+++ page0148.mm
@@ -1,35 +1,35 @@
 <p>Page 148.</p>
 <p>With my excess arm
-out of the way, I was able to clamp shut the jetting artery. I&rsquo;ll
-spare you the specifics. Let&rsquo;s just say it was a temporary
-solution involving my fingers. I then spent the next forty minutes
-getting off the table. It took this long because I had to do
-everything with my mouth. First I pulled back a few layers of
-surgical cloth with my teeth. At this point I gained a much better
-appreciation for why Better Future test subjects were having
-psychological problems. Because all of a sudden, despite everything
-I&rsquo;d ever thought, all I wanted to do was let go of my artery
-and die.</p>
+out of the way, I was able to clamp shut the artery. I&rsquo;ll spare
+you the specifics. Let&rsquo;s just say it was a temporary solution
+involving my fingers. I then spent the next forty minutes getting off
+the table. It took this long because I had to do everything with my
+mouth. First I pulled back the surgical cloth with my teeth. At this
+point I gained a better appreciation of why Better Future test
+subjects were having psychological problems. Because suddenly,
+despite everything I&rsquo;d ever thought, I wanted to let go of my
+artery and die.</p>
 <p>My body ended at
 the sternum. I couldn&rsquo;t tell exactly where. I was wrapped in a
-flexible plastic-silicon bandage: all I knew was that above this was
-flesh and below was metal. I guess it was the abdomen unit I&rsquo;d
-inspected earlier, but it was encased in silver-black titanium.
-Etched into the metal where my belly button should be was a circular
-Better Future logo. My hips were multi-axis servo-magnetics. My legs
-were the black Contour Threes.</p>
+flexible plastic-silicon bandage: above this was flesh and below was
+metal. The abdomen unit I&rsquo;d inspected earlier was probably in
+there, but if so it was encased in silver-black titanium. Etched into
+the metal where my belly button should be was a circular design I
+recognized even upside down as a Better Future logo. My hips were
+multi-axis servo-magnetics. My legs were black Contour Threes.</p>
 <p>I was going to
-vomit. Some part of me that had been silent for a long time flexed in
-violent mental rejection, wailed that <em>this should not be</em>. I
-closed my eyes for a while and thought about Lola until it passed.</p>
+vomit. Some part of me that had been silent a long time shrieked out
+a violent mental rejection: <em>this should not be</em>. I closed my
+eyes and thought about Lola.</p>
 <p>I was weakening. I
 had to forget about who or what I was and concentrate on staying
-alive. That meant getting off this table. Eventually I spotted it: a
-thick plastic handle jutting out of my titanium side that didn&rsquo;t
-look like it belonged. What it looked like was a damper: a safety
-switch that prevented the abdomen from powering up.</p>
-<p>I lay there until I
-was sure I had no better options. Then I released my shoulder,
-grabbed at the handle, and pulled. It put up a fight, but I got it.
-Something clicked. I heard a high-pitched whine, a deep hum. I felt
-warmth. And I began to cry, because now I was going to live.</p>
+alive. That meant getting off this table. I forced myself to look
+again and eventually I spotted it: a thick plastic handle jutting out
+of my metal side that didn&rsquo;t look like it belonged. What it
+looked like was a damper: a safety switch that prevented the abdomen
+from powering up.</p>
+<p>I waited until I
+was sure I had no better options. Then I let go of my artery, grabbed
+at the handle, and pulled. It put up a fight, but I got it. Something
+clicked. I heard a high-pitched whine, and simultaneously a deep hum.
+I felt warmth. And I began to cry, because now I was going to live.</p>
