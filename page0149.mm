c0c9868fb1fbe6555a2bce9e48ad9b65206705c7
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-11 23:10:04 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@552 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0149.mm
+++ page0149.mm
@@ -1,155 +1,23 @@
 <p>Page 149.</p>
 <h5>CHAPTER 
 THIRTY-ONE</h5>
-<p>One time in
-college, I tried to buy a pack of Doritos from a vending machine. I
-fed my bills into the slot, pressed C (firmly) then 4 (firmly), and
-waited. Nothing happened, so I pressed C-4 again (more firmly). Still
-nothing. I tried CANCEL, thinking to start over, and the machine
-whirred, the steel coil revolved, and the Doritos packet edged closer
-to the edge. But not close enough to fall.</p>
-<p>I cursed and kicked
-it, because I was 22 and there were people watching. I said, &ldquo;This
-<em>stupid</em> machine.&rdquo; It is a humiliating experience to be
-defeated by an inanimate object, especially one designed to do what
-you want.</p>
-<p>But as I stared at
-the dangling Doritos packet, a different way of looking at the
-situation occurred to me. In this scenario, the vending machine was
-intelligent and I was a machine. The vending machine, for reasons of
-its own&mdash;I don&rsquo;t think it&rsquo;s important to know the
-vending machine&rsquo;s motivation, but say it was bored&mdash;poked
-my buttons in order to elicite particular behaviors. This scenario
-made sense because if you replaced me with a different human being
-they would act the same way. There was nothing unique or
-unpredictable about me getting mad; an intelligent vending machine
-would know that&rsquo;s what humans do when you dangle snacks in
-front of them.</p>
-<p><A NAME="firstHeading1"></A>I
-spent the rest of the day trying to think why an objective observor
-should believe I was controlling the vending machine in this
-situation and not the other way around. And I couldn&rsquo;t. I had a
-brain and the vending machine didn&rsquo;t, but that was classic
-anthropocentrism:
-for all I knew, the vending machine&rsquo;s consciousness resided in
-a space I didn&rsquo;t understand; it might equally well argue that I
-couldn&rsquo;t have consciousness because I didn&rsquo;t have a coin
-slot. 
-</p>
-<p>Since then, I have
-increasingly believed that human beings are inherently predictable.
-Threaten us with harm and we run away. Warm us and we relax. Offer us
-six hundred dollars a week if we sit at a desk and answer phone
-calls, and we will sit at a desk and answer phone calls. We are more
-complex than vending machines, of course. But the distinction is of
-degrees, not difference. Give me infinite computing power and perfect
-knowledge of environmental variables, and I&rsquo;m pretty sure I can
-model consciousness. I&rsquo;m pretty sure that one day, not too far
-from now, we will have definitive proof that we are all vending
-machines.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Finish
-with either CC imploring Charlie, you&rsquo;re our only hope, you
-need to be top of your game to compete with Carl, or: Show me. I
-think Show Me, save the former for the Friday after the pre-req
-conversation. And put the crouch there too.&gt;&gt;</p>
-<p></p>
-<p>Cassandra Cautery
-crouched in front of me. This seemed unnecessary to me, because it
-reversed our height differential without significantly reducing it. 
-</p>
-<p></p>
-<p>And not just arms.
-We have a pelvis. A lower torso that regulates energy 
-</p>
-<p></p>
-<p>&lt;&lt; Mention
-sheilding. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Actually,
-the pre-reqs should require it. That&rsquo;d be a good page for
-tomorrow. The pre-reqs. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I cried weakly and
-uselessly, what I had done.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I clamped the
-artery again. But I didn&rsquo;t move. 
-</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; sternum?
-&gt;&gt;</p>
-<p>&lt;&lt; The
-coughing: a surgeon, dying in his biological body. &gt;&gt;</p>
-<p>&lt;&lt; Legs
-online: he feels them. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p>Maybe he plugs his
-arm, and he didn&rsquo;t really need to. There&rsquo;s not too much
-blood required by his body any more? Hmm, brain definitely needs it
-though. Maybe he can shut that off. Press a switch, vrrr, 
-</p>
-<p></p>
-<p>CARL TOOK LOLA!!</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; This week
-I think they maybe prepare for Carl, but he doesn&rsquo;t come. So
-then they bulk up a little more. &gt;&gt;</p>
-<p>&lt;&lt; The big
-story here is Charlie getting new parts. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p>A low, keening
-noise came out of Lola. 
-</p>
-<p></p>
-<p></p>
-<p>&ldquo;Think of
-Better Future as a machine with eight thousand employees. An
-artificial intelligence programmed to shift resources around to
-generate wealth and power.&rdquo;</p>
-<p></p>
-</BODY>
-</HTML>
+<p>I stopped feeling
+sorry for myself when I got off the table and saw the man on the
+tiles. He was sitting slumped against the far wall, his green
+surgical scrubs speckled dark down the front. His lips were very red.
+When he saw me, he blinked once, very slowly.</p>
+<p>I was seven feet
+tall, wearing titanium legs. I had a reactor in my abdomen. When I
+had swung my legs off the table, they had moved soundlessly, and when
+the hooves touched the floor some tiny part of them fired into the
+floor and locked with a click. That was new. When I turned, my torso
+swiveled in six ring-like sections.</p>
+<p>&ldquo;Are you
+okay?&rdquo; I said. &ldquo;What can I do?&rdquo; I already had a
+feeling the answer was <em>nothing</em>. If our positions were
+reversed, maybe this guy could help me: cut me open, find whichever
+internal organs were crushed or torn. Except I wasn&rsquo;t sure I
+still had much in the way of internal organs. Not biological ones, at
+least. If I was in this guy&rsquo;s position, I would need a
+mechanic. So maybe neither of us could help the other. All I knew was
+suddenly it didn&rsquo;t seem so bad to be metal.</p>
