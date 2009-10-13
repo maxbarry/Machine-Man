d25dde65fe2cc10b8ab4a28b89e64441fd4746de
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-13 01:37:31 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@555 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0150.mm
+++ page0150.mm
@@ -1,208 +1,35 @@
 <p>Page 150.</p>
-<p>I sucked in breath
-to call for help and coughed instead. The smoke was beginning to
-sting my eyes; this fire sprinkler...</p>
-<p></p>
-<p>I tried again, and
-when that got no response, headed for the doorway. &ldquo;I&rsquo;ll
-get help,&rdquo; I told the dying man in green scrubs, but I don&rsquo;t
-know if he heard me, or believed it. The corridor&rsquo;s checkboard
-ceiling was invisible beyond a roiling layer of smoke, like
-thunderclouds; water fell this, making it quite a realistic
-ecosystem. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Cassandra Cautery
-had not been lying about the improved tactile feedback</p>
-<p></p>
-<p>ceiling of the
-corridor corridor was In the corridor was more smoke and more water
-falling from the ceiling. There were also lots of marks on the walls:
-dents, scrapes, and, in one place, a hole clear through to the
-adjoining room big enough for me to put my head through. I did not
-put my head through it. I broke into a trot.</p>
-<p>These new legs,
-they were really tore up the floor.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I suspected that I
-could help this guy about as much as he could help me if my
-servo-magnetic disks became unaligned. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>One time in
-college, I tried to buy a pack of Doritos from a vending machine. I
-fed my bills into the slot, pressed C (firmly) then 4 (firmly), and
-waited. Nothing happened, so I pressed C-4 again (more firmly). Still
-nothing. I tried CANCEL, thinking to start over, and the machine
-whirred, the steel coil revolved, and the Doritos packet edged
-closer, then stuck there.</p>
-<p>I cursed and kicked
-it, because I was 22 and there were people watching. I said, &ldquo;This
-<em>stupid</em> machine.&rdquo; It was humiliating to be defeated by an
-inanimate object, especially one designed to do what I wanted.</p>
-<p>As I stared at the
-dangling Doritos packet, a different way of looking at the situation
-occurred to me. In this scenario, the vending machine was intelligent
-and I was a machine. The vending machine, for reasons of its own&mdash;I
-don&rsquo;t think it&rsquo;s important to know the vending machine&rsquo;s
-motivation, but say it was bored&mdash;poked my buttons in order to
-elicit particular behaviors. This scenario made sense because if you
-replaced me with a different human being they would have acted the
-same way. There was nothing unique or unpredictable about me getting
-mad; an intelligent vending machine would know that&rsquo;s what
-humans do when you dangle snacks in front of them.</p>
-<p><A NAME="firstHeading1"></A>I
-spent the rest of the day trying to think why an objective observor
-should believe I was controlling the vending machine in this
-situation and not the other way around. And
-I couldn&rsquo;t. I had a brain and the vending machine didn&rsquo;t,
-but that was classic
-anthropocentrism: for all I
-knew, the vending machine&rsquo;s consciousness resided in a space I
-didn&rsquo;t understand. It might argue I couldn&rsquo;t have
-consciousness because I didn&rsquo;t have a coin slot. 
-</p>
-<p>That was when I
-decided there wasn&rsquo;t much difference between humans and
-machines. We&rsquo;re more complex, of course, but it&rsquo;s a
-distinction of degrees, not difference. We&rsquo;re all vending
-machines.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>Since then, I have
-increasingly believed that human beings are inherently predictable.
-Threaten us with harm and we run away. Warm us and we relax. Offer us
-six hundred dollars a week if we sit at a desk and answer phone
-calls, and we will sit at a desk and answer phone calls. We are more
-complex than vending machines, of course. But the distinction is of
-degrees, not difference. Give me infinite computing power and perfect
-knowledge of environmental variables, and I&rsquo;m pretty sure I can
-model consciousness. I&rsquo;m pretty sure that one day, not too far
-from now, we will have definitive proof that we are all vending
-machines.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Finish
-with either CC imploring Charlie, you&rsquo;re our only hope, you
-need to be top of your game to compete with Carl, or: Show me. I
-think Show Me, save the former for the Friday after the pre-req
-conversation. And put the crouch there too.&gt;&gt;</p>
-<p></p>
-<p>Cassandra Cautery
-crouched in front of me. This seemed unnecessary to me, because it
-reversed our height differential without significantly reducing it. 
-</p>
-<p></p>
-<p>And not just arms.
-We have a pelvis. A lower torso that regulates energy 
-</p>
-<p></p>
-<p>&lt;&lt; Mention
-sheilding. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Actually,
-the pre-reqs should require it. That&rsquo;d be a good page for
-tomorrow. The pre-reqs. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I cried weakly and
-uselessly, what I had done.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>I clamped the
-artery again. But I didn&rsquo;t move. 
-</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; sternum?
-&gt;&gt;</p>
-<p>&lt;&lt; The
-coughing: a surgeon, dying in his biological body. &gt;&gt;</p>
-<p>&lt;&lt; Legs
-online: he feels them. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Page
-NOTES.</p>
-<p></p>
-<p></p>
-<p>Maybe he plugs his
-arm, and he didn&rsquo;t really need to. There&rsquo;s not too much
-blood required by his body any more? Hmm, brain definitely needs it
-though. Maybe he can shut that off. Press a switch, vrrr, 
-</p>
-<p></p>
-<p>CARL TOOK LOLA!!</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; This week
-I think they maybe prepare for Carl, but he doesn&rsquo;t come. So
-then they bulk up a little more. &gt;&gt;</p>
-<p>&lt;&lt; The big
-story here is Charlie getting new parts. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; soft
-sciences: Charlie asks Lola about id. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p><em>The Id comprises the unorganised
-part of the personality structure that contains the basic drives. It
-acts according to the pleasure principle, seeking to avoid pain or
-unpleasure. </em>
-</p>
-<p></p>
-<p></p>
-<p>A low, keening
-noise came out of Lola. 
-</p>
-<p></p>
-<p></p>
-<p>&ldquo;Think of
-Better Future as a machine with eight thousand employees. An
-artificial intelligence programmed to shift resources around to
-generate wealth and power.&rdquo;</p>
-<p></p>
-</BODY>
-</HTML>
+<p>I stumbled out into
+the corridor, calling for help. At least, that&rsquo;s what it felt
+like in my head. In reality, each step I took was machine-smooth and
+solid as steel. My hooves punched into the tiles and gripped. There
+was probably a setting to make them stop doing that, a command like
+<em>imagine flexing your left big toe three times</em> so I wouldn&rsquo;t
+tear up the floor when indoors, but I didn&rsquo;t know it. I had no
+user documentation.</p>
+<p>&ldquo;Help,&rdquo;
+I croaked. This was the biological part of me, failing. Smoke stung
+my eyes. My thumb-and-forefinger grip slipped from my artery, and
+when I tried to pinch it closed again, it hurt. The anaesthetic was
+fading. Where were those drugs Cassandra Cautery had mentioned, the
+ones that would make me <em>functional pretty much immediately</em>? I
+needed those. Not as badly as the man in green scrubs needed a
+doctor, but still. I wondered if there had been some surgical clamps
+back there, maybe in that overturned trolley. I should have looked
+for those before going out for help. I could pass out and that guy
+would die waiting. Would he understand if I went back and hunted
+around for clamps, or would that be really offensive and selfish?</p>
+<p>This
+light-headedness was becoming a problem.</p>
+<p>These Contours felt
+different. I couldn&rsquo;t put my finger on it, ha ha. But
+definitely different. It wasn&rsquo;t something in particular. It was
+everything. Did that make sense? No. It didn&rsquo;t. I glanced back
+(<em>swzzzz</em>, whispered my torso) and saw a lot of blood in the
+water. That was not good. &ldquo;Blood,&rdquo; I said, for no reason.</p>
+<p>A man appeared in
+the corridor ahead of me. It was moments before I recognized him:
+Jason, my lab assistant. &ldquo;He&rsquo;s here!&rdquo; Jason
+shouted. &ldquo;He&rsquo;s alive! Quickly!&rdquo; He gestured to
+people I couldn&rsquo;t see. I had never been particularly fond of
+Jason before, but in this moment, I loved him very much.</p>
---
+++ page0151.mm
@@ -0,0 +1,197 @@
+<p>Page 151.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Things to
+do: (1) clamp that arm (2) get a new arm on (3) call for help for
+that guy (4) discover things about the legs (5) find CC. No need to
+clamp him straight away, though; let him struggle. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; CC morose
+about her corporate performance. <em>Why is this happening to me? I&rsquo;m
+organized. I score highly in tests</em>. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>&ldquo;What
+happened?&rdquo;</p>
+<p>&ldquo;Carl
+happened.&rdquo; Cassandra Cautery leaned forward and spat something
+between her knees. I felt shocked. Things must be serious, if
+Cassandra Cautery was doing that. &ldquo;He busted in and knocked us
+around, that&rsquo;s what happened.&rdquo;</p>
+<p>&ldquo;Looking for
+me?&rdquo; I glanced over my shoulder, in case he was there, standing
+at the end of the corridor with his sledgehammer fists, ready to
+finish the job he started.</p>
+<p>&ldquo;Well, that&rsquo;s
+the thing. He found you. We couldn&rsquo;t stop him. He stood right
+beside your little bed there. I swear to God, the expression on his
+face, I was sure he was about to put his fist through your face.&rdquo;</p>
+<p>&ldquo;But he
+didn&rsquo;t.&rdquo;</p>
+<p>&ldquo;No. He did
+not. He just left you there.&rdquo;</p>
+<p>&ldquo;I thought
+you said he wanted to kill me.&rdquo;</p>
+<p>&ldquo;I said he
+was nuts. I&rsquo;m not, you know, an expert on what passes for logic
+in Carl&rsquo;s head.&rdquo; She stared at her ankle. &ldquo;Next
+time you see him, you ask him what he thinks he&rsquo;s doing.&rdquo;
+She looked at me. &ldquo;Check that. Next time you see him, just kill
+him.&rdquo;</p>
+<p>&ldquo;Why don&rsquo;t
+<em>you</em> kill him,&rdquo; I said. &ldquo;You&rsquo;re the ones with
+guns.&rdquo;</p>
+<p>Cassandra Cautery
+glanced at my arm. &ldquo;Actually...&rdquo;</p>
+<p>&ldquo;Oh,&rdquo; I
+said.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt; I&rsquo;d like
+to do something with D. Peters. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>I sucked in breath
+to call for help and coughed instead. The smoke was beginning to
+sting my eyes; this fire sprinkler...</p>
+<p></p>
+<p>I tried again, and
+when that got no response, headed for the doorway. &ldquo;I&rsquo;ll
+get help,&rdquo; I told the dying man in green scrubs, but I don&rsquo;t
+know if he heard me, or believed it. The corridor&rsquo;s checkboard
+ceiling was invisible beyond a roiling layer of smoke like
+thunderclouds; water fell this, making it quite a realistic
+ecosystem. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>Cassandra Cautery
+had not been lying about the improved tactile feedback</p>
+<p></p>
+<p>ceiling of the
+corridor corridor was In the corridor was more smoke and more water
+falling from the ceiling. There were also lots of marks on the walls:
+dents, scrapes, and, in one place, a hole clear through to the
+adjoining room big enough for me to put my head through. I did not
+put my head through it. I broke into a trot.</p>
+<p>These new legs
+really tore up the floor. There was probably a setting somewhere that
+made them stop doing that, a command like <em>imagine flexing your
+left big toe three times</em>, but I didn&rsquo;t know it. I had no
+user documentation.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I suspected that I
+could help this guy about as much as he could help me if my
+servo-magnetic disks became unaligned. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>One time in
+college, I tried to buy a pack of Doritos from a vending machine. I
+fed my bills into the slot, pressed C (firmly) then 4 (firmly), and
+waited. Nothing happened, so I pressed C-4 again (more firmly). Still
+nothing. I tried CANCEL, thinking to start over, and the machine
+whirred, the steel coil revolved, and the Doritos packet edged
+closer, then stuck there.</p>
+<p>I cursed and kicked
+it, because I was 22 and there were people watching. I said, &ldquo;This
+<em>stupid</em> machine.&rdquo; It was humiliating to be defeated by an
+inanimate object, especially one designed to do what I wanted.</p>
+<p>As I stared at the
+dangling Doritos packet, a different way of looking at the situation
+occurred to me. In this scenario, the vending machine was intelligent
+and I was a machine. The vending machine, for reasons of its own&mdash;I
+don&rsquo;t think it&rsquo;s important to know the vending machine&rsquo;s
+motivation, but say it was bored&mdash;poked my buttons in order to
+elicit particular behaviors. This scenario made sense because if you
+replaced me with a different human being they would have acted the
+same way. There was nothing unique or unpredictable about me getting
+mad; an intelligent vending machine would know that&rsquo;s what
+humans do when you dangle snacks in front of them.</p>
+<p><A NAME="firstHeading1"></A>I
+spent the rest of the day trying to think why an objective observor
+should believe I was controlling the vending machine in this
+situation and not the other way around. And
+I couldn&rsquo;t. I had a brain and the vending machine didn&rsquo;t,
+but that was classic
+anthropocentrism: for all I
+knew, the vending machine&rsquo;s consciousness resided in a space I
+didn&rsquo;t understand. It might argue I couldn&rsquo;t have
+consciousness because I didn&rsquo;t have a coin slot. 
+</p>
+<p>That was when I
+decided there wasn&rsquo;t much difference between humans and
+machines. We&rsquo;re more complex, of course, but it&rsquo;s a
+distinction of degrees, not difference. We&rsquo;re all vending
+machines.</p>
+<p></p>
+<p></p>
+<p></p>
+<p>Since then, I have
+increasingly believed that human beings are inherently predictable.
+Threaten us with harm and we run away. Warm us and we relax. Offer us
+six hundred dollars a week if we sit at a desk and answer phone
+calls, and we will sit at a desk and answer phone calls. We are more
+complex than vending machines, of course. But the distinction is of
+degrees, not difference. Give me infinite computing power and perfect
+knowledge of environmental variables, and I&rsquo;m pretty sure I can
+model consciousness. I&rsquo;m pretty sure that one day, not too far
+from now, we will have definitive proof that we are all vending
+machines.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Legs
+online: he feels them. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p><U>Notes</U></p>
+<p></p>
+<p>CARL TOOK LOLA!!</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+</BODY>
+</HTML>
