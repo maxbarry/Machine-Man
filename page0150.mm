32f4b3576c992af1b5738f39c67a9ac7329f1cb2
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-13 04:48:11 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@556 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0150.mm
+++ page0150.mm
@@ -9,27 +9,26 @@ tear up the floor when indoors, but I didn&rsquo;t know it. I had no
 user documentation.</p>
 <p>&ldquo;Help,&rdquo;
 I croaked. This was the biological part of me, failing. Smoke stung
-my eyes. My thumb-and-forefinger grip slipped from my artery, and
-when I tried to pinch it closed again, it hurt. The anaesthetic was
-fading. Where were those drugs Cassandra Cautery had mentioned, the
-ones that would make me <em>functional pretty much immediately</em>? I
-needed those. Not as badly as the man in green scrubs needed a
-doctor, but still. I wondered if there had been some surgical clamps
-back there, maybe in that overturned trolley. I should have looked
-for those before going out for help. I could pass out and that guy
-would die waiting. Would he understand if I went back and hunted
-around for clamps, or would that be really offensive and selfish?</p>
+my eyes. My thumb-and-forefinger grip slipped from my severed artery,
+and when I tried to pinch it closed, it hurt. Where were those drugs
+Cassandra Cautery mentioned, the ones that would make me <em>functional
+pretty much immediately</em>? I needed those. Not as badly as the man
+in green scrubs needed a doctor, but still. I wondered if there had
+been some surgical clamps back there, maybe in that overturned
+trolley. I should have looked for those before going out for help. I
+could pass out here. Would that guy understand if I went back and
+hunted around for clamps, as he lay there dying?</p>
 <p>This
 light-headedness was becoming a problem.</p>
 <p>These Contours felt
-different. I couldn&rsquo;t put my finger on it, ha ha. But
-definitely different. It wasn&rsquo;t something in particular. It was
-everything. Did that make sense? No. It didn&rsquo;t. I glanced back
-(<em>swzzzz</em>, whispered my torso) and saw a lot of blood in the
-water. That was not good. &ldquo;Blood,&rdquo; I said, for no reason.</p>
+different. I couldn&rsquo;t put my finger on it, so to speak, ha ha.
+It wasn&rsquo;t something in particular. It was everything. Did that
+make sense? No. It didn&rsquo;t. I glanced back (<em>swsss</em>, said
+my torso) and saw a lot of blood in the water. That was not good.
+&ldquo;Blood,&rdquo; I said.</p>
 <p>A man appeared in
-the corridor ahead of me. It was moments before I recognized him:
+the corridor ahead of me. It was a moment before I recognized him as
 Jason, my lab assistant. &ldquo;He&rsquo;s here!&rdquo; Jason
-shouted. &ldquo;He&rsquo;s alive! Quickly!&rdquo; He gestured to
+shouted. &ldquo;He&rsquo;s alive!&rdquo; He gestured urgently to
 people I couldn&rsquo;t see. I had never been particularly fond of
 Jason before, but in this moment, I loved him very much.</p>
