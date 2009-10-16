d9dc9f41b21f9dcb6f4bb142fecbd0546b32f208
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-16 03:28:48 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@573 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0153.mm
+++ page0153.mm
@@ -1,25 +1,30 @@
 <p>Page 153.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>She looked at me
-again. &ldquo;Carl found you. We couldn&rsquo;t stop him. He stood
-right here.&rdquo;</p>
-<p>&ldquo;I thought
-you said he wanted to kill me.&rdquo;</p>
-<p>&ldquo;I said
-Carl&rsquo;s <em>nuts</em>. I don&rsquo;t know what he&rsquo;s
-thinking. Next time you see him, you ask him what the hell he&rsquo;s
-doing. Check that. Next time you see him, just kill him.&rdquo;</p>
+<p>&ldquo;Carl found
+you,&rdquo; said Cassandra Cautery. One tear after another rolled
+down her cheek, but otherwise she was very calm. I found that a
+little disturbing. Of course, I was in the middle of having a
+prosthetic fitted to replace an arm I&rsquo;d sawn off myself, so it
+probably worked both ways. &ldquo;We couldn&rsquo;t stop him. He
+stood right here and looked at you.&rdquo;</p>
+<p>&ldquo;I thought he
+wanted to kill me.&rdquo;</p>
+<p>&ldquo;He probably
+thought you were as good as dead. Your arm was hanging off.&rdquo;</p>
+<p>&ldquo;Still,&rdquo;
+I said. &ldquo;Why not make sure?&rdquo;</p>
+<p>&ldquo;Because he&rsquo;s
+nuts. I don&rsquo;t know what he&rsquo;s thinking. Look, if it
+bothers you so much, next time you see him, ask what the hell he
+thinks he&rsquo;s doing. Check that. Next time you see him, just kill
+him.&rdquo;</p>
 <p>&ldquo;Why don&rsquo;t
 <em>you</em> kill him,&rdquo; I said. &ldquo;You&rsquo;re the ones with
 guns.&rdquo;</p>
-<p>She looked at the
-prosthetic arm. The two men had aligned it with my shoulder. A third
-was fooling around somewhere near my spine. Something locked into
-place with a <em>thunk</em>. I became abruptly aware that my spine was
-not bone. Cassandra Cautery said, &ldquo;Actually...&rdquo;</p>
+<p>Cassandra Cautery
+looked at the prosthetic arm. The two men had aligned it with my
+shoulder; a third was fooling around near my spine. He strained, and
+something locked into place with a sound like: <em>chang</em>. I
+realized that my spine was not bone. Cassandra Cautery said,
+&ldquo;Actually...&rdquo;</p>
 <p>&ldquo;Oh,&rdquo; I
 said.</p>
