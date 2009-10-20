777224fe9c47a680560a94931d40dd8b0c0c9298
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-20 05:53:31 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@591 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0155.mm
+++ page0155.mm
@@ -8,11 +8,11 @@ something of a surprise to everyone here, because, well, there are
 several entrances, and a security guard knows them all. Of course, we
 had people in the lobby. We had snipers on the roof, we had guys in
 mounted Hummers. We deployed weaponry from the Speculative Military
-Products people that I&rsquo;m pretty sure isn&rsquo;t out of
-prototype. There was a sonics gun in the underground parking lot, the
-back lawn was sown with EMP mines, and the lobby guys had... well, I
-guess it was an electroshock cannon. Like a tazer, only firing a
-couple hundred darts a minute.</p>
+Products people I&rsquo;m sure isn&rsquo;t out of prototype. There
+was a sonics gun in the underground parking lot, the back lawn was
+sown with EMP mines, and the lobby guys had... well, I guess it was
+an electroshock cannon. Like a tazer, only firing a couple hundred
+darts a minute.</p>
 <p>&ldquo;So Carl
 turns up. And&mdash;I don&rsquo;t know if anyone told you this, but
 the reason things turned ugly with Carl, it was because he was
@@ -21,14 +21,15 @@ know what a guy wants with multiple sets of hands, but...&rdquo; He
 cleared his throat. &ldquo;Anyway, one of the things Carl took was a
 Fiber Shield. And clearly nobody really planned out all the
 contingencies, because when Carl turned up, he used it, and they shot
-at him, and that&rsquo;s what happened.&rdquo; Jason studied my face.
+at him, and that&rsquo;s what happened.&rdquo;</p>
+<p>He studied my face.
 &ldquo;You went to the Fiber Shield presentation, didn&rsquo;t you?
 It&rsquo;s, um, essentially a bomb. It throws out a fog of tiny fiber
 strips, micro-ribbons. They&rsquo;re harmless, but they float in the
-air, millions of them, and their ends are sticky. If someone shoots
-into that fog, the bullet hits a ribbon and the ribbon sticks to it.
-It gets unbalanced, pulled off-course. It might go left, right, who
-knows. The point is it diverts.</p>
+air, millions or billions of them, and their ends are sticky. If
+someone shoots into that fog, the bullet hits a ribbon and the ribbon
+sticks to it. It gets unbalanced, pulled off-course. It might go
+left, right, who knows. The point is it diverts.</p>
 <p>&ldquo;This
 presentation, they showed how it&rsquo;s almost impossible to hit a
 target standing twenty feet behind the fog. They did five or six test
@@ -38,15 +39,15 @@ wouldn&rsquo;t want to stake my life on it, because the amount of
 diversion depends on how many ribbons the bullet hits, the angle at
 each collision, all these random variables. The project leader,
 that&rsquo;s Abeline Knudsen, who did that really interesting paper
-on creating disruptive resonance in inner ear fluid, if you saw
+on creating disruptive resonances in inner ear fluid, if you saw
 that... well, she said, you fire enough bullets, eventually one will
 go straight. Or straight enough. So, actually thinking about it now,
 maybe that&rsquo;s what they told the security guys. If he throws
 down the Fiber Shield, pour bullets into it.</p>
 <p>&ldquo;So Carl
 turns up on the front lawn. He sets off that bomb, and everyone opens
-fire. I&rsquo;m in the lab, watching on CCTV, and there are literally
-twenty guys gushing gunfire. And maybe that would have been okay, but
+fire. I&rsquo;m in the lab, watching on CCTV, and there are guys
+everywhere, gushing gunfire. And maybe that would have been okay, but
 the electroshock cannon guys open fire, too, and fill the air with
 million-volt darts. These darts are a lot lighter than bullets, so
 they hit the fog and they go <em>everywhere</em>. They&rsquo;re hitting
