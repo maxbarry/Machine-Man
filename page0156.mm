cf9de1b50e64ef32ba7d66f9051ba92851990792
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-21 06:28:35 +0000

    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@592 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0156.mm
+++ page0156.mm
@@ -1,184 +1,48 @@
-<p>Page 155.</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Charlie
-realizes Carl doesn&rsquo;t sound that insane. &gt;&gt;</p>
-<p></p>
-<p>I felt nervous. I
-felt sick. I felt angry.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;I wasn&rsquo;t
-going to mention this until you were used to the weapon, because I
-didn&rsquo;t want you running off after Carl without knowing what
-you&rsquo;re doing. But if you won&rsquo;t listen to&mdash;&rdquo;</p>
-<p>&ldquo;I&rsquo;m
-not running after Carl! What is wrong with you? <em>I will not shoot
-Carl!</em>&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Nobody realized
-this, I guess, or at least nobody planned on him using one, which is
-going to cost someone their job. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p>These are really
-interesting pieces of technology. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>hoarding equipment,
-and when they told him to put it back, he went nuts. He took 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&rsquo;m not
-shooting at anyone, ever!&rdquo;</p>
-<p></p>
-<p></p>
-<p>&ldquo;That&rsquo;s
-not a problem. That&rsquo;s the <em>opposite</em> of the problem. The
-problem is you&rsquo;ve put a gun on my arm and there is no situation
-in which that is okay!&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>we had put you
-through some basic training, 
-</p>
-<p></p>
-<p>you understood the
-situation properly, because it&rsquo;s really important you
-understand the situation properly, Charlie, it&rsquo;s really
-important. I don&rsquo;t want you running off after Carl until you
-know what you&rsquo;re doing.&rdquo;</p>
-<p></p>
-<p></p>
-<p>I was not used to
-women shouting. 
-</p>
-<p>Her voice startled
-me: I had not heard many women raw emotion.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;Please,
-Charlie,&rdquo; she said. &ldquo;Just give it a chance.&rdquo;</p>
-<p>&ldquo;No! No! I
-don&rsquo;t want a gun! You hear me? I&rsquo;m not a super-soldier! I
-am not doing this!&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;I cannot
-express too strongly how very very much I don&rsquo;t want to be a
-super-soldier.&rdquo;</p>
-<p><em>&lt;&lt; bit cartoony, this: &gt;&gt;</em></p>
-<p>&ldquo;Why,&rdquo;
-I said. &ldquo;Why why...&rdquo; It turned out there was no good way
-to say it. &ldquo;Why did you think I&rsquo;d want a gun on my arm?&rdquo;</p>
-<p>&ldquo;Not just any
-gun,&rdquo; said Jason. &ldquo;This is the Devastator, from Special
-Projects. Four hundred rounds per minute, quick-changing&mdash;&rdquo;</p>
-<p>&ldquo;Shut up,&rdquo;
-I said. &ldquo;I don&rsquo;t care, Jason. <em>I don&rsquo;t want a gun
-on my arm</em>.&rdquo; I tried to gesture, and my gun-arm swung
-through the air. Everybody stepped back. &ldquo;I&rsquo;m not a
-<em>soldier</em>! I&rsquo;m not going to <em>shoot</em> anyone!&rdquo;</p>
-<p>&ldquo;You settle
-down,&rdquo; said Cassandra Cautery. &ldquo;We&rsquo;re out of
-options, Charlie. You think this is what I want? You think that&rsquo;s
-my idea of good corporate governance, strap a missile-mounted to a
-science guy and send him after a rogue employee?&rdquo;</p>
-<p>&ldquo;I don&rsquo;t
-care!&rdquo; I swung the gun again, for emphasis. &ldquo;This is not
-happening! There are no circumstances under which I will do this!&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Why have
-they not been able to kill Carl? (a) guards like him, (b) he stole a
-anti-gun gun&mdash;a thin fiber smart gun, sticks to bullets and
-pulls them off course, (c) &rsquo;s got defenses against regular
-bullets, (c) they tried the sonic gun but ___ &gt;&gt;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; CC
-explains the gun. Charlie: why the FUCK did you think I&rsquo;d want
-a gun in my arm? I&rsquo;m not a fucking soldier! CC: Look, we can&rsquo;t
-stop Carl. We&rsquo;re out of options here. Charlie: Maybe I don&rsquo;t
-have a beef with Carl. He left me alone, he doesn&rsquo;t want to
-kill me, you and Carl sort out your fucking differences. CC: &ldquo;He
-took Lola,&rdquo; she said, and everything washed red. &lt;==
-Melodramatic, but that kinda thing. &gt;&gt;</p>
-<p></p>
-<p>&ldquo;Please, let
-them look at your head,&rdquo; said one of Cautery&rsquo;s
-assistants. &ldquo;It could be fractured.&rdquo;</p>
-<p></p>
-<p></p>
-<p>I couldn&rsquo;t
-find a way to phrase the question that wasn&rsquo;t insane. 
-</p>
-<p></p>
-<p></p>
-<p> why...&rdquo; The
-question felt too big to fit through my brain. &ldquo;Why did you
-think I&rsquo;d want a <em>gun on my arm</em>?&rdquo;</p>
-<p></p>
-<p></p>
+<p>Page 156.</p>
+<p>&ldquo;Dr.
+Neumann.&rdquo; Now it was a woman with blue eyes and wild gray hair.
+She looked vaguely familiar; possibly we had spoken a few times about
+crossover research. &ldquo;I know you&rsquo;re busy, but I would
+really like a moment to speak with you about Lola&rsquo;s heart.&rdquo;</p>
+<p>&ldquo;Registering
+with core,&rdquo; said a man, and another replied, &ldquo;Seeing
+that.&rdquo; I was the kind of busy that didn&rsquo;t require my
+participation. Which was good, because at this point I was focused
+internally. I was asking myself questions and listening for answers.</p>
+<p>&ldquo;I feel very
+uncomfortable about this. We had a lot of questions about the
+procedure in the first place, and several of us felt strongly the
+technology wasn&rsquo;t ready, but management...&rdquo; She threw a
+glance at Cassandra Cautery, who was out in the hallway having a
+doctor peer at her scalp. &ldquo;Anyway, that&rsquo;s in the past.
+The unit&rsquo;s installed now.</p>
+<p>&ldquo;The issue is
+the EMP function. The weapon. It draws a great deal of power. And we
+built in a safety margin, of course, to ensure that even after an EMP
+wave, the battery has plenty of power to maintain regular heart
+function. We were very careful about that! We also ensured the EMP
+won&rsquo;t fire unless the battery is full. So it&rsquo;s
+single-use.&rdquo; Her eyes switched rapidly between mine. &ldquo;Except
+that part is possibly not working completely properly. I don&rsquo;t
+think it&rsquo;s anything to be concerned about. But, well, as I
+mentioned, we were very early in QA and all of a sudden they were
+telling us there&rsquo;s a woman on the table, we had to install...
+and, the thing is, the EMP shouldn&rsquo;t have fired twice. It
+definitely shouldn&rsquo;t have been able to do that. I saw the
+subject briefly, that is, Miss Banks, when you were brought in,
+and... and probably this was the light, but her skin looked gray. She
+seemed to be having some trouble breathing. Which wasn&rsquo;t a
+problem, because we can recharge the battery in-house, it&rsquo;s a
+simple procedure. But before we could, this terrible thing happened
+with all the shooting. And now she&rsquo;s gone. So I&rsquo;m just a
+tiny bit concerned that her battery has been drained to a point where
+it&rsquo;s impacting heart function. And please don&rsquo;t worry,
+because it needs very little charge to pump blood&mdash;it&rsquo;s
+not going to stop! I&rsquo;m almost certain of that. But I am
+thinking that if the safety mechanism isn&rsquo;t working, and her
+heart rate reaches the trigger threshold, then possibly the EMP will
+fire for a third time. And that would be bad. So, again, this is just
+a precaution, and I don&rsquo;t want to make life harder because I
+know you have a lot on your plate, but when you find Lola, it would
+be extremely good if you avoid making her scared or excited or engage
+in any kind of vigorous exercise.&rdquo;</p>
---
+++ page0159.mm
@@ -0,0 +1,187 @@
+<p>Page 159.</p>
+<h5>CHAPTER 
+THIRTY-THREE</h5>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>There were many
+competing thoughts filling my brain. One was. Another, tickling my
+brain with panicked feathery bird wings, went something like: <em>Lola
+Lola Lola you have lost Lola</em>. It was hard to concentrate on
+Jason&rsquo;s story. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p>But I guess I got
+the gist, because when he finished, I had a new thought, and this
+was: <em>Carl doesn&rsquo;t sound that crazy.</em></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Charlie
+realizes Carl doesn&rsquo;t sound that insane. &gt;&gt;</p>
+<p>&lt;&lt; He talks
+to his legs &gt;&gt;</p>
+<p>&lt;&lt; Charlie
+should vomit. Only it&rsquo;s not food. He should still feel weepy
+about what he has become. He&rsquo;s separating from his parts. And
+BF is trying to bundle him out the door as quickly as possible. &gt;&gt;</p>
+<p>&lt;&lt; Releasing
+a cat to catch a mouse... releasing a dog to catch the cat. &gt;&gt;</p>
+<p>&lt;&lt; Lola&rsquo;s
+heart is running out &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; I felt
+nervous. I felt sick. I felt angry. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&ldquo;Please, let
+them look at your head,&rdquo; said one of Cautery&rsquo;s
+assistants. &ldquo;It could be fractured.&rdquo;</p>
+<p></p>
+<p></p>
+<p>I couldn&rsquo;t
+find a way to phrase the question that wasn&rsquo;t insane. 
+</p>
+<p></p>
+<p></p>
+<p> why...&rdquo; The
+question felt too big to fit through my brain. &ldquo;Why did you
+think I&rsquo;d want a <em>gun on my arm</em>?&rdquo;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>One time in
+college, I tried to buy a pack of Doritos from a vending machine. I
+fed my bills into the slot, pressed C (firmly) then 4 (firmly), and
+waited. Nothing happened, so I pressed C-4 again (more firmly). Still
+nothing. I tried CANCEL, thinking to start over, and the machine
+whirred, the steel coil revolved, and the Doritos packet edged
+closer, then stuck there.</p>
+<p>I cursed and kicked
+it, because I was 22 and there were people watching. I said, &ldquo;This
+<em>stupid</em> machine.&rdquo; It was humiliating to be defeated by an
+inanimate object, especially one designed to do what I wanted.</p>
+<p>As I stared at the
+dangling Doritos packet, a different way of looking at the situation
+occurred to me. In this scenario, the vending machine was intelligent
+and I was a machine. The vending machine, for reasons of its own&mdash;I
+don&rsquo;t think it&rsquo;s important to know the vending machine&rsquo;s
+motivation, but say it was bored&mdash;poked my buttons in order to
+elicit particular behaviors. This scenario made sense because if you
+replaced me with a different human being they would have acted the
+same way. There was nothing unique or unpredictable about me getting
+mad; an intelligent vending machine would know that&rsquo;s what
+humans do when you dangle snacks in front of them.</p>
+<p><A NAME="firstHeading1"></A>I
+spent the rest of the day trying to think why an objective observor
+should believe I was controlling the vending machine in this
+situation and not the other way around. And
+I couldn&rsquo;t. I had a brain and the vending machine didn&rsquo;t,
+but that was classic
+anthropocentrism: for all I
+knew, the vending machine&rsquo;s consciousness resided in a space I
+didn&rsquo;t understand. It might argue I couldn&rsquo;t have
+consciousness because I didn&rsquo;t have a coin slot. 
+</p>
+<p>That was when I
+decided there wasn&rsquo;t much difference between humans and
+machines. We&rsquo;re more complex, of course, but it&rsquo;s a
+distinction of degrees, not difference. We&rsquo;re all vending
+machines.</p>
+<p></p>
+<p></p>
+<p></p>
+<p>Since then, I have
+increasingly believed that human beings are inherently predictable.
+Threaten us with harm and we run away. Warm us and we relax. Offer us
+six hundred dollars a week if we sit at a desk and answer phone
+calls, and we will sit at a desk and answer phone calls. We are more
+complex than vending machines, of course. But the distinction is of
+degrees, not difference. Give me infinite computing power and perfect
+knowledge of environmental variables, and I&rsquo;m pretty sure I can
+model consciousness. I&rsquo;m pretty sure that one day, not too far
+from now, we will have definitive proof that we are all vending
+machines.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>The steel felt
+freezing. 
+</p>
+<p>looked okay, but
+many of the other people had sooty faces</p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Legs
+online: he feels them. &gt;&gt;</p>
+<p>Cassandra Cautery
+had not been lying about the improved tactile feedback</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; drugs make
+him happy&mdash;the <em>make </em>him, appropriate term because he is
+now comprised of drugs &gt;&gt;</p>
+<p>At first I missed
+the last word; I thought he said, These drugs will make you, and was
+confused, although then I thought that that was probably accurate. I
+was made of drugs.</p>
+<p>&lt;&lt; you flood
+my brain with endorphins and I become a different person &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt; Ending: I
+could feel them. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>That was what was
+different about the Threes. I could feel them.</p>
+<p></p>
+<p>Each
+glistening raindrop that fell in her hair was a shimmering sphere of
+boundless possibility. 
+</p>
+</BODY>
+</HTML>
