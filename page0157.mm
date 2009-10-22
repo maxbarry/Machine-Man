81641204770a0454dd9b8fd56eb8c0474dcba35d
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-22 06:33:23 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@595 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0157.mm
+++ page0157.mm
@@ -3,46 +3,53 @@
 felt nauseus. I felt euphoric then furious and none of this had
 anything to do with me. That was what I realized. At first I tried to
 sort the real emotions from the chemically stimulated ones; then I
-saw it didn&rsquo;t matter. Maybe I felt rage because Carl had stolen
-Lola from me, or maybe it was a side-effect of something I&rsquo;d
-been injected with, or random feedback from the nerve interface;
-whatever the source, it was a simple neurochemical reaction. That was
-what emotions were. And I didn&rsquo;t need my body to tell me to
-rest or eat or cool down any more: I had onboard systems for that.
-They were obsolete. Even the higher emotions, frustration and joy and
-desire, those were feelings your body made to encourage you to do
-things that would benefit it.</p>
+saw it didn&rsquo;t matter. Maybe I felt rage because Carl had taken
+Lola, or maybe it was a side-effect of something I&rsquo;d been
+injected with, or random feedback from the nerve interface&mdash;whatever
+the source, what I was feeling was a neurochemical reaction. That was
+what emotions were. And I no longer needed my body to tell me to rest
+or eat or cool down: I had onboard systems for that. My emotions were
+obsolete.</p>
 <p>&ldquo;Hub online,&rdquo;
 said Jason. &ldquo;Okay, Dr. Neumann, this is going to feel a little
 weird.&rdquo;</p>
-<p>It didn&rsquo;t
-matter how I felt. That was insignificant compared to what I did. I
-wouldn&rsquo;t get Lola back by feeling. I would get her back by
-extracting maximum efficacy from the resources at my disposal. 
-</p>
+<p>Even the higher
+emotions, like frustration and joy and desire, weren&rsquo;t they
+just neurochemicals your body manufactured in order to encourage you
+to do what it wanted? And I was not my body. I had never believed
+that. So, in that sense, there were no real feelings. They were all
+external and equally irrelevant.</p>
 <p>&ldquo;The hub
 allows us to maintain contact with you. There&rsquo;s a little camera
 right here, in your chest, so we can see what&rsquo;s going on. This
 little dot next to it, that&rsquo;s a microphone.&rdquo;</p>
-<p>I had been trying
-to talk to my parts. It wasn&rsquo;t easy, given all the emotion
-crashing around. It was like trying to whisper in a thunderstorm. I
-had not heard any reply. But I felt something was there. It had been
-in the original Contours. It should be back.</p>
+<p>It didn&rsquo;t
+matter how I felt. That was insignificant compared to what I did. I
+wouldn&rsquo;t get Lola back by feeling. I would get her back by
+applying my logical mind to extract maximum efficacy from the
+resources at my disposal. 
+</p>
 <p>&ldquo;You can also
 use it to track Carl. I&rsquo;m going to activate that right now,
 okay? You&rsquo;ll feel a distinct cold sensation, like a cool draft,
-from the direction he is.&rdquo;</p>
-<p>An idea occurred to
-me. Instead of trying to call out to them one at a time, I imagined
-them as a single entity. I thought: <em>Hello?</em></p>
+from his direction.&rdquo;</p>
+<p>I had been trying
+to talk to my parts. It wasn&rsquo;t easy, given all the emotion
+crashing around. It was like trying to whisper in a thunderstorm. And
+I hadn&rsquo;t got any response. But something had been there in the
+original Contours. I thought something was probably here, too.</p>
+<p>&ldquo;Okay, the
+weapon. Well, you&rsquo;ve clearly figured out how to fire it. But,
+for reference, the mental command is to clench your fist. We&rsquo;ve
+loaded it up but we&rsquo;re not going to unlock it until you&rsquo;re
+clear of the building, if that&rsquo;s okay. To, uh, be safe.&rdquo;</p>
+<p>A thought occurred
+to me. Instead of calling to my parts one at a time, I imagined them
+as a single entity. I thought: <em>Hello?</em></p>
 <p>Something stirred.</p>
-<p>&ldquo;The weapon,
-well, you&rsquo;ve clearly figured out how to fire that. But the
-mental command is to clench your fist. We&rsquo;re not going to
-unlock that until you&rsquo;re clear of the building. Just, uh, to be
-safe.&rdquo; He took a breath, looked around. &ldquo;Well! Okay. I
-think we&rsquo;re about done. Are you ready to do this?&rdquo;</p>
+<p>&ldquo;Well!&rdquo;
+Jason looked around. &ldquo;I think we&rsquo;re about done. Are you
+ready to do this?&rdquo;</p>
 <p>I
-said: <em>Are you?</em> My
+thought: <em>Are you?</em> My
 parts said: <em>Yes</em>.</p>
