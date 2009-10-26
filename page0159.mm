9c79ef6c3bb46c18dacfed92e1bdc10851afac29
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-26 07:34:19 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@606 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0159.mm
+++ page0159.mm
@@ -7,36 +7,37 @@ and lab techs. Sometimes we passed other guards, doctors, and lab
 techs, who stopped what they were doing to stare at me. From their
 expressions, I was either an awe-inspiring technological miracle or
 the worst thing they had ever seen. I wasn&rsquo;t sure myself. As we
-filed down the stairs, the Contour Three hooves cantilevered on each
-step, and I had to admire that. There is something deeply satisfying
-about a mechanical system that works just like it&rsquo;s supposed
-to. I&rsquo;m not sure if everyone feels that way. It might be an
-engineering thing.</p>
+filed down the stairs, though, the Contour Three hooves cantilevered
+on each step, and I had to admire that. There is something deeply
+satisfying about a mechanical system that works just like it&rsquo;s
+supposed to. I&rsquo;m not sure if everyone feels that way. It might
+be an engineering thing. But by the time we reached the bottom of
+those stairs, I was kind of in love.</p>
 <p>Then we emerged
 into a corridor with a guy who might or might not have been trying to
 hide his revulsion. I stopped in front of him. Hands pulled at me,
 trying futilely to tug me onward. I said, &ldquo;What?&rdquo;</p>
 <p>&ldquo;Nothing.&rdquo;
-A young guy in a dusty suit and tie. He could have been from any
-department, except one that did real work. His eyes roved everywhere
-but me.</p>
+Young, in a dusty suit and tie. He could have been from any
+department, except one that did real work. His eyes flicked at me and
+away.</p>
 <p>&ldquo;Look at me.&rdquo;
 I waited until he dragged his gaze to my face. Disgust, definitely.
 &ldquo;You prick,&rdquo; I said.</p>
 <p>Jason put a hand on
-my metal forearm, as if he had any chance of influencing where I
-chose to move. &ldquo;Dr. Neumann, please.&rdquo;</p>
+my gun-arm, as if he had any ability to influence where I chose to
+move. &ldquo;Dr. Neumann, please.&rdquo;</p>
 <p>&ldquo;I&rsquo;m
-going out there to save this company&rsquo;s ass. I&rsquo;m risking
-my life to clean up the mess you guys made.&rdquo;</p>
+going out to save this company&rsquo;s ass. I&rsquo;m risking my life
+to clean up the mess you guys made.&rdquo;</p>
 <p>&ldquo;Shut him
 down,&rdquo; someone said, and someone else said, &ldquo;No, wait.&rdquo;</p>
 <p>&ldquo;Dr.
-Neumann.&rdquo; Jason forced himself between me and the suit guy.
-&ldquo;Please. This way.&rdquo;</p>
+Neumann.&rdquo; Jason forced himself between me and the suit.
+&ldquo;Please. I beg you.&rdquo;</p>
 <p>&ldquo;I want some
 respect.&rdquo; I felt myself choking up. &ldquo;I at least want some
 <em>respect</em>.&rdquo;</p>
 <p>&ldquo;We all
-respect you very much,&rdquo; said Jason. &ldquo;Please. Please.
-Let&rsquo;s go.&rdquo;</p>
+respect you very much,&rdquo; he said. &ldquo;Please. Let&rsquo;s
+go.&rdquo;</p>
