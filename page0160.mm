88185e8720ff32dfbc4d520262efb0f883fca739
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-27 05:28:01 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@609 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0160.mm
+++ page0160.mm
@@ -1,37 +1,33 @@
 <p>Page 160.</p>
 <p>They sent me out
-through the underground parking lot, to avoid being seen by the many
-emergency services people who were apparently above ground. At first
-I didn&rsquo;t understand how the parking lot made any difference,
-since it exited in the same general area, but I was no longer
-worrying about details. I was prepared to assume Better Future would
-take care of things while I focused on the big-picture goal of
+through the underground parking lot, to avoid being seen by the
+emergency services people who were apparently crawling around above
+us. I didn&rsquo;t understand how the parking lot would make any
+difference, since it exited in the same general area, but I was no
+longer worrying about details. I was prepared to assume Better Future
+would take care of things while I focused on the big-picture goal of
 running until I found Carl and then shooting him.</p>
 <p>The parking lot was
 lit by a portable generator and a series of halogen lights, which
 made everything either blindingly bright or lost in impenetrable
 shadow. Better Future vans and Hummers idled in the dark, their
-chrome reflecting like supernovas, their tailpipes belching fumes.
-&ldquo;Hold here a second, please,&rdquo; said Jason, and people
-swarmed around me, performing final checks. Someone wrapped me in a
-top, a security guard jacket with one arm cut off. Until that moment
-I didn&rsquo;t realize I was technically naked.</p>
+chrome reflecting like supernovas, tailpipes belching fumes. &ldquo;Hold
+here a second, please,&rdquo; said Jason, and people swarmed me,
+performing final checks. Someone wrapped me in a top, a security
+guard jacket with one arm cut off.</p>
 <p>My legs hiccupped
 forward. &ldquo;Only another minute,&rdquo; said Jason. He thought it
 was me. It wasn&rsquo;t. I could feel my legs&rsquo; impatience. Soon
-they were going to start running whether these people had finished
-their checks or not.</p>
+they were going to start running whether these people were ready or
+not.</p>
 <p>&ldquo;All clear,&rdquo;
 said a lab tech. They began backing away. People climbed into vans;
 doors slammed.</p>
 <p>&ldquo;Wait.&rdquo;
-This was Cassandra Cautery. She walked up to me. Her head was
-bandaged. &ldquo;Before you go.&rdquo; Then she simply stood there. I
-did not want to tell her to hurry this along, but unless she moved, I
-was going to run through her. &ldquo;I feel like the old woman who
-swallowed a fly. You know that one? <em>She swallowed a spider to
-catch the fly... she swallowed a bird to catch the spider</em>...&rdquo;
-She reached for my face. I was not sure about this, but it was a
-little late to stop trusting Cassandra Cautery, so I bent down. She
-stretched on tip-toes, placed her hands on my cheeks, and kissed me.
-&ldquo;Be careful,&rdquo; she said.</p>
+Cassandra Cautery walked up to me. Her head was bandaged. &ldquo;One
+thing, before you go.&rdquo; Then she simply stood there. I didn&rsquo;t
+want to tell her to hurry up, but unless she moved, I was going to
+run through her. She reached for my face. I was not sure about this,
+but it was a little late to stop trusting Cassandra Cautery, so I
+bent down. She stretched on tip-toes, placed her hands on my cheeks,
+and kissed me. &ldquo;Be careful.&rdquo;</p>
--- page0161.mm
+++ page0161.mm
@@ -1,4 +1,82 @@
 <p>Page 161.</p>
+<p>I followed a Better
+Future van up the ramp. It moved slowly, as if afraid of leaving me
+behind, so must have been driven by someone who had no idea what I
+was. More vehicles followed behind. This was their plan to protect me
+from observation: surround me with cars. I was okay with that. I just
+wished they would drive faster. The Contour Threes were essentially
+ambulating, one hoof remaining in contact with the ground all times,
+and I felt bad for them, like they were being insulted.</p>
+<p>&ldquo;Dr. Neumann?
+This is Jason. Can you hear me?&rdquo;</p>
+<p>I glanced down. I
+had a speaker near my liver, apparently. Actually, I had a speaker
+where I used to have a liver. &ldquo;Yes.&rdquo;</p>
+<p>&ldquo;We&rsquo;re
+almost at the top. Are you ready for some acceleration?&rdquo;</p>
+<p>&ldquo;Yes,&rdquo;
+I said. &ldquo;Yes yes.&rdquo;</p>
+<p>The van ahead
+sprung forward. I didn&rsquo;t need to say anything to the Contours:
+they accelerated by themselves, quickly catching up. Behind me, I
+heard the Hummers&rsquo; engines roaring. A rectangle of light rushed
+toward me, and then we were outside. The cars behind moved up to
+flank me, and 
+</p>
+<p></p>
+<p>The air sang. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>That was the plan
+to protect me from observation: surround me with cars. 
+</p>
+<p>There were two on
+the ramp ahead of me</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>ran up the ramp
+behind a Better Future van. 
+</p>
+<p></p>
+<p></p>
+<p>This was the plan
+to protect me from observation with a cordon of Better Future vans
+and Hummers. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>moving in perfect
+balance, and the feeling, it was like pure joy.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I moved. 
+</p>
+<p></p>
+<p></p>
+<p>&ldquo;I feel like
+the old woman who swallowed a fly. You know that one? <em>She
+swallowed a spider to catch the fly... she swallowed a bird to catch
+the spider</em>...&rdquo; 
+</p>
+<p></p>
+<p></p>
+<p></p>
 <p>&lt;&lt; Car escort
 out of the parking lot: he runs. &gt;&gt;</p>
 <p></p>
