47e50880d84a439b3e3728f4563639148a06e0d8
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-28 09:45:05 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@615 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0161.mm
+++ page0161.mm
@@ -1,9 +1,9 @@
 <p>Page 161.</p>
 <p>I followed a Better
 Future van up the ramp. It moved slowly, as if afraid of leaving me
-behind, and I felt insulted. Didn&rsquo;t they know what I was? <em>Kick
-it</em>, said the Contours, or a sentiment to that effect, and I
-didn&rsquo;t, but I sympathized with their frustration.</p>
+behind. I felt insulted. Didn&rsquo;t they know what I was? <em>Kick
+it</em>, suggested my parts, or some sentiment to that effect. I
+didn&rsquo;t, but I sympathized.</p>
 <p>&ldquo;Dr. Neumann?
 This is Jason. Can you hear me?&rdquo;</p>
 <p>I looked down. I
@@ -11,35 +11,33 @@ had a speaker near my liver. Actually, I had a speaker where I used
 to have a liver. &ldquo;Yes.&rdquo;</p>
 <p>&ldquo;We&rsquo;re
 almost at the top. Are you ready for some acceleration?&rdquo;</p>
-<p>&ldquo;Yes. Yes.&rdquo;</p>
+<p>&ldquo;Yes, yes.&rdquo;</p>
 <p>The van sprang
 ahead. I didn&rsquo;t need to instruct the Contours: they shifted
-into a lope all by themselves. It was a world away from the first
-time I&rsquo;d ridden out on artificial legs, when I had felt as if
-they were trying to shake every bone in my body out through the top
-of my head; improvements to the model, I guessed, plus the fact that
-I had fewer bones. It was a relief to no longer feel as if I was
-risking serious neck damage with each step. 
-</p>
+into a lope by themselves. It was a world away from the first time
+I&rsquo;d ridden out on artificial legs, when they had tried to shake
+every bone in my body out through the top of my head. Improvements to
+the gait model, shock absorption through the torso, or the fact that
+I had fewer bones; whichever was to thank for it, I was no longer
+risking serious neck damage with each step.</p>
 <p>&ldquo;The other
-cars will pull alongside you. They&rsquo;ll form a cordon to try to
-cover you from emergency services.&rdquo;</p>
+cars will pull alongside you. They&rsquo;ll form a cordon to screen
+you from emergency services.&rdquo;</p>
 <p>A rectangle of
-light rushed toward me. As the ramp broadened, a Hummer roared up on
-each side. Then we were outside, and fresh air slapped me in the
-face. We turned onto the road and I got a little too close to the
-Hummer on the left; my gun-arm brushed against it, its tires
-squealed.</p>
+light appeared ahead. As the ramp broadened, Hummers slid up on each
+side. Then we were outside, and fresh air slapped me in the face. We
+turned onto the road and I got too close to the Hummer on the left;
+my gun-arm brushed it and its tires squealed.</p>
 <p><em>That was deliberate</em>,
 I said. <em>Wasn&rsquo;t it?</em>
 Because I had written some of this software: collision avoidance was
-not that hard. My parts were silent. But I could feel their
-satisfaction. <em>Okay</em>, I
-said. <em>Okay, then</em>.</p>
+not that hard. My parts were silent. But I knew what they wanted.
+<em>Okay</em>, I said. <em>Okay,
+then</em>.</p>
 <p>I veered left. My
 gun-arm touched the side of the Hummer. I pushed, gentle but firm. It
-tried to resist; white smoke streamed from its tires. My
-Jason-speaker barked unintelligibly. I pushed the Hummer again,
-harder, until it popped out of formation and spun in a smoking
-half-circle. Then I accelerated through the gap and was running, wind
-blasting past me, and that feeling, it was pure joy.</p>
+fought back; white smoke streamed from its tires. My Jason-speaker
+barked unintelligibly. I pushed the Hummer again, harder, until it
+popped out of formation and spun in a smoking half-circle. I
+accelerated through the gap and then I was running, wind blasting
+past me, leaving them all behind, and that feeling, it was pure joy.</p>
