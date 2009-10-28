8421581bdcb3909ae81bf0897955c5847f0eadac
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-28 09:47:53 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@616 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0161.mm
+++ page0161.mm
@@ -2,8 +2,7 @@
 <p>I followed a Better
 Future van up the ramp. It moved slowly, as if afraid of leaving me
 behind. I felt insulted. Didn&rsquo;t they know what I was? <em>Kick
-it</em>, suggested my parts, or some sentiment to that effect. I
-didn&rsquo;t, but I sympathized.</p>
+it</em>, suggested my parts, or some sentiment to that effect.</p>
 <p>&ldquo;Dr. Neumann?
 This is Jason. Can you hear me?&rdquo;</p>
 <p>I looked down. I
@@ -11,7 +10,7 @@ had a speaker near my liver. Actually, I had a speaker where I used
 to have a liver. &ldquo;Yes.&rdquo;</p>
 <p>&ldquo;We&rsquo;re
 almost at the top. Are you ready for some acceleration?&rdquo;</p>
-<p>&ldquo;Yes, yes.&rdquo;</p>
+<p>&ldquo;Yes. Yes.&rdquo;</p>
 <p>The van sprang
 ahead. I didn&rsquo;t need to instruct the Contours: they shifted
 into a lope by themselves. It was a world away from the first time
@@ -19,21 +18,21 @@ I&rsquo;d ridden out on artificial legs, when they had tried to shake
 every bone in my body out through the top of my head. Improvements to
 the gait model, shock absorption through the torso, or the fact that
 I had fewer bones; whichever was to thank for it, I was no longer
-risking serious neck damage with each step.</p>
+risking serious neck damage with every step.</p>
 <p>&ldquo;The other
-cars will pull alongside you. They&rsquo;ll form a cordon to screen
-you from emergency services.&rdquo;</p>
+cars will pull alongside and form a cordon to screen you from
+emergency services.&rdquo;</p>
 <p>A rectangle of
-light appeared ahead. As the ramp broadened, Hummers slid up on each
-side. Then we were outside, and fresh air slapped me in the face. We
-turned onto the road and I got too close to the Hummer on the left;
-my gun-arm brushed it and its tires squealed.</p>
+light appeared. As the ramp broadened, Hummers slid up on each side.
+Then we were outside, and fresh air slapped me in the face. We turned
+onto the road and I got too close to the Hummer on the left: my
+gun-arm brushed it. It rocked; its tires squealed.</p>
 <p><em>That was deliberate</em>,
 I said. <em>Wasn&rsquo;t it?</em>
-Because I had written some of this software: collision avoidance was
-not that hard. My parts were silent. But I knew what they wanted.
-<em>Okay</em>, I said. <em>Okay,
-then</em>.</p>
+Because I had written some of this software:
+collision avoidance was not that hard. My parts were silent. But I
+knew what they wanted. <em>Okay</em>,
+I said. <em>Okay, then</em>.</p>
 <p>I veered left. My
 gun-arm touched the side of the Hummer. I pushed, gentle but firm. It
 fought back; white smoke streamed from its tires. My Jason-speaker
