b01695043022ed1f18f56be0e5462215f7b0b744
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-29 06:34:42 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@626 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0162.mm
+++ page0162.mm
@@ -15,24 +15,25 @@ represented. All I had to do was keep him at a distance.</p>
 <p>A tingling ran down
 my gun-arm. At first I thought this was my arm getting excited. Then
 I heard the speaker, barely audible over the noise of me tearing up
-bitumen. &ldquo;Dr. Neumann? Your armament is online.&rdquo;</p>
+asphalt. &ldquo;Dr. Neumann? Your armament is online.&rdquo;</p>
 <p>So they could
 activate me remotely. That wasn&rsquo;t terrific news. It implied
 they could deactivate me. But that was hard to focus on, because I
 had an online gun-arm. That wasn&rsquo;t the kind of thing I could
 let sit there. Once, early in my career at Better Future, I made the
-mistake of purchasing a new cellphone the day before my project&rsquo;s
+mistake of purchasing a new cellphone the day before a project&rsquo;s
 final results were due; I thought to ignore it for 24 hours, then
 have a play as a reward for my hard work. Around three the next
 morning, still discovering new features, I decided to call in sick.
 This situation was like that, only attached to me, and with bullets.
 <em>I should test it</em>, I thought. Because I couldn&rsquo;t wait to
-discover how it worked with Carl running at me, swinging sledgehammer
-arms. That would be really poor planning. I looked around.</p>
+discover how it worked until Carl was running at me, swinging
+sledgehammer arms. That would be really poor planning. I looked
+around.</p>
 <p>Coming up on the
 right was a giant billboard. On it, an attractive couple in bright
-clothes laughed or danced or wrestled or something. Possibly they
-were playing a Wii game. I thought: <em>That</em>.</p>
+clothes laughed or danced or wrestled. Possibly they were playing a
+Wii game. I thought: <em>That</em>.</p>
 <p>Imagine a chainsaw.
 An ear-splitting, bone-rattling chainsaw chewing up all before it.
 That is about right.</p>
