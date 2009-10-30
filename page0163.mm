cbc20d00abcd54e1d1892dd7fd941d056c91f35c
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-30 06:31:04 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@629 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0163.mm
+++ page0163.mm
@@ -1,12 +1,12 @@
 <p>Page 163.</p>
 <p>Carl was hiding in
-an eight-storey downtown parking lot. That was according to my
+a downtown multi-level parking garage. That was according to my
 abdomen, which was telling me there was a block of ice millimeters
 away from my belly. There wasn&rsquo;t. The ice was Carl. &ldquo;We&rsquo;re
 reading him very close,&rdquo; squawked my Jason-speaker, and I
 thought: <em>I have a remote sensor, thanks.</em> I was going to have
-to figure out how to turn that thing off. In fact, as soon as this
-was over, I had to shut down this whole remote connection thing.
+to figure out how to turn that off. In fact, as soon as this was
+over, I was going to shut down this whole remote connection thing.
 There would be a conversation.</p>
 <p>But first: Carl. I
 peeled off the road, ran through the barricade, and pounded up the
@@ -14,7 +14,7 @@ parking lot ramp. As I rounded the first level, the cold sensation
 intensified, and I began to feel uneasy. Because this was not a good
 environment for spotting Carl at a distance, not at all. He could be
 hiding between cars. Around a corner. Behind a stairwell door. I
-stopped. There was not much chance of surprising him, with my
+stopped. There was not much chance of surprising him, with the
 Contours firing locking pins into the concrete with each step. I
 shouted: &ldquo;<em>Lola!</em>&rdquo;</p>
 <p>My voice bounced
@@ -24,11 +24,11 @@ into the darkness. I sucked in another breath.</p>
 before I knew it. A family sedan began to back out in front of me,
 red brake lights steaming, and I knocked it aside with my gun-arm. I
 didn&rsquo;t mean to hit it hard, but the Contours braced and the
-sedan bounced into the wall. Then I rounded another corner and Carl
-was kneeling on the concrete, playing with his arms.</p>
+sedan bounced into the wall with a crunch. Then I rounded another
+corner and Carl was kneeling on the concrete, playing with his arms.</p>
 <p>I stopped. I
 couldn&rsquo;t see Lola. I didn&rsquo;t know what Carl was doing. He
-must have heard me. But he didn&rsquo;t raise his head. He stayed
+had surely heard me. But he didn&rsquo;t raise his head. He stayed
 hunched over, scraping at something. I reminded myself that Carl was
 crazy. I shouldn&rsquo;t try to understand his behavior. I should
 just do this. I raised the gun-arm.</p>
@@ -38,8 +38,8 @@ where Lola is</em>, I thought. Then I could shoot him. Or ignore him.
 Because what did I have against Carl? We&rsquo;d had a difference
 opinion about whose parts were whose; we&rsquo;d resolved it; it was
 done. He was basically a good guy, wasn&rsquo;t he? Or had been,
-before the psychological trouble. Maybe Carl could be Better Future&rsquo;s
-problem.</p>
+before the psychological trouble. Maybe I would let Carl be Better
+Future&rsquo;s problem.</p>
 <p>Then I noticed
 something. Carl&rsquo;s posture was odd. He wasn&rsquo;t just
 hunched. He was hunched over something. A kind of lump. A kind of
