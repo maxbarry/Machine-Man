e9815d65cada2d5474ef378a97c969266700d6f6
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-10-30 06:07:43 +0000

    Page update: 2 new 3 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@627 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0163.mm
+++ page0163.mm
@@ -5,40 +5,41 @@ abdomen, which was telling me there was a block of ice millimeters
 away from my belly. There wasn&rsquo;t. The ice was Carl. &ldquo;We&rsquo;re
 reading him very close,&rdquo; squawked my Jason-speaker, and I
 thought: <em>I have a remote sensor, thanks.</em> I was going to have
-to figure out how to turn that thing off. In fact, I had to shut down
-this whole remote connection thing, as soon as this was over.</p>
+to figure out how to turn that thing off. In fact, as soon as this
+was over, I had to shut down this whole remote connection thing.
+There would be a conversation.</p>
 <p>But first: Carl. I
-peeled off the road and pounded up the parking lot ramp. As I rounded
-the first level, the cold sensation intensifying, I began to feel
-uneasy. Because this was not a good environment for spotting Carl at
-a distance, not at all. He could be hiding between two cars. Around a
-corner. Behind a stairwell door. I stopped. There was not much chance
-of surprising him: not with my Contours firing locking pins into the
-concrete with each step. I shouted: &ldquo;<em>Lola!</em>&rdquo;</p>
+peeled off the road, ran through the barricade, and pounded up the
+parking lot ramp. As I rounded the first level, the cold sensation
+intensified, and I began to feel uneasy. Because this was not a good
+environment for spotting Carl at a distance, not at all. He could be
+hiding between cars. Around a corner. Behind a stairwell door. I
+stopped. There was not much chance of surprising him, with my
+Contours firing locking pins into the concrete with each step. I
+shouted: &ldquo;<em>Lola!</em>&rdquo;</p>
 <p>My voice bounced
-back to me from the darkness. I sucked in breath to try again. Then:</p>
+into the darkness. I sucked in another breath.</p>
 <p>&ldquo;<em>Charlie!</em>&rdquo;</p>
 <p>I was running
-before I knew it. Up one more level, then another. A family sedan
-began to back out in front of me, red brake lights steaming, and I
-knocked it aside with my gun-arm. I didn&rsquo;t mean to hit it hard,
-but the Contours braced and the sedan bounced forward and crunched
-into the wall. Then I rounded level eight and Carl was kneeling on
-the floor, playing with his arms.</p>
+before I knew it. A family sedan began to back out in front of me,
+red brake lights steaming, and I knocked it aside with my gun-arm. I
+didn&rsquo;t mean to hit it hard, but the Contours braced and the
+sedan bounced into the wall. Then I rounded another corner and Carl
+was kneeling on the concrete, playing with his arms.</p>
 <p>I stopped. I
 couldn&rsquo;t see Lola. I didn&rsquo;t know what Carl was doing. He
-didn&rsquo;t raise his head, even though he must have heard me. He
-just stayed hunched over, scraping away at something. I reminded
-myself that Carl was crazy. I shouldn&rsquo;t try to understand his
-behavior. I should just do this. I raised the gun-arm.</p>
-<p>But still. Now he
-was here, right in front of me, I didn&rsquo;t know if I could shoot
-him. <em>Ask him where Lola is</em>, I thought. Then I could shoot him.
-Or ignore him altogether. Because what did I have against Carl? We&rsquo;d
-had a difference opinion about whose parts were whose; we&rsquo;d
-resolved it; it was done. He was basically a good guy, wasn&rsquo;t
-he? Or had been, before the psychotic break. It seemed to me that
-Carl could be Better Future&rsquo;s problem.</p>
+must have heard me. But he didn&rsquo;t raise his head. He stayed
+hunched over, scraping at something. I reminded myself that Carl was
+crazy. I shouldn&rsquo;t try to understand his behavior. I should
+just do this. I raised the gun-arm.</p>
+<p>Although. Now he
+was right here, I didn&rsquo;t know I could shoot him. <em>Ask him
+where Lola is</em>, I thought. Then I could shoot him. Or ignore him.
+Because what did I have against Carl? We&rsquo;d had a difference
+opinion about whose parts were whose; we&rsquo;d resolved it; it was
+done. He was basically a good guy, wasn&rsquo;t he? Or had been,
+before the psychological trouble. Maybe Carl could be Better Future&rsquo;s
+problem.</p>
 <p>Then I noticed
 something. Carl&rsquo;s posture was odd. He wasn&rsquo;t just
 hunched. He was hunched over something. A kind of lump. A kind of
--- page0166.mm
+++ page0166.mm
@@ -1,5 +1,83 @@
 <p>Page 166.</p>
 <p>(WED)</p>
 <p></p>
-</BODY>
-</HTML>
+<p>Carl punched me in
+the guts and I staggered backward and sat down on the hood of a car.
+The surprising thing about this was the lack of force. It was more
+like I saw the blow coming and tried to ride what I expected to be a
+massive transfer of momentum. But it wasn&rsquo;t, and I realized
+something: compared to me, Carl&rsquo;s arms were not very strong. Of
+course they weren&rsquo;t: he had biological legs. Even allowing for
+some hidden bracing beneath his uniform, there was no way he was
+carrying around half-ton arms. They would do some damage if I let
+them near my soft parts. But he couldn&rsquo;t pick me up and throw
+me around. He could barely push me.</p>
+<p></p>
+<p></p>
+<p>They were
+lightweight. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>I swung my gun-arm
+in a long, slow loop, aimed at Carl&rsquo;s head. It was ridiculous:
+I already knew he was too close. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt;
+If we see Lola is okay, we LOSE THE RAGE. MUST NOT SEE THAT LOLA IS
+OKAY. &gt;&gt;</p>
+<p></p>
+<p>&lt;&lt;
+The thing is, Carl shouldn&rsquo;t know he&rsquo;s being tracked&mdash;at
+least, not by Charlie. He should expect BF to come after him&mdash;that&rsquo;s
+why he&rsquo;s trying to dig out the locator&mdash;but Charlie is a
+surprise. However, he has heard Charlie coming up the ramp. Charlie
+has called out. Lola has called back. Lola would think: &ldquo;I&rsquo;ll
+run to Charlie!&rdquo; Carl would say: &ldquo;Wait, it&rsquo;s not
+safe, think of his parts.&rdquo; &ldquo;We have to get this tracker
+off as fast as possible.&rdquo; &gt;&gt;</p>
+<p></p>
+<p></p>
