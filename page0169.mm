eb03f6d9ef81c20bc67500c52f3ab84b4f41360b
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-09 07:56:16 +0000

    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@639 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0169.mm
+++ page0169.mm
@@ -1,30 +1,29 @@
 <p>Page 169.</p>
 <h5>CHAPTER 
 THIRTY-FIVE</h5>
-<p>&ldquo;Lola! Oh
+<p>&ldquo;Lola! Oh,
 Lola, Lola, I thought you were move out of the way.&rdquo; The end of
-this sentence surprised me. That wasn&rsquo;t what I meant to say. &ldquo;I
-thought you were dead. I thought Carl killed you.&rdquo;</p>
+this sentence surprised me. That wasn&rsquo;t what I had meant to
+say. &ldquo;I thought you were dead. I thought Carl killed you.&rdquo;</p>
 <p>&ldquo;You shot
 him,&rdquo; Lola said. It was true. I could see Carl a little better
-now. He must have used those metal arms to try to defend himself,
+now, and he must have used those metal arms to try to defend himself,
 because they were battered and torn, but not effectively enough,
-because so was much of his upper torso. His eyes glittered in the
-darkness. <em>Finish him</em>, said
-my parts. <em>He&rsquo;s
-sneaky.</em></p>
-<p>&ldquo;Come here.&rdquo;
-I held out my arm to Lola, the one that didn&rsquo;t carry
-ammunition. &ldquo;He&rsquo;s dangerous.&rdquo;</p>
+because so was a lot of his torso. His eyes glittered in the dark.
+<em>Finish him</em>, said my parts.
+<em>He&rsquo;s sneaky.</em></p>
+<p>&ldquo;Lola, come
+here.&rdquo; I held out my arm to Lola, the one that didn&rsquo;t
+carry ammunition. &ldquo;He&rsquo;s dangerous.&rdquo;</p>
 <p>&ldquo;He&rsquo;s
 not dangerous.&rdquo;</p>
-<p>&ldquo;Lola. He
-is.&rdquo; My legs hopped forward, and it took an act of will to
-reign them in. &ldquo;He&rsquo;s been running around trying to kill
-me. He blew up Better Future. And he kidnapped you.&rdquo;</p>
-<p>&ldquo;He didn&rsquo;t.&rdquo;
-She shook her head violently. I was very happy to see Lola alive, but
-this was rapidly dampening the longer she took to run into my arms. I
-wasn&rsquo;t sure Lola realized what I had gone through for her. I
-wasn&rsquo;t sure what she thought was happening here. &ldquo;Carl
-rescued me.&rdquo;</p>
+<p>&ldquo;He is.&rdquo;
+My legs hopped forward, and it took an act of will to reign them in.
+&ldquo;He&rsquo;s been running around trying to kill me. He blew up
+Better Future. And he kidnapped you.&rdquo;</p>
+<p>Lola shook her
+head. I was very happy to see Lola alive, but this was rapidly
+dampening the longer she took to run into my arms. I wasn&rsquo;t
+sure what Lola thought was happening here. I wasn&rsquo;t sure she
+realized what I had gone through. &ldquo;Carl didn&rsquo;t kidnap me.
+He rescued me.&rdquo;</p>
--- page0172.mm
+++ page0172.mm
@@ -1,11 +1,43 @@
 <p>Page 172.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
 <p>&ldquo;Well that
 doesn&rsquo;t sound too bad,&rdquo; I said.</p>
+<p>&ldquo;Charlie,&rdquo;
+said Lola. &ldquo;Look at me. Tell me you&rsquo;re in control of your
+body.&rdquo;</p>
+<p>&ldquo;I&rsquo;m in
+total control.&rdquo; My parts tried to add, <em>Kill Carl</em>, but I
+stopped them. That would have undermined us. My statement was not
+true, of course; it had become clear that I wasn&rsquo;t in charge of
+this body so much as cohabiting with it. And that aside, at least
+some of me could be turned on or off at a distance by Better Future.
+I was in total control of very little. But I had to object to
+something, because Lola had opened this line of conversation with: <em>We
+have to get rid of your parts</em>.</p>
+<p>&ldquo;Over time,&rdquo;
+said Lola, &ldquo;we can re-train your brain. Your neurons will move
+elsewhere, if we give them the right tasks. Once we replace these
+parts with standard prosthetics&mdash;&rdquo;</p>
+<p>&ldquo;Let me stop
+you there.&rdquo; I worked very hard to keep my voice from shaking.
+&ldquo;Because I know you probably have my best interests in mind,
+Lola, but I&rsquo;m not sure you&rsquo;re really appreciating the
+fact that, at this point, I am essentially <em>a head</em>.&rdquo;</p>
+<p>&ldquo;Charlie&mdash;&rdquo;</p>
+<p>&ldquo;There is no
+<em>getting rid of the parts</em>. I <em>am</em> the parts. I will die
+without the parts. And yes, okay, there is a certain amount of
+psychological adjustment going on</p>
 <p></p>
+<p></p>
+<p>Outside the parking
+garage, the sirens grew louder. There would be flashing blue lights
+soon, and demands over megaphones.</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Charlie
+was nobody before. Not confident. Nobody. &gt;&gt;</p>
 <p></p>
 <p></p>
 <p></p>
