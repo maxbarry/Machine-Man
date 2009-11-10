f8490800a006a5ed5c5470c4fc22f79f5bba3c67
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-10 05:09:33 +0000

    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@641 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0170.mm
+++ page0170.mm
@@ -1,31 +1,31 @@
 <p>Page 170.</p>
 <p>I thought:
-<em>Stockholm Syndrome</em>. That was what they called it, when kidnap
+<em>Stockholm Syndrome</em>. That was what they called it when kidnap
 victims came to sympathize with their abductors. It was a
 psychological condition. &ldquo;Lola,&rdquo; I said carefully. &ldquo;Just
 come here a minute, and we&rsquo;ll talk about me shoot shoot shoot
 Carl dead.&rdquo; I bit my lip. Words were bubbling out again. I
 wondered where they were coming from, although not really, because I
-already knew. &ldquo;I mean...&rdquo;</p>
-<p>Lola rose and began
-to walk toward me. We filled with joy, my parts and I, me because
-maybe Lola was coming around, and my parts because in a moment they
-would get to fill Carl full of holes. Then everything would be fine,
-and we would all return to Better Future together. &ldquo;Charlie,&rdquo;
-Lola said. &ldquo;I&rsquo;m sorry.&rdquo;</p>
+already knew.</p>
+<p>Lola rose and
+walked toward me. We filled with joy, my parts and I, me because at
+last Lola was acting the way she was supposed to, and my parts
+because in a moment they would get to fill Carl full of holes. Then
+everything would be fine and we could all return to Better Future
+together. &ldquo;Charlie,&rdquo; Lola said. &ldquo;I&rsquo;m sorry.&rdquo;</p>
 <p>&ldquo;That&rsquo;s
-okay. You don&rsquo;t have anything to be sorry about.&rdquo;</p>
+okay. What? You don&rsquo;t have anything to be sorry about.&rdquo;</p>
 <p>&ldquo;It&rsquo;s
-my fault.&rdquo; She reached for my face. I felt a flash of
-irritation, because she was still blocking me from a clean shot at
-Carl. I mean, my parts felt irritated. Or we both did. There was a
-certain amount of overlapping going on. &ldquo;I encouraged you.&rdquo;</p>
+kind of my fault.&rdquo; She reached for my face. I felt a flash of
+irritation, because she was still blocking me from a shot at Carl. I
+mean, my parts felt irritated. Or we both did. There was a certain
+amount of overlapping going on. &ldquo;I encouraged you.&rdquo;</p>
 <p>&ldquo;Lola, I&rsquo;m
-really pleased you&rsquo;re alive, but I&rsquo;m having a little
-trouble thinking straight because Carl is there and I have to shoot
-him. I have to shoot him. So please move out of the way.&rdquo;</p>
+thrilled you&rsquo;re alive, but I&rsquo;m having a little trouble
+thinking straight because Carl is there and I have to shoot him. I
+have to shoot him. So please move out of the way.&rdquo;</p>
 <p>Lola used her hands
-to turn my face toward hers. Until this, my eyes had been jumping all
-over Carl. Now I really saw her, I was a little shocked by how pale
-she looked. &ldquo;Charlie, I&rsquo;m really sorry. But we have to
-get rid of your parts.&rdquo;</p>
+to swivel my face toward hers. Until this, my eyes had been jumping
+all over Carl. Now I really saw her, I was a little shocked by how
+pale she looked. &ldquo;I&rsquo;m really sorry, Charlie. But we have
+to get rid of your parts.&rdquo;</p>
