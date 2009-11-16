5f2480fbbc668d57e8fd8f606431dd2bc03ebecc
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-16 09:47:13 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@663 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0174.mm
+++ page0174.mm
@@ -3,36 +3,36 @@
 THIRTY-SIX</h5>
 <p>&ldquo;I love you,
 Charlie,&rdquo; Lola said. &ldquo;But I&rsquo;m not sure how much you
-is in there any more.&rdquo;</p>
+is in there.&rdquo;</p>
 <p>I stared at Carl.
 This didn&rsquo;t seem to be a trick. His chest did not rise or fall.
 He was genuinely non-living.</p>
 <p>&ldquo;I think you
 took a bad turn,&rdquo; she said. &ldquo;Because at first you really
 were making yourself better. You were taking charge of your body. But
-then it started to go the other way. I noticed that a while ago. When
-at Angelica&rsquo;s your legs stopped working, I thought... I
-thought, maybe that&rsquo;s not such a bad thing. Because maybe you
-would start to get in charge again.&rdquo;</p>
+then it started to go the other way. I noticed that a while ago. And
+when at Angelica&rsquo;s your legs stopped working, I thought... I
+thought, maybe that&rsquo;s not such a bad thing. Because you might
+start to get in charge again.&rdquo;</p>
 <p>Carl was off. He
 was non-functional. He was disabled. He was dead.</p>
 <p>&ldquo;I&rsquo;m
 not against self-improvement,&rdquo; she said. &ldquo;I just think
-lately you went backward.&rdquo;</p>
+you&rsquo;ve started going backward.&rdquo;</p>
 <p>The sirens were
 very loud now. Then they stopped. Somewhere below us, I heard
 engines. Voices. When I turned to Lola, reflected blue light
 flickered across her face.</p>
-<p>&ldquo;You&rsquo;re
-the most amazing person I&rsquo;ve ever met, Charlie. But not because
-of your body.&rdquo;</p>
+<p>&ldquo;Charlie,
+you&rsquo;re the most amazing person I&rsquo;ve ever met. But not
+because of your body.&rdquo;</p>
 <p>I felt a bubble of
-grief rising within me. Because she seemed very lovely, standing here
+grief rising within me. Because she was very lovely, standing here
 telling me to shed myself: she was a girl who loved me and they did
 not come along every day; they had not come along in my lifetime
 prior to this. &ldquo;Lola,&rdquo; I said, and my parts said: <em>Shoot
-her</em>. Which kind of proved Lola&rsquo;s point, and my grief
-sharpened, and I realized it wasn&rsquo;t for her. It was for them.
-&ldquo;I love you,&rdquo; I said, and I wasn&rsquo;t sure who I was
-talking to, but my parts said: <em>I love you too</em>.</p>
+her</em>. Which kind of proved Lola&rsquo;s point. My grief sharpened
+and I realized it wasn&rsquo;t for her. It was for them. &ldquo;I
+love you,&rdquo; I said, and I wasn&rsquo;t sure who I was talking
+to, but my parts said: <em>I love you too</em>.</p>
 <p>Then my arm died.</p>
