689e04935a5fb91655d8d51ec67323bcb695d1a0
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-17 06:02:47 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@664 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0175.mm
+++ page0175.mm
@@ -2,14 +2,11 @@
 <p>I gasped and
 mentally retracted everything bad I had ever thought about my parts.
 I wailed: <em>Come back, come back!</em> I tried to flex the arm and it
-moved, and that was when I realized it was not dead. What I had felt
-was an undoing of the tingling sensation that had occurred earlier,
-on my way to the parking garage. What I had felt was the gun going
-offline.</p>
+moved, and that was when I realized it was not dead at all. What I
+had felt was an undoing of the tingling that had occurred earlier, on
+my way here. I had felt the gun going offline.</p>
 <p>Twin beams of light
-slid through the air, bright columns of glittering concrete dust. I
-heard the rumble of a car engine. Doors slammed. The lights fixed on
-Lola and me, washing out my vision.</p>
-<p>I heard Cassandra
-Cautery say: &ldquo;Charlie, thank God you&rsquo;re all right.&rdquo;
-And I recalled the speaker, the microphone in my chest.</p>
+slid through the air, illuminating glittering columns of concrete
+dust. Doors thumped. I heard Cassandra Cautery say: &ldquo;Charlie.
+Thank God you&rsquo;re okay.&rdquo; And I recalled the speaker, the
+microphone in my chest.</p>
