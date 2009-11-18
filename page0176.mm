d74a9c93436e2726f0e6fda75e75b94401503236
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-18 02:33:32 +0000

    Page update: 1 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@665 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0176.mm
+++ page0176.mm
@@ -2,8 +2,9 @@
 <p>Cassandra Cautery
 emerged from the darkness, picking her way across the rubble. She was
 followed by three very cautious-looking Better Future security
-guards, who were not armed, as far as I could tell. Others I couldn&rsquo;t
-see hung back with the vehicle, shuffling and muttering.</p>
+guards, who were not armed, as far as I could tell. Others hung back
+with the vehicle, shuffling and murmuring, clanking things I couldn&rsquo;t
+see.</p>
 <p>Cassandra Catuery
 stopped when she saw Carl. She did not seem particularly surprised,
 or sad, or anything else. But she looked at him for a long time. Then
@@ -11,32 +12,32 @@ her eyes turned back to me, full of sympathy and understanding. &ldquo;Oh,
 Charlie.&rdquo;</p>
 <p>&ldquo;Stay away
 from us,&rdquo; said Lola.</p>
-<p>&ldquo;There are a
-lot of cops downstairs,&rdquo; said Cassandra Cautery, as if Lola
-wasn&rsquo;t there. &ldquo;And I&rsquo;m sorry, Charlie, but we have
-to get you out of here before they come up.&rdquo; She nodded to the
-guards, who headed for Carl&rsquo;s body. &ldquo;Thanks to you, we
-still have a chance to salvage this.&rdquo;</p>
+<p>&ldquo;There are
+cops downstairs,&rdquo; said Cassandra Cautery, as if Lola wasn&rsquo;t
+there. &ldquo;A lot of them. And I&rsquo;m sorry, Charlie, but we
+have to get you out of here before they come up.&rdquo; She nodded to
+the guards, who headed for Carl&rsquo;s body. &ldquo;Thanks to you,
+we still have a chance to salvage this.&rdquo;</p>
 <p>&ldquo;I killed
 Carl.&rdquo;</p>
 <p>She nodded. &ldquo;And
-it&rsquo;s a good thing, Charlie. I know it might not feel that way,
-but it really is. Come back to work. We&rsquo;ll talk it through.&rdquo;
-She glanced at the guards, who were now trying to figure out how to
-pick up Carl. &ldquo;Get in the van and everything will be fine.&rdquo;</p>
-<p>I looked at Lola.
-&ldquo;I don&rsquo;t want to lose my parts.&rdquo;</p>
+it&rsquo;s a good thing, Charlie. It might not feel that way, but it
+really is. Come back to work, and I&rsquo;ll show you.&rdquo; She
+glanced at the guards, who were now trying to figure out how to pick
+up Carl. &ldquo;Get in the van and everything will be fine.&rdquo;</p>
+<p>&ldquo;I don&rsquo;t
+want to lose my parts.&rdquo;</p>
 <p>&ldquo;Of course
 you don&rsquo;t,&rdquo; said Cassandra Cautery. &ldquo;Who said you
 did?&rdquo; I looked at her, because we both knew she had been
-listening in, and she saw that I knew, and her face changed. &ldquo;Okay.
-Look. I don&rsquo;t want to talk trash about your girlfriend,
-Charlie, because she&rsquo;s been through a lot, we all have, but
-what she suggested to you a minute ago, that&rsquo;s monstrous.
-That&rsquo;s the most horrible thing I&rsquo;ve ever heard. And I&rsquo;ve,
-well...&rdquo; A noise came out of her: not quite a laugh, but not
-anything else, either. &ldquo;My honest opinion, if anyone here is
-suffering from psychological issues, it&rsquo;s her.&rdquo;</p>
+listening, and her face changed. &ldquo;Okay. Look. I don&rsquo;t
+want to talk trash about your girlfriend, Charlie, because she&rsquo;s
+been through a lot. Wee all have. But what she suggested to you a
+minute ago, that&rsquo;s monstrous. That&rsquo;s the most horrible
+thing I&rsquo;ve ever heard. And I&rsquo;ve, well...&rdquo; A noise
+came out of her: not quite a laugh, but not anything else, either.
+&ldquo;My honest opinion, Miss Banks has a few psychological issues
+of her own.&rdquo;</p>
 <p>&ldquo;You&rsquo;re
 a twisted person,&rdquo; said Lola. &ldquo;You&rsquo;re really evil.&rdquo;</p>
 <p>&ldquo;You just
@@ -44,7 +45,14 @@ calm down,&rdquo; said Cassandra Cautery. &ldquo;You&rsquo;re enough
 trouble without working yourself up. Charlie, I don&rsquo;t want to
 put pressure on you, but we have to go. Now.&rdquo;</p>
 <p>My eyes met Lola&rsquo;s.
-I thought: <em>Can I trust you?</em> And a part of me said <em>yes</em>,
-and a part said <em>no</em>, and in the end it came down to which part
-I liked better. I liked the part that trusted. I decided I wanted to
-be that. &ldquo;Okay,&rdquo; I said.</p>
+I thought: <em>Can I trust you?</em> A part of me said <em>yes</em> and a
+part said <em>no</em> and in the end it came down to which part I liked
+better. I didn&rsquo;t feel a closer affinity with one part. I didn&rsquo;t
+try to figure out which felt more like me. I had come to suspect
+there was no such thing as a core self: it was all malleable. I was
+more alert when I drank coffee, happier when I was on drugs, and,
+with a certain combination of missing limbs and free-roaming neurons,
+I could kill people. It was pointless to ponder who I was, because
+mostly I was neurochemicals. So instead I chose who I wanted to be
+next. And I liked the part that trusted Lola. I decided I would be
+that. &ldquo;Okay,&rdquo; I said.</p>
--- page0177.mm
+++ page0177.mm
@@ -1,15 +1,28 @@
 <p>Page 177.</p>
 <p>Cassandra Cautery
-hesitated. &ldquo;Okay... as in, okay, you&rsquo;re going to get in
-the van?&rdquo;</p>
-<p>I shook my head.</p>
-<p>&ldquo;Oh,&rdquo;
-she said. &ldquo;Oh, I see. 
-</p>
+hesitated. &ldquo;You mean, okay, as in, &lsquo;Okay, I&rsquo;ll get
+in the van?&rsquo;&rdquo;</p>
+<p>I shook my head. I
+didn&rsquo;t look at her: I was watching Lola smile.</p>
+<p>&ldquo;Oh.&rdquo;
+Cassandra Cautery put her hands on her hips and looked at the ground.
+&ldquo;Oh.&rdquo; This one was softer. I felt a little guilty,
+because I supposed Cassandra Cautery was not really a bad person. She
+was trying to do her best in a difficult situation. I opened my mouth
+to say this, or something like it, but she cut me off. &ldquo;Well,
+the fact is, that doesn&rsquo;t matter very much, Charlie. That only
+means we flip your off switch and put you in the van ourselves.&rdquo;
+She gestured, and something in the darkness whined. I could guess
+that one: a power loader. &ldquo;And you know what? Screw you. There.
+I said it. I held your hand through this entire goddamn process, and
+when I ask you for one thing...&rdquo; She shook her head. &ldquo;It&rsquo;s
+fine. It&rsquo;s fine. We don&rsquo;t need you any more, anyway.</p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
 <p></p>
-</BODY>
-</HTML>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
