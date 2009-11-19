ba3dbf974fe4561b29c95a3f88d0fc20db03db91
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-19 06:22:23 +0000

    Page update: 0 new 2 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@674 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0177.mm
+++ page0177.mm
@@ -7,22 +7,45 @@ didn&rsquo;t look at her: I was watching Lola smile.</p>
 <p>&ldquo;Oh.&rdquo;
 Cassandra Cautery put her hands on her hips and looked at the ground.
 &ldquo;Oh.&rdquo; This one was softer. I felt a little guilty,
-because I supposed Cassandra Cautery was not really a bad person. She
-was trying to do her best in a difficult situation. I opened my mouth
-to say this, or something like it, but she cut me off. &ldquo;Well,
-the fact is, that doesn&rsquo;t matter very much, Charlie. That only
-means we flip your off switch and put you in the van ourselves.&rdquo;
-She gestured, and something in the darkness whined. I could guess
-that one: a power loader. &ldquo;And you know what? Screw you. There.
-I said it. I held your hand through this entire goddamn process, and
-when I ask you for one thing...&rdquo; She shook her head. &ldquo;It&rsquo;s
-fine. It&rsquo;s fine. We don&rsquo;t need you any more, anyway.</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+because Cassandra Cautery was probably not a bad person. She was
+probably just trying to do her best in a difficult situation. I
+opened my mouth to say this. &ldquo;Well, the fact is, that doesn&rsquo;t
+matter very much, Charlie. That only means we flip your off switch
+and put you in the goddamn van ourselves.&rdquo; She gestured. In the
+darkness, something whined. I could guess that one: a power loader.
+&ldquo;And you know what? Screw you. There. I said it.&rdquo;</p>
+<p>More figures
+emerged from the gloom, among them Jason, my lab assistant. He looked
+vaguely embarrassed and carried in one hand a small black device the
+size of a cellphone.</p>
+<p>&ldquo;You don&rsquo;t
+deserve those parts,&rdquo; said Cassandra Cautery. &ldquo;You&rsquo;ve
+been a whiny pain in the ass from day one and I can&rsquo;t wait to
+give them to someone more manageable.&rdquo; Her eyes moved to Lola.
+&ldquo;That goes for you, too.&rdquo; She turned to Jason. &ldquo;Turn
+him off.&rdquo;</p>
+<p>&ldquo;Uh,&rdquo;
+said Jason. I was kind of hoping Jason might be about to reveal a
+previously unsuspected affection for me, but from the way his eyes
+slid away from mine, I realized this was not going to happen. &ldquo;Okay.&rdquo;</p>
+<p>&ldquo;Wait,&rdquo;
+I said. &ldquo;Wait a minute. I didn&rsquo;t think this through.&rdquo;</p>
+<p>&ldquo;You can&rsquo;t
+turn him off!&rdquo; said Lola. &ldquo;He&rsquo;ll die!&rdquo;</p>
+<p>I could grab Lola
+and run. I would make it two steps before my body turned to stone. I
+would be brain-dead within three minutes. It was not a great option.
+I could take one step and club Cassandra Cautery into the van. More
+appealing. But with the same end result: me inert, dying. That left
+Jason, ten steps away. I was fast. I could close that distance in two
+seconds, swat the device out of his hand. It was probably a temporary
+solution, in that this black thing was unlikely to be the only method
+they had of shutting me down. But it was something.</p>
+<p>Jason&rsquo;s thumb
+slid over the device. I realized he was watching me, wearing the same
+expression he&rsquo;d had on the day I sat waiting for him in the
+lab, my one leg dangling over the edge of the Clamp. He&rsquo;d
+lunged for the Big Red Button and been too late. He would not let me
+beat him today.</p>
+<p>&ldquo;Quick
+quick,&rdquo; said Cassandra Cautery. &ldquo;No time to lose.&rdquo;</p>
--- page0178.mm
+++ page0178.mm
@@ -3,7 +3,83 @@
 <p></p>
 <p></p>
 <p></p>
+<p>In my head, I
+stomped my hoof. I created a shock wave that rippled through the
+concrete and tossed Jason into the air. Cassandra Cautery fell to the
+ground with a cry. People shouted. The power loader overturned. Jason
+scrambled on hands and knees toward the device he&rsquo;d lost, but I
+was faster. I crushed it under one hoof. I didn&rsquo;t even have to
+kick over the van: the guards scattered, knowing what I could do. I
+ran back and swept up Lola in one arm. We leaped out of the parking
+garage and landed in the alley. There were police cruisers there, and
+ambulances, but I powered past them all. I ran until we had left them
+all behind. I ran so far Better Future&rsquo;s wireless control
+couldn&rsquo;t reach me. One night, in another city, I broke into an
+electronics store and used their equipment to locate every part of me
+that looked like it transmitted. I taught Lola how to use a magnetic
+drill. She straddled my chest and She freed me.</p>
+<p>This was a good
+plan, in my head.</p>
+<p>In reality, I got
+as far as raising my leg before Jason pushed the button. 
+</p>
 <p></p>
+<p></p>
+<p></p>
+<p>, night air
+whipping through my hair. 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>But it didn&rsquo;t
+matter, because by then I was on top of them. Jason scrambled on
+hands and knees for the device that would 
+</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Can they
+actually CONTROL him? I think yes. Not just SHUT HIM OFF but
+MANIPULATE him. That really rams home how his parts have become the
+enemy. They make him walk toward the van. And I realized it was
+worse. &gt;&gt;</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; maybe some
+setup &gt;&gt;</p>
+<p></p>
+<p></p>
+<p>I drove my gun-arm
+into the floor. It punched through the concrete, titanium squealing
+on metal reinforcements.</p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Lola
+sacrifice? &gt;&gt;</p>
+<p></p>
+<p>I kissed Lola</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>&lt;&lt; Charlie
+should tell Lola to kiss him&mdash;he makes the decision. &gt;&gt;</p>
 <p></p>
 <p></p>
 </BODY>
