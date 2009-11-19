ac33833fa2126fb0c3ecbf85f000b2e7a5a0d4ec
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-19 10:23:22 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@676 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0177.mm
+++ page0177.mm
@@ -9,21 +9,21 @@ Cassandra Cautery put her hands on her hips and looked at the ground.
 &ldquo;Oh.&rdquo; This one was softer. I felt a little guilty,
 because Cassandra Cautery was probably not a bad person. She was
 probably just trying to do her best in a difficult situation. I
-opened my mouth to say this. &ldquo;Well, the fact is, that doesn&rsquo;t
-matter very much, Charlie. That only means we flip your off switch
-and put you in the goddamn van ourselves.&rdquo; She gestured. In the
-darkness, something whined. I could guess that one: a power loader.
-&ldquo;And you know what? Screw you. There. I said it.&rdquo;</p>
-<p>More figures
-emerged from the gloom, among them Jason, my lab assistant. He looked
-vaguely embarrassed and carried in one hand a small black device the
-size of a cellphone.</p>
+opened my mouth to say this, but then she looked up. &ldquo;Well, the
+fact is, that doesn&rsquo;t matter very much, Charlie. That only
+means we flip your off switch and put you in the goddamn van
+ourselves.&rdquo; She gestured. In the darkness, something whined. I
+could guess that one: a power loader. &ldquo;And you know what? Screw
+you. There. I said it.&rdquo;</p>
+<p>Jason, my lab
+assistant, emerged from the gloom. He looked vaguely embarrassed. In
+one hand he carried a black device the size of a cellphone.</p>
 <p>&ldquo;You don&rsquo;t
 deserve those parts,&rdquo; said Cassandra Cautery. &ldquo;You&rsquo;ve
 been a whiny pain in the ass from day one and I can&rsquo;t wait to
 give them to someone more manageable.&rdquo; Her eyes moved to Lola.
-&ldquo;That goes for you, too.&rdquo; She turned to Jason. &ldquo;Turn
-him off.&rdquo;</p>
+&ldquo;That goes for you, too.&rdquo; She waved in Jason&rsquo;s
+direction. &ldquo;Turn him off.&rdquo;</p>
 <p>&ldquo;Uh,&rdquo;
 said Jason. I was kind of hoping Jason might be about to reveal a
 previously unsuspected affection for me, but from the way his eyes
@@ -31,21 +31,21 @@ slid away from mine, I realized this was not going to happen. &ldquo;Okay.&rdquo
 <p>&ldquo;Wait,&rdquo;
 I said. &ldquo;Wait a minute. I didn&rsquo;t think this through.&rdquo;</p>
 <p>&ldquo;You can&rsquo;t
-turn him off!&rdquo; said Lola. &ldquo;He&rsquo;ll die!&rdquo;</p>
+turn him off!&rdquo; Lola shrieked. &ldquo;He&rsquo;ll die!&rdquo;</p>
 <p>I could grab Lola
 and run. I would make it two steps before my body turned to stone. I
 would be brain-dead within three minutes. It was not a great option.
 I could take one step and club Cassandra Cautery into the van. More
 appealing. But with the same end result: me inert, dying. That left
-Jason, ten steps away. I was fast. I could close that distance in two
+Jason, ten steps away. I was fast. I could close that gap in two
 seconds, swat the device out of his hand. It was probably a temporary
-solution, in that this black thing was unlikely to be the only method
-they had of shutting me down. But it was something.</p>
+solution, in that this was unlikely to be the only method they had of
+shutting me down. But it was something.</p>
 <p>Jason&rsquo;s thumb
-slid over the device. I realized he was watching me, wearing the same
-expression he&rsquo;d had on the day I sat waiting for him in the
-lab, my one leg dangling over the edge of the Clamp. He&rsquo;d
-lunged for the Big Red Button and been too late. He would not let me
-beat him today.</p>
+slid over the device. I realized he was watching me very closely now.
+He was wearing the same expression he&rsquo;d had the day I sat
+waiting for him in the lab, with one leg dangling over the edge of
+the Clamp. That day, he&rsquo;d lunged for the Big Red Button and
+been too late. That was not going to happen again.</p>
 <p>&ldquo;Quick
 quick,&rdquo; said Cassandra Cautery. &ldquo;No time to lose.&rdquo;</p>
