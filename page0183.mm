fee488f669c856706f2934d44a3a7298ff0ca11f
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-26 05:19:33 +0000

    Page update: 1 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@685 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0183.mm
+++ page0183.mm
@@ -2,155 +2,83 @@
 <p>&ldquo;Oh,&rdquo;
 someone said. &ldquo;Terrific. Look at that.&rdquo;</p>
 <p>&ldquo;That&rsquo;s...
-what, two-stage?&rdquo;</p>
+what is that, second-stage?&rdquo;</p>
 <p>&ldquo;No, no,
-it&rsquo;s...&rdquo; Several people inhaled. Then there was clapping,
-a whoop, and laughter.</p>
+it&rsquo;s...&rdquo; Several people inhaled. There was clapping, a
+whoop, and laughter.</p>
 <p>&ldquo;Online,&rdquo;
-said the first voice. It made this word sound very heavy, the way you
-might say: <em>We made it</em>. &ldquo;Online.&rdquo;</p>
-<p></p>
-<p>&lt;&lt; I tried to
-blink, but couldn&rsquo;t. I could see. I could see fine. Everything
-was slightly off-color, over-saturated and painfully sharp. I could
-tell I was in a room, though, a very bright room, with the ambiance
-of an operating theater. It was not an operating theater. I had been
-in enough of those to know. This was a lab.&gt;&gt;</p>
-<p></p>
-<p>A face appeared. It
-was a friendly face, somewhat familiar. Perhaps a woman I had worked
-with one time. From behind glasses, her eyes shone at me. &ldquo;Dr.
-Neumann?&rdquo;</p>
+said the first voice. The word sounded heavy, the way you might say:
+<em>We made it</em>. &ldquo;Online.&rdquo;</p>
+<p>I tried to blink.
+This didn&rsquo;t work. I could see, though. I could see a vase. The
+vase was clear and contained three yellow flowers. Daffodils? Yes.
+The vase with the daffodils sat on a plain white surface, and behind
+that was a wall with a painting. It seemed to me that this white
+surface was probably a bedside table, so tried to turn my head to see
+the rest of the room. But this didn&rsquo;t work, either.</p>
+<p>A face appeared
+beside the vase. It was a friendly face, somewhat familiar. Perhaps a
+woman I had worked with one time. From behind brown-rimmed glasses,
+her eyes shone. &ldquo;Dr. Neumann?&rdquo;</p>
 <p>I swallowed, or
-tried to, but that wasn&rsquo;t happening either. I tried: <em>What..?</em></p>
+tried to. I said: <em>What..?</em></p>
 <p>The woman&rsquo;s
-eyes flicked at something behind my head. She was a little close for
-comfort, to be honest. She was all over my personal space. When her
-eyes returned to me, they were so full of joy I thought she might
-kiss me. Instead, she turned and addressed the people I couldn&rsquo;t
-see. &ldquo;That&rsquo;s a response. That&rsquo;s a clear response.&rdquo;</p>
-<p>I
-said: <em>Where am I?</em></p>
-<p>Again, her eyes
-moved to read something behind me. &ldquo;Dr. Neumann, you&rsquo;re
-in a military research and development facility. We&rsquo;re about
-sixty feet underground. A lot of people here have spent a lot of time
-taking care of you.&rdquo;</p>
-<p>I tried to raise my
-head to see these people. But apparently I was completely paralyzed.
-I could feel my body. I wasn&rsquo;t sure what kind it was. But there
-was something. I just couldn&rsquo;t move it.</p>
-<p>&ldquo;After the
-Better Future corporation was found to be engaging in a range of
-human rights and corporate governance abuses, it was closed down and
-its parts sold off. Some of those parts were acquired by private
-firms, government departments, or joint ventures between the two. You
-are in one of the latter.&rdquo; She glanced over her shoulder.
-&ldquo;Should I..?&rdquo;</p>
+eyes flicked at something or someone behind me. She was a little
+close for comfort, this woman. I was glad to be awake and hopefully
+safe but I wished she would back up out of my personal space. When
+her eyes returned to mine, she was even happier than before. For a
+second I thought we were about to kiss. Instead, she turned to the
+people I couldn&rsquo;t see. &ldquo;A response. That&rsquo;s a clear
+response.&rdquo;</p>
+<p><em>Where am I? </em>I
+tried to say, but again, I made no sound. I felt panic, because when
+no part of your body is doing what you tell it, not a single muscle,
+that&rsquo;s pretty worrisome. But it didn&rsquo;t seem to matter to
+the woman: she nodded, her eyes roving above my head, as if she
+understood anyway.</p>
+<p>&ldquo;You&rsquo;re
+in a research and development facility, Dr. Neumann. A lot of people
+here have spent a lot of time taking care of you. You were brought
+here as part of the asset diversification process that occurred
+following the breakup of Better Future. After the hearings, various
+parts were acquired by different private, government, and joint
+venture interests, and...&rdquo; She glanced behind her. &ldquo;Should
+I..?&rdquo;</p>
 <p>&ldquo;Go ahead,&rdquo;
 someone said.</p>
-<p>&ldquo;When the
-paramedics found you, Dr. Neumann, you were already unconscious. They
-couldn&rsquo;t move you; they could only keep you alive until a
-larger medical team could arrive to separate you from your artificial
-body. You might appreciate the challenge this involved, since you had
-no human heart to compress, and little in the way of functional lungs
-or other organs. You were considered extremely unlikely to survive.</p>
-<p>&ldquo;But you did.
-You nearly died a dozen times, but each time the medical team found a
-way to keep your brain alive. Many argued advocated giving up, since
-there was, frankly, so little of you left to save. They thought it
-would be a kindness to let you go. But others argued vociferously
-against this.&rdquo; Her eyes twinkled. &ldquo;One in particular.&rdquo;</p>
-<p>I tried to say:
-<em>Lola?</em></p>
-<p>&ldquo;I&rsquo;m
-not sure if...&rdquo; She looked around. &ldquo;If we really want
-to...&rdquo; Whatever she saw there, she nodded and turned back to
-me. &ldquo;Dr. Neumann, what I&rsquo;m about to say will come as a
-shock. There&rsquo;s really no way for me to break this to you
-gently.</p>
-<p>&ldquo;Over the
-months, various parts of your living body became unsupportable. You
-must remember that you essentially had no major organs. Without a
-liver, your immune system ... Your . Several times the medical team
-had to abandon part of your body in order to preserve the brain.</p>
-<p>&ldquo;I&rsquo;ll
-be honest with you, Dr. Neumann. At this point you essentially became
-a research project. You no longer had hope of a meaningful recovery,
-of gaining any quality of life. 
-</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;Oh,&rdquo;
-he said. &ldquo;I&rsquo;m a whistleblower. Better Future is... well,
-it doesn&rsquo;t exist any more. Parts were absorbed into the
-military, others were shut down. A few were sold off.&rdquo; He
-shrugged. &ldquo;This happened years ago.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p> &lt;&lt; things he
-can see &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>&lt;&lt; You&rsquo;d
-think there would be people there. Especially Lola. &gt;&gt;</p>
-<p></p>
-<p></p>
-<p>&ldquo;Hello.&rdquo;
-A face loomed over me. It was D. Peters, my old boss. I hadn&rsquo;t
-seen that guy in a while. &ldquo;Hello, Charlie.&rdquo;</p>
-<p>I swallowed, or
-tried to, but that wasn&rsquo;t happening either. I tried to say:
-<em>What?</em></p>
-<p>D Peters looked at
-something to my left. His eyes flicked back and forth. He was a
-little close for comfort, to be honest. I didn&rsquo;t see why he
-needed to be in my personal space. &ldquo;You&rsquo;re recovering
-from a serious trauma, Charlie. Don&rsquo;t expect everything to work
-fine right away.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>Good. That&rsquo;s
-good, Charlie.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p>&ldquo;Oh,&rdquo;
-he said. &ldquo;I&rsquo;m a whistleblower. Better Future is... well,
-it doesn&rsquo;t exist any more. Parts were absorbed into the
-military, others were shut down. A few were sold off.&rdquo; He
-shrugged. &ldquo;This happened years ago.&rdquo;</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<p>&lt;&lt; Angelica?
-D. Peters? Others? &gt;&gt; 
-</p>
-<p>&lt;&lt; So he&rsquo;s
-worried he&rsquo;s in BF. &gt;&gt;</p>
-<p></p>
-</BODY>
-</HTML>
+<p>Her eyes settled on
+me. They did not rove around my face, I noticed. They were steady and
+unblinking, like almonds. &ldquo;When the paramedics found you, you
+had lost consciousness. They couldn&rsquo;t move you. They could only
+keep you alive until a larger medical team arrived. You might
+appreciate their challenge, since you had no human heart they could
+compress, and little in the way of functional major organs. But they
+did it. They kept you alive. During the ensuring weeks, you nearly
+died many times. But each time, a way was found to save you. This...&rdquo;
+She looked around. &ldquo;I&rsquo;m not sure we really want to...&rdquo;
+She nodded and turned back to me. &ldquo;Dr. Neumann, this next part
+will come as a shock. There&rsquo;s no way for me to break this to
+you gently.</p>
+<p>&ldquo;The battle
+to save you, it required sacrifices. Several times the decision had
+to be made to abandon part of your body in order to save the
+remainder. And... well, eventually it became clear that you had no
+real chance of making a meaningful recovery. Of regaining any kind of
+quality of life. Many believed that no further measures should be
+taken to support you. But others... others did not share that view. A
+compromise was reached. As part of that compromise, you came here.
+Essentially, you became a research project.&rdquo;</p>
+<p>The
+woman hesitated. &ldquo;It&rsquo;s probably best if I show you.&rdquo;
+She reached out and turned my head with one hand, her thumb
+and fingers a couple inches apart, with no sign of exertion. The room
+span by. And then I was looking at a gray box. At first I thought it
+was a piece of medical equipment. It had tubes and wires, one of
+which snaked toward me, and an attached monitor screen. I thought:
+<em>What is that?</em> and words spilled across the screen: <em>WHAT IS
+THAT</em>.</p>
+<p>In the
+screen&rsquo;s reflection, I could see the daffodils. Beside them,
+sitting on the white benchtop, was a small black cylinder mounted on
+a block of plastic. The cylinder had a dark lens at closest end and a
+wire at the other. I was looking at myself.</p>
