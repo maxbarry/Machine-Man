c61e99c81b3dbc98f5c43be0aa59c13391d8c4e5
mbarry <mbarry@40ef8932-b262-0410-9730-8c1c2ddff0ae>
2009-11-27 08:54:26 +0000

    Page update: 0 new 1 modified
    git-svn-id: svn+ssh://svn.whuffy.com/home/mbarry/svn/maxbarrycom/blog/rawnews/machineman/pages@690 40ef8932-b262-0410-9730-8c1c2ddff0ae

--- page0183.mm
+++ page0183.mm
@@ -7,74 +7,73 @@ what is that, second-stage?&rdquo;</p>
 it&rsquo;s...&rdquo; Several people inhaled. There was clapping, a
 whoop, and laughter.</p>
 <p>&ldquo;Online,&rdquo;
-said the someone. He made the word sound heavy, the way you might
-say: <em>We made it</em>. &ldquo;Online.&rdquo;</p>
+said the someone. The word sounded heavy, the way you might say: <em>We
+made it</em>. &ldquo;Online.&rdquo;</p>
 <p>I tried to blink.
 This didn&rsquo;t work. I could see, though. I could see a vase. The
-vase was clear and contained three yellow flowers. Daffodils? Yes.
-The vase sat on a plain white surface, behind which were a wall and a
-painting. It seemed to me this white surface was probably a bedside
-table, so I tried to turn my head to see the rest of the room. This
-didn&rsquo;t work, either.</p>
+vase contained three yellow flowers. Daffodils? Yes. It sat on a
+plain white surface, behind which were a wall and a painting. It
+seemed to me this white surface was a bedside table, so I tried to
+turn my head to see the rest of the room. This didn&rsquo;t work,
+either.</p>
 <p>A face appeared
-beside the vase. It was a friendly face, a woman&rsquo;s, possibly
-Indian. Almond eyes shone at me from behind brown-rimmed glasses.
-&ldquo;Dr. Neumann?&rdquo;</p>
+beside the vase. It was a friendly face, a woman&rsquo;s. Almond eyes
+shone at me from behind brown-rimmed glasses. &ldquo;Charles
+Neumann?&rdquo;</p>
 <p>I swallowed, or
 tried to. I said: <em>What..?</em></p>
 <p>The woman&rsquo;s
 eyes flicked at someone or something behind me. She was a little
 close for comfort, this woman. I was glad to be awake and hopefully
 safe but I wished she would back up a little out of my personal
-space. When her eyes returned to mine, they were even more excited
+space. When her eyes returned to mine, they shone even more brightly
 than before. For a second I thought we were about to kiss. She turned
 to the people I couldn&rsquo;t see. &ldquo;A response. A clear
 response.&rdquo;</p>
 <p><em>Where am I? </em>I
-tried to say, but again, I made no sound. I felt panic, because when
-no part of your body is doing what you tell it, not a single muscle,
-that&rsquo;s worrisome. But it didn&rsquo;t seem to matter to the
-woman: she nodded as if she understood anyway.</p>
+tried to say, but made no sound. I felt panic, because when no part
+of your body is doing what you tell it, not a single muscle, that&rsquo;s
+worrisome. But it didn&rsquo;t seem to matter to the woman: she
+nodded as if she understood me anyway.</p>
 <p>&ldquo;You&rsquo;re
-in a research and development facility. We&rsquo;ve been taking care
-of you. You came here as part of the asset diversification process
-that occurred following the hearings and breakup of Better Future.
-We&rsquo;re a private-public joint venture between a consortium of
-private and government interests. You...&rdquo; She glanced behind
-her. &ldquo;Should I..?&rdquo;</p>
+in a research and development facility. You came here as part of the
+asset diversification process that occurred following the hearings
+into the Better Future corporation. We&rsquo;re a private-public
+joint venture between a consortium of private and government
+interests, and you...&rdquo; She glanced behind her. &ldquo;Should
+I..?&rdquo;</p>
 <p>&ldquo;Go ahead,&rdquo;
 someone said.</p>
 <p>Her eyes settled on
 me, steady and unblinking. &ldquo;When the paramedics found you,
 you&rsquo;d already lost consciousness. They couldn&rsquo;t move you.
 They could only keep you alive until a larger medical team arrived,
-with specialized equipment. I wasn&rsquo;t there, but you can
-appreciate their challenge, since you had no human heart they could
-compress, and little in the way of functional major organs. But they
-did it. They kept you alive. During the ensuring weeks, you came
-close to death on many occasions. But each time, a way was found to
-save you. This...&rdquo; She looked around. &ldquo;I&rsquo;m not sure
-we really want to...&rdquo; She nodded, turned back to me. &ldquo;Dr.
-Neumann, this next part will come as a shock. There&rsquo;s no way to
-break this to you gently.</p>
+with more specialized equipment. I wasn&rsquo;t there, but you can
+appreciate their challenge: you had no human heart they could
+compress, little in the way of functional major organs. But they did
+it. During the ensuring weeks, you came very close to death. But each
+time, a way was found to save you. Until...&rdquo; She looked around.
+&ldquo;I&rsquo;m not sure we want to...&rdquo; She nodded, turned
+back. &ldquo;Charles, this next part will come as a shock. There&rsquo;s
+no way to break this to you gently.</p>
 <p>&ldquo;The battle
 to save you required sacrifices. Several times the medical team had
 to abandon part of your body in order to save the remainder. And...
-well, it eventually became clear you had no real chance of a
-meaningful recovery. Of regaining any kind of quality of life. At
-that point, it was decided that no further measures should be taken
-to sustain you. But others... others did not share that view. A
-compromise was reached. As part of that compromise, you came here.
-And at that point, you essentially became a research project.</p>
-<p>&ldquo;What
-this means...&rdquo; She hesitated. &ldquo;It&rsquo;s probably best
-if I show you.&rdquo; She reached out and turned my head with
-one hand, her thumb and fingers a couple inches apart, with no sign
-of exertion. The room span by. Then I was looking at a steel gray
-box. At first I thought it was a piece of medical equipment. It had a
-few tubes and wires, one of which snaked toward me. Alongside the box
-sat a flatscreen monitor. I thought: <em>What is that?</em> and letters
-spilled across the screen: <em>WHAT IS THAT</em>.</p>
+well, it became clear that you had no chance of a meaningful
+recovery. Of regaining any kind of quality of life. At that point, it
+was decided that no further measures should be taken to sustain you.
+However, others... others did not share that view. A compromise was
+reached. As part of that compromise, you were brought here. At that
+point, you essentially became a research project. What
+I mean is...&rdquo; She hesitated. &ldquo;It&rsquo;s probably best if
+I show you.&rdquo;</p>
+<p>She reached out and turned my head with one hand, her thumb
+and fingers a couple inches apart, with no sign of exertion. The room
+span by. Then I was looking at a gray box. At first I thought it was
+a piece of medical equipment. It sported a few buttons, a green power
+light, and about a hundred black wires, one of which snaked toward
+me. Next to this box sat a flatscreen monitor. I thought: <em>What is
+that?</em> and letters spilled across the screen: <em>WHAT IS THAT</em>.</p>
 <p>In the screen&rsquo;s
 reflection, I saw daffodils. Beside them, sitting on the white
 benchtop, was a tiny black cylinder mounted on a block of plastic.
